b5c9c88eeed965e2ae483005738d5f37e143867b
leonardo <leonardopedro4@gmail.com>
2008-09-25 13:38:50 +0000

    git-svn-id: file:///tmp/svnroot_only_rec/ReC/trunk@57 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- src/pt/utl/ist/elab/driver/serial/stamp/gamma/processors/StampGammaProcessor.java
+++ src/pt/utl/ist/elab/driver/serial/stamp/gamma/processors/StampGammaProcessor.java
@@ -38,24 +38,24 @@ public class StampGammaProcessor extends AbstractStampProcessor
         int pressao = 0;
 	int time = 0;
         
-	String[] splitedStr = command.getCommand().split(" ");
+	String[] splitedStr = command.getCommand().split("\t");
 	
 	if(command.getCommandIdentifier().equalsIgnoreCase(COMMAND_IDENTIFIER) && splitedStr!=null && splitedStr.length>=3 && splitedStr[0]!=null && splitedStr[1]!=null && splitedStr[2]!=null)
 	{
 	    try
-	    {                
-                pressao = Integer.parseInt(splitedStr[0]);
-		Float oPressao = new Float(106 + ( (pressao * 1.68) * 1000) / 4096F);
-		command.addCommandData(PRESSAO, oPressao);
-                
-                ondamic = Integer.parseInt(splitedStr[1]);
-		Float oOndamic = new Float(( (ondamic) * 5) / 4096F);
-		command.addCommandData(ONDA_MIC, oOndamic);                		
-		
-                time = Integer.parseInt(splitedStr[0]);
+	    { 
+		time = Integer.parseInt(splitedStr[0]);
 		Float oTime = new Float(time / 2F);
 		command.addCommandData(TIME, oTime);
+              
+            pressao = Integer.parseInt(splitedStr[1]);
+		Float oPressao = new Float(pressao*0.398+98);
+		command.addCommandData(PRESSAO, oPressao);
                 
+            ondamic = Integer.parseInt(splitedStr[2]);
+		Float oOndamic = new Float( (ondamic) * 5);
+		command.addCommandData(ONDA_MIC, oOndamic);                		
+		    
 		command.setData(true);
 		return true;
 		
