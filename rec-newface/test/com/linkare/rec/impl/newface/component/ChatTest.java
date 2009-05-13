65d8fe2d2875c60e329aa27edcb2139571b80af4
hfernandes <hfernandes@linkare.com>
2009-05-13 17:27:00 +0000

    Resolução do recurso não encontrado - chatTemplate.html
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/branches/DocumentationAndJava6@214 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ rec-newface/test/com/linkare/rec/impl/newface/component/ChatTest.java
@@ -0,0 +1,17 @@
+package com.linkare.rec.impl.newface.component;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.InputStream;
+
+import org.junit.Test;
+
+public class ChatTest {
+
+  @Test
+  public void testChatTemplateHtmlResource() throws Exception {
+    InputStream resourceAsStream = Chat.class.getResourceAsStream("resources/chatTemplate.htm");
+    assertNotNull("resource not found", resourceAsStream);
+  }
+  
+}
