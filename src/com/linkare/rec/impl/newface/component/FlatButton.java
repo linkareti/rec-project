883d70dbf8b0fc690a58ab9e598f5771e2194ca9
jflorindo <jflorindo@linkare.com>
2009-05-14 00:09:43 +0000

    versão 1.1.1
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/branches/DocumentationAndJava6/rec-newface@217 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- src/com/linkare/rec/impl/newface/component/FlatButton.java
+++ src/com/linkare/rec/impl/newface/component/FlatButton.java
@@ -21,14 +21,17 @@ public class FlatButton extends JButton implements Serializable{
 
 	private static final String uiClassID = "FlatButtonUI";
 	
-	private Color gradientTopColor = null;
-	private Color gradientBottomColor = null;
-	private Color pressedGradientTopColor = null;
-	private Color pressedGradientBottomColor = null;
-	private boolean isBackgroundGradient = false;
+	private Color gradientTopOff = new Color(0xacd651);
+	private Color gradientBottomOff = new Color(0x9fcb42);
+	private Color gradientTopOn = new Color(0xffae48);
+	private Color gradientBottomOn = new Color(0xff930d);
 	
-    //private final PropertyChangeSupport pcs = new PropertyChangeSupport( this );
-    
+	private Color colorBorderOff = new Color(0x7da921);
+	private Color colorBorderOn = new Color(0xe38b1d);
+	
+	private Color foregroundOff = new Color(0x2B7A3B);
+	private Color foregroundOn = new Color(0x89381E);
+	
 	public FlatButton() {
 		super();
 
@@ -36,78 +39,138 @@ public class FlatButton extends JButton implements Serializable{
             setUI(new MetalButtonUI());
         }
 	}
+	
+	
 
 	public String getUIClassID() {
 		return uiClassID;
 	}
 
+
 	/**
-	 * @param gradientTopColor the gradientTopColor to set
+	 * @param gradientTopOff the gradientTopOff to set
 	 */
-	public void setGradientTopColor(Color gradientTopColor) {
-		this.gradientTopColor = gradientTopColor;
+	public void setGradientTopOff(Color gradientTopOff) {
+		this.gradientTopOff = gradientTopOff;
 	}
 
 	/**
-	 * @return the gradientTopColor
+	 * @return the gradientTopOff
 	 */
-	public Color getGradientTopColor() {
-		return gradientTopColor;
+	public Color getGradientTopOff() {
+		return gradientTopOff;
 	}
 
 	/**
-	 * @param gradientBottomColor the gradientBottomColor to set
+	 * @param gradientBottomOff the gradientBottomOff to set
 	 */
-	public void setGradientBottomColor(Color gradientBottomColor) {
-		this.gradientBottomColor = gradientBottomColor;
+	public void setGradientBottomOff(Color gradientBottomOff) {
+		this.gradientBottomOff = gradientBottomOff;
 	}
 
 	/**
-	 * @return the gradientBottomColor
+	 * @return the gradientBottomOff
 	 */
-	public Color getGradientBottomColor() {
-		return gradientBottomColor;
+	public Color getGradientBottomOff() {
+		return gradientBottomOff;
 	}
 
 	/**
-	 * @param pressedGradientTopColor the pressedGradientTopColor to set
+	 * @param gradientTopOn the gradientTopOn to set
 	 */
-	public void setPressedGradientTopColor(Color pressedGradientTopColor) {
-		this.pressedGradientTopColor = pressedGradientTopColor;
+	public void setGradientTopOn(Color gradientTopOn) {
+		this.gradientTopOn = gradientTopOn;
 	}
 
 	/**
-	 * @return the pressedGradientTopColor
+	 * @return the gradientTopOn
 	 */
-	public Color getPressedGradientTopColor() {
-		return pressedGradientTopColor;
+	public Color getGradientTopOn() {
+		return gradientTopOn;
 	}
 
 	/**
-	 * @param pressedGradientBottomColor the pressedGradientBottomColor to set
+	 * @param gradientBottomOn the gradientBottomOn to set
 	 */
-	public void setPressedGradientBottomColor(Color pressedGradientBottomColor) {
-		this.pressedGradientBottomColor = pressedGradientBottomColor;
+	public void setGradientBottomOn(Color gradientBottomOn) {
+		this.gradientBottomOn = gradientBottomOn;
 	}
 
 	/**
-	 * @return the pressedGradientBottomColor
+	 * @return the gradientBottomOn
 	 */
-	public Color getPressedGradientBottomColor() {
-		return pressedGradientBottomColor;
+	public Color getGradientBottomOn() {
+		return gradientBottomOn;
 	}
 
 	/**
-	 * @param isBackgroundGradient the isBackgroundGradient to set
+	 * @param colorBorderOn the colorBorderOn to set
 	 */
-	public void setBackgroundGradient(boolean isBackgroundGradient) {
-		this.isBackgroundGradient = isBackgroundGradient;
+	public void setColorBorderOn(Color colorBorderOn) {
+		this.colorBorderOn = colorBorderOn;
 	}
 
+
+
 	/**
-	 * @return the isBackgroundGradient
+	 * @return the colorBorderOn
 	 */
-	public boolean isBackgroundGradient() {
-		return isBackgroundGradient;
+	public Color getColorBorderOn() {
+		return colorBorderOn;
 	}
+
+
+
+	/**
+	 * @param colorBorderOff the colorBorderOff to set
+	 */
+	public void setColorBorderOff(Color colorBorderOff) {
+		this.colorBorderOff = colorBorderOff;
+	}
+
+
+
+	/**
+	 * @return the colorBorderOff
+	 */
+	public Color getColorBorderOff() {
+		return colorBorderOff;
+	}
+
+
+
+	/**
+	 * @param foregroundOff the foregroundOff to set
+	 */
+	public void setForegroundOff(Color foregroundOff) {
+		this.foregroundOff = foregroundOff;
+	}
+
+
+
+	/**
+	 * @return the foregroundOff
+	 */
+	public Color getForegroundOff() {
+		return foregroundOff;
+	}
+
+
+
+	/**
+	 * @param foregroundOn the foregroundOn to set
+	 */
+	public void setForegroundOn(Color foregroundOn) {
+		this.foregroundOn = foregroundOn;
+	}
+
+
+
+	/**
+	 * @return the foregroundOn
+	 */
+	public Color getForegroundOn() {
+		return foregroundOn;
+	}
+
 }
