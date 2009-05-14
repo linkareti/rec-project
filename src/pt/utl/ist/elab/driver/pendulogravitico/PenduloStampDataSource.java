e56e37ffeb493df0dee91ed345a3726f2b5eb90e
aleitao <aleitao@linkare.com>
2009-05-14 11:16:13 +0000

    refactored package name for the experiment
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/branches/DocumentationAndJava6@240 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ src/pt/utl/ist/elab/driver/pendulogravitico/PenduloStampDataSource.java
@@ -0,0 +1,104 @@
+/*
+ * RadioactividadeStampDataSource.java
+ *
+ * Created on 15 de Maio de 2003, 19:38
+ */
+
+package pt.utl.ist.elab.driver.pendulogravitico;
+
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import pt.utl.ist.elab.driver.pendulogravitico.processors.StampPenduloProcessor;
+import pt.utl.ist.elab.driver.serial.stamp.AbstractStampDataSource;
+import pt.utl.ist.elab.driver.serial.stamp.transproc.StampCommand;
+
+import com.linkare.rec.data.acquisition.PhysicsValue;
+import com.linkare.rec.data.config.HardwareAcquisitionConfig;
+import com.linkare.rec.impl.data.PhysicsValueFactory;
+/**
+ *
+ * @author José Pedro Pereira - Linkare TI
+ */
+public class PenduloStampDataSource extends AbstractStampDataSource
+{
+    
+    private int counter = 0;
+    private int total_samples = 0;
+    private float calib_angle = -1;
+    
+    public static String PENDULO_DS_LOGGER="PenduloDataSource.Logger";
+    
+    static
+    {
+        Logger l=LogManager.getLogManager().getLogger(PENDULO_DS_LOGGER);
+        if(l==null)
+        {
+            LogManager.getLogManager().addLogger(Logger.getLogger(PENDULO_DS_LOGGER));
+        }
+    }
+    
+    /** Creates a new instance of RadioactividadeStampDataSource */
+    public PenduloStampDataSource()
+    {
+    }
+    
+    public void processDataCommand(StampCommand cmd)
+    {
+        if(stopped || cmd==null || !cmd.isData() || cmd.getCommandIdentifier()==null)
+            return;
+        
+        if(cmd.getCommandIdentifier().equals(StampPenduloProcessor.COMMAND_IDENTIFIER))
+        {
+            float angle = 0;
+            float angle_adc = 0;
+            
+            PhysicsValue[] values=new PhysicsValue[1];
+            try
+            {
+                if(calib_angle == -1)
+                    calib_angle = ((Float)cmd.getCommandData(StampPenduloProcessor.ANGLE_ADC)).floatValue();
+                else
+                {
+                    angle_adc = ((Float)cmd.getCommandData(StampPenduloProcessor.ANGLE_ADC)).floatValue();
+                    
+                    angle = (calib_angle - angle_adc) / 49.41f;
+                    
+                    values[0]=PhysicsValueFactory.fromFloat(angle, getAcquisitionHeader().getChannelsConfig(0).getSelectedScale());
+                    super.addDataRow(values);
+                    
+                }
+            }
+            catch(ClassCastException e)
+            {
+                e.printStackTrace();
+                return;
+            }
+            
+            if(counter == total_samples)
+            {
+                Logger.getLogger(PENDULO_DS_LOGGER).log(Level.INFO,"DataSource Ended");
+                setDataSourceEnded();
+            }
+            
+            counter++;                        
+        }
+        
+    }
+    
+    public void setAcquisitionHeader(HardwareAcquisitionConfig config)
+    {
+        super.setAcquisitionHeader(config);
+        total_samples = config.getTotalSamples();
+        calib_angle = -1;
+    }
+    
+    private boolean stopped = false;
+    
+    public void stopNow()
+    {
+        stopped = true;
+        setDataSourceStoped();
+    }
+}
