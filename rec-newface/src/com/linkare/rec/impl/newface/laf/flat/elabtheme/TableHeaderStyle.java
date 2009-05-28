7ec6581eb16a19da40f3eb64d02303a7fbf348bd
jflorindo <jflorindo@linkare.com>
2009-05-28 00:35:33 +0000

    versão inicial
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/branches/DocumentationAndJava6@336 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ rec-newface/src/com/linkare/rec/impl/newface/laf/flat/elabtheme/TableHeaderStyle.java
@@ -0,0 +1,49 @@
+/* 
+ * ListStyle.java created on 2009/04/30
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.linkare.rec.impl.newface.laf.flat.elabtheme;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.util.Map;
+
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+import javax.swing.plaf.FontUIResource;
+
+/**
+ * 
+ * @author João FLorindo
+ */
+public class TableHeaderStyle extends DefaultStyle{
+
+	//ENABLED COLORS
+	private static final Color COLOR_HEADER_BG = new Color(0xCED7D5);
+	private static final Color COLOR_HEADER_FG = new Color(0x41494D);
+	public static final Border HEADER_BORDER = BorderFactory.createEmptyBorder(2, 3, 2, 3);
+	
+	public static final String ID = "TableHeader";
+	
+	/**
+	 * Creates the <code>TableHeaderStyle</code>.
+	 */
+	public TableHeaderStyle() {
+		super();
+	}
+
+	@Override
+	protected String defineStyleId() {
+		return ID;
+	}
+
+	@Override
+	public void updatePropertyMap(Map<String, Object> map) {
+		super.updatePropertyMap(map);
+		map.put(FOREGROUND, COLOR_HEADER_FG);
+		map.put(BACKGROUND, COLOR_HEADER_BG);
+		map.put("cellBorder", HEADER_BORDER);
+	}
+}
