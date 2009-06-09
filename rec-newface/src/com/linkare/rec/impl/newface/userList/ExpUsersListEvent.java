3bdbeca9523c041c939e5642e6448f5ea454e9f6
jflorindo <jflorindo@linkare.com>
2009-06-09 19:35:52 +0000

    git-svn-id: file:///tmp/svnroot_only_rec/ReC/branches/DocumentationAndJava6@372 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ rec-newface/src/com/linkare/rec/impl/newface/userList/ExpUsersListEvent.java
@@ -0,0 +1,27 @@
+/*
+ * ExpUsersListChangeEvent.java
+ *
+ * Created on July 29, 2004, 4:09 PM
+ */
+
+package com.linkare.rec.impl.newface.userList;
+
+/**
+ *
+ * @author André Neto - LEFT - IST
+ */
+public class ExpUsersListEvent extends java.util.EventObject
+{
+    private com.linkare.rec.acquisition.UserInfo[] userInfo;
+    
+    public ExpUsersListEvent(Object source, com.linkare.rec.acquisition.UserInfo[] userInfo)
+    {
+	super(source);
+	this.userInfo = userInfo;
+    }
+    
+    public com.linkare.rec.acquisition.UserInfo[] getUserInfo()
+    {
+	return userInfo;
+    }
+}
