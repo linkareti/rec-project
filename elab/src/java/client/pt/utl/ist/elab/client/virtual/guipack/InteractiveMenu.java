0890903f1745c413155de7c9c7068a63f69f10ee
aleitao <aleitao@linkare.com>
2009-08-07 15:37:52 +0000

    git-svn-id: file:///tmp/svnroot_only_rec/ReC/branches/DocumentationAndJava6@460 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ elab/src/java/client/pt/utl/ist/elab/client/virtual/guipack/InteractiveMenu.java
@@ -0,0 +1,19 @@
+/*
+ * InteractiveMenu.java
+ *
+ * Created on 21 de Dezembro de 2004, 17:18
+ */
+
+package pt.utl.ist.elab.client.virtual.guipack;
+
+import java.awt.event.*;
+
+/**
+ *
+ * @author  nomead
+ */
+public interface InteractiveMenu extends ActionListener {
+    
+    public void update();
+    
+}
---
+++ elab/src/java/client/pt/utl/ist/elab/virtual/guipack/graphs/GraphSamplesFunction.java
@@ -0,0 +1,19 @@
+/*
+ * GraphSamplesFunction.java
+ *
+ * Created on 23 de Março de 2005, 16:54
+ */
+
+package pt.utl.ist.elab.virtual.guipack.graphs;
+
+/**
+ *
+ * @author  nomead
+ */
+public interface GraphSamplesFunction {
+    
+    public double getValueX(int ch, double val);
+    
+    public double getValueY(int ch, double val);
+    
+}
