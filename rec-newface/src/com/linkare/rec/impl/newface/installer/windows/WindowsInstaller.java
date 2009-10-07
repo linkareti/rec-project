d4794ec5efc6c251e244ac03e812f10b489af60a
bcatarino <bcatarino@linkare.com>
2009-10-07 23:28:09 +0000

    Normalização dos installers
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/branches/DocumentationAndJava6@517 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- rec-newface/src/com/linkare/rec/impl/newface/installer/windows/WindowsInstaller.java
+++ rec-newface/src/com/linkare/rec/impl/newface/installer/windows/WindowsInstaller.java
@@ -1,10 +1,11 @@
 package com.linkare.rec.impl.newface.installer.windows;
 
 import com.linkare.rec.impl.newface.installer.Installer;
+import com.linkare.rec.impl.newface.utils.ZipExtractor;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import javax.jnlp.ExtensionInstallerService;
-import javax.jnlp.ServiceManager;
 import javax.jnlp.UnavailableServiceException;
 import javax.swing.JOptionPane;
 
@@ -15,21 +16,26 @@ import javax.swing.JOptionPane;
 public class WindowsInstaller extends Installer {
 
     @Override
-    public void install(String[] args) {
+    public void install(String[] args) throws UnavailableServiceException {
 
-        super.install(args);
+        log.fine("Beginning installation");
+
+        log.fine("Installed native libraries and extracted media plugins");
 
         ExtensionInstallerService installerService = null;
         try {
 
-            installerService = (ExtensionInstallerService) ServiceManager.
-                        		lookup("javax.jnlp.ExtensionInstallerService");
+            super.install(args);
 
             int result = JOptionPane.showConfirmDialog(null,
 				"A aplicação requer que os codecs de xvid estejam instalados." +
                 System.getProperty("line.separator") + 
-                "Se não tem, ou não tem a certeza se estão instalados, carregue ok. ",
-                "Instalação de codecs",
+                "Se não tem, ou não tem a certeza se estão instalados, carregue ok. " +
+                System.getProperty("line.separator") +
+                "Não poderá visualizar o vídeo se não tiver os codecs " +
+                System.getProperty("line.separator") +
+                "necessários instalados na sua máquina.",
+                "Instalação de codecs xvid",
 				JOptionPane.YES_NO_OPTION,
 				JOptionPane.QUESTION_MESSAGE );
 			boolean agree = (result==JOptionPane.YES_OPTION);
@@ -37,48 +43,58 @@ public class WindowsInstaller extends Installer {
             boolean installOk = false;
 			if (agree) {
                 installOk = installXvid();
-			} else {
-                installOk = true;
 			}
 
             if (installOk) {
-                System.out.println("!!!!!!!!!!!!!!!!!!Instalação com sucesso!!!!!!!!!!!!!!!!!!!!!!");
-                installerService.installSucceeded(false);
+                JOptionPane.showMessageDialog(null, "Codecs instalados com sucesso");
             } else {
-                System.out.println("===================Instalação falhou=============================");
-                //Bruno não pode dar installation failed se já tiver codecs.
-//                installerService.installFailed();
+                JOptionPane.showMessageDialog(null, "Os codecs não foram instalados. O vídeo poderá não ser visível.");
             }
 
-            System.out.println("############################### Terminou instalação ###############################");
+        } catch (Exception ex) {
+            
+            int installationResult = JOptionPane.showConfirmDialog(null,
+                            "Erro na instalação. Deseja prosseguir, mesmo podendo não ter vídeo?",
+                            "",
+                            JOptionPane.OK_CANCEL_OPTION,
+                            JOptionPane.QUESTION_MESSAGE );
+            
+            if (installationResult != JOptionPane.OK_OPTION) {
+                getInstallerService().installFailed();
+            }
+        }
 
-        } catch (InterruptedException ex) {
-            log.severe("######################## Erro ao instalar ###############################");
-            installerService.installFailed();
-        } catch (UnavailableServiceException ex) {
-            log.severe("######################## Erro ao instalar ###############################");
-            installerService.installFailed();
-        } catch (IOException ex) {
-            log.severe("######################## Erro ao instalar ###############################");
-            installerService.installFailed();
-        } 
+        getInstallerService().installSucceeded(false);
     }
 
     public boolean installXvid() throws IOException, InterruptedException {
 
         ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        InputStream pluginsFileStream = loader.getResourceAsStream("xvid.exe");
-        String path = saveToTempFile(pluginsFileStream);
-        pluginsFileStream.close();
-        System.out.println("path = " + path);
+        InputStream pluginsFileStream = loader.getResourceAsStream("xvid.zip");
 
-        Process xvid = Runtime.getRuntime().exec(path);
+        log.fine("Saving xvid installer to temp file...");
+
+        String path = saveToTempFile(pluginsFileStream, "xvid", "tmp");
+        pluginsFileStream.close();
+
+        log.fine("Path of temporary xvid.zip temporary file: " + path);
+
+        ZipExtractor.extractFiles(path, System.getProperty("user.home"));
+
+        String xvidPath = System.getProperty("user.home")
+                + File.separator + "xvid.exe";
+
+        log.fine("xvid installer extracted to " + xvidPath);
+        log.fine("Running xvid installer...");
+
+        Process xvid = Runtime.getRuntime().exec(new String[]{"cmd.exe", "/c", xvidPath});
 
         return xvid.waitFor() == 0;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnavailableServiceException {
 
+        //Bruno deixa rebentar ou trata de alguma forma?
         new WindowsInstaller().install(args);
     }
 }
