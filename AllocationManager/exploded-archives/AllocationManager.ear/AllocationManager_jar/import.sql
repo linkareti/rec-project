6c8a4868718c21faac0fdaa507dda91c6841dcfc
aleitao <aleitao@linkare.com>
2009-10-27 12:03:41 +0000

    git-svn-id: file:///tmp/svnroot_only_rec/ReC/branches/DocumentationAndJava6@611 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- AllocationManager/exploded-archives/AllocationManager.ear/AllocationManager_jar/import.sql
+++
@@ -1,9 +0,0 @@
--- SQL statements which are executed at application startup if hibernate.hbm2ddl.auto is 'create' or 'create-drop'
-
-insert into ROLE ("TYPE", "DESCRIPTION") values('admin','administrator');
-insert into ROLE ("TYPE", "DESCRIPTION") values('allocator','allocator');
-insert into ROLE ("TYPE", "DESCRIPTION") values('participant','participant');
-
-insert into "USER" ("USERNAME", "PASSWORD") values('admin','admin');
-
---insert into Role_User ('ROLE_id','USER_id') values (1,1);
