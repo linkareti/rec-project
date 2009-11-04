caa6f1e865e2b013fd6021d25c2d4cca41b4593b
fdias <fdias@linkare.com>
2009-11-04 22:10:15 +0000

    git-svn-id: file:///tmp/svnroot_only_rec/ReC/trunk@651 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- src/pt/utl/ist/elab/driver/serial/stamp/statsound/StatSoundStampDataSource.java
+++ src/pt/utl/ist/elab/driver/serial/stamp/statsound/StatSoundStampDataSource.java
@@ -250,8 +250,8 @@ public class StatSoundStampDataSource extends AbstractStampDataSource implements
 					values = new PhysicsValue[7];
 					values[0] = PhysicsValueFactory.fromInt(i, config.getChannelsConfig(0).getSelectedScale());
 					values[1] = PhysicsValueFactory.fromDouble(freqIni, config.getChannelsConfig(1).getSelectedScale());
-					values[2] = PhysicsValueFactory.fromDouble(acqByte[i] + acqByte[i+1]*256, config.getChannelsConfig(2).getSelectedScale());
-					values[3] = PhysicsValueFactory.fromDouble(acqByte[i+2] + acqByte[i+3]*256, config.getChannelsConfig(3).getSelectedScale());
+					values[2] = PhysicsValueFactory.fromDouble(acqByte[i*10] + acqByte[(i*10)+1]*256, config.getChannelsConfig(2).getSelectedScale());
+					values[3] = PhysicsValueFactory.fromDouble(acqByte[(i*10)+2] + acqByte[(i*10)+3]*256, config.getChannelsConfig(3).getSelectedScale());
 					super.addDataRow(values);
 				}
 
