4b66ffcea7475b30a444165a0ba4a2c9ea093ff5
fdias <fdias@linkare.com>
2009-11-06 18:14:57 +0000

    git-svn-id: file:///tmp/svnroot_only_rec/ReC/trunk@674 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- src/pt/utl/ist/elab/driver/serial/stamp/statsound/audio/Oscilador.java
+++ src/pt/utl/ist/elab/driver/serial/stamp/statsound/audio/Oscilador.java
@@ -123,7 +123,7 @@ public class Oscilador extends AudioInputStream {
 			// PULSE
 		} else if (type == 3) {
 			System.out.println("#: " + audioFormat.getFrameRate() + " " + frequencia1);
-			int perFrames = Math.round(audioFormat.getFrameRate() / frequencia1);
+			int perFrames = Math.round(audioFormat.getFrameRate() / ((float)frequencia1 * 15.2f ));
 			int perFrames10 = Math.round(perFrames / 10f);
 			float fValue = 0f;
 			for (int nFrame = 0; nFrame < nFullLengthInFrames; nFrame++) {
