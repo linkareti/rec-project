10d5dd4ebeee06fccf7dbc53341703f636543ec6
fdias <fdias@linkare.com>
2009-12-18 16:04:42 +0000

    git-svn-id: file:///tmp/svnroot_only_rec/ReC/trunk@698 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- src/pt/utl/ist/elab/driver/serial/stamp/statsound/audio/Oscilador.java
+++ src/pt/utl/ist/elab/driver/serial/stamp/statsound/audio/Oscilador.java
@@ -59,9 +59,16 @@ public class Oscilador extends AudioInputStream {
 
 		// SIN
 		if (type == 1) {
+			
+			double dCurrentPeriod = 0;
+			
 			for (int nFrame = 0; nFrame < nFullLengthInFrames; nFrame++) {
-				double dCurrentPeriod = 1. / (double) (frequencia1 + (double) nFrame / (double) nFullLengthInFrames * (frequencia2 - frequencia1));
 
+				if (nFrame % 1000 == 0) {
+					dCurrentPeriod = 1. / (double) (frequencia1 + (double) nFrame / (double) nFullLengthInFrames * (frequencia2 - frequencia1));
+					System.out.println("Frequencia : " + 1 / dCurrentPeriod);
+				}
+				
 				float fFullPosition = (float) nFrame / (float) nFullLengthInFrames * (lengthInSeconds);
 
 				float fValue = (float) Math.cos(fFullPosition * 2.0 * Math.PI / dCurrentPeriod);
