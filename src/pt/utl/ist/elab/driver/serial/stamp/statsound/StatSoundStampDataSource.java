94d5611ea7031ed5a8421400153a61fee4e8e998
fdias <fdias@linkare.com>
2010-01-13 18:41:56 +0000

    git-svn-id: file:///tmp/svnroot_only_rec/ReC/trunk@720 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- src/pt/utl/ist/elab/driver/serial/stamp/statsound/StatSoundStampDataSource.java
+++ src/pt/utl/ist/elab/driver/serial/stamp/statsound/StatSoundStampDataSource.java
@@ -264,7 +264,7 @@ public class StatSoundStampDataSource extends AbstractStampDataSource implements
 					nPoints = acqByte.length / 2;
 				}
 				
-				for (int i = 0; i < Math.round(nPoints/40); i++) {
+				for (int i = 0; i < Math.round(nPoints/4); i++) {
 					values = new PhysicsValue[7];
 					
 					String string = "";
