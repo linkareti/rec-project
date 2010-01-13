06d23367e2c1add88ae8d6c4585d4b2dab2b6455
fdias <fdias@linkare.com>
2010-01-13 19:48:04 +0000

    git-svn-id: file:///tmp/svnroot_only_rec/ReC/trunk@726 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- src/pt/utl/ist/elab/driver/serial/stamp/statsound/StatSoundStampDataSource.java
+++ src/pt/utl/ist/elab/driver/serial/stamp/statsound/StatSoundStampDataSource.java
@@ -280,7 +280,7 @@ public class StatSoundStampDataSource extends AbstractStampDataSource implements
 					values[1] = PhysicsValueFactory.fromDouble(freqIni, config.getChannelsConfig(1).getSelectedScale());
 					
 					//if (i % 4 == 0 || i % 3 == 0) {
-						values[2] = PhysicsValueFactory.fromDouble( (short) (acqByte[i*4+1] << 8 | (255 & acqByte[i*4])) /*acqByte[i*4] + (acqByte[i*4+1])*255*/, config.getChannelsConfig(2).getSelectedScale());
+						values[2] = PhysicsValueFactory.fromDouble( (short) (acqByte[i*4+1] << 8 | (255 & acqByte[i*4])  / 1000) /*acqByte[i*4] + (acqByte[i*4+1])*255*/, config.getChannelsConfig(2).getSelectedScale());
 						values[3] = PhysicsValueFactory.fromDouble( (short) (acqByte[i*4+3] << 8 | (255 & acqByte[i*4+2])) /*acqByte[i*4+2] + (acqByte[i*4+3])*255*/, config.getChannelsConfig(3).getSelectedScale());
 					//}
 					//else {
