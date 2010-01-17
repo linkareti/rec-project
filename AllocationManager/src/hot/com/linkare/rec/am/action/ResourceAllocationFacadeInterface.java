e5d63e874be9d84f389a6b9ce7b4cbd0b3b7347f
nsimoes <nsimoes@linkare.com>
2010-01-17 23:16:45 +0000

    git-svn-id: file:///tmp/svnroot_only_rec/ReC/branches/DocumentationAndJava6@767 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- AllocationManager/src/hot/com/linkare/rec/am/action/ResourceAllocationFacadeInterface.java
+++ AllocationManager/src/hot/com/linkare/rec/am/action/ResourceAllocationFacadeInterface.java
@@ -1,8 +1,18 @@
 package com.linkare.rec.am.action;
 
+import java.util.Date;
+
 import javax.ejb.Remote;
+
+import com.linkare.rec.am.model.Laboratory;
 
 @Remote
 public interface ResourceAllocationFacadeInterface {
+	
+	public String getReservations(String laboratorio, Date startDate,
+			Date endDate) throws Exception;
+	
+	public void initData();
+	
 
 }
