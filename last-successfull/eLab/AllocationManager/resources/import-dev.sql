f1ec0b2dc627f8a66ffdd9cdc01feb55a4b82e3d
pzenida <pzenida@linkare.com>
2010-01-19 00:03:13 +0000

    Delete old tag by SvnTag Hudson plugin.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/elab@776 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- last-successfull/eLab/AllocationManager/resources/import-dev.sql
+++
@@ -1,9 +0,0 @@
--- SQL statements which are executed at application startup if hibernate.hbm2ddl.auto is 'create' or 'create-drop'
-
---insert into ROLE (`TYPE`, `DESCRIPTION`) values('admin','administrator');
---insert into ROLE (`TYPE`, `DESCRIPTION`) values('allocator','allocator');
---insert into ROLE (`TYPE`, `DESCRIPTION`) values('participant','participant');
-
---insert into USER (`USERNAME`, `PASSWORD`) values('admin','admin');
-
---insert into USER_ROLE (`ROLES_ID`,`USERS_ID`) values (1,1);
