ba59715d821aef029bff152767f2d763ee7ad64c
pzenida <pzenida@linkare.com>
2010-01-20 01:06:38 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-20.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec@793 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/client/com/linkare/rec/impl/i18n/ReCListResourceBundle.java
@@ -0,0 +1,34 @@
+/*
+ * ReCListResourceBundle.java
+ *
+ * Created on 22 de Janeiro de 2004, 0:15
+ */
+
+package com.linkare.rec.impl.i18n;
+
+import java.util.Enumeration;
+import java.util.ListResourceBundle;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public class ReCListResourceBundle extends ReCResourceBundle {
+	private ListResourceBundle delegate = null;
+
+	/** Creates a new instance of ReCListResourceBundle */
+	public ReCListResourceBundle(ListResourceBundle delegate) {
+		this.delegate = delegate;
+	}
+
+	public final Object handleGetObject(String key) {
+		return delegate.handleGetObject(key);
+	}
+
+	/**
+	 * Implementation of ResourceBundle.getKeys.
+	 */
+	public Enumeration<String> getKeys() {
+		return delegate.getKeys();
+	}
+}
