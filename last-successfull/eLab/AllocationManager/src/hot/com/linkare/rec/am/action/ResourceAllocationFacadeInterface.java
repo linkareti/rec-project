f1c7c2b707526788f3c675dc2462bdee9c411a02
pzenida <pzenida@linkare.com>
2010-01-23 01:05:58 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-eLab-21.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/elab@830 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/AllocationManager/src/hot/com/linkare/rec/am/action/ResourceAllocationFacadeInterface.java
@@ -0,0 +1,20 @@
+package com.linkare.rec.am.action;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import com.linkare.rec.am.model.Experiment;
+import com.linkare.rec.am.model.Laboratory;
+
+@Remote
+public interface ResourceAllocationFacadeInterface {
+	
+	public List<String> getReservations(String laboratorio, Date startDate,
+			Date endDate) throws Exception;
+	
+	public void initData();
+	
+
+}
---
+++ last-successfull/eLab/AllocationManager/src/main/com/linkare/rec/am/model/User.java
@@ -0,0 +1,71 @@
+package com.linkare.rec.am.model;
+
+import java.util.List;
+
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.jboss.seam.annotations.security.management.UserPassword;
+import org.jboss.seam.annotations.security.management.UserPrincipal;
+import org.jboss.seam.annotations.security.management.UserRoles;
+
+@Entity
+@Table (name="USER")
+public class User {
+
+	@Id 
+	@GeneratedValue
+	private long id;
+	
+	@Basic
+	@Column (unique=true)
+	@UserPrincipal
+	private String username;
+	
+	@Basic
+	@UserPassword (hash="none")
+	private String password;
+	
+	@Basic
+	@ManyToMany (cascade=CascadeType.ALL)
+	@UserRoles
+	private List<Role> roles;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+}
