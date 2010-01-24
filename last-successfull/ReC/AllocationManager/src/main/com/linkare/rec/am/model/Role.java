5a606418f4dda04efa6ebd8cbdc400d2fca24139
pzenida <pzenida@linkare.com>
2010-01-24 01:07:10 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-25.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec@832 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/AllocationManager/src/main/com/linkare/rec/am/model/Role.java
@@ -0,0 +1,73 @@
+package com.linkare.rec.am.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.jboss.seam.annotations.security.management.RoleName;
+
+@Entity
+@Table(name = "ROLE")
+public class Role {
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	@Basic
+	@RoleName
+	private String type;
+
+	@Basic
+	private String description;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@ManyToMany(mappedBy = "roles")
+	private List<User> users = new ArrayList<User>();
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+	public void addUser(User user) {
+
+		this.users.add(user);
+	}
+
+	public void removeUser(User user) {
+		this.users.remove(user);
+	}
+}
