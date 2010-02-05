52d6e1c85f4c93fa8a2ea6164dedb90d78912d0b
pzenida <pzenida@linkare.com>
2010-02-05 01:12:43 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-eLab-31.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/elab@892 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/elab/src/java/driver/pt/utl/ist/elab/driver/serial/serialportgeneric/command/SerialPortCommand.java
@@ -0,0 +1,134 @@
+/*
+ * SerialPortCommand.java
+ *
+ * Created on 11 de Novembro de 2002, 15:34
+ */
+
+package pt.utl.ist.elab.driver.serial.serialportgeneric.command;
+
+import java.util.HashMap;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ * 
+ * @author fdias
+ * @date 10/2009
+ * 
+ * 
+ */
+public class SerialPortCommand implements java.io.Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7857767506247594478L;
+	
+	private String commandIdentifier = null;
+	private String command = null;
+	private HashMap<String, String> commandDataMap = null;
+	private HashMap<Integer,String> returnHashMap = null;
+	
+	private boolean isData = false;
+
+	/**
+	 * Creates a new instance of SerialPortCommand
+	 * 
+	 * @param commandIdentifier
+	 */
+	public SerialPortCommand(String commandIdentifier) {
+		this.commandIdentifier = commandIdentifier;
+	}
+	
+	public void setCommand(String command) {
+		this.command = command;
+	}
+
+	public String getCommand() {
+		return this.command;
+	}
+
+	public void setCommandIdentifier(String commandIdentifier) {
+		this.commandIdentifier = commandIdentifier;
+	}
+
+	public String getCommandIdentifier() {
+		return this.commandIdentifier;
+	}
+
+	public void addCommandData(String commandDataKey, String commandDataObject) {
+		if (this.commandDataMap == null)
+			this.commandDataMap = new HashMap<String, String>(1);
+
+		if (this.commandDataMap.containsKey(commandDataKey))
+			this.commandDataMap.remove(commandDataKey);
+
+		this.commandDataMap.put(commandDataKey, commandDataObject);
+	}
+
+	public Object getCommandData(Object commandDataKey) {
+		return this.commandDataMap.get(commandDataKey);
+	}
+
+	public void setData(boolean isData) {
+		this.isData = isData;
+	}
+
+	public boolean isData() {
+		return this.isData;
+	}
+
+	/**
+	 * @author fdias
+	 * @return HashMap<Integer,String>
+	 */
+	public HashMap<Integer,String> getDataHashMap() {
+		// no command, no donut for you
+		if (command == null)
+			return null;
+		// there is a command but no data hashmap? populate it
+		if (returnHashMap == null) {
+			returnHashMap = new HashMap<Integer,String>();
+			String[] commandStringArray = command.split("\t");
+			for (int i = 1; i < commandStringArray.length; i++) {
+				returnHashMap.put(i, commandStringArray[i]);
+			}
+			return returnHashMap;
+		}
+		// but if it is already populated, reuse it and save time
+		else
+			return returnHashMap;
+	}
+	
+	public static boolean isResponse(String response, String sentMessage) {
+		if (response == null || sentMessage == null)
+			return false;
+		String[] responseArray = response.split("\t", 2);
+		String[] sentMessageArray = sentMessage.split("\t", 2);
+		if (responseArray.length < 1 || sentMessageArray.length <1)
+			return false;
+		if (responseArray[0].isEmpty() || sentMessageArray[0].isEmpty())
+			return false;
+		// both are equivalent but response is upper case and set message is lower case
+		if (responseArray[0].equalsIgnoreCase(sentMessageArray[0]) &&
+				responseArray[0].toUpperCase().equals(responseArray[0]) &&
+				sentMessageArray[0].toLowerCase().equals(sentMessageArray[0])) {
+			// Okay, it can be a valid response!
+			
+			if (responseArray.length == 2 && sentMessageArray.length == 2)  {
+				// command information must be equal
+				if (responseArray[1].equals(sentMessageArray[1])) {
+					return true;
+				}
+				else
+					return false;
+			}
+			else
+				return true;
+			
+		}
+		else
+			return false;
+	}
+
+}
