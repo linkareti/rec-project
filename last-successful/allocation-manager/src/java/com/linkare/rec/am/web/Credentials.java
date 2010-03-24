e86a173990f6da67a42b6f4af9c723fc79dcabcc
respiritosanto <respiritosanto@linkare.com>
2010-03-24 17:16:51 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-allocation-manager-3.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/AllocationManager@953 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successful/allocation-manager/src/java/com/linkare/rec/am/web/Credentials.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.linkare.rec.am.web;
+
+import java.io.Serializable;
+import java.security.Principal;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Default;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Joao
+ *
+ * This is just a simple container class Weld bean for the username
+ * and password entry values.
+ */
+@Named
+@RequestScoped
+@Default
+public class Credentials implements Serializable {
+
+    private String username = null;
+    private String password = null;
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
