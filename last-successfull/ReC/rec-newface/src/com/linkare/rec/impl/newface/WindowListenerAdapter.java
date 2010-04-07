9b03a975c5b24d74cfc019c0ac83075289b5fcd4
pzenida <pzenida@linkare.com>
2010-04-07 00:04:24 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-109.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec@1113 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec-newface/src/com/linkare/rec/impl/newface/WindowListenerAdapter.java
@@ -0,0 +1,50 @@
+package com.linkare.rec.impl.newface;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+/**
+ * A WindowListenerAdapter.
+ * 
+ * @see WindowListener
+ * 
+ * @author <A HREF="mailto:hfernandes@linkare.com">Henrique Fernandes<A />
+ */
+public class WindowListenerAdapter implements WindowListener {
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// adapter
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// adapter
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// adapter
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// adapter
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// adapter
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// adapter
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// adapter
+	}
+
+}
