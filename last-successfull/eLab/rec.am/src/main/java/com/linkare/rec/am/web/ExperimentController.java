f2f29e41c3ce0c3c1a451bc23ab138b93132cda5
pzenida <pzenida@linkare.com>
2010-04-15 00:41:11 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-eLab-53.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/elab@1184 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.am/src/main/java/com/linkare/rec/am/web/ExperimentController.java
@@ -0,0 +1,128 @@
+package com.linkare.rec.am.web;
+
+import java.util.ResourceBundle;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import com.linkare.rec.am.model.Experiment;
+import com.linkare.rec.am.model.ExperimentFacade;
+import com.linkare.rec.am.web.controller.AbstractController;
+import com.linkare.rec.am.web.util.JsfUtil;
+
+@ManagedBean(name = "experimentController")
+@RequestScoped
+public class ExperimentController extends AbstractController<Experiment, ExperimentFacade> {
+
+    private static final long serialVersionUID = 1L;
+
+    @EJB
+    private ExperimentFacade ejbFacade;
+
+    @Override
+    public Experiment getCurrent() {
+	if (current == null || current.getPk() == null) {
+	    current = (Experiment) JsfUtil.getObjectFromRequestParameter("current", new ExperimentConverter());
+	}
+	return current;
+    }
+
+    @Override
+    public void setCurrent(Experiment current) {
+	this.current = current;
+    }
+
+    public final Experiment getSelected() {
+	if (current == null) {
+	    current = new Experiment();
+	    selectedItemIndex = -1;
+	}
+	return current;
+    }
+
+    @Override
+    protected ExperimentFacade getFacade() {
+	return ejbFacade;
+    }
+
+    @Override
+    public final String prepareCreate() {
+	current = new Experiment();
+	selectedItemIndex = -1;
+	return CREATE;
+    }
+
+    @Override
+    public final String create() {
+	try {
+	    getFacade().create(current);
+	    JsfUtil.addSuccessMessage(ResourceBundle.getBundle(BUNDLE).getString("info.create"));
+	    return prepareCreate();
+	} catch (Exception e) {
+	    JsfUtil.addErrorMessage(e, ResourceBundle.getBundle(BUNDLE).getString("error.persistence"));
+	    return null;
+	}
+    }
+
+    @Override
+    public final String update() {
+	try {
+	    getFacade().edit(current);
+	    JsfUtil.addSuccessMessage(ResourceBundle.getBundle(BUNDLE).getString("info.update"));
+	    return VIEW;
+	} catch (Exception e) {
+	    JsfUtil.addErrorMessage(e, ResourceBundle.getBundle(BUNDLE).getString("error.persistence"));
+	    return null;
+	}
+    }
+
+    protected void performDestroy() {
+	try {
+	    getFacade().remove(current);
+	    JsfUtil.addSuccessMessage(ResourceBundle.getBundle(BUNDLE).getString("info.remove"));
+	} catch (Exception e) {
+	    JsfUtil.addErrorMessage(e, ResourceBundle.getBundle(BUNDLE).getString("error.persistence"));
+	}
+    }
+
+    @FacesConverter(value = "experimentConverter", forClass = Experiment.class)
+    public static class ExperimentConverter implements Converter {
+
+	@Override
+	public final Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
+	    if (value == null || value.length() == 0) {
+		return null;
+	    }
+	    ExperimentController controller = (ExperimentController) facesContext.getApplication().getELResolver().getValue(facesContext.getELContext(), null,
+															    "experimentController");
+	    return controller.ejbFacade.find(getKey(value));
+	}
+
+	private Long getKey(String value) {
+	    return Long.valueOf(value);
+	}
+
+	private String getStringKey(Long value) {
+	    return value.toString();
+	}
+
+	@Override
+	public final String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+	    if (object == null) {
+		return null;
+	    }
+	    if (object instanceof Experiment) {
+		Experiment o = (Experiment) object;
+		return getStringKey(o.getPk());
+	    } else {
+		throw new IllegalArgumentException("object " + object + " is of type " + object.getClass().getName() + "; expected type: "
+			+ Experiment.class.getName());
+	    }
+	}
+    }
+}
---
+++ last-successfull/eLab/rec.am/src/main/java/com/linkare/rec/am/wsgen/moodle/LoginReturn.java
@@ -0,0 +1,155 @@
+/**
+ * LoginReturn.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.linkare.rec.am.wsgen.moodle;
+
+public class LoginReturn  implements java.io.Serializable {
+    private java.math.BigInteger client;
+
+    private java.lang.String sessionkey;
+
+    public LoginReturn() {
+    }
+
+    public LoginReturn(
+           java.math.BigInteger client,
+           java.lang.String sessionkey) {
+           this.client = client;
+           this.sessionkey = sessionkey;
+    }
+
+
+    /**
+     * Gets the client value for this LoginReturn.
+     * 
+     * @return client
+     */
+    public java.math.BigInteger getClient() {
+        return client;
+    }
+
+
+    /**
+     * Sets the client value for this LoginReturn.
+     * 
+     * @param client
+     */
+    public void setClient(java.math.BigInteger client) {
+        this.client = client;
+    }
+
+
+    /**
+     * Gets the sessionkey value for this LoginReturn.
+     * 
+     * @return sessionkey
+     */
+    public java.lang.String getSessionkey() {
+        return sessionkey;
+    }
+
+
+    /**
+     * Sets the sessionkey value for this LoginReturn.
+     * 
+     * @param sessionkey
+     */
+    public void setSessionkey(java.lang.String sessionkey) {
+        this.sessionkey = sessionkey;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof LoginReturn)) return false;
+        LoginReturn other = (LoginReturn) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            ((this.client==null && other.getClient()==null) || 
+             (this.client!=null &&
+              this.client.equals(other.getClient()))) &&
+            ((this.sessionkey==null && other.getSessionkey()==null) || 
+             (this.sessionkey!=null &&
+              this.sessionkey.equals(other.getSessionkey())));
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        if (getClient() != null) {
+            _hashCode += getClient().hashCode();
+        }
+        if (getSessionkey() != null) {
+            _hashCode += getSessionkey().hashCode();
+        }
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(LoginReturn.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("http://localhost/moodle/wspp/wsdl", "loginReturn"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("client");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "client"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "integer"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("sessionkey");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "sessionkey"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
