cac5f1ef674095e82224366093726568bcac0643
pzenida <pzenida@linkare.com>
2010-04-21 00:04:25 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-126.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec@1230 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/AllocationManager/src/java/com/linkare/rec/am/web/util/ExtendedMenuRenderer.java
@@ -0,0 +1,140 @@
+package com.linkare.rec.am.web.util;
+
+import java.io.IOException;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.convert.Converter;
+import javax.faces.model.SelectItem;
+
+import com.sun.faces.renderkit.html_basic.MenuRenderer;
+
+
+/**
+ * Extended menu renderer which renders the 'optionClasses' attribute above the standard menu
+ * renderer. To use it, define it as follows in the render-kit tag of faces-config.xml.
+ *
+ * <pre>
+ * &lt;renderer&gt;
+ *     &lt;component-family&gt;javax.faces.SelectOne&lt;/component-family&gt;
+ *     &lt;renderer-type&gt;javax.faces.Menu&lt;/renderer-type&gt;
+ *     &lt;renderer-class&gt;net.balusc.jsf.renderer.html.ExtendedMenuRenderer&lt;/renderer-class&gt;
+ * &lt;/renderer&gt;
+ * &lt;renderer&gt;
+ *     &lt;component-family&gt;javax.faces.SelectMany&lt;/component-family&gt;
+ *     &lt;renderer-type&gt;javax.faces.Menu&lt;/renderer-type&gt;
+ *     &lt;renderer-class&gt;net.balusc.jsf.renderer.html.ExtendedMenuRenderer&lt;/renderer-class&gt;
+ * &lt;/renderer&gt;
+ * </pre>
+ *
+ * And define the 'optionClasses' attribute as a f:attribute of the h:selectOneMenu or
+ * h:selectManyMenu as follows:
+ *
+ * <pre>
+ * &lt;f:attribute name="optionClasses" value="option1,option2,option3" /&gt;
+ * </pre>
+ *
+ * It accepts a comma separated string of CSS class names which are to be applied on the options
+ * repeatedly (the same way as you use rowClasses in h:dataTable). The optionClasses will be
+ * rendered only if there is no 'disabledClass' or 'enabledClass' being set as an attribute.
+ *
+ * @author BalusC
+ * @link http://balusc.blogspot.com/styling-options-in-hselectonemenu.html
+ */
+public class ExtendedMenuRenderer extends MenuRenderer {
+
+    // Override -----------------------------------------------------------------------------------
+
+    /**
+     * @see com.sun.faces.renderkit.html_basic.MenuRenderer#renderOption(
+     *      javax.faces.context.FacesContext, javax.faces.component.UIComponent,
+     *      javax.faces.convert.Converter, javax.faces.model.SelectItem, java.lang.Object,
+     *      java.lang.Object[])
+     */
+    protected final void renderOption(FacesContext context, UIComponent component, Converter converter,
+        SelectItem currentItem, Object currentSelections, Object[] submittedValues)
+            throws IOException
+    {
+        // Copied from MenuRenderer#renderOption() (and a bit rewritten, but that's just me) ------
+
+        // Get writer.
+        ResponseWriter writer = context.getResponseWriter();
+        assert (writer != null);
+
+        // Write 'option' tag.
+        writer.writeText("\t", component, null);
+        writer.startElement("option", component);
+
+        // Write 'value' attribute.
+        String valueString = getFormattedValue(context, component, currentItem.getValue(), converter);
+        writer.writeAttribute("value", valueString, "value");
+
+        // Write 'selected' attribute.
+        Object valuesArray;
+        Object itemValue;
+        if (containsaValue(submittedValues)) {
+            valuesArray = submittedValues;
+            itemValue = valueString;
+        } else {
+            valuesArray = currentSelections;
+            itemValue = currentItem.getValue();
+        }
+        if (isSelected(context, component,itemValue, valuesArray, converter)) {
+            writer.writeAttribute("selected", true, "selected");
+        }
+
+        // Write 'disabled' attribute.
+        Boolean disabledAttr = (Boolean) component.getAttributes().get("disabled");
+        boolean componentDisabled = disabledAttr != null && disabledAttr.booleanValue();
+        if (!componentDisabled && currentItem.isDisabled()) {
+            writer.writeAttribute("disabled", true, "disabled");
+        }
+
+        // Write 'class' attribute.
+        String labelClass;
+        if (componentDisabled || currentItem.isDisabled()) {
+            labelClass = (String) component.getAttributes().get("disabledClass");
+        } else {
+            labelClass = (String) component.getAttributes().get("enabledClass");
+        }
+
+        // Inserted custom code which checks the optionClasses attribute --------------------------
+
+        if (labelClass == null) {
+            String optionClasses = (String) component.getAttributes().get("optionClasses");
+            if (optionClasses != null) {
+                String[] labelClasses = optionClasses.split("\\s*,\\s*");
+                String indexKey = component.getClientId(context) + "_currentOptionIndex";
+                Integer index = (Integer) component.getAttributes().get(indexKey);
+                if (index == null || index == labelClasses.length) {
+                    index = 0;
+                }
+                labelClass = labelClasses[index];
+                component.getAttributes().put(indexKey, ++index);
+            }
+        }
+
+        // The remaining copy of MenuRenderer#renderOption() --------------------------------------
+
+        if (labelClass != null) {
+            writer.writeAttribute("class", labelClass, "labelClass");
+        }
+
+        // Write option body (the option label).
+        if (currentItem.isEscape()) {
+            String label = currentItem.getLabel();
+            if (label == null) {
+                label = valueString;
+            }
+            writer.writeText(label, component, "label");
+        } else {
+            writer.write(currentItem.getLabel());
+        }
+
+        // Write 'option' end tag.
+        writer.endElement("option");
+        writer.writeText("\n", component, null);
+    }
+
+}
---
+++ last-successfull/ReC/rec.am/src/main/java/com/linkare/rec/am/web/ScheduleController.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright 2009 Prime Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.linkare.rec.am.web;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.model.SelectItem;
+
+import org.primefaces.event.DateSelectEvent;
+import org.primefaces.event.ScheduleEntryMoveEvent;
+import org.primefaces.event.ScheduleEntryResizeEvent;
+import org.primefaces.event.ScheduleEntrySelectEvent;
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleEvent;
+import org.primefaces.model.ScheduleModel;
+
+import com.linkare.rec.am.model.ExternalCourseFacade;
+import com.linkare.rec.am.model.Reservation;
+import com.linkare.rec.am.model.ReservationFacade;
+import com.linkare.rec.am.model.UserPrincipal;
+import com.linkare.rec.am.model.UserPrincipalFacade;
+import com.linkare.rec.am.model.moodle.MoodleRecord;
+import com.linkare.rec.am.web.auth.UserView;
+import com.linkare.rec.am.web.moodle.SessionHelper;
+import com.linkare.rec.am.web.util.JsfUtil;
+
+@ManagedBean(name = "scheduleController")
+@ViewScoped
+public class ScheduleController implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private ScheduleModel eventModel;
+
+    private static final int MINUTE_STEP = 30;
+
+    private Reservation event = new Reservation();
+
+    @EJB
+    private ReservationFacade ejbFacade;
+
+    @EJB
+    private UserPrincipalFacade ejbUserPrincipalFacade;
+
+    private SelectItem[] externalCourses;
+
+    private String theme;
+
+    private static Calendar cal = Calendar.getInstance();
+
+    private List<ScheduleEvent> getEvents() {
+	final UserView userView = SessionHelper.getUserView();
+	if (userView.isExternal()) {
+	    return ejbFacade.findReservationsFor(userView.getUsername());
+	}
+	final UserPrincipal user = ejbUserPrincipalFacade.findByUsername(userView.getUsername());
+	return ejbFacade.findReservationsFor(user);
+    }
+
+    public ScheduleModel getEventModel() {
+	return eventModel = eventModel == null ? new DefaultScheduleModel(getEvents()) : eventModel;
+    }
+
+    public Reservation getEvent() {
+	return event;
+    }
+
+    public void setEvent(Reservation event) {
+	this.event = event;
+    }
+
+    public void addEvent(ActionEvent actionEvent) {
+	if (event.getId() == null) {
+	    eventModel.addEvent(event);
+	    createOrUpdate();
+	} else {
+	    eventModel.updateEvent(event);
+	    createOrUpdate();
+	}
+	event = new Reservation();
+    }
+
+    public void removeEvent(ActionEvent actionEvent) {
+	if (event.getId() != null) {
+	    eventModel.deleteEvent(event);
+	    ejbFacade.remove(event);
+	}
+    }
+
+    public final String createOrUpdate() {
+	if (event.getId() == null) {
+	    final String startTimeSlot = event.getStartTimeSlot();
+	    final StringTokenizer startTimeSlotTokenizer = new StringTokenizer(startTimeSlot, ":");
+
+	    cal.setTime(event.getStartDate());
+	    cal.set(Calendar.HOUR, Integer.parseInt(startTimeSlotTokenizer.nextToken()));
+	    cal.set(Calendar.MINUTE, Integer.parseInt(startTimeSlotTokenizer.nextToken()));
+	    event.setStartDate(cal.getTime());
+	    event.setStartTimeSlot(startTimeSlot);
+
+	    final String endTimeSlot = event.getEndTimeSlot();
+	    final StringTokenizer endTimeSlotTokenizer = new StringTokenizer(endTimeSlot, ":");
+	    cal.setTime(event.getEndDate());
+	    cal.set(Calendar.HOUR, Integer.parseInt(endTimeSlotTokenizer.nextToken()));
+	    cal.set(Calendar.MINUTE, Integer.parseInt(endTimeSlotTokenizer.nextToken()));
+
+	    event.setEndDate(cal.getTime());
+	    event.setEndTimeSlot(cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE));
+
+	    eventModel.addEvent(event);
+	    ejbFacade.create(event);
+	} else {
+	    eventModel.updateEvent(event);
+	    ejbFacade.edit(event);
+	}
+	return null;
+    }
+
+    public void onEventSelect(ScheduleEntrySelectEvent selectEvent) {
+	event = (Reservation) selectEvent.getScheduleEvent();
+    }
+
+    public void onDateSelect(DateSelectEvent selectEvent) {
+	final UserView userView = SessionHelper.getUserView();
+	event = new Reservation("", selectEvent.getDate(), selectEvent.getDate());
+
+	if (userView.isExternal()) {
+	    event.setMoodleRecord(new MoodleRecord());
+	    event.getMoodleRecord().setExternalUser(userView.getUsername());
+	    event.getMoodleRecord().setMoodleUrl(userView.getDomain());
+	} else {
+	    event.setUserPrincipal(ejbUserPrincipalFacade.findByUsername(userView.getUsername()));
+	}
+    }
+
+    public void onEventMove(ScheduleEntryMoveEvent selectEvent) {
+	FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event moved", "Day delta:" + selectEvent.getDayDelta() + ", Minute delta:"
+		+ selectEvent.getMinuteDelta());
+	event = (Reservation) selectEvent.getScheduleEvent();
+	moveEventTime(event, selectEvent.getMinuteDelta());
+	createOrUpdate();
+	addMessage(message);
+    }
+
+    private void moveEventTime(final Reservation reservation, final int minuteDelta) {
+	cal.setTime(reservation.getStartDate());
+	cal.set(Calendar.MINUTE, cal.get(Calendar.MINUTE) + minuteDelta);
+	reservation.setStartDate(cal.getTime());
+
+	cal.setTime(reservation.getEndDate());
+	cal.set(Calendar.MINUTE, cal.get(Calendar.MINUTE) + minuteDelta);
+	reservation.setEndDate(cal.getTime());
+    }
+
+    private void extendEventTime(final Reservation reservation, final int minuteDelta) {
+	cal.setTime(reservation.getEndDate());
+	cal.set(Calendar.MINUTE, cal.get(Calendar.MINUTE) + minuteDelta);
+	reservation.setEndDate(cal.getTime());
+    }
+
+    public void onEventResize(ScheduleEntryResizeEvent selectEvent) {
+	FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event resized", "Day delta:" + selectEvent.getDayDelta() + ", Minute delta:"
+		+ selectEvent.getMinuteDelta());
+	event = (Reservation) selectEvent.getScheduleEvent();
+	extendEventTime(event, selectEvent.getMinuteDelta());
+	createOrUpdate();
+	addMessage(message);
+    }
+
+    private void addMessage(FacesMessage message) {
+	FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+    public String getTheme() {
+	return theme;
+    }
+
+    public void setTheme(String theme) {
+	this.theme = theme;
+    }
+
+    /**
+     * @return the externalCourses
+     */
+    public SelectItem[] getExternalCourses() {
+	return externalCourses = externalCourses == null ? JsfUtil.getSelectItems(new ExternalCourseFacade().findAll(), true) : externalCourses;
+    }
+
+    /**
+     * @param externalCourses
+     *            the externalCourses to set
+     */
+    public void setExternalCourses(SelectItem[] externalCourses) {
+	this.externalCourses = externalCourses;
+    }
+}
