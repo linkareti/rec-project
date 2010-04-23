a9f100af532278919baa8d39508ae2bd9cfd9d4c
aleitao <aleitao@linkare.com>
2010-04-23 18:36:09 +0000

    added scripts to start multicast controller.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/trunk@1266 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- templates/scripts/StartMultiCastController.sh
+++ templates/scripts/StartMultiCastController.sh
@@ -3,7 +3,11 @@
 clear
 echo Starting multicast
 
-source ./SetProperties.sh
+OUTPUT_BASE_DIR=/multicast
+export SYSTEM_PROPS="-Dorg.omg.CORBA.ORBClass=org.openorb.orb.core.ORB -Dorg.omg.CORBA.ORBSingletonClass=org.openorb.orb.core.ORBSingleton -Dopenorb.config=$OUTPUT_BASE_DIR/orb/OpenORBMultiCast.xml"
+
+RECCLASSPATH=$OUTPUT_BASE_DIR/openorb/xml-apis_sig.jar:$OUTPUT_BASE_DIR/openorb/tools-1.4.0_sig.jar:$OUTPUT_BASE_DIR/openorb/openorb_orb-1.4.0_sig.jar:$OUTPUT_BASE_DIR/openorb/openorb_pss-1.4.0_sig.jar:$OUTPUT_BASE_DIR/openorb/openorb_orb_omg-1.4.0_sig.jar:$OUTPUT_BASE_DIR/openorb/openorb_ots-1.4.0_sig.jar:$OUTPUT_BASE_DIR/openorb/logkit_sig.jar:$OUTPUT_BASE_DIR/openorb/xercesImpl_sig.jar:$OUTPUT_BASE_DIR/openorb/avalon-framework_sig.jar:$OUTPUT_BASE_DIR/recbase/ReCData_sig.jar:$OUTPUT_BASE_DIR/recbase/ReCMulticastController_sig.jar:.:$CLASSPATH
+export PATH OUTPUT_BASE_DIR SYSTEM_PROPS RECCLASSPATH
 
 export SYSTEM_PROPS="-Dopenorb.profile=ReCMultiCastController -DReC.MultiCastController.BindName=MultiCastController -DReC.MultiCastController.InitRef=MultiCastController -Djava.util.logging.config.file=$OUTPUT_BASE_DIR/recbase/loggers.config.properties $SYSTEM_PROPS -Xms128m -Xmx256m"
 
