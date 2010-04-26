08ad1589917bd08ed0662c7cff4802397d982bca
aleitao <aleitao@linkare.com>
2010-04-26 12:57:10 +0000

    added script to start experiment driver.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/trunk@1309 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- templates/scripts/StartExperiment.sh
+++ templates/scripts/StartExperiment.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 clear
 
-echo Starting Driver
+echo Starting @experiment.name@ Driver
 
-INITIAL_HEAP_MEM=@driver.initial.heap@
-MAX_HEAP_MEM=@driver.max.heap@
+INITIAL_HEAP_MEM=@hardwareserver.initial.heap@
+MAX_HEAP_MEM=@hardwareserver.max.heap@
 
-DRIVER_BASE_DIR=./driver
+DRIVER_BASE_DIR=./hardwareserver
 
 export GENERIC_ORB_SYSPROPS="-Dorg.omg.CORBA.ORBClass=org.openorb.orb.core.ORB -Dorg.omg.CORBA.ORBSingletonClass=org.openorb.orb.core.ORBSingleton -Dopenorb.config=$DRIVER_BASE_DIR/etc/openorb.xml "
 export DRIVER_ORB_SYSPROPS="-Dopenorb.profile=ReCHardware -DReC.MultiCastController.BindName=MultiCastController -DReC.MultiCastController.InitRef=MultiCastController"
 export MEM_SYSPROPS="-Xms$INITIAL_HEAP_MEM -Xmx$MAX_HEAP_MEM"
 export LOG_SYSPROPS="-Djava.util.logging.config.file=$DRIVER_BASE_DIR/etc/loggers.config.properties" 
-export DRIVER_HARWARE_INFO_SYSPROPS="-DeLab.@experiment.name@.HardwareInfo=HardwareInfo.xml"
+export DRIVER_HARWARE_INFO_SYSPROPS="-DeLab.@experiment.name@.HardwareInfo=@experiment.name@HardwareInfo.xml"
 export TOOLKIT_SYSPROPS="-Dawt.toolkit=sun.awt.motif.MToolkit"
 
 export RECCLASSPATH=$DRIVER_BASE_DIR/lib/xml-apis.jar:$DRIVER_BASE_DIR/lib/tools-1.4.0.jar:$DRIVER_BASE_DIR/lib/openorb_orb-1.4.0.jar:$DRIVER_BASE_DIR/lib/openorb_pss-1.4.0.jar:$DRIVER_BASE_DIR/lib/openorb_orb_omg-1.4.0.jar:$DRIVER_BASE_DIR/lib/openorb_ots-1.4.0.jar:$DRIVER_BASE_DIR/lib/logkit.jar:$DRIVER_BASE_DIR/lib/xercesImpl.jar:$DRIVER_BASE_DIR/lib/avalon-framework.jar
-export DRIVER_CLASSPATH=$DRIVER_BASE_DIR/ReCHardwareDriver.jar:$DRIVER_BASE_DIR/ReCCommon.jar
+export DRIVER_CLASSPATH=$DRIVER_BASE_DIR/@experiment.name@Driver.jar:$DRIVER_BASE_DIR/ELabHardwareServer.jar:$DRIVER_BASE_DIR/ReCHardwareServer.jar:$DRIVER_BASE_DIR/ReCCommon.jar
 
 #export ARGS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,address=60001,suspend=n" 
 
