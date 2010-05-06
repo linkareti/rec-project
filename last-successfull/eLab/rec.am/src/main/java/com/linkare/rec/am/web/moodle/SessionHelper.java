226a69135b113f90ee48381461465daefb63e31b
pzenida <pzenida@linkare.com>
2010-05-06 00:07:21 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-eLab-75.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/elab@1474 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.am/src/main/java/com/linkare/rec/am/web/moodle/SessionHelper.java
@@ -0,0 +1,40 @@
+package com.linkare.rec.am.web.moodle;
+
+import com.linkare.rec.am.web.auth.ExternalUserView;
+import com.linkare.rec.am.web.auth.UserView;
+import com.linkare.rec.am.web.util.ConstantUtils;
+import com.linkare.rec.am.web.util.JsfUtil;
+import com.linkare.rec.am.wsgen.moodle.LoginReturn;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ * 
+ */
+public final class SessionHelper {
+
+    private SessionHelper() {
+    }
+
+    public static LoginReturn getLoginReturn() {
+	final UserView userView = getUserView();
+	if (userView.isExternal()) {
+	    return ((ExternalUserView) userView).getLoginReturn();
+	}
+	return null;
+    }
+
+    public static String getUsername() {
+	final UserView userView = getUserView();
+	return userView == null ? null : userView.getUsername();
+    }
+
+    public static String getLoginDomain() {
+	final UserView userView = getUserView();
+	return userView == null ? null : userView.getDomain();
+    }
+
+    public static UserView getUserView() {
+	return (UserView) JsfUtil.getSession().getAttribute(ConstantUtils.USER_VIEW_SESSION_KEY);
+    }
+}
