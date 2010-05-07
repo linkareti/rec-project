4f819c5659908f6ee8d34342e29934983ff44629
pzenida <pzenida@linkare.com>
2010-05-07 00:06:05 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-143.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec@1477 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.am/src/main/java/com/linkare/rec/am/model/moodle/ExternalCourse.java
@@ -0,0 +1,169 @@
+package com.linkare.rec.am.model.moodle;
+
+import com.linkare.commons.jpa.Deletable;
+import com.linkare.commons.jpa.Identifiable;
+import com.linkare.rec.am.wsgen.moodle.CourseRecord;
+
+public class ExternalCourse implements Identifiable<String>, Deletable {
+
+    private static final long serialVersionUID = 1L;
+
+    private CourseRecord delegate;
+
+    public ExternalCourse() {
+	super();
+	this.delegate = new CourseRecord();
+    }
+
+    public ExternalCourse(final CourseRecord courseRecord) {
+	this();
+	this.delegate = courseRecord;
+    }
+
+    @Override
+    public String id() {
+	return delegate.getShortname();
+    }
+
+    public java.lang.String getError() {
+	return delegate.getError();
+    }
+
+    public java.math.BigInteger getId() {
+	return delegate.getId();
+    }
+
+    public java.math.BigInteger getCategory() {
+	return delegate.getCategory();
+    }
+
+    public java.math.BigInteger getSortorder() {
+	return delegate.getSortorder();
+    }
+
+    public java.lang.String getPassword() {
+	return delegate.getPassword();
+    }
+
+    public java.lang.String getFullname() {
+	return delegate.getFullname();
+    }
+
+    public java.lang.String getShortname() {
+	return delegate.getShortname();
+    }
+
+    public java.lang.String getIdnumber() {
+	return delegate.getIdnumber();
+    }
+
+    public java.lang.String getSummary() {
+	return delegate.getSummary();
+    }
+
+    public java.lang.String getFormat() {
+	return delegate.getFormat();
+    }
+
+    public java.math.BigInteger getShowgrades() {
+	return delegate.getShowgrades();
+    }
+
+    public java.math.BigInteger getNewsitems() {
+	return delegate.getNewsitems();
+    }
+
+    public java.lang.String getTeacher() {
+	return delegate.getTeacher();
+    }
+
+    public java.lang.String getTeachers() {
+	return delegate.getTeachers();
+    }
+
+    public java.lang.String getStudent() {
+	return delegate.getStudent();
+    }
+
+    public java.lang.String getStudents() {
+	return delegate.getStudents();
+    }
+
+    public java.math.BigInteger getGuest() {
+	return delegate.getGuest();
+    }
+
+    public java.math.BigInteger getStartdate() {
+	return delegate.getStartdate();
+    }
+
+    public java.math.BigInteger getEnrolperiod() {
+	return delegate.getEnrolperiod();
+    }
+
+    public java.math.BigInteger getNumsections() {
+	return delegate.getNumsections();
+    }
+
+    public java.math.BigInteger getMarker() {
+	return delegate.getMarker();
+    }
+
+    public java.math.BigInteger getMaxbytes() {
+	return delegate.getMaxbytes();
+    }
+
+    public java.math.BigInteger getVisible() {
+	return delegate.getVisible();
+    }
+
+    public java.math.BigInteger getHiddensections() {
+	return delegate.getHiddensections();
+    }
+
+    public java.math.BigInteger getGroupmode() {
+	return delegate.getGroupmode();
+    }
+
+    public java.math.BigInteger getGroupmodeforce() {
+	return delegate.getGroupmodeforce();
+    }
+
+    public java.lang.String getLang() {
+	return delegate.getLang();
+    }
+
+    public java.lang.String getTheme() {
+	return delegate.getTheme();
+    }
+
+    public java.lang.String getCost() {
+	return delegate.getCost();
+    }
+
+    public java.math.BigInteger getTimecreated() {
+	return delegate.getTimecreated();
+    }
+
+    public java.math.BigInteger getTimemodified() {
+	return delegate.getTimemodified();
+    }
+
+    public java.math.BigInteger getMetacourse() {
+	return delegate.getMetacourse();
+    }
+
+    public java.math.BigInteger getMyrole() {
+	return delegate.getMyrole();
+    }
+
+    @Override
+    public String toString() {
+	return getShortname();
+    }
+
+    @Override
+    public boolean delete() {
+	return false;
+    }
+}
