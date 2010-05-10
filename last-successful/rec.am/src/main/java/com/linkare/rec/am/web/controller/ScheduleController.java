6f9742f17292232610c4c068024f45951feb1e2a
respiritosanto <respiritosanto@linkare.com>
2010-05-10 20:01:20 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-rec.am-23.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec.am@1627 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successful/rec.am/src/main/java/com/linkare/rec/am/web/controller/ScheduleController.java
@@ -0,0 +1,240 @@
+/*
+ * Copyright 2009 Prime Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.linkare.rec.am.web.controller;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+import javax.faces.model.SelectItem;
+
+import org.joda.time.DateTime;
+import org.primefaces.event.DateSelectEvent;
+import org.primefaces.event.ScheduleEntryMoveEvent;
+import org.primefaces.event.ScheduleEntryResizeEvent;
+import org.primefaces.event.ScheduleEntrySelectEvent;
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleEvent;
+import org.primefaces.model.ScheduleModel;
+
+import com.linkare.commons.jpa.exceptions.DomainException;
+import com.linkare.commons.jpa.security.User;
+import com.linkare.commons.utils.StringUtils;
+import com.linkare.jsf.utils.JsfUtil;
+import com.linkare.rec.am.model.Reservation;
+import com.linkare.rec.am.model.moodle.ExternalCourse;
+import com.linkare.rec.am.service.ExternalCourseServiceBean;
+import com.linkare.rec.am.service.ReservationService;
+import com.linkare.rec.am.service.ReservationServiceLocal;
+import com.linkare.rec.am.service.UserService;
+import com.linkare.rec.am.service.UserServiceLocal;
+import com.linkare.rec.am.web.moodle.MoodleClientHelper;
+import com.linkare.rec.am.web.moodle.SessionHelper;
+import com.linkare.rec.am.web.util.ConstantUtils;
+
+@ManagedBean(name = "scheduleController")
+@ViewScoped
+public class ScheduleController implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private ScheduleModel eventModel;
+
+    private ScheduleModel lazyEventModel;
+
+    private Reservation event;
+
+    @EJB(beanInterface = ReservationServiceLocal.class)
+    private ReservationService reservationService;
+
+    @EJB(beanInterface = UserServiceLocal.class)
+    private UserService userService;
+
+    private SelectItem[] externalCourses;
+
+    private String theme;
+
+    private ExternalCourse externalCourse;
+
+    private List<ScheduleEvent> getEvents() {
+	final List<ScheduleEvent> events = reservationService.findReservationsFor(SessionHelper.getUserView());
+	for (final ScheduleEvent scheduleEvent : events) {
+	    if (scheduleEvent instanceof Reservation) {
+		((Reservation) scheduleEvent).setStyleClass("myevents");
+	    }
+	}
+	return events;
+    }
+
+    private List<ScheduleEvent> getAllEvents() {
+	return reservationService.findAllReservations(SessionHelper.getUserView());
+    }
+
+    public ScheduleModel getEventModel() {
+	return eventModel = eventModel == null ? new DefaultScheduleModel(getEvents()) : eventModel;
+    }
+
+    // TODO: It may be necessary to use a lazy event model later!!!
+    public ScheduleModel getLazyEventModel() {
+	return lazyEventModel = lazyEventModel == null ? new DefaultScheduleModel(getAllEvents()) : lazyEventModel;
+    }
+
+    public Reservation getEvent() {
+	if (event == null) {
+	    event = new Reservation();
+	    event.setStyleClass("myevents");
+	}
+	return event;
+    }
+
+    public void setEvent(Reservation event) {
+	this.event = event;
+    }
+
+    public void addEvent(ActionEvent actionEvent) {
+	if (event.getId() == null) {
+	    createOrUpdate();
+	} else {
+	    createOrUpdate();
+	}
+	event = new Reservation();
+	event.setStyleClass("myevents");
+    }
+
+    public void removeEvent(ActionEvent actionEvent) {
+	if (event.getId() != null) {
+	    eventModel.deleteEvent(event);
+	    reservationService.remove(event);
+	}
+    }
+
+    public final String createOrUpdate() {
+	if (StringUtils.isBlank(event.getId())) {
+	    try {
+		event.setUser(getUser());
+		event.setStyleClass("myevents");
+		// It is necessary to first update the model so that the event has a reservation id
+		eventModel.addEvent(event);
+		reservationService.create(event);
+	    } catch (Exception e) {
+		if (e.getCause() instanceof DomainException) {
+		    JsfUtil.addErrorMessage(ConstantUtils.LABEL_ERROR, ResourceBundle.getBundle(ConstantUtils.BUNDLE).getString(e.getCause().getMessage()));
+		} else {
+		    JsfUtil.addErrorMessage(ConstantUtils.LABEL_ERROR, ResourceBundle.getBundle(ConstantUtils.BUNDLE)
+										     .getString(ConstantUtils.ERROR_PERSISTENCE_KEY));
+		}
+		// Remove the newly added event if an exception was captured
+		eventModel.deleteEvent(event);
+	    }
+	} else {
+	    try {
+		// It is necessary to get the merged event and only after that, update the event model
+		event = reservationService.edit(event);
+		eventModel.updateEvent(event);
+	    } catch (Exception e) {
+		if (e.getCause() instanceof DomainException) {
+		    JsfUtil.addErrorMessage(ConstantUtils.LABEL_ERROR, ResourceBundle.getBundle(ConstantUtils.BUNDLE).getString(e.getCause().getMessage()));
+		} else {
+		    JsfUtil.addErrorMessage(ConstantUtils.LABEL_ERROR, ResourceBundle.getBundle(ConstantUtils.BUNDLE)
+										     .getString(ConstantUtils.ERROR_PERSISTENCE_KEY));
+		}
+		event = reservationService.find(event.getIdInternal());
+		eventModel.updateEvent(event);
+	    }
+	}
+	return null;
+    }
+
+    private User getUser() {
+	return userService.findByUsername(SessionHelper.getUserView().getFullUsername());
+    }
+
+    public void onEventSelect(ScheduleEntrySelectEvent selectEvent) {
+	event = (Reservation) selectEvent.getScheduleEvent();
+	event.setExternalCourse(MoodleClientHelper.findCourse(event.getReservedTo(), SessionHelper.getLoginDomain(), SessionHelper.getLoginReturn()));
+    }
+
+    public void onDateSelect(DateSelectEvent selectEvent) {
+	event = new Reservation("", selectEvent.getDate(), selectEvent.getDate());
+    }
+
+    public void onEventMove(ScheduleEntryMoveEvent selectEvent) {
+	event = (Reservation) selectEvent.getScheduleEvent();
+	event.setExternalCourse(MoodleClientHelper.findCourse(event.getReservedTo(), SessionHelper.getLoginDomain(), SessionHelper.getLoginReturn()));
+	moveEvent(true, event, selectEvent.getMinuteDelta());
+	createOrUpdate();
+    }
+
+    private void moveEvent(final boolean moveBothDates, final Reservation event, final int minuteDelta) {
+	if (moveBothDates) {
+	    DateTime startDate = new DateTime(event.getStartDate());
+	    startDate = startDate.plusMinutes(minuteDelta);
+	    event.setStartDate(startDate.toDate());
+	}
+	DateTime endDate = new DateTime(event.getEndDate());
+	endDate = endDate.plusMinutes(minuteDelta);
+	event.setEndDate(endDate.toDate());
+    }
+
+    public void onEventResize(ScheduleEntryResizeEvent selectEvent) {
+	event = (Reservation) selectEvent.getScheduleEvent();
+	moveEvent(false, event, selectEvent.getMinuteDelta());
+	createOrUpdate();
+    }
+
+    public String getTheme() {
+	return theme;
+    }
+
+    public void setTheme(String theme) {
+	this.theme = theme;
+    }
+
+    /**
+     * @return the externalCourses
+     */
+    public SelectItem[] getExternalCourses() {
+	return externalCourses = externalCourses == null ? JsfUtil.getSelectItems(new ExternalCourseServiceBean().findAll(), true) : externalCourses;
+    }
+
+    /**
+     * @param externalCourses
+     *            the externalCourses to set
+     */
+    public void setExternalCourses(SelectItem[] externalCourses) {
+	this.externalCourses = Arrays.copyOf(externalCourses, externalCourses.length);
+    }
+
+    /**
+     * @return the externalCourse
+     */
+    public ExternalCourse getExternalCourse() {
+	return externalCourse;
+    }
+
+    /**
+     * @param externalCourse
+     *            the externalCourse to set
+     */
+    public void setExternalCourse(ExternalCourse externalCourse) {
+	this.externalCourse = externalCourse;
+    }
+}
