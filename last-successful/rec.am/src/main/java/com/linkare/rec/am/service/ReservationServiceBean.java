2857f65f12345431e79e021a3bdfb37d2f4c7a85
respiritosanto <respiritosanto@linkare.com>
2010-05-10 18:01:19 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-rec.am-22.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec.am@1623 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successful/rec.am/src/main/java/com/linkare/rec/am/service/ReservationServiceBean.java
@@ -0,0 +1,183 @@
+package com.linkare.rec.am.service;
+
+import static com.linkare.rec.am.model.Reservation.COUNT_ALL_QUERYNAME;
+import static com.linkare.rec.am.model.Reservation.FIND_ALL_QUERYNAME;
+import static com.linkare.rec.am.model.Reservation.FIND_BY_EXPERIMENT_NAME_IN_INTERVAL_QUERYNAME;
+import static com.linkare.rec.am.model.Reservation.FIND_FOR_USER_AFTER_DATE_QUERYNAME;
+import static com.linkare.rec.am.model.Reservation.FIND_FOR_USER_IN_DATE_QUERYNAME;
+import static com.linkare.rec.am.model.Reservation.FIND_FOR_USER_QUERYNAME;
+import static com.linkare.rec.am.model.Reservation.QUERY_PARAM_END_DATE;
+import static com.linkare.rec.am.model.Reservation.QUERY_PARAM_EXPERIMENT_NAME;
+import static com.linkare.rec.am.model.Reservation.QUERY_PARAM_START_DATE;
+import static com.linkare.rec.am.model.Reservation.QUERY_PARAM_USERNAME;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+import javax.persistence.Query;
+import javax.persistence.TemporalType;
+
+import org.primefaces.model.ScheduleEvent;
+
+import com.linkare.commons.jpa.exceptions.DomainException;
+import com.linkare.commons.jpa.security.Group;
+import com.linkare.commons.jpa.security.User;
+import com.linkare.commons.utils.BooleanResult;
+import com.linkare.rec.am.model.Reservation;
+import com.linkare.rec.am.model.moodle.ExternalCourse;
+import com.linkare.rec.am.model.moodle.ExternalUser;
+import com.linkare.rec.am.web.auth.UserView;
+
+/**
+ * 
+ * @author Joao
+ */
+@Local(ReservationServiceLocal.class)
+@Stateless(name = "ReservationService")
+public class ReservationServiceBean extends BusinessServiceBean<Reservation, Long> implements ReservationService {
+
+    @EJB(beanInterface = GroupServiceLocal.class)
+    private GroupService groupService;
+
+    @EJB(beanInterface = UserServiceLocal.class)
+    private UserService userService;
+
+    @Override
+    public void create(final Reservation reservation) {
+	final BooleanResult operationResult = canCreate(reservation);
+	if (operationResult.getResult() == Boolean.TRUE) {
+	    if (!reservation.isInternal()) {
+		final ExternalCourse externalCourse = reservation.getExternalCourse();
+		// Generate some unique name, relating this group with the unique registration uuid
+		final Group reservationGroup = new Group(externalCourse.getShortname() + "@" + reservation.getId());
+		groupService.create(reservationGroup);
+		reservation.setGroup(reservationGroup);
+		final List<String> studentsUsernames = getStudentsUsernames(externalCourse.getStudents());
+		final List<User> users = userService.getOrCreateUsers(studentsUsernames);
+		groupService.setUsersMembership(reservationGroup, users);
+	    }
+	    getEntityManager().persist(reservation);
+	} else {
+	    throw new DomainException(operationResult.getMessage());
+	}
+    }
+
+    private List<String> getStudentsUsernames(final List<ExternalUser> externalUsers) {
+	final List<String> result = new ArrayList<String>();
+	for (final ExternalUser externalUser : externalUsers) {
+	    result.add(externalUser.getFullUsername());
+	}
+	return result;
+    }
+
+    protected BooleanResult canCreate(final Reservation reservation) {
+	return isValid(reservation);
+    }
+
+    public Reservation edit(final Reservation reservation) {
+	final BooleanResult operationResult = canEdit(reservation);
+	if (operationResult.getResult() == Boolean.TRUE) {
+	    return getEntityManager().merge(reservation);
+	} else {
+	    throw new DomainException(operationResult.getMessage());
+	}
+    }
+
+    protected BooleanResult canEdit(final Reservation reservation) {
+	return isValid(reservation);
+    }
+
+    @SuppressWarnings("unchecked")
+    private BooleanResult isValid(final Reservation reservation) {
+	final Query query = getEntityManager().createNamedQuery(FIND_BY_EXPERIMENT_NAME_IN_INTERVAL_QUERYNAME).setParameter(QUERY_PARAM_START_DATE,
+															    reservation.getStartDate())
+					      .setParameter(QUERY_PARAM_END_DATE, reservation.getEndDate()).setParameter(QUERY_PARAM_EXPERIMENT_NAME,
+															 reservation.getExperiment().getName());
+	final List<Reservation> reservationsForExperimentNameInInterval = query.getResultList();
+	return reservation.isValid(reservationsForExperimentNameInInterval);
+    }
+
+    @Override
+    public void remove(final Reservation reservation) {
+	final Reservation mergedReservation = getEntityManager().merge(reservation);
+	mergedReservation.delete();
+	getEntityManager().remove(mergedReservation);
+    }
+
+    @Override
+    public Reservation find(final Long id) {
+	return getEntityManager().find(Reservation.class, id);
+    }
+
+    @Override
+    public List<Reservation> findRange(final int[] range) {
+	return find(false, range[0], range[1]);
+    }
+
+    @Override
+    public List<Reservation> findAll() {
+	return find(true, -1, -1);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Reservation> find(final boolean all, final int firstResult, final int maxResults) {
+	Query q = getEntityManager().createNamedQuery(FIND_ALL_QUERYNAME);
+	if (!all) {
+	    q.setMaxResults(maxResults);
+	    q.setFirstResult(firstResult);
+	}
+	return q.getResultList();
+    }
+
+    @Override
+    public int count() {
+	final Query query = getEntityManager().createNamedQuery(COUNT_ALL_QUERYNAME);
+	return ((Long) query.getSingleResult()).intValue();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<ScheduleEvent> findReservationsFor(final Date start, final Date end, final UserView userView) {
+	if (end == null) {
+	    return getEntityManager().createNamedQuery(FIND_FOR_USER_AFTER_DATE_QUERYNAME).setParameter(QUERY_PARAM_USERNAME, userView.getFullUsername())
+				     .setParameter(QUERY_PARAM_START_DATE, start, TemporalType.TIMESTAMP).getResultList();
+	} else {
+	    return getEntityManager().createNamedQuery(FIND_FOR_USER_IN_DATE_QUERYNAME).setParameter(QUERY_PARAM_USERNAME, userView.getFullUsername())
+				     .setParameter(QUERY_PARAM_START_DATE, start, TemporalType.TIMESTAMP).setParameter(QUERY_PARAM_END_DATE, end,
+														       TemporalType.TIMESTAMP).getResultList();
+	}
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<ScheduleEvent> findAllReservations(final UserView userView) {
+	final List<ScheduleEvent> events = getEntityManager().createNamedQuery(FIND_ALL_QUERYNAME).getResultList();
+	addStyleclassesToEvents(userView, events);
+	return events;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<ScheduleEvent> findReservationsFor(UserView userView) {
+	final List<ScheduleEvent> events = getEntityManager().createNamedQuery(FIND_FOR_USER_QUERYNAME).setParameter(QUERY_PARAM_USERNAME,
+														     userView.getFullUsername())
+							     .getResultList();
+	addStyleclassesToEvents(userView, events);
+	return events;
+    }
+
+    private void addStyleclassesToEvents(UserView userView, final List<ScheduleEvent> events) {
+	for (final ScheduleEvent scheduleEvent : events) {
+	    final Reservation reservation = (Reservation) scheduleEvent;
+	    if (userView.getFullUsername().equals(reservation.getUser().getUsername())) {
+		reservation.setStyleClass("myevents");
+	    } else {
+		reservation.setStyleClass("others_events");
+	    }
+	}
+    }
+}
