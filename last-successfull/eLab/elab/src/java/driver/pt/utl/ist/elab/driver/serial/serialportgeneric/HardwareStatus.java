a86caff5ca09bf7c481acc9873b4fabfda6a3312
pzenida <pzenida@linkare.com>
2010-05-10 00:07:09 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-eLab-79.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/elab@1604 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/elab/src/java/driver/pt/utl/ist/elab/driver/serial/serialportgeneric/HardwareStatus.java
@@ -0,0 +1,16 @@
+package pt.utl.ist.elab.driver.serial.serialportgeneric;
+
+public enum HardwareStatus {
+	RESETED, STOPED, CONFIGURED, STARTED, ERROR, UNKNOWN;
+	
+	public static boolean isValid(String status) {
+		if (status == null || status.equals(""))
+			return false;
+		for (HardwareStatus hs : HardwareStatus.values()) {
+			if (hs.toString().equals(status))
+				return true;
+		}
+		return false;
+	}
+	
+}
