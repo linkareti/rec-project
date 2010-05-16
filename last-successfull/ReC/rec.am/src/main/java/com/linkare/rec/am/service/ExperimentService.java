9ab1f6b095b839275e70446a955e574e5d993319
pzenida <pzenida@linkare.com>
2010-05-16 00:05:48 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-152.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec@1725 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.am/src/main/java/com/linkare/rec/am/service/ExperimentService.java
@@ -0,0 +1,11 @@
+package com.linkare.rec.am.service;
+
+import com.linkare.rec.am.model.Experiment;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ * 
+ */
+public interface ExperimentService extends BusinessService<Experiment, Long> {
+}
