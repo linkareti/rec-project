7a6459fa8749023368e19b96a205764815a1b849
pzenida <pzenida@linkare.com>
2010-05-19 00:07:11 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-155.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec@1816 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/common/com/linkare/rec/impl/data/SamplesPacketMatrixIO.java
@@ -0,0 +1,74 @@
+/*
+ * SamplesPacketMatrixIO.java
+ *
+ * Created on 5 de Janeiro de 2004, 17:59
+ */
+
+package com.linkare.rec.impl.data;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import com.linkare.rec.data.acquisition.SamplesPacket;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public class SamplesPacketMatrixIO implements Serializable {
+	SequencedObjectIO ioDelegate = null;
+
+	public SamplesPacketMatrixIO() throws IOException {
+		ioDelegate = new SequencedObjectIO();
+	}
+
+	/**
+	 * Getter for property file.
+	 * 
+	 * @return Value of property file.
+	 * 
+	 */
+	public File getFile() throws IOException {
+		return ioDelegate.getFile();
+	}
+
+	/**
+	 * Setter for property file.
+	 * 
+	 * @param file New value of property file.
+	 * 
+	 */
+	public void setFile(File file) throws IOException {
+		ioDelegate.setFile(file);
+	}
+
+	public SamplesPacket[] getSamplesPackets(int packetStart, int packetEnd) throws SamplesPacketReadException {
+		try {
+			Object[] oRead = ioDelegate.readObjects(packetStart, packetEnd);
+			SamplesPacket[] retVal = new SamplesPacket[packetEnd - packetStart + 1];
+			System.arraycopy(oRead, 0, retVal, 0, retVal.length);
+			return retVal;
+		} catch (SequencedObjectReadException e) {
+			throw new SamplesPacketReadException(e, e.getErrorIndex());
+		}
+	}
+
+	public void write(SamplesPacket[] packets) throws IOException {
+		ioDelegate.writeObjects(packets);
+	}
+
+	public void write(Object[] packets) throws IOException {
+		ioDelegate.writeObjects(packets);
+	}
+
+	public void write(ArrayList packets) throws IOException {
+		ioDelegate.writeObjects(packets.toArray());
+	}
+
+	public int size() {
+		return ioDelegate.size();
+	}
+
+}
