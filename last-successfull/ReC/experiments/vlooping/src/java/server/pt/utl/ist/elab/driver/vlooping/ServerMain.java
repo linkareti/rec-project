5a7f8a2fd1f8a6b7532b644f1a2cd14ed8a882aa
pzenida <pzenida@linkare.com>
2010-05-22 00:05:30 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-161.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec@1859 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/vlooping/src/java/server/pt/utl/ist/elab/driver/vlooping/ServerMain.java
@@ -0,0 +1,47 @@
+/*
+ * RobotServer.java
+ *
+ * Created on 10 de Abril de 2003, 20:07
+ */
+
+package pt.utl.ist.elab.driver.vlooping;
+
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import com.linkare.rec.impl.driver.BaseHardware;
+import com.linkare.rec.impl.logging.LoggerUtil;
+import com.linkare.rec.impl.utils.ORBBean;
+
+/**
+ * 
+ * @author Emanuel Antunes
+ */
+public class ServerMain {
+	// O código desta classe é sempre igual!!! Alterar só os nomes para o vosso
+	// caso!
+	private static String LOOPING_HARDWARE_LOGGER = "LOOPING.Logger";
+	static {
+		Logger l = LogManager.getLogManager().getLogger(LOOPING_HARDWARE_LOGGER);
+		if (l == null) {
+			LogManager.getLogManager().addLogger(Logger.getLogger(LOOPING_HARDWARE_LOGGER));
+		}
+	}
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		try {
+			ORBBean.getORBBean(args);
+
+			BaseHardware baseHardware = new BaseHardware(new LoopingDriver());
+
+			Thread.currentThread().join();
+
+		} catch (Exception e) {
+			ORBBean.getORBBean(args).killORB();
+			LoggerUtil.logThrowable("Error on Main...", e, Logger.getLogger(LOOPING_HARDWARE_LOGGER));
+		}
+	}
+}
---
+++ last-successfull/ReC/staging_experiments/vdpendulum/src/java/client/pt/utl/ist/elab/client/vdpendulum/displays/Theta1.java
@@ -0,0 +1,25 @@
+/*
+ * Theta1.java
+ *
+ * Created on 13 February 2004, 20:23
+ */
+
+package pt.utl.ist.elab.client.vdpendulum.displays;
+
+/**
+ * 
+ * @author André Neto - LEFT - IST
+ */
+public class Theta1 extends com.linkare.rec.impl.baseUI.graph.DefaultXYExperimentGraph {
+
+	/** Creates a new instance of Theta1 */
+	public Theta1() {
+		super();
+		setChannelDisplayX(0);
+		setChannelDisplayY(1);
+	}
+
+	public String getName() {
+		return "Grafico 1";
+	}
+}
