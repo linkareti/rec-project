05bb2468940e44a4e36fea5b2aa29216ba25724e
pzenida <pzenida@linkare.com>
2010-05-28 00:08:53 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-eLab-101.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/elab@1950 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/client/com/linkare/rec/impl/client/experiment/ExpDataDisplay.java
@@ -0,0 +1,27 @@
+/*
+ * ExpDataDisplay.java
+ *
+ * Created on 7 de Maio de 2003, 12:44
+ */
+
+package com.linkare.rec.impl.client.experiment;
+
+import javax.swing.Icon;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public interface ExpDataDisplay {
+	public void setExpDataModel(ExpDataModel model);
+
+	public javax.swing.JComponent getDisplay();
+
+	public String getName();
+
+	public Icon getIcon();
+
+	public javax.swing.JMenuBar getMenuBar();
+
+	public javax.swing.JToolBar getToolBar();
+}
