b91062f3e97e1b62ddf2303e3089c25273c6158d
pzenida <pzenida@linkare.com>
2010-05-29 00:09:04 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-eLab-102.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/elab@1958 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/elab/src/java/driver/pt/utl/ist/elab/driver/serial/serialportgeneric/genericexperiment/GenericSerialPortDataSource.java
@@ -0,0 +1,24 @@
+/*
+ * GenericStampDataSource.java
+ *
+ * Created on 15 de Maio de 2003, 19:38
+ */
+
+package pt.utl.ist.elab.driver.serial.serialportgeneric.genericexperiment;
+
+import pt.utl.ist.elab.driver.serial.serialportgeneric.AbstractSerialPortDataSource;
+import pt.utl.ist.elab.driver.serial.serialportgeneric.command.SerialPortCommand;
+
+import com.linkare.rec.data.config.HardwareAcquisitionConfig;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public class GenericSerialPortDataSource extends AbstractSerialPortDataSource {
+
+	/** Creates a new instance of GenericStampDataSource */
+	public GenericSerialPortDataSource() {
+	}
+
+}
