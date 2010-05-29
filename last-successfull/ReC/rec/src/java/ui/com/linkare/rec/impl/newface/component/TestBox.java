48597fbe06d307ed946c5bb1de5555607073b042
pzenida <pzenida@linkare.com>
2010-05-29 00:07:03 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-168.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec@1955 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/ui/com/linkare/rec/impl/newface/component/TestBox.java
@@ -0,0 +1,25 @@
+/*
+ * TestBox.java created on Apr 23, 2009
+ *
+ * Copyright 2009 HFernandes. All rights reserved.
+ * HFernandes PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.linkare.rec.impl.newface.component;
+
+import javax.swing.JTextField;
+
+import com.linkare.rec.impl.newface.laf.flat.Focus;
+
+/**
+ * 
+ * @author Henrique Fernandes
+ */
+@Focus(display = false)
+public class TestBox extends JTextField {
+
+	public TestBox() {
+		super();
+	}
+
+}
