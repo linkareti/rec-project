dd4a35bb9f23dfe6679213ce1642091f53c04ed4
pzenida <pzenida@linkare.com>
2010-05-31 12:07:10 +0000

    Removed call to deprecated method show(). Replaced with setVisible(true) instead.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/trunk@2057 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- experiments/g/src/java/client/pt/utl/ist/elab/client/g/ClientMain.java
+++ experiments/g/src/java/client/pt/utl/ist/elab/client/g/ClientMain.java
@@ -23,6 +23,6 @@ public class ClientMain {
 		JFrame.setDefaultLookAndFeelDecorated(true);
 		ReCBaseUI ui = new ReCBaseUI();
 		ui.setApparatusAutoConnectID("ELAB_G_STAMP_V02");
-		ui.show();
+		ui.setVisible(true);
 	}
 }
