fdee0f25418e79f668ef30ffa734416db2c4302f
respiritosanto <respiritosanto@linkare.com>
2010-06-04 18:01:33 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-rec.am-58.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec.am@2178 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successful/rec.am/src/main/java/com/linkare/rec/am/web/UserWizard.java
@@ -0,0 +1,195 @@
+package com.linkare.rec.am.web;
+
+import java.io.Serializable;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+
+import org.apache.commons.validator.EmailValidator;
+import org.primefaces.event.FlowEvent;
+
+import com.linkare.commons.dao.security.LoginDAO;
+import com.linkare.commons.jpa.exceptions.DomainException;
+import com.linkare.commons.jpa.security.User;
+import com.linkare.commons.utils.EqualityUtils;
+import com.linkare.jsf.utils.JsfUtil;
+import com.linkare.rec.am.model.Person;
+import com.linkare.rec.am.service.UserService;
+import com.linkare.rec.am.service.UserServiceLocal;
+import com.linkare.rec.am.web.util.ConstantUtils;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ * 
+ */
+@ManagedBean(name = "userWizard")
+@ViewScoped
+public class UserWizard implements Serializable {
+
+    private static final long serialVersionUID = -1668168683856714667L;
+
+    private boolean skip;
+
+    private String effect = "fade";
+
+    private User user;
+
+    private UIInput passwordInput;
+
+    private UIInput confirmPasswordInput;
+
+    private boolean isNew;
+
+    @EJB(beanInterface = UserServiceLocal.class)
+    private UserService userService;
+
+    /**
+     * @return the passwordInput
+     */
+    public UIInput getPasswordInput() {
+	return passwordInput;
+    }
+
+    /**
+     * @param passwordInput
+     *            the passwordInput to set
+     */
+    public void setPasswordInput(UIInput passwordInput) {
+	this.passwordInput = passwordInput;
+    }
+
+    /**
+     * @return the confirmPasswordInput
+     */
+    public UIInput getConfirmPasswordInput() {
+	return confirmPasswordInput;
+    }
+
+    /**
+     * @param confirmPasswordInput
+     *            the confirmPasswordInput to set
+     */
+    public void setConfirmPasswordInput(UIInput confirmPasswordInput) {
+	this.confirmPasswordInput = confirmPasswordInput;
+    }
+
+    /**
+     * @return the isNew
+     */
+    public boolean isNew() {
+	return isNew;
+    }
+
+    /**
+     * @param isNew
+     *            the isNew to set
+     */
+    public void setNew(boolean isNew) {
+	this.isNew = isNew;
+    }
+
+    /**
+     * @return the user
+     */
+    public User getUser() {
+	if (user == null) {
+	    final String userId = JsfUtil.getRequestParameter("userId");
+	    if (userId == null) {
+		isNew = true;
+		user = new User();
+	    } else {
+		user = userService.find(Long.valueOf(userId));
+	    }
+	}
+	if (user.getSubject() == null) {
+	    user.setSubject(new Person());
+	}
+	return user;
+    }
+
+    /**
+     * @param user
+     *            the user to set
+     */
+    public void setUser(User user) {
+	this.user = user;
+    }
+
+    public String save() {
+	try {
+	    if (isNew()) {
+		userService.create(getUser());
+		JsfUtil.addGlobalSuccessMessage(ConstantUtils.BUNDLE, ConstantUtils.LABEL_INFO_KEY, ConstantUtils.INFO_CREATE_KEY);
+		return ConstantUtils.CREATE;
+	    } else {
+		userService.edit(getUser());
+		JsfUtil.addGlobalSuccessMessage(ConstantUtils.BUNDLE, ConstantUtils.LABEL_INFO_KEY, ConstantUtils.INFO_UPDATE_KEY);
+		return ConstantUtils.VIEW;
+	    }
+	} catch (Exception e) {
+	    getUser().setIdInternal(null);
+	    if (e.getCause() instanceof DomainException) {
+		JsfUtil.addGlobalErrorMessage(ConstantUtils.BUNDLE, ConstantUtils.LABEL_ERROR_KEY, e.getCause().getMessage());
+	    } else {
+		JsfUtil.addGlobalErrorMessage(ConstantUtils.BUNDLE, ConstantUtils.LABEL_ERROR_KEY, ConstantUtils.ERROR_PERSISTENCE_KEY);
+	    }
+	    return null;
+	}
+    }
+
+    public String getEffect() {
+	return effect;
+    }
+
+    public void setEffect(String effect) {
+	this.effect = effect;
+    }
+
+    public boolean isSkip() {
+	return skip;
+    }
+
+    public void setSkip(boolean skip) {
+	this.skip = skip;
+    }
+
+    public String onFlowProcess(FlowEvent event) {
+	if (skip) {
+	    return "confirm";
+	} else {
+	    return event.getNewStep();
+	}
+    }
+
+    public void validateEmail(FacesContext context, UIComponent toValidate, Object value) {
+	String email = (String) value;
+
+	if (!EmailValidator.getInstance().isValid(email)) {
+	    ((UIInput) toValidate).setValid(false);
+	    JsfUtil.addErrorMessage(ConstantUtils.BUNDLE, toValidate.getClientId(context), ConstantUtils.LABEL_ERROR_KEY, "error.email.invalidFormat");
+	}
+    }
+
+    public void validatePasswords(FacesContext context, UIComponent toValidate, Object value) {
+	final String password = JsfUtil.getRequestParameter(getPasswordInput().getClientId());
+	final String confirmPassword = JsfUtil.getRequestParameter(getConfirmPasswordInput().getClientId());
+
+	if (!EqualityUtils.equals(password, confirmPassword)) {
+	    getConfirmPasswordInput().setValid(false);
+	    JsfUtil.addErrorMessage(ConstantUtils.BUNDLE, getConfirmPasswordInput().getClientId(context), ConstantUtils.LABEL_ERROR_KEY,
+				    LoginDAO.ERROR_PASSWORD_AND_CONFIRM_PASSWORD_ARE_DIFFERENT);
+	    getPasswordInput().setValid(false);
+	    JsfUtil.addErrorMessage(ConstantUtils.BUNDLE, getPasswordInput().getClientId(context), ConstantUtils.LABEL_ERROR_KEY,
+				    LoginDAO.ERROR_PASSWORD_AND_CONFIRM_PASSWORD_ARE_DIFFERENT);
+	}
+    }
+
+    public boolean isValidPasswords() {
+	return EqualityUtils.equals(getUser().getLogin().getPassword(), getUser().getLogin().getConfirmPassword());
+    }
+}
