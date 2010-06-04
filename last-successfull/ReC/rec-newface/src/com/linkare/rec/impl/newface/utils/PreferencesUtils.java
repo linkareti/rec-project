fa68309d8108dee2c6233c2357bf6f9978f111b0
pzenida <pzenida@linkare.com>
2010-06-04 00:09:37 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-174.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec@2149 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec-newface/src/com/linkare/rec/impl/newface/utils/PreferencesUtils.java
@@ -0,0 +1,45 @@
+package com.linkare.rec.impl.newface.utils;
+
+import java.util.logging.Logger;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
+/**
+ * Wrapper class for defining and reading preferences using the Preferences API.
+ * 
+ * @author bcatarino
+ */
+public class PreferencesUtils {
+
+	private static final Logger log = Logger.getLogger(PreferencesUtils.class.getName());
+
+	/**
+	 * Returns a user preference for a given key.
+	 * 
+	 * @param key
+	 * @return A string containing the value of the property for the given key. If there is not a user preference for
+	 *         the given key, null is returned instead.
+	 */
+	public static String readUserPreference(String key) {
+		return Preferences.userRoot().get(key, null);
+	}
+
+	/**
+	 * Saves a new value to a given key as a preference.
+	 * 
+	 * @param key
+	 *            The key name of the preference to save.
+	 * @param value
+	 *            The new value to set the given key.
+	 */
+	public static void writeUserPreference(String key, String value) {
+		
+		Preferences.userRoot().put(key, value);
+
+		try {
+			Preferences.userRoot().sync();
+		} catch (BackingStoreException e) {
+			log.severe("Error writing preference " + key + "with value " + value + ": " + e.getMessage());
+		}
+	}
+}
---
+++ last-successfull/ReC/rec.am/src/main/java/com/linkare/rec/am/service/LaboratoryService.java
@@ -0,0 +1,11 @@
+package com.linkare.rec.am.service;
+
+import com.linkare.rec.am.model.Laboratory;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ * 
+ */
+public interface LaboratoryService extends BusinessService<Laboratory, Long> {
+}
