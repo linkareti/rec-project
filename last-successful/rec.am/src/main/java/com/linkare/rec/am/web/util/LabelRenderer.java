55b18dff08c5fe6c2489d52c75a9131a1638c036
respiritosanto <respiritosanto@linkare.com>
2010-06-07 20:01:33 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-rec.am-64.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec.am@2222 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successful/rec.am/src/main/java/com/linkare/rec/am/web/util/LabelRenderer.java
@@ -0,0 +1,91 @@
+package com.linkare.rec.am.web.util;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.render.Renderer;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.linkare.commons.utils.StringUtils;
+
+// TODO: Move to the JSF project!
+/**
+ * Custom LabelRenderer component that adds asterisks for required fields. Based off of an example from David Geary on the MyFaces Mailing list.
+ * 
+ * @author Matt Raible
+ */
+public class LabelRenderer extends Renderer {
+    protected final Log log = LogFactory.getLog(LabelRenderer.class);
+
+    public boolean getRendersChildren() {
+	return false;
+    }
+
+    public void encodeBegin(FacesContext context, UIComponent component) throws java.io.IOException {
+	ResponseWriter writer = context.getResponseWriter();
+
+	Map<String, Object> attrs = component.getAttributes();
+	String id = (String) attrs.get("for");
+	UIInput input = null;
+
+	if (!StringUtils.isBlank(id)) {
+	    input = (UIInput) component.findComponent(id);
+	}
+
+	writer.startElement("label", component);
+
+	String styleClass = (String) component.getAttributes().get("styleClass");
+
+	boolean hasErrors = hasMessages(context, input);
+
+	if (styleClass != null) {
+	    if (hasErrors)
+		styleClass += " error";
+	    writer.writeAttribute("class", styleClass, null);
+	} else if (hasErrors) {
+	    writer.writeAttribute("class", "error", null);
+	}
+	String renderedId = (input != null) ? input.getClientId(context) : component.getClientId(context);
+	writer.writeAttribute("for", renderedId, null);
+	writer.write(attrs.get("value").toString());
+    }
+
+    public void encodeEnd(FacesContext context, UIComponent component) throws java.io.IOException {
+	ResponseWriter writer = context.getResponseWriter();
+	Map<String, Object> attrs = component.getAttributes();
+	String id = (String) attrs.get("for");
+	UIInput input = null;
+
+	if (!StringUtils.isBlank(id)) {
+	    input = (UIInput) component.findComponent(id);
+	}
+
+	if ((input != null) && input.isRequired()) {
+	    writer.write(" <span class=\"req\">*</span>");
+	}
+
+	writer.endElement("label");
+    }
+
+    private boolean hasMessages(FacesContext context, UIComponent component) {
+	Iterator<String> it = context.getClientIdsWithMessages();
+	boolean found = false;
+
+	while (it.hasNext()) {
+	    String id = it.next();
+
+	    if ((component != null) && id.equals(component.getClientId(context))) {
+		found = true;
+		break;
+	    }
+	}
+
+	return found;
+    }
+}
