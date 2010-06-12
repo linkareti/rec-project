5536d0040847a9530b26426bb8884dcb423b57be
pzenida <pzenida@linkare.com>
2010-06-12 00:07:15 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-183.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec@2325 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/vstdmap/src/java/server/pt/utl/ist/elab/driver/vstdmap/ServerMain.java
@@ -0,0 +1,46 @@
+/*
+ * ServerMain.java
+ *
+ * Created on 10 de Abril de 2003, 20:07
+ */
+
+package pt.utl.ist.elab.driver.vstdmap;
+
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import com.linkare.rec.impl.driver.BaseHardware;
+import com.linkare.rec.impl.logging.LoggerUtil;
+import com.linkare.rec.impl.utils.ORBBean;
+
+/**
+ * 
+ * @author Antonio Jose Rodrigues Figueiredo
+ */
+public class ServerMain {
+
+	private static String STDMAP_HARDWARE_LOGGER = "STDMAP.Logger";
+	static {
+		Logger l = LogManager.getLogManager().getLogger(STDMAP_HARDWARE_LOGGER);
+		if (l == null) {
+			LogManager.getLogManager().addLogger(Logger.getLogger(STDMAP_HARDWARE_LOGGER));
+		}
+	}
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		try {
+			ORBBean.getORBBean();
+
+			BaseHardware baseHardware = new BaseHardware(new STDMAPDriver());
+
+			Thread.currentThread().join();
+
+		} catch (Exception e) {
+			ORBBean.getORBBean().killORB();
+			LoggerUtil.logThrowable("Error on Main...", e, Logger.getLogger(STDMAP_HARDWARE_LOGGER));
+		}
+	}
+}
