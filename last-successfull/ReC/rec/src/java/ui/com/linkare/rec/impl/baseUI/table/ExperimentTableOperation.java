4a1b82f0d06f6c7997d1009630d5b0094f135d89
pzenida <pzenida@linkare.com>
2010-06-19 00:07:25 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-190.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec@2584 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/ui/com/linkare/rec/impl/baseUI/table/ExperimentTableOperation.java
@@ -0,0 +1,19 @@
+/* 
+ * ExperimentTableOperation.java created on 14 Jun 2010
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.linkare.rec.impl.baseUI.table;
+
+/**
+ * 
+ * @author npadriano
+ */
+public interface ExperimentTableOperation {
+	
+	public void selectAll();
+	
+	public void copyToClipBoard();
+
+}
