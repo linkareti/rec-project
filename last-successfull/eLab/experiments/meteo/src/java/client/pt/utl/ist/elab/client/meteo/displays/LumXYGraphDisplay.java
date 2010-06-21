c593591e4f4cc273f78f915738d9b428af7866a1
pzenida <pzenida@linkare.com>
2010-06-21 00:10:03 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-eLab-123.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/elab@2618 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/meteo/src/java/client/pt/utl/ist/elab/client/meteo/displays/LumXYGraphDisplay.java
@@ -0,0 +1,25 @@
+/*
+ * PVXYGraphDisplay.java
+ *
+ * Created on 10 de Julho de 2003, 11:14
+ */
+
+package pt.utl.ist.elab.client.meteo.displays;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public class LumXYGraphDisplay extends MeteoTimeExperimentGraph {
+
+	/** Creates a new instance of PVXYGraphDisplay */
+	public LumXYGraphDisplay() {
+		super();
+		setChannelTime(0);
+		setChannelDisplayY(7);
+	}
+
+	public String getName() {
+		return "Luminosidade";
+	}
+}
