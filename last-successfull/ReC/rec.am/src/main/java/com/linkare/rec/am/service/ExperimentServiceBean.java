d777c522b432ca1e3ae4a5303e215dc76fe91c2f
pzenida <pzenida@linkare.com>
2010-07-07 00:07:33 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-208.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec@2799 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.am/src/main/java/com/linkare/rec/am/service/ExperimentServiceBean.java
@@ -0,0 +1,69 @@
+package com.linkare.rec.am.service;
+
+import static com.linkare.rec.am.model.Experiment.COUNT_ALL_QUERYNAME;
+import static com.linkare.rec.am.model.Experiment.FIND_ALL_ACTIVE_QUERYNAME;
+import static com.linkare.rec.am.model.Experiment.FIND_ALL_QUERYNAME;
+
+import java.util.List;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+import javax.persistence.Query;
+
+import com.linkare.rec.am.model.Experiment;
+
+/**
+ * 
+ * @author Joao
+ */
+@Local(ExperimentServiceLocal.class)
+@Stateless(name = "ExperimentService")
+public class ExperimentServiceBean extends BusinessServiceBean<Experiment, Long> implements ExperimentService {
+
+    @Override
+    public void create(final Experiment experiment) {
+	getEntityManager().persist(experiment);
+    }
+
+    @Override
+    public Experiment edit(final Experiment experiment) {
+	return getEntityManager().merge(experiment);
+    }
+
+    @Override
+    public Experiment find(final Long id) {
+	return getEntityManager().find(Experiment.class, id);
+    }
+
+    @Override
+    public List<Experiment> findRange(final int[] range) {
+	return find(false, range[0], range[1]);
+    }
+
+    @Override
+    public List<Experiment> findAll() {
+	return find(true, -1, -1);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Experiment> find(final boolean all, final int firstResult, final int maxResults) {
+	Query q = getEntityManager().createNamedQuery(FIND_ALL_QUERYNAME);
+	if (!all) {
+	    q.setMaxResults(maxResults);
+	    q.setFirstResult(firstResult);
+	}
+	return q.getResultList();
+    }
+
+    @Override
+    public int count() {
+	final Query query = getEntityManager().createNamedQuery(COUNT_ALL_QUERYNAME);
+	return ((Long) query.getSingleResult()).intValue();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Experiment> findAllActiveExperiments() {
+	return getEntityManager().createNamedQuery(FIND_ALL_ACTIVE_QUERYNAME).getResultList();
+    }
+}
