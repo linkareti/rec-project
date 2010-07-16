0c2e7e45d229643b488d23bb95d0bdd4d3f4f309
pzenida <pzenida@linkare.com>
2010-07-16 00:07:30 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-217.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/rec@2857 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/common/com/linkare/rec/impl/utils/Deactivatable.java
@@ -0,0 +1,23 @@
+/*
+ * Deactivatable.java
+ *
+ * Created on 5 de Novembro de 2002, 14:52
+ */
+
+package com.linkare.rec.impl.utils;
+
+import org.omg.PortableServer.POA;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public interface Deactivatable {
+	public boolean alreadySerialized();
+
+	public boolean isDeactivationPossible();
+
+	public String getOID();
+
+	public POA getPOA();
+}
