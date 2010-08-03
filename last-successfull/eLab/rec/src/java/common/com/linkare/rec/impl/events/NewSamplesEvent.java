ded9f02dda26705bf6c98d32848ac144ae1ac6db
pzenida <pzenida@linkare.com>
2010-08-03 00:10:10 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-eLab-165.
    git-svn-id: file:///tmp/svnroot_only_rec/ReC/tags/elab@3003 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/common/com/linkare/rec/impl/events/NewSamplesEvent.java
@@ -0,0 +1,67 @@
+/*
+ * NewSamplesEvent.java
+ *
+ * Created on 6 de Novembro de 2002, 12:46
+ */
+
+package com.linkare.rec.impl.events;
+
+import com.linkare.rec.impl.utils.IntersectableEvent;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public class NewSamplesEvent implements IntersectableEvent {
+
+	private int largestNumPacket;
+
+	/**
+	 * Creates a new instance of NewSamplesEvent
+	 * 
+	 * @param largestNumPacket
+	 */
+	public NewSamplesEvent(int largestNumPacket) {
+		this.largestNumPacket = largestNumPacket;
+	}
+
+	/**
+	 * Getter for property numPacketEnd.
+	 * 
+	 * @return Value of property numPacketEnd.
+	 * 
+	 */
+	public int getLargestNumPacket() {
+		return largestNumPacket;
+	}
+
+	public boolean intersectTo(IntersectableEvent other) {
+		if (!(other instanceof NewSamplesEvent))
+			return false;
+
+		NewSamplesEvent evt = (NewSamplesEvent) other;
+		largestNumPacket = Math.max(largestNumPacket, evt.getLargestNumPacket());
+		return true;
+	}
+	
+	/**
+	 * @return
+	 */
+	public boolean isPoisoned() {
+		return false;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder("[");
+		builder.append(getClass().getSimpleName());
+		builder.append(">largestNumPacket: ");
+		builder.append(largestNumPacket);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
