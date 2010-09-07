ce622bf6f563ddb1e8841b06882604fb17adf86a
npadriano <npadriano@linkare.com>
2010-09-07 11:06:14 +0000

    (related to bug 535) When the data client is gone and if is the hardware owner then, when the experiment is running, it must be stoped!
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/trunk@3403 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- rec/src/java/multicast/com/linkare/rec/impl/multicast/ReCMultiCastHardware.java
+++ rec/src/java/multicast/com/linkare/rec/impl/multicast/ReCMultiCastHardware.java
@@ -615,6 +615,8 @@ public class ReCMultiCastHardware implements MultiCastHardwareOperations {
 			return;
 
 		if (dcfq.equals(ownerDataClient)) {
+			// FIXME if the client is the owner then if the experiment is running it must be stoped!
+			
 			locked = false;
 			locking = false;
 
