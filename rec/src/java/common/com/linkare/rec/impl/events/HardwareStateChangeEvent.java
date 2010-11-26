aede7b6caeb7492096712ff412a9c442cf51f9f6
npadriano <npadriano@linkare.com>
2010-11-26 11:30:57 +0000

    (related to bug 605) The hardware events shouldn't be intersectable.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/trunk@3572 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- rec/src/java/common/com/linkare/rec/impl/events/HardwareStateChangeEvent.java
+++ rec/src/java/common/com/linkare/rec/impl/events/HardwareStateChangeEvent.java
@@ -6,22 +6,20 @@
 
 package com.linkare.rec.impl.events;
 
-import com.linkare.rec.impl.utils.IntersectableEvent;
 
 /**
  * 
  * @author José Pedro Pereira - Linkare TI
  */
-public class HardwareStateChangeEvent implements IntersectableEvent {
+public class HardwareStateChangeEvent {
 
 	/** Holds value of property newState. */
 	private com.linkare.rec.acquisition.HardwareState newState;
-	private long systemTime = 0;
 
-	/** Creates a new instance of HardwareStateChangeEvent */
+	/** Creates a new instance of HardwareStateChangeEvent 
+	 * @param newState */
 	public HardwareStateChangeEvent(com.linkare.rec.acquisition.HardwareState newState) {
 		this.newState = newState;
-		systemTime = System.currentTimeMillis();
 	}
 
 	/**
@@ -31,19 +29,6 @@ public class HardwareStateChangeEvent implements IntersectableEvent {
 	 */
 	public com.linkare.rec.acquisition.HardwareState getNewState() {
 		return this.newState;
-	}
-
-	public boolean intersectTo(IntersectableEvent other) {
-		if (other instanceof HardwareStateChangeEvent) {
-			HardwareStateChangeEvent evt = ((HardwareStateChangeEvent) other);
-			if (evt.systemTime < systemTime)
-				return false;
-			else
-				this.newState = evt.getNewState();
-
-			return true;
-		}
-		return false;
 	}
 	
 	/**
--- rec/src/java/multicast/com/linkare/rec/impl/events/HardwareChangeEvent.java
+++ rec/src/java/multicast/com/linkare/rec/impl/events/HardwareChangeEvent.java
@@ -6,20 +6,15 @@
 
 package com.linkare.rec.impl.events;
 
-import com.linkare.rec.impl.utils.IntersectableEvent;
 
 /**
  * 
  * @author José Pedro Pereira - Linkare TI
  */
-public class HardwareChangeEvent implements IntersectableEvent {
+public class HardwareChangeEvent {
 
 	/** Creates a new instance of HardwareChangeEvent */
 	public HardwareChangeEvent() {
-	}
-
-	public boolean intersectTo(IntersectableEvent other) {
-		return (other instanceof HardwareChangeEvent);
 	}
 
 }
