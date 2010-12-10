76be3b787589073acf19187c314c2b8566c92464
handyman <handyman@linkare.com>
2010-12-10 01:13:03 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-eLab-295.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@3657 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/optica/src/java/client/pt/utl/ist/elab/client/optica/ClientMain.java
@@ -0,0 +1,23 @@
+
+package pt.utl.ist.elab.client.optica;
+
+import javax.swing.JFrame;
+
+import com.linkare.rec.impl.baseUI.ReCBaseUI;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public class ClientMain {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		ReCBaseUI ui = new ReCBaseUI();
+		ui.setApparatusAutoConnectID("OPTICA");
+		ui.setVisible(true);
+	}
+}
