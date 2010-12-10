946df83a57afa8d8717cdd5a58fad6ec012de597
handyman <handyman@linkare.com>
2010-12-10 01:10:59 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-381.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@3655 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/ui/com/linkare/rec/impl/newface/component/LabComboBoxModel.java
@@ -0,0 +1,71 @@
+/* 
+ * LabComboBoxModel.java created on 6 Oct 2010
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.linkare.rec.impl.newface.component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.DefaultComboBoxModel;
+
+import com.linkare.rec.impl.newface.config.Lab;
+
+/**
+ * 
+ * @author npadriano
+ */
+public class LabComboBoxModel extends DefaultComboBoxModel {
+
+	/** Generated UID */
+	private static final long serialVersionUID = -6839802903970136273L;
+	
+	/**
+	 * Lab list for the lab login combo
+	 */
+	protected List<Lab> labList = new ArrayList<Lab>();
+	
+	/**
+	 * Creates the <code>LabComboBoxModel</code>.
+	 */
+	public LabComboBoxModel() {
+		super();
+	}
+	
+	/**
+	 * Creates the <code>LabComboBoxModel</code>.
+	 * @param labList 
+	 */
+	public LabComboBoxModel(List<Lab> labList) {
+		super();
+		this.labList = labList;
+	}
+	
+	/**
+	 * @param index
+	 * @return
+	 */
+	public Lab getLab(int index) {
+		return labList.get(index);
+	}
+	
+	/**
+	 * @param lab
+	 */
+	public void addLab(Lab lab) {
+		labList.add(lab);
+		addElement(lab);
+	}
+	
+	/**
+	 * @param labList
+	 */
+	public void addLabList(List<Lab> labList) {
+		for (Lab lab : labList) {
+			addLab(lab);
+		}
+	}
+	
+}
