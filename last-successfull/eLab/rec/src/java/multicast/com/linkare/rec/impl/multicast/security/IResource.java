fad74963f0557876ac47d31062139b869a05190e
handyman <handyman@linkare.com>
2010-12-14 01:22:40 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-eLab-299.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@3683 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/multicast/com/linkare/rec/impl/multicast/security/IResource.java
@@ -0,0 +1,22 @@
+/*
+ * IResource.java
+ *
+ * Created on 2 de Janeiro de 2004, 15:22
+ */
+
+package com.linkare.rec.impl.multicast.security;
+
+import java.util.Map;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public interface IResource extends java.io.Serializable {
+	
+	IResource getEnclosingResource();
+
+	Map<String, String> getProperties();
+
+	ResourceType getResourceType();
+}
