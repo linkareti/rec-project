88ebb7504e76a311d7c836ae939a4cb18b91f8de
handyman <handyman@linkare.com>
2010-12-20 01:08:57 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-391.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@3749 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/multicast/com/linkare/rec/impl/multicast/security/ISecurityManager.java
@@ -0,0 +1,27 @@
+/*
+ * ISecurityManager.java
+ *
+ * Created on 2 de Janeiro de 2004, 15:20
+ */
+
+package com.linkare.rec.impl.multicast.security;
+
+import java.util.List;
+
+import com.linkare.rec.impl.multicast.ReCMultiCastHardware;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public interface ISecurityManager {
+
+	boolean authorize(IResource resource, IUser user, IOperation op);
+
+	boolean authenticate(IResource resource, IUser user);
+	
+	void registerMultiCastHardware(List<ReCMultiCastHardware> multiCastHardwares);
+	
+	void registerSecurityCommunicator(ISecurityCommunicator communicator);
+
+}
