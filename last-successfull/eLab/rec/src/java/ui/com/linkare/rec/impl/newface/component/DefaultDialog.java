62f9b2847a8d91e9f5b75523c31fbeebc6c2bf78
handyman <handyman@linkare.com>
2010-12-24 01:15:23 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-eLab-309.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@3804 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/ui/com/linkare/rec/impl/newface/component/DefaultDialog.java
@@ -0,0 +1,55 @@
+/* 
+ * DefaultDialog.java created on 2009/02/06
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.linkare.rec.impl.newface.component;
+
+import java.awt.Window;
+
+import javax.swing.JDialog;
+import javax.swing.WindowConstants;
+
+/**
+ * Represents the default dialog for the ReC Application.
+ * <p>
+ * All common dialogs must be launched using <code>DefaultDialog</code>.
+ * 
+ * @param <C>
+ *            The dialog content type.
+ * 
+ * @author Henrique Fernandes
+ */
+public class DefaultDialog<C extends AbstractContentPane> extends JDialog {
+
+	private static final long serialVersionUID = -4557205313830746322L;
+
+	protected C content;
+
+	public DefaultDialog(C content) {
+		this(null, "", content);
+	}
+
+	public DefaultDialog(Window owner, String title, C content) {
+		super(owner, title);
+		this.content = content;
+		this.content.setContainer(this);
+		add(content);
+		init();
+	}
+
+	protected void init() {
+		setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+		setModal(true);
+	}
+	
+	/**
+	 * @return the dialog content.
+	 */
+	public C getContent() {
+		return content;
+	}
+
+}
