a355b52e7e5905d0298e8e69783c01d3798e7bad
handyman <handyman@linkare.com>
2010-12-25 01:14:14 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-eLab-310.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@3809 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/multicast/com/linkare/rec/impl/multicast/security/DefaultResource.java
@@ -0,0 +1,139 @@
+/*
+ * DefaultResource.java Created on 2 de Janeiro de 2004, 16:02
+ */
+
+package com.linkare.rec.impl.multicast.security;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class implements the default method for Resources, which area a way of
+ * integrating security in the SecurityManager
+ * 
+ * @see com.linkare.rec.impl.multicast.security.SecurityManagerFactory
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public class DefaultResource implements IResource {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7520963159666907685L;
+
+	private Map<String, String> properties = null;
+
+	private IResource enclosingResource = null;
+
+	private ResourceType resourceType = ResourceType.ROOT;
+
+	/** Creates a new instance of DefaultResource */
+	public DefaultResource(IResource enclosingResource, ResourceType resourceType, Map<String, String> properties) {
+		setProperties(properties);
+		setEnclosingResource(enclosingResource);
+		setResourceType(resourceType);
+	}
+
+	public DefaultResource(IResource enclosingResource, Map<String, String> properties) {
+		this(enclosingResource, deriveChildResourceType(enclosingResource), properties);
+	}
+
+	public DefaultResource(IResource enclosingResource) {
+		this(enclosingResource, deriveChildResourceType(enclosingResource), new HashMap<String, String>());
+	}
+
+	public DefaultResource(Map<String, String> properties) {
+		this(null, deriveChildResourceType(null), new HashMap<String, String>(properties));
+	}
+
+	public DefaultResource() {
+		this(null, deriveChildResourceType(null), new HashMap<String, String>());
+	}
+
+	/**
+	 * Setter for property properties.
+	 * 
+	 * @param properties New value of property properties.
+	 */
+	public void setProperties(Map<String, String> properties) {
+		this.properties = new HashMap<String, String>(properties);
+	}
+
+	/**
+	 * Setter for property enclosingResource.
+	 * 
+	 * @param enclosingResource New value of property enclosingResource.
+	 */
+	public void setEnclosingResource(IResource enclosingResource) {
+		this.enclosingResource = enclosingResource;
+	}
+
+	/**
+	 * Setter for property resourceType.
+	 * 
+	 * @param resourceType New value of property resourceType.
+	 */
+	public void setResourceType(ResourceType resourceType) {
+		this.resourceType = resourceType;
+	}
+
+	private static ResourceType deriveChildResourceType(IResource resource) {
+		if (resource == null)
+			return ResourceType.MCCONTROLLER;
+		return resource.getResourceType().getChildType();
+	}
+
+	public DefaultResource createChildResource() {
+		return new DefaultResource(this);
+	}
+
+	public DefaultResource createChildResource(Map<String, String> properties) {
+		return new DefaultResource(this, properties);
+	}
+
+	/**
+	 * Getter for property properties.
+	 * 
+	 * @return Value of property properties.
+	 */
+	public Map<String, String> getProperties() {
+		return properties;
+	}
+
+	/**
+	 * Getter for property enclosingResource.
+	 * 
+	 * @return Value of property enclosingResource.
+	 */
+	public IResource getEnclosingResource() {
+		return enclosingResource;
+	}
+
+	/**
+	 * Getter for property resourceType.
+	 * 
+	 * @return Value of property resourceType.
+	 */
+	public ResourceType getResourceType() {
+		return resourceType;
+	}
+
+	public String toString() {
+
+		return getResourceType().getName() + " @ " + getProperties().get(getResourceType().getPropertyKey());
+
+	}
+
+	public boolean equals(Object other) {
+		if (other == null || !(other instanceof IResource))
+			return false;
+
+		IResource otherResource = (IResource) other;
+
+		return otherResource.getResourceType() == getResourceType()
+				&& otherResource.getProperties().equals(getProperties());
+
+	}
+
+}
