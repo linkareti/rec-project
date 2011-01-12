ccd744cc0090658d7fbf70607287cec685f4daf1
handyman <handyman@linkare.com>
2011-01-12 01:14:55 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-eLab-328.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@3948 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/vpoisson/src/java/server/pt/utl/ist/elab/driver/vpoisson/ServerMain.java
@@ -0,0 +1,45 @@
+/*
+ * ServerMain.java
+ *
+ * Created on 27 de Fevereiro de 2005, 20:07
+ */
+
+package pt.utl.ist.elab.driver.vpoisson;
+
+import com.linkare.rec.impl.utils.*;
+import com.linkare.rec.impl.driver.*;
+import com.linkare.rec.impl.logging.*;
+import java.util.logging.*;
+
+/**
+ * 
+ * @author andre
+ * 
+ */
+public class ServerMain {
+
+	private static String POISSON_HARDWARE_LOGGER = "Poisson.Logger";
+	static {
+		Logger l = LogManager.getLogManager().getLogger(POISSON_HARDWARE_LOGGER);
+		if (l == null) {
+			LogManager.getLogManager().addLogger(Logger.getLogger(POISSON_HARDWARE_LOGGER));
+		}
+	}
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		try {
+			ORBBean.getORBBean();
+
+			BaseHardware baseHardware = new BaseHardware(new PoissonDriver());
+
+			Thread.currentThread().join();
+
+		} catch (Exception e) {
+			ORBBean.getORBBean().killORB();
+			LoggerUtil.logThrowable("Error on Main...", e, Logger.getLogger(POISSON_HARDWARE_LOGGER));
+		}
+	}
+}
