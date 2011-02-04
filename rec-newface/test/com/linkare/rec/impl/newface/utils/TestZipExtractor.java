198af9f5256370796d3cc94a7cecf25e3c29f6a2
pzenida <pzenida@linkare.com>
2011-02-04 15:24:42 +0000

    (related to bug 798) Removed unnecessary comment.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/trunk@4070 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- rec-newface/test/com/linkare/rec/impl/newface/utils/TestZipExtractor.java
+++ rec-newface/test/com/linkare/rec/impl/newface/utils/TestZipExtractor.java
@@ -11,15 +11,11 @@ public class TestZipExtractor {
 	public static void main(String[] args) {
 
 		System.out.println(System.getProperty("os.arch") + "; " + System.getProperty("os.name"));
-		//        String fileName = "libsvlc_linux.jar";
+		// String fileName = "libsvlc_linux.jar";
 		String fileName = "vlcplugins_linux.jar";
 		String userDir = System.getProperty("user.home");
 		System.out.println("Userdir = " + userDir);
 		System.out.println("Filename: " + userDir + File.separator + fileName);
 		String destDir = userDir + File.separator + ".eLab" + File.separator + "plugins";
-
-		//TODO bcatarino update to modified api
-		//ZipExtractor.extractFiles(userDir + File.separator + fileName, destDir);
-
 	}
 }
