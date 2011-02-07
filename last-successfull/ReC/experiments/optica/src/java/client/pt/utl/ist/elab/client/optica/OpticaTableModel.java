e5480ae774865275c91de828b9f0ef3bdc6deb1e
handyman <handyman@linkare.com>
2011-02-07 01:12:42 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-428.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@4080 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/optica/src/java/client/pt/utl/ist/elab/client/optica/OpticaTableModel.java
@@ -0,0 +1,31 @@
+/* 
+ * OpticaTableModel.java created on 6 Jan 2011
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package pt.utl.ist.elab.client.optica;
+
+import com.linkare.rec.impl.baseUI.table.DefaultExperimentDataTable;
+
+/**
+ * 
+ * @author npadriano
+ */
+public class OpticaTableModel extends DefaultExperimentDataTable {
+
+	/** Generated UID */
+	private static final long serialVersionUID = -6452291751943433228L;
+	
+	/**
+	 * Creates the <code>OpticaTableModel</code>.
+	 */
+	public OpticaTableModel() {
+		super();
+		
+		OpticaTableModelProxy model = new OpticaTableModelProxy();
+		setActualTableModel(model);
+		setExpDataModelContainer(model);
+	}
+
+}
