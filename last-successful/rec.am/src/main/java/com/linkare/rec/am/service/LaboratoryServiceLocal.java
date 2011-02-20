4134a47684cfdcbc2fd8b7f9ca984f5e3f352ccb
handyman <handyman@linkare.com>
2011-02-20 13:47:14 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-rec.am-179.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec.am@4160 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successful/rec.am/src/main/java/com/linkare/rec/am/service/LaboratoryServiceLocal.java
@@ -0,0 +1,12 @@
+package com.linkare.rec.am.service;
+
+import javax.ejb.Local;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ * 
+ */
+@Local
+public interface LaboratoryServiceLocal extends LaboratoryService {
+}
