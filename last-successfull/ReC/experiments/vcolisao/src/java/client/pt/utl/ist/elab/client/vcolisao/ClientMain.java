b9c1d57aa4555bd278e72ca465793a22d882b1e3
handyman <handyman@linkare.com>
2011-02-21 01:13:59 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-442.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@4164 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/vcolisao/src/java/client/pt/utl/ist/elab/client/vcolisao/ClientMain.java
@@ -0,0 +1,29 @@
+/*
+ * ClientMain.java
+ *
+ * Created on 16 de Outubro de 2004, 11:29
+ */
+
+package pt.utl.ist.elab.client.vcolisao;
+
+import javax.swing.JFrame;
+
+import com.linkare.rec.impl.baseUI.ReCBaseUI;
+
+/**
+ * 
+ * @author Ean
+ */
+public class ClientMain {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		ReCBaseUI ui = new ReCBaseUI();
+		ui.setApparatusAutoConnectID("COLISAO_V1.0");
+		ui.setVisible(true);
+	}
+}
