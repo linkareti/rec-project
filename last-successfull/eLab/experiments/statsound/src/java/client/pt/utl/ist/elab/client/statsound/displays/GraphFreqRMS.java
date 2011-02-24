22ad065df721948ba3e2dc1804ae33508e1b9c6f
handyman <handyman@linkare.com>
2011-02-24 01:24:28 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-eLab-354.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@4181 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/statsound/src/java/client/pt/utl/ist/elab/client/statsound/displays/GraphFreqRMS.java
@@ -0,0 +1,24 @@
+/*
+ * PistonRMS.java
+ *
+ * Created on October 13, 2003, 5:55 PM
+ */
+
+package pt.utl.ist.elab.client.statsound.displays;
+
+/**
+ * 
+ * @author André Neto - LEFT - IST
+ */
+public class GraphFreqRMS extends MyDefaultXYExperimentGraph {
+	/** Creates a new instance of PistonRMS */
+	public GraphFreqRMS() {
+		super();
+		setChannelDisplayX(1);
+		setChannelDisplayYArray(new int[] { 3, 4 });
+	}
+
+	public String getName() {
+		return "Chart Frequency vs RMS";
+	}
+}
