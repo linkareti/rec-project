1a3a5962a892cb8a9fddf13a25e885b82efe7ab6
handyman <handyman@linkare.com>
2011-03-12 01:22:52 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-ReC-461.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@4284 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/multicast/com/linkare/rec/impl/events/HardwareChangeEvent.java
@@ -0,0 +1,20 @@
+/*
+ * HardwareChangeEvent.java
+ *
+ * Created on 6 de Novembro de 2002, 12:38
+ */
+
+package com.linkare.rec.impl.events;
+
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public class HardwareChangeEvent {
+
+	/** Creates a new instance of HardwareChangeEvent */
+	public HardwareChangeEvent() {
+	}
+
+}
