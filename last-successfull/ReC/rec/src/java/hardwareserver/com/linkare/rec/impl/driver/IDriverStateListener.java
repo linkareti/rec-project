2c22c3af92c5f293f1076bcd19c157fcd75e4e54
handyman <handyman@linkare.com>
2011-04-10 00:11:29 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-489.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@4452 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/hardwareserver/com/linkare/rec/impl/driver/IDriverStateListener.java
@@ -0,0 +1,33 @@
+/*
+ * IDriverStateListener.java
+ *
+ * Created on 9 de Maio de 2003, 18:12
+ */
+
+package com.linkare.rec.impl.driver;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public interface IDriverStateListener extends java.util.EventListener {
+	public void driverInited();
+
+	public void driverConfiguring();
+
+	public void driverConfigured();
+
+	public void driverStarting();
+
+	public void driverStarted();
+
+	public void driverStoping();
+
+	public void driverStoped();
+
+	public void driverReseting();
+
+	public void driverReseted();
+
+	public void driverShutdown();
+}
