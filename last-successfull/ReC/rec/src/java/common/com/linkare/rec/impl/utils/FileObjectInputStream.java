94564af745af41f7291147713bc0b61f5dc86984
handyman <handyman@linkare.com>
2011-04-15 00:12:05 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-494.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@4483 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/common/com/linkare/rec/impl/utils/FileObjectInputStream.java
@@ -0,0 +1,49 @@
+/*
+ * FileObjectOutputStream.java
+ *
+ * Created on 13 de Janeiro de 2004, 15:40
+ */
+
+package com.linkare.rec.impl.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public class FileObjectInputStream extends ObjectInputStream {
+	private File file = null;
+
+	/** Creates a new instance of FileObjectOutputStream 
+	 * @param file 
+	 * @throws IOException */
+	public FileObjectInputStream(File file) throws IOException {
+		super(new FileInputStream(file));
+		setFile(file);
+	}
+
+	/**
+	 * Getter for property file.
+	 * 
+	 * @return Value of property file.
+	 * 
+	 */
+	public File getFile() {
+		return file;
+	}
+
+	/**
+	 * Setter for property file.
+	 * 
+	 * @param file New value of property file.
+	 * 
+	 */
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+}
