83302bd7acc385f3fe77923bf142311a19ac381d
acorreia <acorreia@linkare.com>
2011-04-21 14:42:52 +0000

    (related to bug 984) correction on DozerBeanMapper. Add dozer-global-configuration.xml file
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/branches/dev-expresult-20110401@4516 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- rec.am/src/main/java/com/linkare/rec/am/model/util/converter/DozerBeanMapperSingletonWrapper.java
+++ rec.am/src/main/java/com/linkare/rec/am/model/util/converter/DozerBeanMapperSingletonWrapper.java
@@ -1,26 +1,9 @@
 package com.linkare.rec.am.model.util.converter;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import org.dozer.DozerBeanMapper;
-import org.dozer.Mapper;
 import org.dozer.MappingException;
-import org.dozer.loader.api.BeanMappingBuilder;
-import org.dozer.loader.api.TypeMappingOptions;
-
-import com.linkare.rec.am.model.ChannelAcquisitionConfig;
-import com.linkare.rec.am.model.DateTime;
-import com.linkare.rec.am.model.Frequency;
-import com.linkare.rec.am.model.HardwareAcquisitionConfig;
-import com.linkare.rec.am.model.ParameterConfig;
-import com.linkare.rec.am.model.PhysicsVal;
-import com.linkare.rec.am.repository.ChannelAcquisitionConfigDTO;
-import com.linkare.rec.am.repository.DateTimeDTO;
-import com.linkare.rec.am.repository.FrequencyDTO;
-import com.linkare.rec.am.repository.HardwareAcquisitionConfigDTO;
-import com.linkare.rec.am.repository.ParameterConfigDTO;
-import com.linkare.rec.am.repository.PhysicsValDTO;
 
 /**
  * 
@@ -29,141 +12,29 @@ import com.linkare.rec.am.repository.PhysicsValDTO;
  * @author Artur Correia - Linkare TI
  * 
  */
-public final class DozerBeanMapperSingletonWrapper implements Mapper {
+public final class DozerBeanMapperSingletonWrapper {
 
-    private static final Mapper INSTANCE = new DozerBeanMapperSingletonWrapper();
+    private static final DozerBeanMapperSingletonWrapper INSTANCE = new DozerBeanMapperSingletonWrapper();
 
     private final DozerBeanMapper mapper;
 
     private DozerBeanMapperSingletonWrapper() {
-	mapper = new DozerBeanMapper();
-	//make available all builders for complex type conversion
-	// dozer need this information to mapp complex types
-	mapper.setMappings(DozerBuildersRegistry.getDozerBuilders());
+
+	mapper = new DozerBeanMapper(Arrays.asList(new String[] { DozerBeanMapperSingletonWrapper.class.getPackage().getName().replaceAll("\\.", "/")
+		+ "/dozer-global-configuration.xml" }));
+
     }
 
-    public static Mapper getInstance() {
+    public static DozerBeanMapperSingletonWrapper getInstance() {
 	return INSTANCE;
     }
 
-    @Override
     public <T> T map(Object arg0, Class<T> arg1) {
 	try {
 	    return mapper.map(arg0, arg1);
 	} catch (MappingException e) {
 	    throw new RecMappingException(e);
 	}
-
-    }
-
-    @Override
-    public void map(Object arg0, Object arg1) {
-	try {
-	    mapper.map(arg0, arg1);
-	} catch (MappingException e) {
-	    throw new RecMappingException(e);
-	}
-
-    }
-
-    @Override
-    public <T> T map(Object arg0, Class<T> arg1, String arg2) throws MappingException {
-	try {
-	    return mapper.map(arg0, arg1);
-	} catch (MappingException e) {
-	    throw new RecMappingException(e);
-	}
-    }
-
-    @Override
-    public void map(Object arg0, Object arg1, String arg2) throws MappingException {
-	try {
-	    mapper.map(arg0, arg1);
-	} catch (MappingException e) {
-	    throw new RecMappingException(e);
-	}
-    }
-
-    private final static class DozerBuildersRegistry {
-
-	private static final List<BeanMappingBuilder> builders;
-
-	static {
-
-	    builders = new ArrayList<BeanMappingBuilder>(10);
-
-	    BeanMappingBuilder aux = new BeanMappingBuilder() {
-
-		@Override
-		protected void configure() {
-		    mapping(HardwareAcquisitionConfigDTO.class, HardwareAcquisitionConfig.class);
-		}
-
-	    };
-
-	    builders.add(aux);
-
-	    aux = new BeanMappingBuilder() {
-
-		@Override
-		protected void configure() {
-		    mapping(DateTimeDTO.class, DateTime.class);
-		}
-
-	    };
-
-	    builders.add(aux);
-
-	    aux = new BeanMappingBuilder() {
-
-		@Override
-		protected void configure() {
-		    mapping(FrequencyDTO.class, Frequency.class);
-		}
-
-	    };
-
-	    builders.add(aux);
-
-	    aux = new BeanMappingBuilder() {
-
-		@Override
-		protected void configure() {
-		    mapping(ParameterConfigDTO.class, ParameterConfig.class);
-		}
-
-	    };
-
-	    builders.add(aux);
-
-	    aux = new BeanMappingBuilder() {
-
-		@Override
-		protected void configure() {
-		    mapping(ChannelAcquisitionConfigDTO.class, ChannelAcquisitionConfig.class);
-		}
-
-	    };
-
-	    builders.add(aux);
-
-	    aux = new BeanMappingBuilder() {
-
-		@Override
-		protected void configure() {
-		    mapping(PhysicsValDTO.class, PhysicsVal.class, TypeMappingOptions.beanFactory(PhysicsValFactory.class.getName()));
-		}
-
-	    };
-
-	    builders.add(aux);
-
-	}
-
-	private static List<BeanMappingBuilder> getDozerBuilders() {
-	    return builders;
-	}
-
     }
 
 }
