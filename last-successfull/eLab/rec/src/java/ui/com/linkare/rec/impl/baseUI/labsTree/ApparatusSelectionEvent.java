8d043bb464b0c8bb989cb9bcf0ec7ad97a600506
handyman <handyman@linkare.com>
2011-04-22 00:19:09 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-409.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@4520 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/ui/com/linkare/rec/impl/baseUI/labsTree/ApparatusSelectionEvent.java
@@ -0,0 +1,41 @@
+/*
+ * ApparatusSelectionEvent.java
+ *
+ * Created on 19/07/04 4:42pm
+ */
+
+package com.linkare.rec.impl.baseUI.labsTree;
+
+/**
+ *
+ * @author André Neto - LEFT - IST
+ */
+
+import com.linkare.rec.impl.baseUI.config.Apparatus;
+import com.linkare.rec.impl.baseUI.config.Lab;
+
+public class ApparatusSelectionEvent extends LabSelectionEvent {
+
+	/** Holds value of property Apparatus. */
+	private Apparatus app;
+
+	/** Creates a new instance of ApparatusSelectionEvent */
+	public ApparatusSelectionEvent(Object source, Apparatus app) {
+		this(source, app, null);
+	}
+
+	/** Creates a new instance of ApparatusSelectionEvent */
+	public ApparatusSelectionEvent(Object source, Apparatus app, Lab lab) {
+		super(source, lab);
+		this.app = app;
+	}
+
+	/**
+	 * Getter for property Apparatus.
+	 * 
+	 * @return Value of property Apparatus.
+	 */
+	public Apparatus getApparatus() {
+		return this.app;
+	}
+}
