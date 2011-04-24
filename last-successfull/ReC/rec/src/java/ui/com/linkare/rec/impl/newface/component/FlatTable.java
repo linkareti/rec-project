87340f6b36c598fa04bc890138f5fb99d247234f
handyman <handyman@linkare.com>
2011-04-24 00:12:13 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-503.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@4526 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/ui/com/linkare/rec/impl/newface/component/FlatTable.java
@@ -0,0 +1,39 @@
+package com.linkare.rec.impl.newface.component;
+
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.plaf.basic.BasicTableUI;
+
+/**
+ * 
+ * @author João FLorindo
+ */
+public class FlatTable extends JTable {
+
+	private static final String uiClassID = "FlatTableUI";
+
+	public FlatTable() {
+		super();
+
+		if (UIManager.get(getUIClassID()) == null) {
+			setUI(new BasicTableUI());
+		}
+
+		setRowHeight(30);
+		getTableHeader().setResizingAllowed(false);
+		getTableHeader().setReorderingAllowed(false);
+
+	}
+
+	@Override
+	public String getUIClassID() {
+		return uiClassID;
+	}
+
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
+
+}
