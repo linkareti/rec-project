4e8398ef71cc1e90f9daee97940c4127073192fc
handyman <handyman@linkare.com>
2011-04-26 00:10:48 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-505.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@4534 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/statsound/src/java/client/pt/utl/ist/elab/client/statsound/displays/TableSoundVelocity.java
@@ -0,0 +1,35 @@
+package pt.utl.ist.elab.client.statsound.displays;
+
+import com.linkare.rec.impl.baseUI.table.MultSeriesTable;
+import com.linkare.rec.impl.i18n.ReCResourceBundle;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ */
+public class TableSoundVelocity extends MultSeriesTable {
+
+	private static final String NAME = ReCResourceBundle
+			.findString("statsound$rec.exp.display.statsound.title.soundVelocityTable");
+
+	/**
+	 * 
+	 * Creates the <code>TableSoundVelocity</code>.
+	 * 
+	 * It sets the array indexes from which it should fetch the data to be
+	 * presented.
+	 * 
+	 * Notice that the sample number and the acquisition time are calculated on
+	 * the model proxy instance.
+	 */
+	public TableSoundVelocity() {
+		super(new TableSoundVelocityModelProxy());
+		// sample number, acquisition time, wave1, wave2, piston position,
+		// temperature
+		setColArray(new int[] { 100, 111, 4, 5, 0, 1 });
+	}
+
+	public String getName() {
+		return NAME;
+	}
+}
