ce0de8b7235a43c753ea2a9f569c7621ac4c0ce0
handyman <handyman@linkare.com>
2011-04-30 00:17:21 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-416.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@4548 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/ui/com/linkare/rec/impl/newface/laf/flat/FlatOriginalButtonUI.java
@@ -0,0 +1,48 @@
+/*
+ * FlatButtonUI.java created on 2009/04/29
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.linkare.rec.impl.newface.laf.flat;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+import javax.swing.AbstractButton;
+import javax.swing.JComponent;
+import javax.swing.plaf.ComponentUI;
+import javax.swing.plaf.metal.MetalButtonUI;
+
+/**
+ * 
+ * @author João Florindo
+ */
+public class FlatOriginalButtonUI extends MetalButtonUI {
+
+	private final static String propertyPrefix = "Button" + ".";
+
+	public static ComponentUI createUI(JComponent x) {
+		return new FlatOriginalButtonUI();
+	}
+
+	@Override
+	protected String getPropertyPrefix() {
+		return propertyPrefix;
+	}
+
+	// ********************************
+	//          Paint
+	// ********************************
+
+	@Override
+	protected void paintButtonPressed(Graphics g, AbstractButton b) {
+		// FIXME João: Podes confirmar se este noop é está mesmo relacionado com o focus? Não poderá influenciar o comportamento do button pressed?
+	}
+
+	@Override
+	protected void paintFocus(Graphics g, AbstractButton b, Rectangle viewRect, Rectangle textRect, Rectangle iconRect) {
+		// Para não ser desenhado o focus no Button
+	}
+
+}
