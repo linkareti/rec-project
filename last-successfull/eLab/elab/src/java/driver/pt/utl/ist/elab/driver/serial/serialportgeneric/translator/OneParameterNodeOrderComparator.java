a3021888e15ba95c86a032d17099b338887a18e5
handyman <handyman@linkare.com>
2011-05-05 00:15:44 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-421.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@4586 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/elab/src/java/driver/pt/utl/ist/elab/driver/serial/serialportgeneric/translator/OneParameterNodeOrderComparator.java
@@ -0,0 +1,27 @@
+/* 
+ * OneParameterNodeComparator.java created on 19 Aug 2010
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package pt.utl.ist.elab.driver.serial.serialportgeneric.translator;
+
+import java.util.Comparator;
+
+import pt.utl.ist.elab.driver.serial.serialportgeneric.config.OneParameterNode;
+
+/**
+ * 
+ * @author npadriano
+ */
+public class OneParameterNodeOrderComparator implements Comparator<OneParameterNode> {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int compare(OneParameterNode arg0, OneParameterNode arg1) {
+		return arg0.getOrder().compareTo(arg1.getOrder());
+	}
+
+}
