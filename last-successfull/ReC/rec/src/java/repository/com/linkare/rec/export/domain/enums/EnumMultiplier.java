9faa4f808a9a314c5f88f791988ffa4f2e3bb87c
handyman <handyman@linkare.com>
2011-05-11 00:12:54 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-519.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@4608 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/repository/com/linkare/rec/export/domain/enums/EnumMultiplier.java
@@ -0,0 +1,18 @@
+package com.linkare.rec.export.domain.enums;
+
+/**
+ *
+ * @author artur
+ */
+public enum EnumMultiplier{
+    FENTO,
+    PICO,
+    NANO,
+    MICRO,
+    MILI,
+    NONE,
+    KILO,
+    MEGA,
+    GIGA,
+    TERA
+}
