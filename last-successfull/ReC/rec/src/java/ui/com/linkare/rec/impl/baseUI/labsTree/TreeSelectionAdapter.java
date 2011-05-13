6d393891c45420e2a35730230f1520976017615d
handyman <handyman@linkare.com>
2011-05-13 00:12:37 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-521.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@4616 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/ui/com/linkare/rec/impl/baseUI/labsTree/TreeSelectionAdapter.java
@@ -0,0 +1,30 @@
+/*
+ * TreeSelectionAdapter.java
+ *
+ * Created on July 19, 2004, 6:14 PM
+ */
+
+package com.linkare.rec.impl.baseUI.labsTree;
+
+/**
+ * 
+ * @author André Neto - LEFT - IST
+ */
+public abstract class TreeSelectionAdapter implements TreeSelectionChangeListener {
+
+	public void apparatusSelectionChange(ApparatusSelectionEvent selectevent) {
+	}
+
+	public void defaultConfigSelectionChange(DefaultConfigSelectionEvent selectevent) {
+	}
+
+	public void displaySelectionChange(DisplaySelectionEvent selectevent) {
+	}
+
+	public void labSelectionChange(LabSelectionEvent selectevent) {
+	}
+
+	public void webResourceSelectionChange(WebResourceSelectionEvent selectevent) {
+	}
+
+}
