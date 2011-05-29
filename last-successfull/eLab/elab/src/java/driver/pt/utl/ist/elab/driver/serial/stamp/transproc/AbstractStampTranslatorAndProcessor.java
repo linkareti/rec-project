ab252f84fdd219e62ff43ead58b2631c925fe21c
handyman <handyman@linkare.com>
2011-05-29 00:14:47 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-449.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@4701 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/elab/src/java/driver/pt/utl/ist/elab/driver/serial/stamp/transproc/AbstractStampTranslatorAndProcessor.java
@@ -0,0 +1,26 @@
+/*
+ * AbstractSerialPortTranslatorAndProcessor.java
+ *
+ * Created on 12 de Novembro de 2002, 15:15
+ */
+
+package pt.utl.ist.elab.driver.serial.stamp.transproc;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public abstract class AbstractStampTranslatorAndProcessor implements StampTranslator, StampProcessor {
+	private String commandIdentifier = null;
+
+	/** Creates a new instance of AbstractSerialPortTranslatorAndProcessor */
+	public AbstractStampTranslatorAndProcessor(final String commandIdentifier) {
+		this.commandIdentifier = commandIdentifier;
+		StampTranslatorProcessorManager.registerProcessor(this);
+	}
+
+	public boolean accepts(final StampCommand command) {
+		return commandIdentifier.equals(command.getCommandIdentifier());
+	}
+
+}
