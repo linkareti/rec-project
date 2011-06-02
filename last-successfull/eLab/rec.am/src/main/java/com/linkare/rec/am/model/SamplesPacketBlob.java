bc873a7581dd78801b0a5bd4d70514b208a1bfde
handyman <handyman@linkare.com>
2011-06-02 00:14:07 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-455.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@4722 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.am/src/main/java/com/linkare/rec/am/model/SamplesPacketBlob.java
@@ -0,0 +1,38 @@
+package com.linkare.rec.am.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+import com.linkare.commons.jpa.DefaultDomainObject;
+
+/**
+ * 
+ * Only to make lazy loadind of blob possible
+ * 
+ * 
+ * @author Artur Correia - Linkare TI
+ * 
+ */
+@Entity
+@Table(name = "SAMPLES_PACKET_BLOB")
+public class SamplesPacketBlob extends DefaultDomainObject {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    @Lob
+    @Column(name = "SAMPLES_PACKET_MATRIX_BLOB")
+    private byte[] samplesPacketMatrixSerialized;
+
+    public byte[] getSamplesPacketMatrixSerialized() {
+	return samplesPacketMatrixSerialized;
+    }
+
+    public void setSamplesPacketMatrixSerialized(byte[] samplesPacketMatrixSerialized) {
+	this.samplesPacketMatrixSerialized = samplesPacketMatrixSerialized;
+    }
+
+}
