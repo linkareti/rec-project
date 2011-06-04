737e3161532600c6e9a5c318bc33c4e307729614
handyman <handyman@linkare.com>
2011-06-04 00:12:12 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-545.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@4728 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/statsound/src/java/client/pt/utl/ist/elab/client/statsound/displays/GraphPistonRMS.java
@@ -0,0 +1,41 @@
+/*
+ * PistonRMS.java
+ *
+ * Created on October 13, 2003, 5:55 PM
+ */
+
+package pt.utl.ist.elab.client.statsound.displays;
+
+import com.linkare.rec.data.config.HardwareAcquisitionConfig;
+import com.linkare.rec.impl.i18n.ReCResourceBundle;
+
+/**
+ * 
+ * @author André Neto - LEFT - IST
+ */
+public class GraphPistonRMS extends MyDefaultXYExperimentGraph {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4701714266593827092L;
+	private static final String CHART_NAME = ReCResourceBundle
+			.findString("statsound$rec.exp.display.statsound.title.4");
+
+	/** Creates a new instance of PistonRMS */
+	public GraphPistonRMS() {
+		super();
+		setChannelDisplayX(0);
+		setChannelDisplayYArray(new int[] { 3, 4 });
+	}
+
+	@Override
+	public String getName() {
+		return GraphPistonRMS.CHART_NAME;
+	}
+
+	@Override
+	protected String getChartName(final HardwareAcquisitionConfig header) {
+		return getName();
+	}
+}
