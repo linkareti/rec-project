268a2ea51ac49e0b683273443cb9426eaa7d2d09
handyman <handyman@linkare.com>
2011-06-11 00:12:50 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-552.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@4758 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/ui/com/linkare/rec/impl/newface/config/ReCModel.java
@@ -0,0 +1,28 @@
+/* 
+ * ReCModel.java created on Jan 30, 2009
+ * 
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.linkare.rec.impl.newface.config;
+
+/**
+ * The <code>ReCModel</code> interface specifies the methods the
+ * <code>ReCView</code> will use to access the Remote Experience Control data
+ * model.
+ * <p>
+ * 
+ * The <code>ReCView</code> can be set up to display and interact with any data
+ * model which implements the <code>ReCModel</code>.
+ * 
+ * @deprecated
+ * @author Henrique Fernandes
+ * @see ReCView
+ */
+@Deprecated
+public interface ReCModel {
+
+	// Dimension getApplicationPreferredSize();
+
+}
