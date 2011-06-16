4c71d703c9ad7f052e4a28eeec7b6a29eef747b6
handyman <handyman@linkare.com>
2011-06-16 00:12:12 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-557.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@4787 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/vlooping/src/java/client/pt/utl/ist/elab/client/vlooping/displays/DataTable.java
@@ -0,0 +1,27 @@
+/*
+ * TableFreqRMS.java
+ *
+ * Created on 16 October 2003, 23:33
+ */
+
+package pt.utl.ist.elab.client.vlooping.displays;
+
+import com.linkare.rec.impl.baseUI.table.MultSeriesTable;
+
+/**
+ * 
+ * @author Emanuel A.
+ */
+public class DataTable extends MultSeriesTable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7044296836646537587L;
+
+	/** Creates a new instance of TableFreqRMS */
+	public DataTable() {
+		super();
+		setColArray(new int[] { 0, 1, 2, 3, 4, 5 });
+	}
+}
