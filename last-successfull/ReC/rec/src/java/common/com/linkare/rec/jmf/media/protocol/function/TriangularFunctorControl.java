2752d034ad85a39cf3cfb8e724db4e5998ef39f4
handyman <handyman@linkare.com>
2011-06-17 00:10:54 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-558.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@4791 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/common/com/linkare/rec/jmf/media/protocol/function/TriangularFunctorControl.java
@@ -0,0 +1,18 @@
+package com.linkare.rec.jmf.media.protocol.function;
+
+import java.awt.Component;
+
+import javax.media.Control;
+
+public class TriangularFunctorControl implements Control {
+
+    public TriangularFunctorControl(FunctorType functorType) {
+	// TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Component getControlComponent() {
+	return null;
+    }
+
+}
