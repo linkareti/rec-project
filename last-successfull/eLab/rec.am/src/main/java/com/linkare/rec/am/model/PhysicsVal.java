21461b2c603e12bdb50be80136b7e500564c6e21
handyman <handyman@linkare.com>
2011-06-26 00:15:29 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-479.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@4829 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.am/src/main/java/com/linkare/rec/am/model/PhysicsVal.java
@@ -0,0 +1,23 @@
+package com.linkare.rec.am.model;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+import com.linkare.commons.jpa.DefaultDomainObject;
+
+/**
+ * 
+ * @author Artur Correia - Linkare TI
+ */
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Table(name = "PHYSICS_VAL")
+@DiscriminatorColumn(name = "VALUE_TYPE")
+public abstract class PhysicsVal extends DefaultDomainObject {
+
+    private static final long serialVersionUID = 1L;
+
+}
