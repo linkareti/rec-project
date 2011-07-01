aff2894034d7542694627059b30cf01fae488df5
handyman <handyman@linkare.com>
2011-07-01 00:16:25 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-484.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@4849 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/common/com/linkare/rec/impl/utils/ObjectID.java
@@ -0,0 +1,45 @@
+/*
+ * ObjectID.java
+ *
+ * Created on 8 de Julho de 2003, 13:45
+ */
+
+package com.linkare.rec.impl.utils;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public class ObjectID implements java.io.Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1519605890599740771L;
+	/** Holds value of property oid. */
+	private byte[] oid;
+
+	/** Creates a new instance of ObjectID */
+	public ObjectID() {
+	}
+
+	/**
+	 * Getter for property oid.
+	 * 
+	 * @return Value of property oid.
+	 */
+	public byte[] getOid() {
+		return oid;
+	}
+
+	/**
+	 * Setter for property oid.
+	 * 
+	 * @param oid New value of property oid.
+	 */
+	@SuppressWarnings("PMD.ArrayIsStoredDirectly")
+	public void setOid(final byte[] oid) {
+		this.oid = oid;
+	}
+
+}
