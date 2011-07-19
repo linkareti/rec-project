729a3578e5147a9ba826990705ce1b3f1094045f
handyman <handyman@linkare.com>
2011-07-19 00:10:12 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-590.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@4922 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/common/com/linkare/rec/jmf/media/protocol/function/WhiteNoiseFunctor.java
@@ -0,0 +1,28 @@
+package com.linkare.rec.jmf.media.protocol.function;
+
+import java.util.Random;
+
+public class WhiteNoiseFunctor implements Functor {
+
+    private static final double RANGE_SHIFT = 1.;
+    private static final double RANGE_LENGTH = 2.;
+
+    private Random random = new Random();
+    private double timeDelta;
+
+    @Override
+    public void setTimeDelta(double seconds) {
+	this.timeDelta = seconds;
+    }
+
+    @Override
+    public double getNextValue() {
+	return random.nextDouble() * RANGE_LENGTH - RANGE_SHIFT;
+    }
+
+    @Override
+    public double getTimeDelta() {
+	return timeDelta;
+    }
+
+}
