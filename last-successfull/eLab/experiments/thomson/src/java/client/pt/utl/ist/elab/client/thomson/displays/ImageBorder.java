328efb75d97cdfc05fc891c8f88b6ccb956ef49d
handyman <handyman@linkare.com>
2011-07-23 00:15:22 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-506.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@4948 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/thomson/src/java/client/pt/utl/ist/elab/client/thomson/displays/ImageBorder.java
@@ -0,0 +1,63 @@
+/*
+ * ImageBorder.java
+ *
+ * Created on September 2, 2005, 6:41 PM
+ */
+
+package pt.utl.ist.elab.client.thomson.displays;
+
+/**
+ *
+ * @author André Neto - LEFT - IST
+ */
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Insets;
+
+import javax.swing.border.Border;
+
+public class ImageBorder implements Border {
+	private final Image image;
+
+	/** Creates a new instance of ImageBorder */
+	public ImageBorder(final Image image, final boolean fill) {
+		this.image = image;
+		this.fill = fill;
+	}
+
+	@Override
+	public void paintBorder(final Component c, final Graphics g, final int x, final int y, final int width,
+			final int height) {
+		if (image == null) {
+			final Color color = c.getBackground();
+			g.setColor(color);
+			g.fillRect(0, 0, width, height);
+		}
+		if (!fill) {
+			final int x0 = x + (width - image.getWidth(null)) / 2;
+			final int y0 = y + (height - image.getHeight(null)) / 2;
+			g.drawImage(image, x0, y0, null);
+		} else {
+			g.drawImage(image, 0, 0, width, height, null);
+		}
+	}
+
+	@Override
+	public Insets getBorderInsets(final Component c) {
+		return new Insets(0, 0, 0, 0);
+	}
+
+	@Override
+	public boolean isBorderOpaque() {
+		return true;
+	}
+
+	private boolean fill = true;
+
+	public void setFill(final boolean fill) {
+		this.fill = fill;
+	}
+}
---
+++ last-successfull/eLab/rec/src/java/common/com/linkare/rec/repository/RepositoryManagerPOA.java
@@ -0,0 +1,86 @@
+package com.linkare.rec.repository;
+
+import java.util.Hashtable;
+
+import org.omg.CORBA.BAD_OPERATION;
+import org.omg.CORBA.CompletionStatus;
+import org.omg.CORBA.ORB;
+import org.omg.CORBA.portable.InputStream;
+import org.omg.CORBA.portable.InvokeHandler;
+import org.omg.CORBA.portable.OutputStream;
+import org.omg.CORBA.portable.ResponseHandler;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.Servant;
+
+import com.linkare.rec.acquisition.DataProducer;
+import com.linkare.rec.acquisition.DataProducerHelper;
+import com.linkare.rec.acquisition.UserInfo;
+import com.linkare.rec.acquisition.UserInfoHelper;
+
+@SuppressWarnings("unchecked")
+public abstract class RepositoryManagerPOA extends Servant implements RepositoryManagerOperations, InvokeHandler {
+
+	// Constructors
+
+	private static Hashtable _methods = new Hashtable();
+	static {
+		RepositoryManagerPOA._methods.put("getDataProducer", new java.lang.Integer(0));
+		RepositoryManagerPOA._methods.put("listDataProducers", new java.lang.Integer(1));
+	}
+
+	@Override
+	public OutputStream _invoke(final String $method, final InputStream in, final ResponseHandler $rh) {
+		OutputStream out = null;
+		final java.lang.Integer __method = (java.lang.Integer) RepositoryManagerPOA._methods.get($method);
+		if (__method == null) {
+			throw new BAD_OPERATION(0, CompletionStatus.COMPLETED_MAYBE);
+		}
+
+		switch (__method.intValue()) {
+		case 0: // com/linkare/rec/repository/RepositoryManager/getDataProducer
+		{
+			final UserInfo user = UserInfoHelper.read(in);
+			final String id = DataProducerIdHelper.read(in);
+			DataProducer $result = null;
+			$result = getDataProducer(user, id);
+			out = $rh.createReply();
+			DataProducerHelper.write(out, $result);
+			break;
+		}
+
+		case 1: // com/linkare/rec/repository/RepositoryManager/listDataProducers
+		{
+			final UserInfo user = UserInfoHelper.read(in);
+			final HardwareAcquisitionConfigSearch[] search_params = VTHardwareAcquisitionConfigSearchListHelper
+					.read(in);
+			DataProducerConfig $result[] = null;
+			$result = listDataProducers(user, search_params);
+			out = $rh.createReply();
+			DataProducerConfigListHelper.write(out, $result);
+			break;
+		}
+
+		default:
+			throw new BAD_OPERATION(0, CompletionStatus.COMPLETED_MAYBE);
+		}
+
+		return out;
+	} // _invoke
+
+	// Type-specific CORBA::Object operations
+	private static String[] __ids = { "IDL:com/linkare/rec/repository/RepositoryManager:1.0" };
+
+	@Override
+	public String[] _all_interfaces(final POA poa, final byte[] objectId) {
+		return RepositoryManagerPOA.__ids.clone();
+	}
+
+	public RepositoryManager _this() {
+		return RepositoryManagerHelper.narrow(super._this_object());
+	}
+
+	public RepositoryManager _this(final ORB orb) {
+		return RepositoryManagerHelper.narrow(super._this_object(orb));
+	}
+
+} // class RepositoryManagerPOA
