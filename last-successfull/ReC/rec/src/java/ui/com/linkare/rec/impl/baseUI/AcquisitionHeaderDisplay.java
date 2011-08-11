a19a27e222aacaa55d9aead7362073b397d374ae
handyman <handyman@linkare.com>
2011-08-11 00:11:55 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-613.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@5036 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/ui/com/linkare/rec/impl/baseUI/AcquisitionHeaderDisplay.java
@@ -0,0 +1,23 @@
+/*
+ * AcquisitionHeaderDisplay.java
+ *
+ * Created on 9 de Maio de 2003, 12:21
+ */
+
+package com.linkare.rec.impl.baseUI;
+
+import javax.swing.JComponent;
+
+import com.linkare.rec.data.config.HardwareAcquisitionConfig;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public interface AcquisitionHeaderDisplay {
+	public JComponent getDisplayComponent();
+
+	public void setAcquisitionHeader(HardwareAcquisitionConfig header);
+
+	public HardwareAcquisitionConfig getAcquisitionHeader();
+}
