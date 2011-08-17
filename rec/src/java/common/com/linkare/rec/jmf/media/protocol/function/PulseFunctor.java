af300ad9b234d4cb607541c4c02d7972684fde6b
pzenida <pzenida@linkare.com>
2011-08-17 16:30:11 +0000

    (related to bug 886) Fixing the generation of the pulse.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/trunk@5072 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- rec/src/java/common/com/linkare/rec/jmf/media/protocol/function/PulseFunctor.java
+++ rec/src/java/common/com/linkare/rec/jmf/media/protocol/function/PulseFunctor.java
@@ -2,55 +2,55 @@ package com.linkare.rec.jmf.media.protocol.function;
 
 public class PulseFunctor implements Functor {
 
-    private double currentTime = 0.;
+	private double currentTime = 0.;
 
-    private double timeDelta = 1.;
+	private double timeDelta = 1.;
 
-    private double pulseLengthPercent = 0.;
+	private double pulseLengthPercent = 0.1;
 
-    private double pulseLength = 0.;
+	private double frequency = 1.;
 
-    private double frequency = 1.;
+	private double period = 1. / frequency;
 
-    private double period = 1. / frequency;
+	private double pulseLength = period * pulseLengthPercent;
 
-    public void setPulseLengthPercent(double percent) {
-	this.pulseLengthPercent = percent;
-	this.pulseLength = period * percent;
-    }
-
-    public void setFrequency(double frequency) {
-	this.frequency = frequency;
-	this.period = 1. / frequency;
-	this.pulseLength = period * pulseLengthPercent;
-    }
-
-    @Override
-    public void setTimeDelta(double seconds) {
-	this.timeDelta = seconds;
-    }
-
-    @Override
-    public double getNextValue() {
-	currentTime += this.timeDelta;
-	if (currentTime > period) {
-	    currentTime -= period;
+	public void setPulseLengthPercent(double percent) {
+		this.pulseLengthPercent = percent;
+		this.pulseLength = period * percent;
 	}
 
-	return (currentTime > (period - pulseLength) || currentTime < 0) ? 1. : 0.;
-    }
+	public void setFrequency(double frequency) {
+		this.frequency = frequency;
+		this.period = 1. / frequency;
+		this.pulseLength = period * pulseLengthPercent;
+	}
 
-    @Override
-    public double getTimeDelta() {
-	return timeDelta;
-    }
+	@Override
+	public void setTimeDelta(double seconds) {
+		this.timeDelta = seconds;
+	}
 
-    public double getPulseLengthPercent() {
-	return pulseLengthPercent;
-    }
+	@Override
+	public double getNextValue() {
+		currentTime += this.timeDelta;
+		if (currentTime > period) {
+			currentTime -= period;
+		}
 
-    public double getFrequency() {
-	return frequency;
-    }
+		return (currentTime > (period - pulseLength)) ? 1. : 0.;
+	}
+
+	@Override
+	public double getTimeDelta() {
+		return timeDelta;
+	}
+
+	public double getPulseLengthPercent() {
+		return pulseLengthPercent;
+	}
+
+	public double getFrequency() {
+		return frequency;
+	}
 
 }
