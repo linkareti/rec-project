25dd6c762168f3cf27767fd6b9ac478560d844a1
handyman <handyman@linkare.com>
2011-09-19 00:16:31 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-562.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@5327 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/common/com/linkare/rec/impl/data/SamplesReadException.java
@@ -0,0 +1,47 @@
+package com.linkare.rec.impl.data;
+
+/**
+ * This exception is thrown whenever there is a problem reading data samples
+ * from a {@link SamplesSource}
+ * 
+ * @author José Pedro Pereira
+ */
+public class SamplesReadException extends java.io.IOException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8422668385063715056L;
+	/**
+	 * The number of the sample data index that caused the error
+	 */
+	private int errorSampleNumber = 0;
+
+	/**
+	 * Creates a new instance of <code>PacketReadException</code> without detail
+	 * message.
+	 * @param e 
+	 * @param errorSampleNumber 
+	 */
+	public SamplesReadException(final java.io.IOException e, final int errorSampleNumber) {
+		super(e.getMessage());
+		setStackTrace(e.getStackTrace());
+		this.errorSampleNumber = errorSampleNumber;
+	}
+
+	public SamplesReadException(final String message, final int errorSampleNumber) {
+		super(message);
+		this.errorSampleNumber = errorSampleNumber;
+	}
+
+	/**
+	 * Getter for property errorSampleNumber.
+	 * 
+	 * @return Value of property errorSampleNumber.
+	 * 
+	 */
+	public int getErrorSampleNumber() {
+		return errorSampleNumber;
+	}
+
+}
