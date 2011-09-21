f0a648554219487a41a937566a00027d35a74110
handyman <handyman@linkare.com>
2011-09-21 00:17:54 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-564.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@5335 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.am/src/main/java/com/linkare/rec/am/util/LoggerSingleton.java
@@ -0,0 +1,17 @@
+package com.linkare.rec.am.util;
+
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ * 
+ */
+public class LoggerSingleton implements Serializable {
+
+    public static final Logger LOGGER = Logger.getLogger("LoggerSingleton");
+
+    private LoggerSingleton() {
+    }
+}
