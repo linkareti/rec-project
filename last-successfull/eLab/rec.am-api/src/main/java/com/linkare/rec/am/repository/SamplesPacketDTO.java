2505cd7c4c3774b3a70a456250a9191487150d6a
handyman <handyman@linkare.com>
2011-09-28 00:14:33 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-571.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@5363 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.am-api/src/main/java/com/linkare/rec/am/repository/SamplesPacketDTO.java
@@ -0,0 +1,76 @@
+package com.linkare.rec.am.repository;
+
+import java.util.List;
+
+/**
+ * 
+ * 
+ * @author Artur Correia - Linkare TI
+ * 
+ */
+public class SamplesPacketDTO extends AbstractBaseDTO {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    private int packetNumber;
+
+    private int totalPackets;
+
+    private List<ColumnPhysicsValueDTO> data;
+
+    private DateTimeDTO timeStart = null;
+
+    public SamplesPacketDTO() {
+    }
+
+    public int getPacketNumber() {
+	return packetNumber;
+    }
+
+    public void setPacketNumber(int packetNumber) {
+	this.packetNumber = packetNumber;
+    }
+
+    public int getTotalPackets() {
+	return totalPackets;
+    }
+
+    public void setTotalPackets(int totalPackets) {
+	this.totalPackets = totalPackets;
+    }
+
+    public List<ColumnPhysicsValueDTO> getData() {
+	return data;
+    }
+
+    public void setData(List<ColumnPhysicsValueDTO> data) {
+	this.data = data;
+    }
+
+    public DateTimeDTO getTimeStart() {
+	return timeStart;
+    }
+
+    public void setTimeStart(DateTimeDTO timeStart) {
+	this.timeStart = timeStart;
+    }
+
+    @Override
+    public String toString() {
+	StringBuilder builder = new StringBuilder();
+	builder.append("SamplesPacketDTO [packetNumber=");
+	builder.append(packetNumber);
+	builder.append(", totalPackets=");
+	builder.append(totalPackets);
+	builder.append(", data=");
+	builder.append(data);
+	builder.append(", timeStart=");
+	builder.append(timeStart);
+	builder.append("]");
+	return builder.toString();
+    }
+
+}
