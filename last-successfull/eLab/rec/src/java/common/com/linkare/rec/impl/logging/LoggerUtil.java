938b4eb53ee25d3dc21739610f0af31e2d953ca3
handyman <handyman@linkare.com>
2011-10-05 00:17:20 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-578.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@5396 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/common/com/linkare/rec/impl/logging/LoggerUtil.java
@@ -0,0 +1,44 @@
+package com.linkare.rec.impl.logging;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * A simple utility class to help in
+ * 
+ * @author José Pedro Pereira
+ */
+public class LoggerUtil {
+
+	/**
+	 * This utility method writes a logging entry in SEVERE mode to the logger
+	 * and logs also the full stack trace and info message in FINEST level if
+	 * the logger is currenly configured to get that level of detail
+	 * 
+	 * The warning message printed also includes information at the last class,
+	 * method and line where the error really occurred
+	 * 
+	 * @param info_message An additional message to be presented
+	 * @param t The {@link Throwable} to be logged
+	 * @param logger The {@link Logger} on which to log the exception and
+	 *            message
+	 */
+	public static void logThrowable(String info_message, final Throwable t, final Logger logger) {
+		if (info_message != null && logger != null && logger.isLoggable(Level.SEVERE)) {
+			if (t != null) {
+				final StackTraceElement[] trace = t.getStackTrace();
+				if (trace != null && trace.length > 0) {
+					info_message = " @class " + trace[0].getClassName() + " ,@method " + trace[0].getMethodName()
+							+ " ,@line " + trace[0].getLineNumber() + " " + info_message;
+				}
+			}
+
+			logger.log(Level.SEVERE, info_message);
+		}
+
+		if (t != null && logger != null && logger.isLoggable(Level.FINEST)) {
+			logger.log(Level.FINEST, info_message, t);
+		}
+	}
+
+}
