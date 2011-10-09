943360329e34c761e863c5d64c5bda638085a098
handyman <handyman@linkare.com>
2011-10-09 00:24:56 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-582.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@5413 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/client/com/linkare/customizer/JPropertyObjectButton.java
@@ -0,0 +1,144 @@
+package com.linkare.customizer;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * 
+ * @author jp
+ */
+public class JPropertyObjectButton extends JButton {
+	static final long serialVersionUID = 6053878914227672535L;
+	PropertyObject value = null;
+
+	private boolean isSelected = false;
+	private boolean hasFocus = false;
+
+	protected static Border noFocusBorder = new EmptyBorder(1, 1, 1, 1);
+
+	// We need a place to store the color the JButton should be returned
+	// to after its foreground and background colors have been set
+	// to the selection background color.
+	// These ivars will be made protected when their names are finalized.
+	private Color unselectedForeground;
+	private Color unselectedBackground;
+	private final JTable table;
+
+	/**
+	 * Creates a new instance of JIndexedPropertyObjectButton
+	 * 
+	 * @param value The object (cell content) under editing
+	 * @param isSelected was this object select
+	 * @param hasFocus Does it have the focus
+	 * @param table What table is it being shown on
+	 */
+	public JPropertyObjectButton(final PropertyObject value, final boolean isSelected, final boolean hasFocus,
+			final JTable table) {
+		this.table = table;
+		this.isSelected = isSelected;
+		this.hasFocus = hasFocus;
+		this.value = value;
+		setHorizontalAlignment(SwingConstants.LEFT);
+		setMargin(new java.awt.Insets(0, 0, 0, 0));
+		setOpaque(true);
+		setBorder(JPropertyObjectButton.noFocusBorder);
+	}
+
+	/**
+	 * Overrides <code>JComponent.setForeground</code> to assign the
+	 * unselected-foreground color to the specified color.
+	 * 
+	 * @param c set the foreground color to this value
+	 */
+	@Override
+	public void setForeground(final Color c) {
+		super.setForeground(c);
+		unselectedForeground = c;
+	}
+
+	/**
+	 * Overrides <code>JComponent.setBackground</code> to assign the
+	 * unselected-background color to the specified color.
+	 * 
+	 * @param c set the background color to this value
+	 */
+	@Override
+	public void setBackground(final Color c) {
+		super.setBackground(c);
+		unselectedBackground = c;
+	}
+
+	/**
+	 * Notification from the <code>UIManager</code> that the look and feel [L&F]
+	 * has changed. Replaces the current UI object with the latest version from
+	 * the <code>UIManager</code>.
+	 * 
+	 * @see JComponent#updateUI
+	 */
+	@Override
+	public void updateUI() {
+		super.updateUI();
+		setForeground(null);
+		setBackground(null);
+	}
+
+	@Override
+	protected void paintComponent(final Graphics g) {
+		if (isSelected) {
+			super.setForeground(table.getSelectionForeground());
+			super.setBackground(table.getSelectionBackground());
+		} else {
+			super.setForeground((unselectedForeground != null) ? unselectedForeground : table.getForeground());
+			super.setBackground((unselectedBackground != null) ? unselectedBackground : table.getBackground());
+		}
+
+		setFont(table.getFont());
+
+		if (hasFocus) {
+			setBorder(UIManager.getBorder("Table.focusCellHighlightBorder"));
+			if (value.getPropertyEditor() != null) {
+				super.setForeground(UIManager.getColor("Table.focusCellForeground"));
+				super.setBackground(UIManager.getColor("Table.focusCellBackground"));
+			}
+		} else {
+			if (isSelected) {
+				super.setForeground(table.getSelectionForeground());
+				super.setBackground(table.getSelectionBackground());
+			} else {
+				super.setForeground((unselectedForeground != null) ? unselectedForeground : table.getForeground());
+				super.setBackground((unselectedBackground != null) ? unselectedBackground : table.getBackground());
+			}
+
+			setBorder(JPropertyObjectButton.noFocusBorder);
+		}
+
+		if (value.getPropertyEditor() != null) {
+			value.getPropertyEditor().setValue(value.getValue());
+			if (value.getPropertyEditor().isPaintable()) {
+				setText("a");
+				super.paintComponent(g);
+				final Rectangle draw = new Rectangle(g.getClipBounds().x + 2, g.getClipBounds().y + 2,
+						g.getClipBounds().width - 4, g.getClipBounds().height - 4);
+				value.getPropertyEditor().paintValue(g, draw);
+			} else if (value.getPropertyEditor().getAsText() != null) {
+				setText(value.getPropertyEditor().getAsText());
+				super.paintComponent(g);
+			}
+
+		} else {
+			setText("null");
+			super.paintComponent(g);
+		}
+
+	}
+
+}
