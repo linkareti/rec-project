1af254f6e50c27e40be99b7f8e175c3f72f7a3da
handyman <handyman@linkare.com>
2011-10-11 00:19:15 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-584.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@5421 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/vquantum/src/java/server/pt/utl/ist/elab/driver/vquantum/ServerMain.java
@@ -0,0 +1,46 @@
+/*
+ * ServerMain.java
+ *
+ * Created on 10 de Abril de 2003, 20:07
+ */
+
+package pt.utl.ist.elab.driver.vquantum;
+
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import com.linkare.rec.impl.driver.BaseHardware;
+import com.linkare.rec.impl.logging.LoggerUtil;
+import com.linkare.rec.impl.utils.ORBBean;
+
+/**
+ * 
+ * @author nomead
+ */
+public class ServerMain {
+
+	private static String Quantum_HARDWARE_LOGGER = "Quantum.Logger";
+	static {
+		final Logger l = LogManager.getLogManager().getLogger(ServerMain.Quantum_HARDWARE_LOGGER);
+		if (l == null) {
+			LogManager.getLogManager().addLogger(Logger.getLogger(ServerMain.Quantum_HARDWARE_LOGGER));
+		}
+	}
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(final String[] args) {
+		try {
+			ORBBean.getORBBean();
+
+			final BaseHardware baseHardware = new BaseHardware(new QuantumDriver());
+
+			Thread.currentThread().join();
+
+		} catch (final Exception e) {
+			ORBBean.getORBBean().killORB();
+			LoggerUtil.logThrowable("Error on Main...", e, Logger.getLogger(ServerMain.Quantum_HARDWARE_LOGGER));
+		}
+	}
+}
