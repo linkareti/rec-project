9a15ce245f99b261270bd562d5c86b41a1dfe42b
handyman <handyman@linkare.com>
2011-10-11 00:09:44 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-674.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@5419 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/ui/com/linkare/rec/impl/newface/config/LocalizationBundle.java
@@ -0,0 +1,73 @@
+package com.linkare.rec.impl.newface.config;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType
+public class LocalizationBundle extends AbstractConfigBean {
+
+	private String location;
+
+	private String name;
+
+	public LocalizationBundle() {
+		super();
+	}
+
+	/**
+	 * @return the location
+	 */
+	@XmlAttribute
+	public java.lang.String getLocation() {
+		return location;
+	}
+
+	/**
+	 * @return the name
+	 */
+	@XmlAttribute
+	public java.lang.String getName() {
+		return name;
+	}
+
+	/**
+	 * @param location the location to set
+	 */
+	public void setLocation(final java.lang.String location) {
+		this.location = location;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(final java.lang.String name) {
+		this.name = name;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final LocalizationBundle other = (LocalizationBundle) obj;
+		if ((location == null) ? (other.location != null) : !location.equals(other.location)) {
+			return false;
+		}
+		if ((name == null) ? (other.name != null) : !name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 5;
+		hash = 97 * hash + (location != null ? location.hashCode() : 0);
+		hash = 97 * hash + (name != null ? name.hashCode() : 0);
+		return hash;
+	}
+
+}
