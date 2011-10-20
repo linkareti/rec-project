d587a31086aca5b5e70d9f6b8b45a908ebd479db
handyman <handyman@linkare.com>
2011-10-20 00:09:34 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-683.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@5458 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/common/com/linkare/rec/impl/threading/factory/RecThreadFactory.java
@@ -0,0 +1,38 @@
+package com.linkare.rec.impl.threading.factory;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 
+ * 
+ * @author Artur Correia - Linkare TI
+ */
+public class RecThreadFactory implements ThreadFactory {
+	static final AtomicInteger poolNumber = new AtomicInteger(1);
+	final ThreadGroup group;
+	final AtomicInteger threadNumber = new AtomicInteger(1);
+	final String namePrefix;
+
+	public RecThreadFactory(final String prefix) {
+		final SecurityManager s = System.getSecurityManager();
+		group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+		namePrefix = new StringBuilder("RecPool-").append(RecThreadFactory.poolNumber.getAndIncrement()).append("-")
+				.append(prefix).append("-").toString();
+	}
+
+	@Override
+	public Thread newThread(final Runnable r) {
+		final Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
+		if (t.isDaemon()) {
+			t.setDaemon(true);
+		}
+
+		if (t.getPriority() != Thread.NORM_PRIORITY) {
+			t.setPriority(Thread.NORM_PRIORITY);
+		}
+
+		return t;
+	}
+
+}
