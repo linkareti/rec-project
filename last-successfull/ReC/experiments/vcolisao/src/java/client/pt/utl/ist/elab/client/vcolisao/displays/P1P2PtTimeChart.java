23f9cdcd43c7fd92d2b2e4c5131b53329be438b3
handyman <handyman@linkare.com>
2011-10-31 01:08:34 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-700.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@5494 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/vcolisao/src/java/client/pt/utl/ist/elab/client/vcolisao/displays/P1P2PtTimeChart.java
@@ -0,0 +1,27 @@
+/*
+ * AnguloIntensidadeChart.java
+ *
+ * Created on October 15, 2004, 2:06 PM
+ */
+
+package pt.utl.ist.elab.client.vcolisao.displays;
+
+/**
+ * 
+ * @author Emanuel A.
+ */
+public class P1P2PtTimeChart extends com.linkare.rec.impl.baseUI.graph.MultSeriesXYExperimentGraph {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7028830242719177308L;
+
+	/** Creates a new instance of AnguloIntensidadeChart */
+	public P1P2PtTimeChart() {
+		super();
+		setChannelDisplayX(0);
+		setChannelDisplayYArray(new int[] { 5, 6, 7, 8, 9, 10 });
+	}
+
+}
