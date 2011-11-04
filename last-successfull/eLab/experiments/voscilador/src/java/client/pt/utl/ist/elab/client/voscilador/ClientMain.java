e7520cd87f537d510f1a6f302b29453a84fc11a7
handyman <handyman@linkare.com>
2011-11-04 01:14:43 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-606.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@5512 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/voscilador/src/java/client/pt/utl/ist/elab/client/voscilador/ClientMain.java
@@ -0,0 +1,30 @@
+/*
+ * ClientMain.java
+ *
+ * Created on 29 de Mar�o de 2005, 1:15
+ */
+
+package pt.utl.ist.elab.client.voscilador;
+
+import javax.swing.JFrame;
+
+import com.linkare.rec.impl.baseUI.ReCBaseUI;
+
+/**
+ * 
+ * @author RF
+ */
+public class ClientMain {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(final String[] args) {
+		// Este código é sempre igual, só altera o nome, ou melhor, o ID da exp,
+		// que neste caso vai ser MASSA_MOLA_V1.0
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		final ReCBaseUI ui = new ReCBaseUI();
+		ui.setApparatusAutoConnectID("OSCILADOR_V1.0");
+		ui.setVisible(true);
+	}
+}
