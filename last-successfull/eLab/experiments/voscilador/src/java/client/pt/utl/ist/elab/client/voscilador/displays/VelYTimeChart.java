269beb979caaa3ed7c507b40970697c2a8d7bdd7
handyman <handyman@linkare.com>
2011-11-10 01:15:54 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-612.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@5536 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/voscilador/src/java/client/pt/utl/ist/elab/client/voscilador/displays/VelYTimeChart.java
@@ -0,0 +1,27 @@
+/*
+ * AnguloIntensidadeChart.java
+ *
+ * Created on October 15, 2004, 2:06 PM
+ */
+
+package pt.utl.ist.elab.client.voscilador.displays;
+
+/**
+ * 
+ * @author andre
+ */
+public class VelYTimeChart extends com.linkare.rec.impl.baseUI.graph.DefaultXYExperimentGraph {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1973620374198272996L;
+
+	/** Creates a new instance of AnguloIntensidadeChart */
+	public VelYTimeChart() {
+		super();
+		setChannelDisplayX(0);
+		setChannelDisplayY(6);
+	}
+
+}
