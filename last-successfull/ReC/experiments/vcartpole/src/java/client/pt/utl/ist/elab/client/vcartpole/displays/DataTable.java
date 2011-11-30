00666e43c89a5c82addce978c8d2bff0d3b5177a
handyman <handyman@linkare.com>
2011-11-30 01:08:58 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-731.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@5625 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/vcartpole/src/java/client/pt/utl/ist/elab/client/vcartpole/displays/DataTable.java
@@ -0,0 +1,27 @@
+/*
+ * DataTable.java
+ *
+ * Created on 2 de Dezembro de 2004, 2:38
+ */
+
+package pt.utl.ist.elab.client.vcartpole.displays;
+
+import com.linkare.rec.impl.baseUI.table.MultSeriesTable;
+
+/**
+ * 
+ * @author nomead
+ */
+public class DataTable extends MultSeriesTable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6569853184505473937L;
+
+	/** Creates a new instance of DataTable */
+	public DataTable() {
+		super();
+		setColArray(new int[] { 0, 2, 1, 3, 4, 5 });
+	}
+}
