24316b31f4dbc6e0e209bdcdb5a5d831378ef803
handyman <handyman@linkare.com>
2011-12-03 01:09:23 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-734.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@5639 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/ui/com/linkare/rec/impl/baseUI/config/BooleanXMLParser.java
@@ -0,0 +1,31 @@
+/*
+ * BooleanXMLParser.java
+ *
+ * Created on 27 de Janeiro de 2004, 2:24
+ */
+
+package com.linkare.rec.impl.baseUI.config;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public class BooleanXMLParser {
+
+	public static boolean parseBoolean(final String value) {
+		if (value == null) {
+			return false;
+		}
+
+		if (value.equalsIgnoreCase("true") || value.equalsIgnoreCase("1") || value.equalsIgnoreCase("on")
+				|| value.equalsIgnoreCase("yes")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static String toString(final boolean value) {
+		return value ? "true" : "false";
+	}
+}
