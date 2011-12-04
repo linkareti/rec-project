c245fba6a7692d15fdf9eed0e1193513d42d50ed
handyman <handyman@linkare.com>
2011-12-04 01:15:53 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-636.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@5647 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/common/com/linkare/rec/impl/mbean/MBeanObjectNameFactory.java
@@ -0,0 +1,62 @@
+/* 
+ * MBeanObjectNameFactory.java created on Mar 22, 2011
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.linkare.rec.impl.mbean;
+
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+
+/**
+ * 
+ * @author Artur Correia - Linkare TI
+ */
+public class MBeanObjectNameFactory {
+
+	private enum DomainsEnum {
+		DEFAULT_DOMAIN("com.linkare.rec");
+
+		private final String domain;
+
+		private DomainsEnum(final String domain) {
+			this.domain = domain;
+		}
+
+		public String getDomain() {
+			return domain + ":";
+		}
+	}
+
+	/**
+	 * Single point to define MBean names
+	 * 
+	 * @author Artur Correia - Linkare TI
+	 */
+	private enum MBeansNameEnum {
+		THREADPOOLEXECUTORSTATISTICS("ThreadPoolExecutorStatistics");
+
+		private static final String NAME_ATTRIBUTE = "name";
+
+		private final String mbeanName;
+
+		private MBeansNameEnum(final String mbeanName) {
+			this.mbeanName = new StringBuilder(MBeansNameEnum.NAME_ATTRIBUTE).append("=").append(mbeanName).toString();
+		}
+
+		public String getMBeanName() {
+			return mbeanName;
+		}
+	}
+
+	public static ObjectName getThreadPoolExecutorStatisticsObjectName() {
+		try {
+			return new ObjectName(DomainsEnum.DEFAULT_DOMAIN.getDomain()
+					+ MBeansNameEnum.THREADPOOLEXECUTORSTATISTICS.getMBeanName());
+		} catch (final MalformedObjectNameException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
