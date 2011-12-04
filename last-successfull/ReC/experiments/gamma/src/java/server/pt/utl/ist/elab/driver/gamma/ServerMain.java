b340abbdffb70b70b66e3cc92a6068e6cf2845ab
handyman <handyman@linkare.com>
2011-12-04 01:08:52 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-735.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@5645 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/gamma/src/java/server/pt/utl/ist/elab/driver/gamma/ServerMain.java
@@ -0,0 +1,51 @@
+/*
+ * HardwareServerMain.java
+ *
+ * Created on 26 de Junho de 2002, 16:44
+ */
+
+package pt.utl.ist.elab.driver.gamma;
+
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import com.linkare.rec.impl.driver.BaseHardware;
+import com.linkare.rec.impl.logging.LoggerUtil;
+import com.linkare.rec.impl.utils.ORBBean;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public class ServerMain {
+
+	private static String GAMMA_HARDWARE_LOGGER = "GammaHardware.Logger";
+
+	static {
+		final Logger l = LogManager.getLogManager().getLogger(ServerMain.GAMMA_HARDWARE_LOGGER);
+		if (l == null) {
+			LogManager.getLogManager().addLogger(Logger.getLogger(ServerMain.GAMMA_HARDWARE_LOGGER));
+		}
+	}
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(final String[] args) {
+		try {
+			ORBBean.getORBBean();
+
+			final BaseHardware baseHardware = new BaseHardware(new GammaStampDriver());
+
+			try {
+				Thread.currentThread().join();
+			} catch (final Exception ignored) {
+			}
+
+			ORBBean.getORBBean().killORB();
+		} catch (final Exception e) {
+			LoggerUtil.logThrowable("Error on Main...", e, Logger.getLogger(ServerMain.GAMMA_HARDWARE_LOGGER));
+		}
+	}
+
+}
