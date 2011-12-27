600bd62c81de46f876452b2920a4e592a521d7ea
handyman <handyman@linkare.com>
2011-12-27 01:19:06 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-659.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@5750 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.am/src/main/java/com/linkare/rec/am/service/LaboratoryServiceBean.java
@@ -0,0 +1,57 @@
+package com.linkare.rec.am.service;
+
+import static com.linkare.rec.am.model.Laboratory.COUNT_ALL_QUERYNAME;
+import static com.linkare.rec.am.model.Laboratory.FIND_ALL_QUERYNAME;
+
+import java.util.List;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+import javax.persistence.Query;
+
+import com.linkare.rec.am.model.Laboratory;
+
+/**
+ * 
+ * @author Joao
+ */
+@Local(LaboratoryServiceLocal.class)
+@Stateless(name = "LaboratoryService")
+public class LaboratoryServiceBean extends BusinessServiceBean<Laboratory, Long> implements LaboratoryService {
+
+    @Override
+    public void create(final Laboratory laboratory) {
+	getEntityManager().persist(laboratory);
+    }
+
+    @Override
+    public Laboratory find(final Long id) {
+	return getEntityManager().find(Laboratory.class, id);
+    }
+
+    @Override
+    public List<Laboratory> findRange(final int[] range) {
+	return find(false, range[0], range[1]);
+    }
+
+    @Override
+    public List<Laboratory> findAll() {
+	return find(true, -1, -1);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Laboratory> find(final boolean all, final int firstResult, final int maxResults) {
+	Query q = getEntityManager().createNamedQuery(FIND_ALL_QUERYNAME);
+	if (!all) {
+	    q.setMaxResults(maxResults);
+	    q.setFirstResult(firstResult);
+	}
+	return q.getResultList();
+    }
+
+    @Override
+    public int count() {
+	final Query query = getEntityManager().createNamedQuery(COUNT_ALL_QUERYNAME);
+	return ((Long) query.getSingleResult()).intValue();
+    }
+}
