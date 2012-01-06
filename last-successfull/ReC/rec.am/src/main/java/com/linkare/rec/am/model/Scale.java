464cc9ddbfe33fb233e05517fec1c25357317f89
handyman <handyman@linkare.com>
2012-01-06 01:09:07 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-768.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@5792 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.am/src/main/java/com/linkare/rec/am/model/Scale.java
@@ -0,0 +1,120 @@
+package com.linkare.rec.am.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.linkare.commons.jpa.DefaultDomainObject;
+import com.linkare.rec.am.repository.MultiplierEnum;
+
+/**
+ * 
+ * @author Artur Correia - Linkare TI
+ */
+@Entity
+@Table(name = "SCALE")
+public class Scale extends DefaultDomainObject {
+
+    private static final long serialVersionUID = 1L;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "APPLIED_MULTIPLIER")
+    private MultiplierEnum multiplier;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "KEY_PHYSICS_VAL_MINVALUE")
+    private PhysicsVal minValue;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "KEY_PHYSICS_VAL_MAXVALUE")
+    private PhysicsVal maxValue;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "KEY_PHYSICS_VAL_DEFAULTERROR")
+    private PhysicsVal defaultError;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "KEY_PHYSICS_VAL_STEP")
+    private PhysicsVal step;
+
+    @Column(name = "PHYSICS_UNIT_NAME")
+    private String physicsUnitName;
+
+    @Column(name = "PHYSICS_UNIT_SYMBOL")
+    private String physicsUnitSymbol;
+
+    @Column(name = "SCALE_LABEL")
+    private String scaleLabel;
+
+    public Scale() {
+    }
+
+    public MultiplierEnum getMultiplier() {
+	return multiplier;
+    }
+
+    public void setMultiplier(MultiplierEnum appliedMultiplier) {
+	this.multiplier = appliedMultiplier;
+    }
+
+    public PhysicsVal getMinValue() {
+	return minValue;
+    }
+
+    public void setMinValue(PhysicsVal minValue) {
+	this.minValue = minValue;
+    }
+
+    public PhysicsVal getMaxValue() {
+	return maxValue;
+    }
+
+    public void setMaxValue(PhysicsVal maxValue) {
+	this.maxValue = maxValue;
+    }
+
+    public PhysicsVal getDefaultError() {
+	return defaultError;
+    }
+
+    public void setDefaultError(PhysicsVal defaultError) {
+	this.defaultError = defaultError;
+    }
+
+    public PhysicsVal getStep() {
+	return step;
+    }
+
+    public void setStep(PhysicsVal step) {
+	this.step = step;
+    }
+
+    public String getPhysicsUnitName() {
+	return physicsUnitName;
+    }
+
+    public void setPhysicsUnitName(String physicsUnitName) {
+	this.physicsUnitName = physicsUnitName;
+    }
+
+    public String getPhysicsUnitSymbol() {
+	return physicsUnitSymbol;
+    }
+
+    public void setPhysicsUnitSymbol(String physicsUnitSymbol) {
+	this.physicsUnitSymbol = physicsUnitSymbol;
+    }
+
+    public String getScaleLabel() {
+	return scaleLabel;
+    }
+
+    public void setScaleLabel(String scaleLabel) {
+	this.scaleLabel = scaleLabel;
+    }
+}
