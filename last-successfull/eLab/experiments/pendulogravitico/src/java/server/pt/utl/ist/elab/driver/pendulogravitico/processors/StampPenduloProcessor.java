c21d7d0e59f9e8952a3a19ee605616626dd6f3d9
handyman <handyman@linkare.com>
2012-01-10 01:21:41 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-673.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@5814 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/pendulogravitico/src/java/server/pt/utl/ist/elab/driver/pendulogravitico/processors/StampPenduloProcessor.java
@@ -0,0 +1,62 @@
+/*
+ * StampHelloProcessor.java
+ *
+ * Created on 12 de Novembro de 2002, 16:19
+ */
+
+package pt.utl.ist.elab.driver.pendulogravitico.processors;
+
+import pt.utl.ist.elab.driver.serial.stamp.transproc.AbstractStampProcessor;
+import pt.utl.ist.elab.driver.serial.stamp.transproc.StampCommand;
+
+/**
+ * 
+ * @author bruno
+ */
+public class StampPenduloProcessor extends AbstractStampProcessor {
+	public static final String COMMAND_IDENTIFIER = "D";
+	public static final String ANGLE_ADC = "Angulo_ADC";
+
+	/** Creates a new instance of StampHelloProcessor */
+	public StampPenduloProcessor() {
+		super(StampPenduloProcessor.COMMAND_IDENTIFIER);
+	}
+
+	/**
+	 * This method makes the Processor process the command passed in...
+	 * 
+	 * @param command the command that this processor will have to process
+	 * @return boolean - wether the processing was successfull
+	 * 
+	 */
+	@Override
+	public boolean process(final StampCommand command) {
+		float angle = 0;
+		final String[] splitedStr = command.getCommand().split(" ");
+
+		if (command.getCommandIdentifier().equalsIgnoreCase(StampPenduloProcessor.COMMAND_IDENTIFIER)
+				&& splitedStr[0] != null) {
+			try {
+				angle = Float.parseFloat(splitedStr[0]);
+				final Float oangle = new Float(angle);
+				command.addCommandData(StampPenduloProcessor.ANGLE_ADC, oangle);
+
+				command.setData(true);
+
+				return true;
+
+			} catch (final NumberFormatException e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean isData() {
+		return true;
+	}
+
+}
