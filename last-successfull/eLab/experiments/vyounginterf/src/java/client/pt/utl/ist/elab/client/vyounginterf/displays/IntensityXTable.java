eccd585af9a96d0dcbd9cb6b1ad1de20d17106c1
handyman <handyman@linkare.com>
2012-01-11 01:18:18 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-674.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@5821 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/vyounginterf/src/java/client/pt/utl/ist/elab/client/vyounginterf/displays/IntensityXTable.java
@@ -0,0 +1,27 @@
+/*
+ * ItensityXTable.java
+ *
+ * Created on ????
+ */
+
+package pt.utl.ist.elab.client.vyounginterf.displays;
+
+import com.linkare.rec.impl.baseUI.table.MultSeriesTable;
+
+/**
+ * 
+ * @author Emanuel Antunes
+ */
+public class IntensityXTable extends MultSeriesTable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7456068659800144418L;
+
+	/** Creates a new instance of ItensityXTable */
+	public IntensityXTable() {
+		super();
+		setColArray(new int[] { 0, 1 });
+	}
+}
