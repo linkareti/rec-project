ab3d99219e9baeb45cae9c54b3d69a9dd9d2b463
handyman <handyman@linkare.com>
2012-01-14 01:20:25 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-677.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@5839 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/repository/com/linkare/rec/export/domain/enums/EnumPhysicsValueType.java
@@ -0,0 +1,17 @@
+package com.linkare.rec.export.domain.enums;
+
+/**
+ *
+ * @author artur
+ */
+public enum EnumPhysicsValueType {
+
+    BOOLEAN_VAL,
+    BYTE_VAL,
+    SHORT_VAL,
+    INT_VAL,
+    LONG_VAL,
+    FLOAT_VAL,
+    DOUBLE_VAL,
+    BYTEARRAY_VAL
+}
