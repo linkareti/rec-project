1ff0ead2e9a650bdb7c86dfc177e0e50e492644e
handyman <handyman@linkare.com>
2012-01-24 01:23:42 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-687.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@5897 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.am/src/main/java/com/linkare/rec/am/model/util/converter/PhysicsValFactory.java
@@ -0,0 +1,78 @@
+package com.linkare.rec.am.model.util.converter;
+
+import org.dozer.BeanFactory;
+
+import com.linkare.rec.am.model.BooleanVal;
+import com.linkare.rec.am.model.ByteArrayVal;
+import com.linkare.rec.am.model.ByteArrayValue;
+import com.linkare.rec.am.model.ByteVal;
+import com.linkare.rec.am.model.DoubleVal;
+import com.linkare.rec.am.model.FloatVal;
+import com.linkare.rec.am.model.IntVal;
+import com.linkare.rec.am.model.LongVal;
+import com.linkare.rec.am.model.ShortVal;
+import com.linkare.rec.am.repository.PhysicsValDTO;
+
+public final class PhysicsValFactory implements BeanFactory {
+
+    @Override
+    public Object createBean(Object source, Class<?> sourceClass, String targetBeanId) {
+	if (source != null) {
+
+	    if (!(sourceClass == PhysicsValDTO.class)) {
+		throw new IllegalArgumentException("invalid sourceClass");
+	    }
+
+	    final PhysicsValDTO dto = (PhysicsValDTO) source;
+
+	    switch (dto.getValueType()) {
+
+	    case BOOLEAN_VAL: {
+		final BooleanVal result = new BooleanVal();
+		result.setValue((Boolean) dto.getValue());
+		return result;
+	    }
+	    case BYTE_VAL: {
+		final ByteVal result = new ByteVal();
+		result.setValue((Byte) dto.getValue());
+		return result;
+	    }
+	    case SHORT_VAL: {
+		final ShortVal result = new ShortVal();
+		result.setValue((Short) dto.getValue());
+		return result;
+	    }
+	    case INT_VAL: {
+		final IntVal result = new IntVal();
+		result.setValue((Integer) dto.getValue());
+		return result;
+	    }
+	    case LONG_VAL: {
+		final LongVal result = new LongVal();
+		result.setValue((Long) dto.getValue());
+		return result;
+	    }
+	    case FLOAT_VAL: {
+		final FloatVal result = new FloatVal();
+		result.setValue((Float) dto.getValue());
+		return result;
+	    }
+	    case DOUBLE_VAL: {
+		final DoubleVal result = new DoubleVal();
+		result.setValue((Double) dto.getValue());
+		return result;
+	    }
+	    case BYTEARRAY_VAL: {
+		final ByteArrayVal result = new ByteArrayVal();
+		result.setValue(DozerBeanMapperSingletonWrapper.getInstance().map(dto.getValue(), ByteArrayValue.class));
+		return result;
+	    }
+	    default:
+		throw new IllegalArgumentException("invalida valueType");
+	    }
+
+	} else {
+	    return null;
+	}
+    }
+}
---
+++ last-successfull/eLab/rec/src/java/common/com/linkare/rec/jmf/media/datasink/capture/ChannelDataFrame.java
@@ -0,0 +1,58 @@
+package com.linkare.rec.jmf.media.datasink.capture;
+
+import javax.media.format.AudioFormat;
+
+public class ChannelDataFrame {
+
+    private double[][] channelsData;
+    private double[] channelsVRMS;
+    private AudioFormat captureFormat;
+
+    public ChannelDataFrame(double[][] channelsData, double[] channelsVRMS, AudioFormat captureFormat) {
+        this.channelsData = channelsData.clone();
+        this.channelsVRMS = channelsVRMS.clone();
+        this.captureFormat = captureFormat;
+    }
+
+    public double[][] getChannelsData() {
+        return channelsData;
+    }
+
+    public void setChannelsData(double[][] channelsData) {
+        this.channelsData = channelsData.clone();
+    }
+
+    public double[] getChannelsVRMS() {
+        return channelsVRMS;
+    }
+
+    public void setChannelsVRMS(double[] channelsVRMS) {
+        this.channelsVRMS = channelsVRMS.clone();
+    }
+
+    public int getNumChannels() {
+        return channelsData.length;
+    }
+
+    public double[] getChannelData(int channel) {
+        return channelsData[channel];
+    }
+
+    public double getChannelVRMS(int channel) {
+        return channelsVRMS[channel];
+    }
+
+    /**
+     * @return the captureFormat
+     */
+    public AudioFormat getCaptureFormat() {
+        return captureFormat;
+    }
+
+    /**
+     * @param captureFormat the captureFormat to set
+     */
+    public void setCaptureFormat(AudioFormat captureFormat) {
+        this.captureFormat = captureFormat;
+    }
+}
