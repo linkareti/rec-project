8455a561ad28e87a64365ba2dbed4608fac6e03a
respiritosanto <respiritosanto@linkare.com>
2012-01-31 15:47:04 +0000

    bug 1304: marked the command as data
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/trunk@5961 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- experiments/condensadorcilindrico/src/java/server/pt/utl/ist/elab/driver/condensadorcilindrico/CCStampDataSource.java
+++ experiments/condensadorcilindrico/src/java/server/pt/utl/ist/elab/driver/condensadorcilindrico/CCStampDataSource.java
@@ -3,7 +3,6 @@
  *
  * Created on 15 de Maio de 2003, 19:38
  */
-
 package pt.utl.ist.elab.driver.condensadorcilindrico;
 
 import pt.utl.ist.elab.driver.condensadorcilindrico.processors.StampCCProcessor;
@@ -11,8 +10,10 @@ import pt.utl.ist.elab.driver.serial.stamp.AbstractStampDataSource;
 import pt.utl.ist.elab.driver.serial.stamp.transproc.StampCommand;
 
 import com.linkare.rec.data.acquisition.PhysicsValue;
+import com.linkare.rec.data.config.ChannelAcquisitionConfig;
 import com.linkare.rec.data.config.HardwareAcquisitionConfig;
 import com.linkare.rec.impl.data.PhysicsValFactory;
+import com.linkare.rec.impl.i18n.ReCResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,67 +23,75 @@ import java.util.logging.Logger;
  */
 public class CCStampDataSource extends AbstractStampDataSource {
 
-	private int counter = 0;
-	private int total_samples = 0;
+    private int counter = 0;
+    private int total_samples = 0;
     private static final Logger LOGGER = Logger.getLogger(CCStampDataSource.class.getName());
 
-	public CCStampDataSource() {
-	}
+    public CCStampDataSource() {
+    }
 
-	@Override
-	public void processDataCommand(final StampCommand cmd) {
-		if (cmd == null || !cmd.isData() || cmd.getCommandIdentifier() == null) {
+    @Override
+    public void processDataCommand(final StampCommand cmd) {
+        if (cmd == null || !cmd.isData() || cmd.getCommandIdentifier() == null) {
             LOGGER.finest("discarting a non data or null command");
-			return;
-		}
+            return;
+        }
 
-		if (cmd.getCommandIdentifier().equals(StampCCProcessor.COMMAND_IDENTIFIER)) {
-                        
-			final Float capacity;
-			final Float distance;
-			final PhysicsValue[] values = new PhysicsValue[2];
-			try {
-				capacity = (Float) cmd.getCommandData(StampCCProcessor.CAPACITY);
-				distance = (Float) cmd.getCommandData(StampCCProcessor.DISTANCE);
-			} catch (final ClassCastException e) {
-				LOGGER.log(Level.SEVERE, "Couldn't parse the capacity or distance while processing data!", e);
-				return;
-			}
-			final float valorDistancia = distance.floatValue();
-			final float valorCapacidade = capacity.floatValue();
+        if (cmd.getCommandIdentifier().equals(StampCCProcessor.COMMAND_IDENTIFIER)) {
+
+            final Float capacity;
+            final Float distance;
+            final PhysicsValue[] values = new PhysicsValue[2];
+            try {
+                capacity = (Float) cmd.getCommandData(StampCCProcessor.CAPACITY);
+                distance = (Float) cmd.getCommandData(StampCCProcessor.DISTANCE);
+            } catch (final ClassCastException e) {
+                LOGGER.log(Level.SEVERE, "Couldn't parse the capacity or distance while processing data!", e);
+                return;
+            }
             
-            values[0] = new PhysicsValue(PhysicsValFactory.fromFloat(valorDistancia), getAcquisitionHeader()
-						.getChannelsConfig(1).getSelectedScale().getDefaultErrorValue(), getAcquisitionHeader()
-						.getChannelsConfig(1).getSelectedScale().getMultiplier());
-            values[1] = new PhysicsValue(PhysicsValFactory.fromFloat(valorCapacidade), getAcquisitionHeader()
-						.getChannelsConfig(1).getSelectedScale().getDefaultErrorValue(), getAcquisitionHeader()
-						.getChannelsConfig(1).getSelectedScale().getMultiplier());
+            final float valorDistancia = distance.floatValue();
+            final float valorCapacity = capacity.floatValue();
+
+            final String distanceChannelName = ReCResourceBundle.findString("condensadorcilindrico$rec.exp.cc.hardwareinfo.channel.0.name");
+            final String capacityChannelName = ReCResourceBundle.findString("condensadorcilindrico$rec.exp.cc.hardwareinfo.channel.1.name");
             
+            final ChannelAcquisitionConfig distanceChannel = getAcquisitionHeader().getChannelsConfig(distanceChannelName);
+            final ChannelAcquisitionConfig capacityChannel = getAcquisitionHeader().getChannelsConfig(capacityChannelName);
             
-			super.addDataRow(values);
+            values[0] = new PhysicsValue(PhysicsValFactory.fromFloat(valorDistancia),
+                distanceChannel.getSelectedScale().getDefaultErrorValue(),
+                distanceChannel.getSelectedScale().getMultiplier());
+            values[1] = new PhysicsValue(PhysicsValFactory.fromFloat(valorCapacity),
+                capacityChannel.getSelectedScale().getDefaultErrorValue(),
+                capacityChannel.getSelectedScale().getMultiplier());
 
-            LOGGER.finest("Added two new values as capacity: " + valorCapacidade + " and distance: " + valorDistancia);
-            
-			counter++;
-			if (counter == total_samples) {
-				try {
-					Thread.currentThread();
-					Thread.sleep(1000);
-					setDataSourceEnded();
-				} catch (final InterruptedException ignored) {}
-			}
-		}
-	}
 
-	@Override
-	public void setAcquisitionHeader(final HardwareAcquisitionConfig config) {
-		super.setAcquisitionHeader(config);
+            super.addDataRow(values);
 
-		total_samples = config.getTotalSamples();
-	}
+            LOGGER.finest("Added two new values as capacity: " + valorCapacity + " and distance: " + valorDistancia);
 
-	@Override
-	public void stopNow() {
-		setDataSourceStoped();
-	}
+            counter++;
+            if (counter == total_samples) {
+                try {
+                    Thread.currentThread();
+                    Thread.sleep(1000);
+                    setDataSourceEnded();
+                } catch (final InterruptedException ignored) {
+                }
+            }
+        }
+    }
+
+    @Override
+    public void setAcquisitionHeader(final HardwareAcquisitionConfig config) {
+        super.setAcquisitionHeader(config);
+
+        total_samples = config.getTotalSamples();
+    }
+
+    @Override
+    public void stopNow() {
+        setDataSourceStoped();
+    }
 }
