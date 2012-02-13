8f155e42eaf54d9a16f1cf189a9deaf47bb05443
handyman <handyman@linkare.com>
2012-02-13 01:09:35 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-808.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@6022 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/vpend2m/src/java/client/pt/utl/ist/elab/client/vpend2m/ClientMain.java
@@ -0,0 +1,28 @@
+/*
+ * ClientMain.java
+ *
+ * Created on 27 de Fevereiro de 2005, 03:34 AM
+ */
+
+package pt.utl.ist.elab.client.vpend2m;
+
+import javax.swing.JFrame;
+
+import com.linkare.rec.impl.baseUI.ReCBaseUI;
+
+/**
+ * 
+ * @author Antonio J. R. Figueiredo Last Review : 6/04/2005
+ */
+public class ClientMain {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(final String[] args) {
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		final ReCBaseUI ui = new ReCBaseUI();
+		ui.setApparatusAutoConnectID("PENDULO_DUPLO_MOTORIZADO_V1.0");
+		ui.setVisible(true);
+	}
+}
