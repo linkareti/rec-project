8747e02a904fd6b417807f3dd01a0c857783f4df
handyman <handyman@linkare.com>
2012-03-16 01:09:24 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-840.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@6177 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/vcartpole/src/java/server/pt/utl/ist/elab/driver/vcartpole/ServerMain.java
@@ -0,0 +1,46 @@
+/*
+ * ServerMain.java
+ *
+ * Created on 10 de Abril de 2003, 20:07
+ */
+
+package pt.utl.ist.elab.driver.vcartpole;
+
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import com.linkare.rec.impl.driver.BaseHardware;
+import com.linkare.rec.impl.logging.LoggerUtil;
+import com.linkare.rec.impl.utils.ORBBean;
+
+/**
+ * 
+ * @author nomead
+ */
+public class ServerMain {
+
+	private static String CartPole_HARDWARE_LOGGER = "CartPole.Logger";
+	static {
+		final Logger l = LogManager.getLogManager().getLogger(ServerMain.CartPole_HARDWARE_LOGGER);
+		if (l == null) {
+			LogManager.getLogManager().addLogger(Logger.getLogger(ServerMain.CartPole_HARDWARE_LOGGER));
+		}
+	}
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(final String[] args) {
+		try {
+			ORBBean.getORBBean();
+
+			final BaseHardware baseHardware = new BaseHardware(new CartPoleDriver());
+
+			Thread.currentThread().join();
+
+		} catch (final Exception e) {
+			ORBBean.getORBBean().killORB();
+			LoggerUtil.logThrowable("Error on Main...", e, Logger.getLogger(ServerMain.CartPole_HARDWARE_LOGGER));
+		}
+	}
+}
