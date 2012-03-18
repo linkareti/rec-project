98ac5e547db89529f730e7c1bbc4eb2be581e848
handyman <handyman@linkare.com>
2012-03-18 01:15:46 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-743.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@6187 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/vquantum/src/java/client/pt/utl/ist/elab/client/vquantum/displays/CoeficientsTable.java
@@ -0,0 +1,27 @@
+/*
+ * CoeficientsTable.java
+ *
+ * Created on 2 de Dezembro de 2004, 2:38
+ */
+
+package pt.utl.ist.elab.client.vquantum.displays;
+
+import com.linkare.rec.impl.baseUI.table.MultSeriesTable;
+
+/**
+ * 
+ * @author nomead
+ */
+public class CoeficientsTable extends MultSeriesTable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5174544680493953879L;
+
+	/** Creates a new instance of CoeficientsTable */
+	public CoeficientsTable() {
+		super();
+		setColArray(new int[] { 9, 2, 3 });
+	}
+}
