c6b493e00ffc32e2e4328f32a23b8a5077821cea
handyman <handyman@linkare.com>
2012-04-25 00:25:03 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-781.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@6345 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/vmm/src/java/server/pt/utl/ist/elab/driver/vmm/MMDriver.java
@@ -0,0 +1,151 @@
+/*
+ * RobotDriver.java
+ *
+ * Created on 24 de Abril de 2003, 8:53
+ */
+
+package pt.utl.ist.elab.driver.vmm;
+
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import pt.utl.ist.elab.driver.virtual.VirtualBaseDataSource;
+import pt.utl.ist.elab.driver.virtual.VirtualBaseDriver;
+
+import com.linkare.rec.acquisition.IncorrectStateException;
+import com.linkare.rec.acquisition.WrongConfigurationException;
+import com.linkare.rec.data.config.HardwareAcquisitionConfig;
+import com.linkare.rec.data.metadata.HardwareInfo;
+import com.linkare.rec.impl.driver.IDataSource;
+import com.linkare.rec.impl.logging.LoggerUtil;
+import com.linkare.rec.impl.protocols.ReCProtocols;
+import com.linkare.rec.impl.utils.Defaults;
+
+/**
+ * 
+ * @author Andr�
+ */
+
+public class MMDriver extends VirtualBaseDriver {
+	// O código desta classe é sempre igual!!! Alterar só os nomes para o vosso
+	// caso!
+	private static String MM_DRIVER_LOGGER = "MM.Logger";
+	static {
+		final Logger l = LogManager.getLogManager().getLogger(MMDriver.MM_DRIVER_LOGGER);
+		if (l == null) {
+			LogManager.getLogManager().addLogger(Logger.getLogger(MMDriver.MM_DRIVER_LOGGER));
+		}
+	}
+
+	/* Hardware and driver related variables */
+	private static final String APPLICATION_IDENTIFIER = "E-Lab (Massa-mola Driver)";
+	private static final String DRIVER_UNIQUE_ID = "MASSA_MOLA_V1.0";
+	private static final String HW_VERSION = "0.1";
+
+	protected VirtualBaseDataSource dataSource = null;
+	protected HardwareAcquisitionConfig config = null;
+	protected HardwareInfo info = null;
+
+	/** Creates a new instance of RobotDriver */
+	public MMDriver() {
+	}
+
+	@Override
+	public void config(final HardwareAcquisitionConfig config, final HardwareInfo info) throws IncorrectStateException,
+			WrongConfigurationException {
+		fireIDriverStateListenerDriverConfiguring();
+		info.validateConfig(config);
+		extraValidateConfig(config, info);
+		try {
+			configure(config, info);
+		} catch (final Exception e) {
+			LoggerUtil.logThrowable("Error on config...", e, Logger.getLogger(MMDriver.MM_DRIVER_LOGGER));
+			throw new WrongConfigurationException();
+		}
+	}
+
+	@Override
+	public void configure(final HardwareAcquisitionConfig config, final HardwareInfo info)
+			throws WrongConfigurationException {
+		// Recebemos ordem para configurar, no HardwareAcquisitionConfig estao
+		// todas as informacoes escolhidas pelo cliente...agora e' so' pedir
+		this.config = config;
+		this.info = info;
+
+		final float kmola = Float.parseFloat(config.getSelectedHardwareParameterValue("kmola"));
+		final float massa = Float.parseFloat(config.getSelectedHardwareParameterValue("massa"));
+		final float atrito = Float.parseFloat(config.getSelectedHardwareParameterValue("atrito"));
+		final float xini = Float.parseFloat(config.getSelectedHardwareParameterValue("xini"));
+		final int tbs = (int) config.getSelectedFrequency().getFrequency();
+		final int nSamples = config.getTotalSamples();
+
+		// Vamos criar o nosso produtor de dados!
+		dataSource = new MMDataProducer(this, kmola, massa, atrito, xini, tbs, nSamples);
+
+		// NECESSARIO!! Colocar o numero de amostra para todos os canais!
+		for (int i = 0; i < config.getChannelsConfig().length; i++) {
+			config.getChannelsConfig(i).setTotalSamples(config.getTotalSamples());
+		}
+
+		// NAO ESQUECER ESTA LINHA!
+		dataSource.setAcquisitionHeader(config);
+
+		fireIDriverStateListenerDriverConfigured();
+	}
+
+	@Override
+	public String getDriverUniqueID() {
+		return MMDriver.DRIVER_UNIQUE_ID;
+	}
+
+	@Override
+	public void shutdown() {
+		if (dataSource != null) {
+			dataSource.stopNow();
+		}
+		super.shutDownNow();
+	}
+
+	@Override
+	public IDataSource start(final HardwareInfo info) throws IncorrectStateException {
+		fireIDriverStateListenerDriverStarting();
+		dataSource.startProduction();
+		fireIDriverStateListenerDriverStarted();
+		return dataSource;
+	}
+
+	@Override
+	public void stop(final HardwareInfo info) throws IncorrectStateException {
+		fireIDriverStateListenerDriverStoping();
+		dataSource.stopNow();
+		fireIDriverStateListenerDriverStoped();
+	}
+
+	@Override
+	public Object getHardwareInfo() {
+		fireIDriverStateListenerDriverReseting();
+
+		final String baseHardwareInfoFile = "recresource://" + getClass().getPackage().getName().replaceAll("\\.", "/")
+				+ "/HardwareInfo.xml";
+		String prop = Defaults.defaultIfEmpty(System.getProperty("HardwareInfo"), baseHardwareInfoFile);
+
+		if (prop.indexOf("://") == -1) {
+			prop = "file:///" + System.getProperty("user.dir") + "/" + prop;
+		}
+
+		java.net.URL url = null;
+		try {
+			url = ReCProtocols.getURL(prop);
+		} catch (final java.net.MalformedURLException e) {
+			LoggerUtil.logThrowable("Unable to load resource: " + prop, e, Logger.getLogger("DPendulum"));
+			try {
+				url = new java.net.URL(baseHardwareInfoFile);
+			} catch (final java.net.MalformedURLException e2) {
+				LoggerUtil.logThrowable("Unable to load resource: " + baseHardwareInfoFile, e2,
+						Logger.getLogger("DPendulum"));
+			}
+		}
+		fireIDriverStateListenerDriverReseted();
+		return url;
+	}
+}
