78d114cf8b275fdc6e933a829ee5552ffa2ef1c0
handyman <handyman@linkare.com>
2012-04-30 00:14:49 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-887.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@6366 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/gamma/src/java/client/pt/utl/ist/elab/client/gamma/displays/OscTable.java
@@ -0,0 +1,26 @@
+/*
+ * AnguloIntensidadeChart.java
+ *
+ * Created on October 15, 2004, 2:06 PM
+ */
+
+package pt.utl.ist.elab.client.gamma.displays;
+
+/**
+ * 
+ * @author André Neto - LEFT - IST
+ */
+public class OscTable extends com.linkare.rec.impl.baseUI.table.MultSeriesTable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7030541293096070223L;
+
+	/** Creates a new instance of AnguloIntensidadeChart */
+	public OscTable() {
+		super();
+		setColArray(new int[] { 3 });
+	}
+
+}
