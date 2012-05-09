fd2a351047c198ebca3ea8e08ea6354b8991bad2
handyman <handyman@linkare.com>
2012-05-09 00:11:59 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-896.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@6410 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.am/src/main/java/com/linkare/rec/am/service/MailServiceLocal.java
@@ -0,0 +1,30 @@
+package com.linkare.rec.am.service;
+
+import javax.ejb.Local;
+
+import com.linkare.rec.am.model.util.BusinessException;
+import com.linkare.rec.am.model.util.MailMessageRequest;
+
+/**
+ * 
+ * @author Bruno Catarino - Linkare TI
+ *
+ */
+@Local
+public interface MailServiceLocal {
+
+    /**
+     * Sends the mail represented by the <code>mailMessageRequest</code> to its destination, while the number of failCount is inferior to the max number set on
+     * the server. All the e-mails present in the to fields and the recipients field are sent always as BCC.
+     * 
+     * @param mailMessageRequest
+     *            The information about the mail to be sent. At least one e-mail is mandatory in the <code>to</code> or in the <code>recipients</code>
+     *            properties.
+     * @param failCount
+     *            Number of attempts to send that e-mail already made. If more than X tries are made to send the e-mail, the mail will be discarded and will not
+     *            be delivered.
+     * @throws BusinessException
+     *             If one of the e-mails are invalid or if any other unexpected error occurs.
+     */
+    public void sendMessages(MailMessageRequest mailMessageRequest, int failCount);
+}
