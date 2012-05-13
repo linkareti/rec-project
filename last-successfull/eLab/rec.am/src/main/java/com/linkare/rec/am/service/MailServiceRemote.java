9740d3c50a72ca672b539ab9d4f34ada0e19f47c
handyman <handyman@linkare.com>
2012-05-13 00:19:42 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-800.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@6442 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.am/src/main/java/com/linkare/rec/am/service/MailServiceRemote.java
@@ -0,0 +1,40 @@
+package com.linkare.rec.am.service;
+
+import java.rmi.RemoteException;
+
+import javax.ejb.Remote;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+import com.linkare.rec.am.mail.MailMessageRequest;
+import com.linkare.rec.am.mail.NoValidRecipientsFoundForMessage;
+import com.linkare.rec.am.model.HardwareAcquisitionConfig;
+import com.linkare.rec.am.model.util.BusinessException;
+
+/**
+ * 
+ * @author Bruno Catarino - Linkare TI
+ * 
+ */
+@WebService
+@Remote
+public interface MailServiceRemote {
+
+    /**
+     * Queues a new mail message in the queue to be processed asynchronously.
+     * 
+     * @param request
+     *            The information about the mail to process. The <code>to</code> or the <code>recipients</code> properties should be correctly filled with at
+     *            least one valid e-mail. The <code>subject</code> must be equal to the experience identifier (OID). This is mandatory, since this id will be
+     *            used to get the experience data from the database. The <code>content</code> should contain a string representing the hardware configuration (
+     *            {@link HardwareAcquisitionConfig#toString()}). This String will be used to create an attachment with the experiment configuration.
+     * @throws BusinessException
+     *             If it's not possible to get the mail configuration properties from the server or if an unexpected error occurs while adding the message to
+     *             the queue.
+     * @throws NoValidRecipientsFoundForMessage
+     *             If no valid e-mails are given.
+     * @throws RemoteException
+     */
+    @WebMethod
+    void queueMessage(MailMessageRequest request) throws NoValidRecipientsFoundForMessage, RemoteException;
+}
