2bf38876cd7ba80f8b041982d5add183cde88446
handyman <handyman@linkare.com>
2012-05-18 00:12:52 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-905.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@6486 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/ui/com/linkare/rec/impl/baseUI/labsTree/DisplayTreeNodeUtils.java
@@ -0,0 +1,98 @@
+/*
+ * DisplayTreeNodeUtils.java
+ *
+ * Created on August 11, 2004, 1:18 AM
+ */
+
+package com.linkare.rec.impl.baseUI.labsTree;
+
+/**
+ *
+ * @author André Neto - LEFT - IST
+ */
+
+import com.linkare.rec.impl.baseUI.config.Apparatus;
+import com.linkare.rec.impl.baseUI.config.DefaultAcquisitionConfig;
+import com.linkare.rec.impl.baseUI.config.Display;
+import com.linkare.rec.impl.baseUI.config.Lab;
+
+public abstract class DisplayTreeNodeUtils {
+	/** Disable all the nodes in a lab 
+	 * @param lab */
+	public static void disableLab(final Lab lab) {
+		if (lab == null) {
+			return;
+		}
+
+		lab.setEnabled(false);
+		final Apparatus[] apps = lab.getApparatus();
+		for (final Apparatus app : apps) {
+			app.setEnabled(false);
+			app.setConnected(false);
+
+			final Display[] displays = app.getDisplay();
+			for (int d = 0; d < displays.length; d++) {
+				if (!displays[d].getOfflineCapable()) {
+					displays[d].setEnabled(false);
+				}
+			}
+
+			final DefaultAcquisitionConfig[] dfacq = app.getDefaultAcquisitionConfig();
+			for (final DefaultAcquisitionConfig element : dfacq) {
+				element.setEnabled(false);
+			}
+		}
+	}
+
+	/** Disable all the nodes in a lab 
+	 * @param lab */
+	public static void disableAllApparatus(final Lab lab) {
+		if (lab == null) {
+			return;
+		}
+
+		final Apparatus[] apps = lab.getApparatus();
+		for (final Apparatus app : apps) {
+			final Display[] displays = app.getDisplay();
+			for (int d = 0; d < displays.length; d++) {
+				if (!displays[d].getOfflineCapable()) {
+					displays[d].setEnabled(false);
+				}
+			}
+
+			final DefaultAcquisitionConfig[] dfacq = app.getDefaultAcquisitionConfig();
+			for (final DefaultAcquisitionConfig element : dfacq) {
+				element.setEnabled(false);
+			}
+
+			app.setEnabled(false);
+			app.setConnected(false);
+		}
+	}
+
+	public static void enableAllApparatusContents(final Apparatus app) {
+		final com.linkare.rec.impl.baseUI.config.Display[] dps = app.getDisplay();
+		app.setConnected(true);
+		for (final Display dp : dps) {
+			dp.setEnabled(true);
+		}
+
+		final com.linkare.rec.impl.baseUI.config.DefaultAcquisitionConfig[] dftacq = app.getDefaultAcquisitionConfig();
+		for (final DefaultAcquisitionConfig element : dftacq) {
+			element.setEnabled(true);
+		}
+	}
+
+	public static void disableAllApparatusContents(final Apparatus app) {
+		final com.linkare.rec.impl.baseUI.config.Display[] dps = app.getDisplay();
+		app.setConnected(false);
+		for (final Display dp : dps) {
+			dp.setEnabled(false);
+		}
+
+		final com.linkare.rec.impl.baseUI.config.DefaultAcquisitionConfig[] dftacq = app.getDefaultAcquisitionConfig();
+		for (final DefaultAcquisitionConfig element : dftacq) {
+			element.setEnabled(false);
+		}
+	}
+}
