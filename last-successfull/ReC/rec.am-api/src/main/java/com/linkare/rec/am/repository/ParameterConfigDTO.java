b6229e829aad39944db9506d06ecf452d0736dbb
handyman <handyman@linkare.com>
2012-05-23 00:13:23 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-910.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@6556 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.am-api/src/main/java/com/linkare/rec/am/repository/ParameterConfigDTO.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.linkare.rec.am.repository;
+
+/**
+ * 
+ * @author Artur Correia - Linkare TI
+ */
+public class ParameterConfigDTO extends AbstractBaseDTO {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private String parameterName;
+    private String parameterValue;
+
+    public String getParameterName() {
+	return parameterName;
+    }
+
+    public void setParameterName(String parameterName) {
+	this.parameterName = parameterName;
+    }
+
+    public String getParameterValue() {
+	return parameterValue;
+    }
+
+    public void setParameterValue(String parameterValue) {
+	this.parameterValue = parameterValue;
+    }
+
+    @Override
+    public String toString() {
+	StringBuilder builder = new StringBuilder();
+	builder.append("ParameterConfigDTO [parameterName=");
+	builder.append(parameterName);
+	builder.append(", parameterValue=");
+	builder.append(parameterValue);
+	builder.append("]");
+	return builder.toString();
+    }
+
+}
