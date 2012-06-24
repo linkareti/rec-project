ddca0cdb0922495946678976d01aed2fc6654122
handyman <handyman@linkare.com>
2012-06-24 00:21:58 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-842.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@6772 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/vpend2m/src/java/client/pt/utl/ist/elab/client/vpend2m/ClientMain.java
@@ -0,0 +1,26 @@
+package pt.utl.ist.elab.client.vpend2m;
+
+
+import com.linkare.rec.impl.newface.ReCApplication;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * 
+ * This class is just a wrapper class for the true main class. The reason for
+ * its existence is only to guarantee that the certificate shown to the user is
+ * the eLab's one and not the ReC's framework Linkare default
+ * 
+ * @author Gedsimon Pereira - Linkare TI
+ */
+public class ClientMain {
+        /**
+	 * @param args the command line arguments
+	 */
+	public static void main(final String[] args) {
+		ReCApplication.main(args);
+	}
+}
---
+++ last-successfull/eLab/rec.am/src/main/java/com/linkare/rec/am/model/DeployedExperiment.java
@@ -0,0 +1,106 @@
+package com.linkare.rec.am.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Represents an experiment deployed on a laboratory. Contains all the information about the experiment, its current state, users currently connected and other
+ * information regarding an experiment registered on a multicast.
+ * 
+ * @author Bruno Catarino - Linkare TI
+ * 
+ */
+public class DeployedExperiment {
+
+    private Experiment experiment;
+    private HardwareState state;
+    private Set<String> usersConnected;
+    private int numberOfUsers;
+
+    public DeployedExperiment(final DeployedExperiment experiment) {
+	this.experiment = experiment.getExperiment();
+	this.state = experiment.getState();
+	this.usersConnected = new HashSet<String>(experiment.getUsersConnected());
+    }
+
+    public DeployedExperiment() {
+    }
+
+    public Experiment getExperiment() {
+	return experiment;
+    }
+
+    public void setExperiment(Experiment experiment) {
+	this.experiment = experiment;
+    }
+
+    public HardwareState getState() {
+	return state;
+    }
+
+    public void setState(HardwareState state) {
+	this.state = state;
+    }
+
+    public Set<String> getUsersConnected() {
+	return usersConnected;
+    }
+
+    public void setUsersConnected(final Set<String> usersConnected) {
+	this.usersConnected = usersConnected;
+    }
+
+    public int getNumberOfUsers() {
+	if (usersConnected != null) {
+	    numberOfUsers = usersConnected.size();
+	}
+	return numberOfUsers;
+    }
+    
+    public boolean isLabRunning() {
+	return state == HardwareState.STARTED;
+    }
+    
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((experiment == null) ? 0 : experiment.hashCode());
+	result = prime * result + numberOfUsers;
+	result = prime * result + ((state == null) ? 0 : state.hashCode());
+	result = prime * result + ((usersConnected == null) ? 0 : usersConnected.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	DeployedExperiment other = (DeployedExperiment) obj;
+	if (experiment == null) {
+	    if (other.experiment != null)
+		return false;
+	} else if (!experiment.equals(other.experiment))
+	    return false;
+	if (numberOfUsers != other.numberOfUsers)
+	    return false;
+	if (state != other.state)
+	    return false;
+	if (usersConnected == null) {
+	    if (other.usersConnected != null)
+		return false;
+	} else if (!usersConnected.equals(other.usersConnected))
+	    return false;
+	return true;
+    }
+
+    @Override
+    public String toString() {
+	return "DeployedExperiment [experiment=" + experiment + ", state=" + state + ", usersConnected=" + usersConnected + ", numberOfUsers=" + numberOfUsers
+		+ "]";
+    }
+}
