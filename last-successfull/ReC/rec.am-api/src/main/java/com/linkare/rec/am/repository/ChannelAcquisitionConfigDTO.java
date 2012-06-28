674275d996e9cecd56f1df7ac6f3e77a8df402cc
handyman <handyman@linkare.com>
2012-06-28 00:12:12 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-946.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@6812 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.am-api/src/main/java/com/linkare/rec/am/repository/ChannelAcquisitionConfigDTO.java
@@ -0,0 +1,103 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.linkare.rec.am.repository;
+
+import java.util.List;
+
+/**
+ * 
+ * @author Artur Correia - Linkare TI
+ */
+public class ChannelAcquisitionConfigDTO extends AbstractBaseDTO {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    private DateTimeDTO timeStart;
+
+    private FrequencyDTO frequency;
+
+    private ScaleDTO scale;
+
+    private List<ParameterConfigDTO> channelParameters;
+
+    private int totalSamples;
+
+    private String channelName;
+
+    public ChannelAcquisitionConfigDTO() {
+    }
+
+    public DateTimeDTO getTimeStart() {
+	return timeStart;
+    }
+
+    public void setTimeStart(DateTimeDTO timeStart) {
+	this.timeStart = timeStart;
+    }
+
+    public FrequencyDTO getFrequency() {
+	return frequency;
+    }
+
+    public void setFrequency(FrequencyDTO frequency) {
+	this.frequency = frequency;
+    }
+
+    public ScaleDTO getScale() {
+	return scale;
+    }
+
+    public void setScale(ScaleDTO scale) {
+	this.scale = scale;
+    }
+
+    public List<ParameterConfigDTO> getChannelParameters() {
+	return channelParameters;
+    }
+
+    public void setChannelParameters(List<ParameterConfigDTO> channelParameters) {
+	this.channelParameters = channelParameters;
+    }
+
+    public int getTotalSamples() {
+	return totalSamples;
+    }
+
+    public void setTotalSamples(int totalSamples) {
+	this.totalSamples = totalSamples;
+    }
+
+    public String getChannelName() {
+	return channelName;
+    }
+
+    public void setChannelName(String channelName) {
+	this.channelName = channelName;
+    }
+
+    @Override
+    public String toString() {
+	StringBuilder builder = new StringBuilder();
+	builder.append("ChannelAcquisitionConfigDTO [timeStart=");
+	builder.append(timeStart);
+	builder.append(", frequency=");
+	builder.append(frequency);
+	builder.append(", scale=");
+	builder.append(scale);
+	builder.append(", channelParameters=");
+	builder.append(channelParameters);
+	builder.append(", totalSamples=");
+	builder.append(totalSamples);
+	builder.append(", channelName=");
+	builder.append(channelName);
+	builder.append("]");
+	return builder.toString();
+    }
+
+}
