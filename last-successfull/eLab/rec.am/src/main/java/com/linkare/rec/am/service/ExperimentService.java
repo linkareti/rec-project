a9f173405dfba2302a0c1fc7db55c46b5e73efd1
handyman <handyman@linkare.com>
2012-06-28 00:20:57 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-846.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@6814 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.am/src/main/java/com/linkare/rec/am/service/ExperimentService.java
@@ -0,0 +1,21 @@
+package com.linkare.rec.am.service;
+
+import java.util.List;
+
+import com.linkare.rec.am.model.Experiment;
+import com.linkare.rec.am.model.Laboratory;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ * @author Bruno Catarino - Linkare TI
+ * 
+ */
+public interface ExperimentService extends BusinessService<Experiment, Long> {
+
+    public List<Experiment> findAllActiveExperiments();
+    
+    public List<Experiment> findExperimentsByLaboratory(Laboratory labId);
+
+    public Experiment findByExternalID(final String externalID);
+}
