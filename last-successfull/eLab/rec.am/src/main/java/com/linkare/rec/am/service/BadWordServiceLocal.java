3dff25d9d0f01bbaec4248fc55695b1778d9457f
handyman <handyman@linkare.com>
2012-07-06 00:23:19 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-853.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@6855 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.am/src/main/java/com/linkare/rec/am/service/BadWordServiceLocal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.linkare.rec.am.service;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Gedsimon Pereira - Linkare TI
+ */
+@Local
+public interface BadWordServiceLocal extends BadWordService{
+    
+}
