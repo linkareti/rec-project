e437b85b538a2d8eed8a12a6e0c261a4e5d857d4
handyman <handyman@linkare.com>
2012-07-12 00:19:22 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-859.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@6900 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.am/src/main/java/com/linkare/rec/am/util/MBeanNotificationListenerInfo.java
@@ -0,0 +1,32 @@
+package com.linkare.rec.am.util;
+
+import javax.management.NotificationFilter;
+import javax.management.NotificationListener;
+import javax.management.ObjectName;
+
+public class MBeanNotificationListenerInfo {
+    
+    private final NotificationListener listener;
+    private final ObjectName objectName;
+    private final NotificationFilter filter;
+
+
+    public MBeanNotificationListenerInfo(final NotificationListener listener, final ObjectName objectName, final NotificationFilter filter) {
+	this.listener = listener;
+	this.objectName = objectName;
+	this.filter = filter;
+    }
+
+    public ObjectName getObjectName() {
+	return objectName;
+    }
+
+    public NotificationFilter getFilter() {
+	return filter;
+    }
+
+    public NotificationListener getListener() {
+	return listener;
+    }
+
+}
