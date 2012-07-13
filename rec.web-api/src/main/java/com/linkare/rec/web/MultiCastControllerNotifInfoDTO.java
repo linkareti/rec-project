93c23fe0831286a25cebf18aff64e9eab59d7893
gpereira <gpereira@linkare.com>
2012-07-13 15:56:02 +0000

    (related to bug 1768) Changed the project name and all references of rec.am to rec.web
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/trunk@6958 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ rec.web-api/src/main/java/com/linkare/rec/web/MultiCastControllerNotifInfoDTO.java
@@ -0,0 +1,77 @@
+package com.linkare.rec.web;
+
+import java.io.Serializable;
+
+public class MultiCastControllerNotifInfoDTO implements Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    private long uptime;
+
+    private String labID;
+
+    private String notificationType;
+
+    private RegisteredHardwareDTO registeredHardwareDTO;
+
+    private HardwareStateChangeDTO hardwareStateChangeDTO;
+
+    private ClientInfoDTO clientInfoDTO;
+
+    public MultiCastControllerNotifInfoDTO(final String labID, final String notificationType) {
+	this.labID = labID;
+	this.notificationType = notificationType;
+    }
+
+    public String getLabID() {
+	return labID;
+    }
+
+    public void setLabID(String labID) {
+	this.labID = labID;
+    }
+
+    public String getNotificationType() {
+	return notificationType;
+    }
+
+    public void setNotificationType(String notificationType) {
+	this.notificationType = notificationType;
+    }
+
+    public RegisteredHardwareDTO getRegisteredHardwareDTO() {
+	return registeredHardwareDTO;
+    }
+
+    public void setRegisteredHardwareDTO(RegisteredHardwareDTO registeredHardwareDTO) {
+	this.registeredHardwareDTO = registeredHardwareDTO;
+    }
+
+    public HardwareStateChangeDTO getHardwareStateChangeDTO() {
+	return hardwareStateChangeDTO;
+    }
+
+    public void setHardwareStateChangeDTO(HardwareStateChangeDTO hardwareStateDTO) {
+	this.hardwareStateChangeDTO = hardwareStateDTO;
+    }
+
+    public ClientInfoDTO getClientInfoDTO() {
+        return clientInfoDTO;
+    }
+
+    public void setClientInfoDTO(ClientInfoDTO clientInfoDTO) {
+        this.clientInfoDTO = clientInfoDTO;
+    }
+
+    public long getUptime() {
+	return uptime;
+    }
+
+    public void setUptime(long uptime) {
+	this.uptime = uptime;
+    }
+
+}
