83585bc296ff324bca9e2f91552740558a475b37
jpereira <jpereira@linkare.com>
2012-07-13 00:30:04 +0000

    (related to bug 1764) Use the logger classes and references correctly. Avoid LoggerUtil class
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/trunk@6941 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- rec/src/java/client/com/linkare/rec/impl/client/customizer/CustomizerUIUtil.java
+++ rec/src/java/client/com/linkare/rec/impl/client/customizer/CustomizerUIUtil.java
@@ -8,6 +8,7 @@ package com.linkare.rec.impl.client.customizer;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.JComponent;
@@ -16,7 +17,6 @@ import javax.swing.JMenuBar;
 
 import com.linkare.rec.data.config.HardwareAcquisitionConfig;
 import com.linkare.rec.data.metadata.HardwareInfo;
-import com.linkare.rec.impl.logging.LoggerUtil;
 
 /**
  * 
@@ -44,101 +44,9 @@ public class CustomizerUIUtil {
 			}
 
 		} catch (final Exception e2) {
-			LoggerUtil.logThrowable("Couldn't load Customizer from URL :" + url, e2, LOGGER);
+			LOGGER.log(Level.SEVERE, "Couldn't load Customizer from URL :" + url, e2);
 			return null;
 		}
-
-		/*
-		 * try { DownloadService ds =
-		 * (DownloadService)ServiceManager.lookup("javax.jnlp.DownloadService");
-		 * 
-		 * DownloadServiceListener dsl=ds.getDefaultProgressWindow();
-		 * 
-		 * String jarURL=url.substring("jar:".length(),url.indexOf("!")-1);
-		 * 
-		 * ds.loadResource(new URL(jarURL),"1.0", dsl);
-		 * 
-		 * String className=url.substring(url.indexOf("!")+1);
-		 * className=className.replaceAll("/",".").replaceAll(".class","");
-		 * Class c=className.getClass().getClassLoader().loadClass(className);
-		 * 
-		 * Object o=c.newInstance();
-		 * 
-		 * if(o instanceof ICustomizer) return (ICustomizer)o; else return null;
-		 * 
-		 * }catch(Exception e2) {LoggerUtil.logThrowable(
-		 * "Couldn't load Customizer (using DownloadService) from URL :"+
-		 * url,e2,Logger.getLogger(UI_CLIENT_LOGGER)); return null; }
-		 */
-
-		/*
-		 * try { URL fulljarURL=new URL(url);
-		 * 
-		 * JarURLConnection con=(JarURLConnection)fulljarURL.openConnection();
-		 * 
-		 * URLClassLoader loader=new URLClassLoader(new URL[]
-		 * {con.getJarFileURL()});
-		 * 
-		 * String className=con.getEntryName();
-		 * 
-		 * className=className.replaceAll("/",".").replaceAll(".class","");
-		 * 
-		 * 
-		 * 
-		 * 
-		 * Logger.getLogger(UI_CLIENT_LOGGER).log(Level.INFO,"ClassName to load: "
-		 * + className);
-		 * 
-		 * Class c=loader.loadClass(className);
-		 * 
-		 * java.security.cert.Certificate[]
-		 * certs=con.getJarEntry().getCertificates();
-		 * System.out.println("Certificates:" + certs); if(certs!=null &&
-		 * certs.length>0) { java.security.cert.X509Certificate
-		 * cer=(java.security.cert.X509Certificate)certs[0]; String
-		 * message="Do you want to run the configuration utility signed by:";
-		 * message+="\n\rSigned by: "+cer.getSubjectDN().getName();
-		 * message+="\n\rCertified by: "+cer.getIssuerDN().getName(); try {
-		 * cer.checkValidity(); message+="\n\rand with a valid date!";
-		 * }catch(java.security.cert.CertificateExpiredException e) {
-		 * message+="\n\r[Note : Certificate has expired...]!";
-		 * }catch(java.security.cert.CertificateNotYetValidException e) {
-		 * message+="\n\r[Note : Certificate is not yet valid...]!"; }
-		 * 
-		 * if(JOptionPane.showConfirmDialog(null,message,
-		 * "Loading configuration utility..."
-		 * ,JOptionPane.OK_CANCEL_OPTION)==JOptionPane.CANCEL_OPTION) return
-		 * null; }
-		 * 
-		 * Object o=c.newInstance();
-		 * 
-		 * if(o instanceof ICustomizer) return (ICustomizer)o; else return null;
-		 * 
-		 * }catch(Exception e) {
-		 * LoggerUtil.logThrowable("Couldn't load Customizer from URL "+
-		 * url,e,Logger.getLogger(UI_CLIENT_LOGGER));
-		 * 
-		 * try { DownloadService ds =
-		 * (DownloadService)ServiceManager.lookup("javax.jnlp.DownloadService");
-		 * 
-		 * DownloadServiceListener dsl=ds.getDefaultProgressWindow();
-		 * 
-		 * String jarURL=url.substring("jar://".length(),url.indexOf("!")-1);
-		 * 
-		 * ds.loadResource(new URL(jarURL),"1.0", dsl);
-		 * 
-		 * String className=url.substring(url.indexOf("!")+1);
-		 * className=className.replaceAll("/",".").replaceAll(".class","");
-		 * Class c=className.getClass().getClassLoader().loadClass(className);
-		 * 
-		 * Object o=c.newInstance();
-		 * 
-		 * if(o instanceof ICustomizer) return (ICustomizer)o; else return null;
-		 * 
-		 * }catch(Exception e2) {LoggerUtil.logThrowable(
-		 * "Couldn't load Customizer (using DownloadService) from URL :"+
-		 * url,e2,Logger.getLogger(UI_CLIENT_LOGGER)); return null; } }
-		 */
 	}
 
 	public static boolean customizerExists(final String url) {
@@ -148,7 +56,7 @@ public class CustomizerUIUtil {
 			return (c != null);
 
 		} catch (final Exception e2) {
-			LoggerUtil.logThrowable("Couldn't load Customizer from URL :" + url, e2, LOGGER);
+			LOGGER.log(Level.SEVERE, "Couldn't load Customizer from URL :" + url, e2);
 			return false;
 		}
 	}
@@ -199,8 +107,7 @@ public class CustomizerUIUtil {
 			return customizer;
 
 		} catch (final Exception e) {
-			LoggerUtil.logThrowable("Couldn't init Customizer for Experiment " + hardwareInfo.getFamiliarName(), e,
-					LOGGER);
+			LOGGER.log(Level.SEVERE, "Couldn't init Customizer for Experiment " + hardwareInfo.getFamiliarName(), e);
 			return null;
 		}
 	}
