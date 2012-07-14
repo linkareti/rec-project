014cdedf47e5115c721491634deb015a2ddb84c0
handyman <handyman@linkare.com>
2012-07-14 00:14:03 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-962.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@6965 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.am/src/main/java/com/linkare/rec/am/service/BadWordService.java
@@ -0,0 +1,10 @@
+package com.linkare.rec.am.service;
+
+import com.linkare.rec.am.model.BadWord;
+
+/**
+ *
+ * @author Gedsimon Pereira - Linkare TI
+ */
+public interface BadWordService extends BusinessService<BadWord, Long>{
+}
