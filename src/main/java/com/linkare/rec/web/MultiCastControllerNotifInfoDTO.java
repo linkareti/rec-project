630890d52095fadabbf90b15b61331c40c4ec58f
gpereira <gpereira@linkare.com>
2012-07-16 19:23:59 +0000

    (related to bug 1667) Added new Enum type Added new DATA
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/trunk/rec.web-api@6979 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- src/main/java/com/linkare/rec/web/MultiCastControllerNotifInfoDTO.java
+++ src/main/java/com/linkare/rec/web/MultiCastControllerNotifInfoDTO.java
@@ -8,54 +8,49 @@ public class MultiCastControllerNotifInfoDTO implements Serializable {
      * 
      */
     private static final long serialVersionUID = 1L;
-
     private long uptime;
-
     private String labID;
-
     private String notificationType;
-
     private RegisteredHardwareDTO registeredHardwareDTO;
-
     private HardwareStateChangeDTO hardwareStateChangeDTO;
-
     private ClientInfoDTO clientInfoDTO;
+    private RecChatMessageDTO recChatMessageDTO;
 
     public MultiCastControllerNotifInfoDTO(final String labID, final String notificationType) {
-	this.labID = labID;
-	this.notificationType = notificationType;
+        this.labID = labID;
+        this.notificationType = notificationType;
     }
 
     public String getLabID() {
-	return labID;
+        return labID;
     }
 
     public void setLabID(String labID) {
-	this.labID = labID;
+        this.labID = labID;
     }
 
     public String getNotificationType() {
-	return notificationType;
+        return notificationType;
     }
 
     public void setNotificationType(String notificationType) {
-	this.notificationType = notificationType;
+        this.notificationType = notificationType;
     }
 
     public RegisteredHardwareDTO getRegisteredHardwareDTO() {
-	return registeredHardwareDTO;
+        return registeredHardwareDTO;
     }
 
     public void setRegisteredHardwareDTO(RegisteredHardwareDTO registeredHardwareDTO) {
-	this.registeredHardwareDTO = registeredHardwareDTO;
+        this.registeredHardwareDTO = registeredHardwareDTO;
     }
 
     public HardwareStateChangeDTO getHardwareStateChangeDTO() {
-	return hardwareStateChangeDTO;
+        return hardwareStateChangeDTO;
     }
 
     public void setHardwareStateChangeDTO(HardwareStateChangeDTO hardwareStateDTO) {
-	this.hardwareStateChangeDTO = hardwareStateDTO;
+        this.hardwareStateChangeDTO = hardwareStateDTO;
     }
 
     public ClientInfoDTO getClientInfoDTO() {
@@ -67,11 +62,18 @@ public class MultiCastControllerNotifInfoDTO implements Serializable {
     }
 
     public long getUptime() {
-	return uptime;
+        return uptime;
     }
 
     public void setUptime(long uptime) {
-	this.uptime = uptime;
+        this.uptime = uptime;
     }
 
+    public RecChatMessageDTO getRecChatMessageDTO() {
+        return recChatMessageDTO;
+    }
+
+    public void setRecChatMessageDTO(RecChatMessageDTO recChatMessageDTO) {
+        this.recChatMessageDTO = recChatMessageDTO;
+    }
 }
