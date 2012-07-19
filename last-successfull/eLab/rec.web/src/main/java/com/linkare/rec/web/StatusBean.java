1d46bcb847c8a5ab77ded6855ebc3326a7f2ed32
handyman <handyman@linkare.com>
2012-07-19 00:28:38 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-866.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@7005 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.web/src/main/java/com/linkare/rec/web/StatusBean.java
@@ -0,0 +1,128 @@
+package com.linkare.rec.web;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import com.linkare.rec.web.model.DeployedExperiment;
+import com.linkare.rec.web.model.Experiment;
+import com.linkare.rec.web.service.ExperimentServiceLocal;
+import com.linkare.rec.web.util.LaboratoriesMonitor;
+import com.linkare.rec.web.util.MultiThreadDeployedExperimentWrapper;
+import com.linkare.rec.web.util.MultiThreadLaboratoryWrapper;
+import org.primefaces.event.TabChangeEvent;
+
+/**
+ * Defines of the operations to retrieve the existing laboratories and it's status, and also which experiments are in each laboratory, their status, number of
+ * connected users and other management information.
+ * 
+ * @author Bruno Catarino - Linkare TI
+ * @author Artur Correia - Linkare TI
+ * 
+ */
+@ManagedBean(name = "statusBean")
+@ViewScoped
+public class StatusBean implements Serializable {
+
+    private static final long serialVersionUID = 9112911612244451634L;
+    private List<MultiThreadLaboratoryWrapper> labs;
+    private MultiThreadLaboratoryWrapper selectedLab;
+    private List<DeployedExperiment> selectedLabExperiments;
+    private LaboratoriesMonitor laboratoriesMonitor = LaboratoriesMonitor.getInstance();
+    @EJB
+    private ExperimentServiceLocal experimentService;
+    private Map<String, List<Experiment>> activeExperiment = new HashMap<String, List<Experiment>>();
+
+    public List<MultiThreadLaboratoryWrapper> getLabs() {
+        if (labs == null) {
+            labs = LaboratoriesMonitor.getInstance().getActiveLabs();
+        }
+        return labs;
+    }
+
+    public MultiThreadLaboratoryWrapper getSelectedLab() {
+        return selectedLab;
+    }
+
+    public void setSelectedLab(MultiThreadLaboratoryWrapper selectedLab) {
+        this.selectedLab = selectedLab;
+        this.selectedLabExperiments = null;
+    }
+
+    public List<DeployedExperiment> getLabExperiments() {
+        getSelectedLab();
+        refreshExperiments();
+        return selectedLabExperiments;
+    }
+
+    public void updateLabStatus() {
+        labs = LaboratoriesMonitor.getInstance().getActiveLabs();
+        refreshExperiments();
+    }
+
+    public void refreshExperiments() {
+
+        if (selectedLab != null && selectedLab.isAvailable()) {
+            initActiveExperimentMap();
+            selectedLabExperiments = getDeployedExperiment(activeExperiment.get(selectedLab.getName()));
+
+            final MultiThreadLaboratoryWrapper laboratory = laboratoriesMonitor.getLaboratory(selectedLab.getName());
+
+            if (laboratory != null) {
+
+                final Map<String, MultiThreadDeployedExperimentWrapper> liveExperiments = laboratory.getLiveExperiments();
+                if (liveExperiments.size() > 0) {
+                    for (DeployedExperiment experiment : selectedLabExperiments) {
+                        String experimentName = experiment.getExperiment().getExternalId();
+                        MultiThreadDeployedExperimentWrapper mt = liveExperiments.get(experimentName);
+                        if (mt != null) {
+                            experiment.setState(mt.getState());
+                            experiment.setUsersConnected(mt.getUsersConnected());
+                        }
+                    }
+                }
+
+            }
+
+        } else {
+            selectedLabExperiments = Collections.emptyList();
+        }
+    }
+
+    private List<DeployedExperiment> getDeployedExperiment(final List<Experiment> experiment) {
+        List<DeployedExperiment> result = Collections.emptyList();
+
+        if (!experiment.isEmpty()) {
+            result = new ArrayList<DeployedExperiment>(experiment.size());
+            for (final Experiment exp : experiment) {
+                DeployedExperiment deployed = new DeployedExperiment();
+                deployed.setExperiment(exp);
+                result.add(deployed);
+            }
+        }
+
+        return result;
+    }
+
+    public void initActiveExperimentMap() {
+
+        if (selectedLab != null) {
+            final List<Experiment> labExperiments = activeExperiment.get(selectedLab.getName());
+            if (labExperiments == null) {
+                activeExperiment.put(selectedLab.getName(), experimentService.findExperimentsActiveByLaboratory(selectedLab.getName()));
+            }
+        }
+    }
+
+    public void onTabChange(TabChangeEvent event) {
+        selectedLab = (MultiThreadLaboratoryWrapper) event.getData();
+        refreshExperiments();
+    }
+}
---
+++ last-successfull/eLab/rec.web/src/main/java/com/linkare/rec/web/wsgen/moodle/CategoryRecord.java
@@ -0,0 +1,479 @@
+/**
+ * CategoryRecord.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.linkare.rec.web.wsgen.moodle;
+
+public class CategoryRecord  implements java.io.Serializable {
+    private java.lang.String error;
+
+    private java.math.BigInteger id;
+
+    private java.lang.String name;
+
+    private java.lang.String description;
+
+    private java.math.BigInteger parent;
+
+    private java.math.BigInteger sortorder;
+
+    private java.math.BigInteger coursecount;
+
+    private java.math.BigInteger visible;
+
+    private java.math.BigInteger timemodified;
+
+    private java.math.BigInteger depth;
+
+    private java.lang.String path;
+
+    public CategoryRecord() {
+    }
+
+    public CategoryRecord(
+           java.lang.String error,
+           java.math.BigInteger id,
+           java.lang.String name,
+           java.lang.String description,
+           java.math.BigInteger parent,
+           java.math.BigInteger sortorder,
+           java.math.BigInteger coursecount,
+           java.math.BigInteger visible,
+           java.math.BigInteger timemodified,
+           java.math.BigInteger depth,
+           java.lang.String path) {
+           this.error = error;
+           this.id = id;
+           this.name = name;
+           this.description = description;
+           this.parent = parent;
+           this.sortorder = sortorder;
+           this.coursecount = coursecount;
+           this.visible = visible;
+           this.timemodified = timemodified;
+           this.depth = depth;
+           this.path = path;
+    }
+
+
+    /**
+     * Gets the error value for this CategoryRecord.
+     * 
+     * @return error
+     */
+    public java.lang.String getError() {
+        return error;
+    }
+
+
+    /**
+     * Sets the error value for this CategoryRecord.
+     * 
+     * @param error
+     */
+    public void setError(java.lang.String error) {
+        this.error = error;
+    }
+
+
+    /**
+     * Gets the id value for this CategoryRecord.
+     * 
+     * @return id
+     */
+    public java.math.BigInteger getId() {
+        return id;
+    }
+
+
+    /**
+     * Sets the id value for this CategoryRecord.
+     * 
+     * @param id
+     */
+    public void setId(java.math.BigInteger id) {
+        this.id = id;
+    }
+
+
+    /**
+     * Gets the name value for this CategoryRecord.
+     * 
+     * @return name
+     */
+    public java.lang.String getName() {
+        return name;
+    }
+
+
+    /**
+     * Sets the name value for this CategoryRecord.
+     * 
+     * @param name
+     */
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+
+
+    /**
+     * Gets the description value for this CategoryRecord.
+     * 
+     * @return description
+     */
+    public java.lang.String getDescription() {
+        return description;
+    }
+
+
+    /**
+     * Sets the description value for this CategoryRecord.
+     * 
+     * @param description
+     */
+    public void setDescription(java.lang.String description) {
+        this.description = description;
+    }
+
+
+    /**
+     * Gets the parent value for this CategoryRecord.
+     * 
+     * @return parent
+     */
+    public java.math.BigInteger getParent() {
+        return parent;
+    }
+
+
+    /**
+     * Sets the parent value for this CategoryRecord.
+     * 
+     * @param parent
+     */
+    public void setParent(java.math.BigInteger parent) {
+        this.parent = parent;
+    }
+
+
+    /**
+     * Gets the sortorder value for this CategoryRecord.
+     * 
+     * @return sortorder
+     */
+    public java.math.BigInteger getSortorder() {
+        return sortorder;
+    }
+
+
+    /**
+     * Sets the sortorder value for this CategoryRecord.
+     * 
+     * @param sortorder
+     */
+    public void setSortorder(java.math.BigInteger sortorder) {
+        this.sortorder = sortorder;
+    }
+
+
+    /**
+     * Gets the coursecount value for this CategoryRecord.
+     * 
+     * @return coursecount
+     */
+    public java.math.BigInteger getCoursecount() {
+        return coursecount;
+    }
+
+
+    /**
+     * Sets the coursecount value for this CategoryRecord.
+     * 
+     * @param coursecount
+     */
+    public void setCoursecount(java.math.BigInteger coursecount) {
+        this.coursecount = coursecount;
+    }
+
+
+    /**
+     * Gets the visible value for this CategoryRecord.
+     * 
+     * @return visible
+     */
+    public java.math.BigInteger getVisible() {
+        return visible;
+    }
+
+
+    /**
+     * Sets the visible value for this CategoryRecord.
+     * 
+     * @param visible
+     */
+    public void setVisible(java.math.BigInteger visible) {
+        this.visible = visible;
+    }
+
+
+    /**
+     * Gets the timemodified value for this CategoryRecord.
+     * 
+     * @return timemodified
+     */
+    public java.math.BigInteger getTimemodified() {
+        return timemodified;
+    }
+
+
+    /**
+     * Sets the timemodified value for this CategoryRecord.
+     * 
+     * @param timemodified
+     */
+    public void setTimemodified(java.math.BigInteger timemodified) {
+        this.timemodified = timemodified;
+    }
+
+
+    /**
+     * Gets the depth value for this CategoryRecord.
+     * 
+     * @return depth
+     */
+    public java.math.BigInteger getDepth() {
+        return depth;
+    }
+
+
+    /**
+     * Sets the depth value for this CategoryRecord.
+     * 
+     * @param depth
+     */
+    public void setDepth(java.math.BigInteger depth) {
+        this.depth = depth;
+    }
+
+
+    /**
+     * Gets the path value for this CategoryRecord.
+     * 
+     * @return path
+     */
+    public java.lang.String getPath() {
+        return path;
+    }
+
+
+    /**
+     * Sets the path value for this CategoryRecord.
+     * 
+     * @param path
+     */
+    public void setPath(java.lang.String path) {
+        this.path = path;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof CategoryRecord)) return false;
+        CategoryRecord other = (CategoryRecord) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            ((this.error==null && other.getError()==null) || 
+             (this.error!=null &&
+              this.error.equals(other.getError()))) &&
+            ((this.id==null && other.getId()==null) || 
+             (this.id!=null &&
+              this.id.equals(other.getId()))) &&
+            ((this.name==null && other.getName()==null) || 
+             (this.name!=null &&
+              this.name.equals(other.getName()))) &&
+            ((this.description==null && other.getDescription()==null) || 
+             (this.description!=null &&
+              this.description.equals(other.getDescription()))) &&
+            ((this.parent==null && other.getParent()==null) || 
+             (this.parent!=null &&
+              this.parent.equals(other.getParent()))) &&
+            ((this.sortorder==null && other.getSortorder()==null) || 
+             (this.sortorder!=null &&
+              this.sortorder.equals(other.getSortorder()))) &&
+            ((this.coursecount==null && other.getCoursecount()==null) || 
+             (this.coursecount!=null &&
+              this.coursecount.equals(other.getCoursecount()))) &&
+            ((this.visible==null && other.getVisible()==null) || 
+             (this.visible!=null &&
+              this.visible.equals(other.getVisible()))) &&
+            ((this.timemodified==null && other.getTimemodified()==null) || 
+             (this.timemodified!=null &&
+              this.timemodified.equals(other.getTimemodified()))) &&
+            ((this.depth==null && other.getDepth()==null) || 
+             (this.depth!=null &&
+              this.depth.equals(other.getDepth()))) &&
+            ((this.path==null && other.getPath()==null) || 
+             (this.path!=null &&
+              this.path.equals(other.getPath())));
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        if (getError() != null) {
+            _hashCode += getError().hashCode();
+        }
+        if (getId() != null) {
+            _hashCode += getId().hashCode();
+        }
+        if (getName() != null) {
+            _hashCode += getName().hashCode();
+        }
+        if (getDescription() != null) {
+            _hashCode += getDescription().hashCode();
+        }
+        if (getParent() != null) {
+            _hashCode += getParent().hashCode();
+        }
+        if (getSortorder() != null) {
+            _hashCode += getSortorder().hashCode();
+        }
+        if (getCoursecount() != null) {
+            _hashCode += getCoursecount().hashCode();
+        }
+        if (getVisible() != null) {
+            _hashCode += getVisible().hashCode();
+        }
+        if (getTimemodified() != null) {
+            _hashCode += getTimemodified().hashCode();
+        }
+        if (getDepth() != null) {
+            _hashCode += getDepth().hashCode();
+        }
+        if (getPath() != null) {
+            _hashCode += getPath().hashCode();
+        }
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(CategoryRecord.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("http://elab.ist.utl.pt/moodle/wspp/wsdl", "categoryRecord"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("error");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "error"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("id");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "id"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "integer"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("name");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "name"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("description");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "description"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setNillable(true);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("parent");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "parent"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "integer"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("sortorder");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "sortorder"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "integer"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("coursecount");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "coursecount"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "integer"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("visible");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "visible"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "integer"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("timemodified");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "timemodified"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "integer"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("depth");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "depth"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "integer"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("path");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "path"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
