d92d0a14a846ac4532cbcc99b99ab1f6ec63c719
handyman <handyman@linkare.com>
2012-07-20 00:17:04 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-968.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@7011 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/vyounginterf/src/java/server/pt/utl/ist/elab/driver/vyounginterf/ServerMain.java
@@ -0,0 +1,35 @@
+package pt.utl.ist.elab.driver.vyounginterf;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.linkare.rec.impl.driver.BaseHardware;
+import com.linkare.rec.impl.utils.ORBBean;
+
+/**
+ * 
+ * @author Emanuel Antunes
+ */
+public class ServerMain {
+	private static final Logger LOGGER=Logger.getLogger(ServerMain.class.getName());
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(final String[] args) {
+		try {
+			ORBBean.getORBBean();
+
+			new BaseHardware(new YoungInterfDriver());
+
+			try {
+				Thread.currentThread().join();
+			} catch (final Exception ignored) {
+			}
+
+			ORBBean.getORBBean().killORB();
+		} catch (final Exception e) {
+			LOGGER.log(Level.SEVERE,"Error on ServerMain...", e);
+		}
+	}
+}
