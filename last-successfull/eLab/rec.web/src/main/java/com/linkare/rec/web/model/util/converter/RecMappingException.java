e7a3031a3c1cd5ca14f087e63624e687aa8787b3
handyman <handyman@linkare.com>
2012-07-30 00:24:02 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-877.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@7099 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.web/src/main/java/com/linkare/rec/web/model/util/converter/RecMappingException.java
@@ -0,0 +1,28 @@
+package com.linkare.rec.web.model.util.converter;
+
+import javax.ejb.EJBException;
+
+public class RecMappingException extends EJBException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public RecMappingException() {
+	super();
+    }
+
+    public RecMappingException(Exception ex) {
+	super(ex);
+    }
+
+    public RecMappingException(String message, Exception ex) {
+	super(message, ex);
+    }
+
+    public RecMappingException(String message) {
+	super(message);
+    }
+
+}
