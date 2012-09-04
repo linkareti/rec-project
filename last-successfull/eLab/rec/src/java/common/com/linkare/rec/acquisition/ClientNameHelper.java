e94eae6b3a5a9cf27bbbe9126581f67ea9b021df
handyman <handyman@linkare.com>
2012-09-04 00:15:13 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-912.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@7243 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/common/com/linkare/rec/acquisition/ClientNameHelper.java
@@ -0,0 +1,43 @@
+package com.linkare.rec.acquisition;
+
+/** CORBA Helper class */
+abstract public class ClientNameHelper {
+	private static String _id = "IDL:com/linkare/rec/acquisition/ClientName:1.0";
+
+	public static void insert(final org.omg.CORBA.Any a, final String that) {
+		final org.omg.CORBA.portable.OutputStream out = a.create_output_stream();
+		a.type(ClientNameHelper.type());
+		ClientNameHelper.write(out, that);
+		a.read_value(out.create_input_stream(), ClientNameHelper.type());
+	}
+
+	public static String extract(final org.omg.CORBA.Any a) {
+		return ClientNameHelper.read(a.create_input_stream());
+	}
+
+	private static org.omg.CORBA.TypeCode __typeCode = null;
+
+	synchronized public static org.omg.CORBA.TypeCode type() {
+		if (ClientNameHelper.__typeCode == null) {
+			ClientNameHelper.__typeCode = org.omg.CORBA.ORB.init().create_wstring_tc(0);
+			ClientNameHelper.__typeCode = org.omg.CORBA.ORB.init().create_alias_tc(
+					com.linkare.rec.acquisition.ClientNameHelper.id(), "ClientName", ClientNameHelper.__typeCode);
+		}
+		return ClientNameHelper.__typeCode;
+	}
+
+	public static String id() {
+		return ClientNameHelper._id;
+	}
+
+	public static String read(final org.omg.CORBA.portable.InputStream istream) {
+		String value = null;
+		value = istream.read_wstring();
+		return value;
+	}
+
+	public static void write(final org.omg.CORBA.portable.OutputStream ostream, final String value) {
+		ostream.write_wstring(value);
+	}
+
+}
