c1c7e3f53d7008a87bc46034c61f107a34a2ce40
handyman <handyman@linkare.com>
2012-10-13 00:18:06 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-951.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@7404 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.web/src/main/java/com/linkare/rec/web/model/State.java
@@ -0,0 +1,102 @@
+package com.linkare.rec.web.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+/**
+ * 
+ * @author Joao
+ */
+@Embeddable
+public class State implements Serializable {
+
+    @Column(name = "LABEL")
+    private String label;
+
+    @Column(name = "HELP_MESSAGE")
+    private String helpMessage;
+
+    @Column(name = "URL")
+    private String url;
+
+    @Column(name = "IS_ACTIVE")
+    private boolean active;
+
+    /**
+     * Get the value of label
+     * 
+     * @return the value of label
+     */
+    public final String getLabel() {
+	return label;
+    }
+
+    /**
+     * Set the value of label
+     * 
+     * @param label
+     *            new value of label
+     */
+    public final void setLabel(String label) {
+	this.label = label;
+    }
+
+    /**
+     * Get the value of helpMessage
+     * 
+     * @return the value of helpMessage
+     */
+    public final String getHelpMessage() {
+	return helpMessage;
+    }
+
+    /**
+     * Set the value of helpMessage
+     * 
+     * @param helpMessage
+     *            new value of helpMessage
+     */
+    public final void setHelpMessage(String helpMessage) {
+	this.helpMessage = helpMessage;
+    }
+
+    /**
+     * Get the value of url
+     * 
+     * @return the value of url
+     */
+    public final String getUrl() {
+	return url;
+    }
+
+    /**
+     * Set the value of url
+     * 
+     * @param url
+     *            new value of url
+     */
+    public final void setUrl(String url) {
+	this.url = url;
+    }
+
+    /**
+     * Get the value of active
+     * 
+     * @return the value of active
+     */
+    public final boolean isActive() {
+	return active;
+    }
+
+    /**
+     * Set the value of active
+     * 
+     * @param active
+     *            new value of active
+     */
+    public final void setActive(boolean active) {
+	this.active = active;
+    }
+}
