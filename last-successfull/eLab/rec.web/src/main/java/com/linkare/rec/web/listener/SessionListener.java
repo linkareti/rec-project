e104c836c36eb93455778124fd4a356b410f6ad1
handyman <handyman@linkare.com>
2012-10-26 00:15:02 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-964.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@7456 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.web/src/main/java/com/linkare/rec/web/listener/SessionListener.java
@@ -0,0 +1,35 @@
+package com.linkare.rec.web.listener;
+
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * When the user session timedout, ({@link #sessionDestroyed(HttpSessionEvent)}) method will be invoked. This method will make necessary cleanups (logging out
+ * user, updating db and audit logs, etc...) As a result; after this method, we will be in a clear and stable state. So nothing left to think about because
+ * session expired, user can do nothing after this point.
+ * 
+ * @author Joao
+ * 
+ */
+public class SessionListener implements HttpSessionListener {
+
+    private static Logger logger = Logger.getLogger("SessionListener");
+
+    public SessionListener() {
+    }
+
+    @Override
+    public final void sessionCreated(HttpSessionEvent event) {
+	logger.info("session created : " + event.getSession().getId());
+    }
+
+    @Override
+    public final void sessionDestroyed(HttpSessionEvent event) {
+	// get the destroying session...
+	HttpSession session = event.getSession();
+	logger.info("session destroyed :" + session.getId() + " Logging out user...");
+    }
+}
