36eec2672340e52a1a9761e268d763f928c510d1
handyman <handyman@linkare.com>
2012-12-01 01:19:06 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1001.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@7604 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/common/com/linkare/rec/jmf/media/protocol/function/SineFunctor.java
@@ -0,0 +1,44 @@
+package com.linkare.rec.jmf.media.protocol.function;
+
+public class SineFunctor implements Functor {
+
+	private static final double TWO_PI = 2. * Math.PI;
+
+	private double currentTime = 0.;
+
+	private double timeDelta = 1.;
+
+	private double frequency = 1.;
+
+	private double period = 1. / frequency;
+
+	@Override
+	public void setTimeDelta(double seconds) {
+		this.timeDelta = seconds;
+		System.out.println("time-delta=" + timeDelta);
+	}
+
+	@Override
+	public double getNextValue() {
+		currentTime += this.timeDelta;
+		while (currentTime > period) {
+			currentTime -= period;
+		}
+
+		return Math.sin(TWO_PI * frequency * currentTime);
+	}
+
+	public void setFrequency(double frequency) {
+		this.frequency = frequency;
+		this.period = 1. / frequency;
+		currentTime = 0;
+	}
+
+	public double getTimeDelta() {
+		return timeDelta;
+	}
+
+	public double getFrequency() {
+		return frequency;
+	}
+}
