0026e5992a4b0c34d3b89cb70ff3aabeadd173e2
handyman <handyman@linkare.com>
2012-12-05 01:10:35 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-1107.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@7618 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.web/src/main/java/com/linkare/rec/web/aop/AllocationManagerExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.linkare.rec.web.aop;
+
+import com.linkare.commons.jpa.exceptions.DomainException;
+import com.linkare.jsf.utils.JsfUtil;
+import com.linkare.rec.web.ex.AuthenticationException;
+import com.linkare.rec.web.util.ConstantUtils;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ * 
+ */
+public class AllocationManagerExceptionHandler extends LoggerExceptionHandler {
+
+    @Override
+    public Object execute(final Object target, final Object methodResult, Throwable throwable) {
+	super.execute(target, methodResult, throwable);
+	if (throwable.getCause() instanceof DomainException) {
+	    JsfUtil.addGlobalErrorMessage(ConstantUtils.BUNDLE, ConstantUtils.LABEL_ERROR_KEY, throwable.getCause().getMessage());
+	} else if (throwable instanceof AuthenticationException) {
+	    JsfUtil.addGlobalErrorMessage(ConstantUtils.BUNDLE, ConstantUtils.LABEL_ERROR_KEY, ConstantUtils.ERROR_LOGIN_FAILED_KEY);
+	} else {
+	    JsfUtil.addGlobalErrorMessage(ConstantUtils.BUNDLE, ConstantUtils.LABEL_ERROR_KEY, ConstantUtils.ERROR_PERSISTENCE_KEY);
+	}
+	return null;
+    }
+}
