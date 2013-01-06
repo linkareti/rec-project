6bcbab07dfe76c417f31d8cbeadadf1e40be3dea
handyman <handyman@linkare.com>
2013-01-06 01:30:23 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1037.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@7764 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/langmuir/src/java/server/pt/utl/ist/elab/driver/langmuir/LangmuirDriver.java
@@ -0,0 +1,17 @@
+/* 
+ * LangmuirDriver.java created on 27 de Dez de 2012
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package pt.utl.ist.elab.driver.langmuir;
+
+import pt.utl.ist.elab.driver.serial.serialportgeneric.genericexperiment.GenericSerialPortDriver;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ */
+public class LangmuirDriver extends GenericSerialPortDriver {
+
+}
