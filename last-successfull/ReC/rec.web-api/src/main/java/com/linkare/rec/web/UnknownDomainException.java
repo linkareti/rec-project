ab553727b0848dc0d91913f57e71b1351297055e
handyman <handyman@linkare.com>
2013-01-28 01:14:51 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-1161.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@7828 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.web-api/src/main/java/com/linkare/rec/web/UnknownDomainException.java
@@ -0,0 +1,25 @@
+package com.linkare.rec.web;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ * 
+ */
+public class UnknownDomainException extends Exception {
+
+    public UnknownDomainException() {
+	super();
+    }
+
+    public UnknownDomainException(String message, Throwable cause) {
+	super(message, cause);
+    }
+
+    public UnknownDomainException(String message) {
+	super(message);
+    }
+
+    public UnknownDomainException(Throwable cause) {
+	super(cause);
+    }
+}
