3902a62f13636f0715fc68edf4475db8b6ce2cc9
tpereira <tpereira@ipfn.ist.utl.pt>
2013-03-11 20:27:57 +0000

    bug 2038 kill -0
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/trunk@8031 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- templates/scripts/ExperimentDaemon.sh
+++ templates/scripts/ExperimentDaemon.sh
@@ -11,102 +11,106 @@ DEPLOY_DIR="$BASE_USER_HOMEDIR/rec-deployment/$EXPERIMENT_NAME"
 CURDIR=`pwd`
 CURUSER=`id -nu`
 
-start() {
-        if [ -f $DEPLOY_DIR/@experiment.name@.pid ]; then
-             PID=`cat $DEPLOY_DIR/@experiment.name@.pid`
-             if kill -0 $PID > /dev/null 2>&1; then
-                  echo "The service @experiment.name@ is already running"
-                  return 1
-             fi
-        fi
-
-        echo "Starting @experiment.name@ service..."
-
-        if [ "x$CURUSER" != "x$BASE_USER" ]; then
-             su -l $BASE_USER -c "cd $DEPLOY_DIR; nohup $DEPLOY_DIR/Start@experiment.name@Driver.sh 2>&1 >/dev/null 2>/dev/null"
-        else
-             cd $DEPLOY_DIR
-             nohup $DEPLOY_DIR/Start@experiment.name@Driver.sh 2>&1 >/dev/null 2>/dev/null
-        fi
-
-        if [ $? -eq 0 ]; then
-                echo "OK"
-                if [ "x$CURUSER" != "x$BASE_USER" ]; then
-                        su -l $BASE_USER -c "touch $DEPLOY_DIR/@experiment.name@.lock"
-                else
-                        touch $DEPLOY_DIR/@experiment.name@.lock
-                fi
-                return 0
-        else
-                echo "Not OK"
-                return 1
-        fi
+java_is_running() {
+	[[ -f "$DEPLOY_DIR/@experiment.name@.pid" ]] || return 1
+	PID=`cat $DEPLOY_DIR/@experiment.name@.pid`
+	ps aux | grep $PID |grep java > /dev/null 2>&1
+	VAR1=$?
+	kill -0 $PID > /dev/null 2>&1
+	VAR2=$?
+	[ $VAR1 -eq 0 ] && [ $VAR2 -eq 0 ] && return 0 || return 1
 }
+
+
+start() {
+	if ( java_is_running ); then
+		echo "The service @experiment.name@ is already running"
+		return 1
+	fi
+
+	echo "Starting @experiment.name@ service..."
+
+	if [ "x$CURUSER" != "x$BASE_USER" ]; then
+		su -l $BASE_USER -c "cd $DEPLOY_DIR; nohup $DEPLOY_DIR/Start@experiment.name@Driver.sh 2>&1 >/dev/null 2>/dev/null"
+	else
+		cd $DEPLOY_DIR
+		nohup $DEPLOY_DIR/Start@experiment.name@Driver.sh 2>&1 >/dev/null 2>/dev/null
+	fi
+
+	if [ $? -eq 0 ]; then
+		echo "OK"
+		if [ "x$CURUSER" != "x$BASE_USER" ]; then
+			su -l $BASE_USER -c "touch $DEPLOY_DIR/@experiment.name@.lock"
+		else
+			touch $DEPLOY_DIR/@experiment.name@.lock
+		fi
+		return 0
+	else
+		echo "Not OK"
+		return 1
+	fi
+}
+
 
 stop() {
-        if [ -f $DEPLOY_DIR/@experiment.name@.pid ]; then
-            PID=`cat $DEPLOY_DIR/@experiment.name@.pid`
+	if ( java_is_running ); then
+		PID=`cat $DEPLOY_DIR/@experiment.name@.pid`
 
-            [ -f $DEPLOY_DIR/@experiment.name@.lock ] && rm -rf $DEPLOY_DIR/@experiment.name@.lock
-            [ -f $DEPLOY_DIR/@experiment.name@.pid ] && rm -rf $DEPLOY_DIR/@experiment.name@.pid
+		[ -f $DEPLOY_DIR/@experiment.name@.lock ] && rm -rf $DEPLOY_DIR/@experiment.name@.lock
+		[ -f $DEPLOY_DIR/@experiment.name@.pid ] && rm -rf $DEPLOY_DIR/@experiment.name@.pid
 
-            if kill -0 $PID > /dev/null 2>&1; then
-                echo "Stopping @experiment.name@ service..."
-                if [ "x$CURUSER" != "x$BASE_USER" ]; then
-                    su -l $BASE_USER -c "kill $PID"
-                else
-                    kill $PID
-                fi
-                [ $? -eq 0 ] && echo "OK" || echo "Not OK"
-            else
-                echo "The service @experiment.name@ is not running"
-                return 1
-            fi
-        else
-            echo "The service @experiment.name@ is not running"
-            return 1
-        fi
+		echo "Stopping @experiment.name@ service..."
+		if [ "x$CURUSER" != "x$BASE_USER" ]; then
+			su -l $BASE_USER -c "kill $PID"
+		else
+			kill $PID
+		fi
 
-	return 0
+		if [ $? -eq 0 ]; then
+			echo "OK"
+			return 0
+		else
+			echo "Not OK"
+			return 1
+		fi
+	else
+		echo "The service @experiment.name@ is not running"
+		return 1
+	fi
 }
+
 
 restart() {
-        stop
-        start
+	stop
+	start
 }
+
 
 status() {
-        if [ -f $DEPLOY_DIR/@experiment.name@.pid ]; then
-
-            PID=`cat $DEPLOY_DIR/@experiment.name@.pid`
-
-            if kill -0 $PID > /dev/null 2>&1; then
-                echo "The service @experiment.name@ is running"
-            else
-                echo "The service @experiment.name@ is not running"
-            fi
-
-        else
-            echo "The service @experiment.name@ is not running"
-        fi
+	if ( java_is_running ); then
+		echo "The service @experiment.name@ is running"
+	else
+		echo "The service @experiment.name@ is not running"
+	fi
 }
 
+
 case "$1" in
-  start)
-        start
-        ;;
-  stop)
-        stop
-        ;;
-  restart)
-        restart
-        ;;
-  status)
-        status
-        ;;
-  *)
-        echo "Usage: %s {start|stop|restart|status}\n" "$0"
-        exit 1
+	start)
+		start
+	;;
+	stop)
+		stop
+	;;
+	restart)
+		restart
+	;;
+	status)
+		status
+	;;
+	*)
+		echo "Usage: $0 {start|stop|restart|status}"
+		exit 1
 esac
 
 exit $?
