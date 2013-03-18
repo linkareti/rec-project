8df518f03c6f0707fb73b38ee9eaacad19bfc192
handyman <handyman@linkare.com>
2013-03-18 01:13:48 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-1215.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@8060 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.web/src/main/java/com/linkare/rec/web/controller/ErrorMessageController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.linkare.rec.web.controller;
+
+import com.linkare.rec.web.model.ErrorMessage;
+import com.linkare.rec.web.service.ErrorMessageService;
+import com.linkare.rec.web.service.ErrorMessageServiceLocal;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Gedsimon Pereira - Linkare TI
+ */
+@ManagedBean(name = "errorMessageController")
+@RequestScoped
+public class ErrorMessageController extends AbstractController<Long, ErrorMessage, ErrorMessageService>{
+
+    @EJB(beanInterface = ErrorMessageServiceLocal.class)
+    private ErrorMessageService service;
+        
+    @Override
+    public ErrorMessage getSelected() {
+        if (getCurrent() == null) {
+            setCurrent(new ErrorMessage());
+        }
+        return getCurrent();
+    }
+
+    @Override
+    protected ErrorMessageService getService() {
+        return service;
+    }
+
+    @Override
+    public String prepareCreate() {
+        return null;
+    }
+    
+}
