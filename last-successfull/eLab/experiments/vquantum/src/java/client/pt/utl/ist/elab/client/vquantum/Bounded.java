86e8078cd76f7f39b1f4d9523c0ac6a5331fee9e
handyman <handyman@linkare.com>
2013-03-19 01:21:34 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1098.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@8066 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/vquantum/src/java/client/pt/utl/ist/elab/client/vquantum/Bounded.java
@@ -0,0 +1,32 @@
+/*
+ * Bounded.java
+ *
+ * Created on 29 de Mar�o de 2005, 19:29
+ */
+
+package pt.utl.ist.elab.client.vquantum;
+
+import java.awt.Color;
+import java.awt.geom.Rectangle2D;
+
+import org.opensourcephysics.display.DrawingPanel;
+import org.opensourcephysics.display.Interactive;
+
+/**
+ * 
+ * @author nomead
+ */
+public interface Bounded extends Interactive {
+
+	public Rectangle2D getBounds(double x);
+
+	public boolean intersect(Rectangle2D rect);
+
+	public void mouseOut(DrawingPanel panel);
+
+	public void mouseOver(DrawingPanel panel);
+
+	public Color getPaintColor();
+
+	public Color getHighLightColor();
+}
