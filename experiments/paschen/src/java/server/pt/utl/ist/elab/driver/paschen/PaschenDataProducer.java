8047ca1640284b83c99f550307bf319bcc550dd4
jloureiro <joaopsl99@gmail.com>
2013-03-20 22:21:49 +0000

    task 1866: Continuation of the implementation of Paschen Curve Experiment (with linux TUeDACS driver)
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/trunk@8071 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- experiments/paschen/src/java/server/pt/utl/ist/elab/driver/paschen/PaschenDataProducer.java
+++ experiments/paschen/src/java/server/pt/utl/ist/elab/driver/paschen/PaschenDataProducer.java
@@ -6,11 +6,13 @@
 
 package pt.utl.ist.elab.driver.paschen;
 
+
 import pt.utl.ist.elab.driver.virtual.VirtualBaseDataSource;
 import pt.utl.ist.elab.driver.virtual.VirtualBaseDriver;
 
 import com.linkare.rec.data.acquisition.PhysicsValue;
 import com.linkare.rec.impl.data.PhysicsValFactory;
+import com.sun.jna.Memory;
 
 /**
  * 
@@ -24,6 +26,17 @@ public class PaschenDataProducer extends VirtualBaseDataSource implements Runnab
 	private final double press_set;
 		
 	private final double[] state; // voltage, current, pressure, index
+	
+	
+	static TUeTdIo TUeIO = TUeTdIo.INSTANCE;
+	static TUeTdApi TUeApi = TUeTdApi.INSTANCE;
+	
+	static short zero = (short) 0;
+    
+	
+	//Serial Port Stuff
+	public static SerialDriver serialgauge;
+	
 
 	private final int NUM_CHANNELS = 4;
 
@@ -71,37 +84,107 @@ public class PaschenDataProducer extends VirtualBaseDataSource implements Runnab
 
 		@Override
 		public void run() {
+			
+	    	TUeIO.tdOpen();
+			Memory config = new Memory(2);  // allocating space for config
+			config.setShort(0,(short) -1);  // setting the -1 value, if mgConfig fails this stays -1;
+			TUeApi.pgConfig(config);
+			
+			TUeApi.pg_adcSetModeLv(zero, false, false, false, false, zero, zero, false, false);
+			TUeApi.pg_dacSetModeLv(zero, (short) 1, false, false, false, zero, zero, false, false, 0);	
+			
+	        try{
+	        	serialgauge = new SerialDriver();
+	        	serialgauge.connect("/dev/ttyUSB0");
+	        	serialgauge.configureGauge();
+			} 
+	        catch (Exception e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+			}
+			
+	        Memory adcValue = new Memory(2);  // allocating space
+	        adcValue.clear();
+	        
+	        short dacValue;
+
+	        double adcValChannel1 = 0;
+	        double adcValChannel2 = 0;
+	        
+	        short initdac = (short) ((((double) volt_ini)/3500.)*32768);
+	        short finaldac= (short) ((((double) volt_fin)/3500.)*32768);
+	        short stepdac = (short) ((((double) volt_step)/3500.)*32768);
+	             
+	        int timestamp = 0;
+	        
+	        /*
+	        TUeApi.pg_dacPutValue(zero, zero, (short) ((((double) press_set)/100.)*32768));
+	        
+	        try {Thread.sleep(5000);} 
+	        catch (final InterruptedException ex) {
+	        }
+	        
+	        TUeApi.pg_dacPutValue(zero, zero, zero);
+	        */
 			try {
 				Thread.sleep(100);
 
 				PhysicsValue[] value;
 
-				while (!stopped && state[0] <= volt_fin) {
+		        for(dacValue = initdac; !stopped && dacValue<=finaldac; dacValue+=stepdac){
+		            
+		            TUeApi.pg_dacPutValue(zero, (short) 1, dacValue);//Value to DAC Channel 2 controls Fug Voltage
+		                                     
+		            Thread.sleep((long) (volt_step*10));
+		                
+		                TUeApi.pg_adcSoftwareTrigger(zero);//Trig ADC to get data
 
-					step();
-					value = new PhysicsValue[NUM_CHANNELS];
+		                TUeApi.pg_adcGetValue(zero, zero, adcValue);//Retrieve data from channel1
+		                //adcValChannel1 = (20./32768.)*adcValue.getShort(0);
+		                adcValChannel1 = (3500./32768.)*adcValue.getShort(0);
+		  //              adcValChannel1 = dacValue;
+		                
+		                
+		                TUeApi.pg_adcGetValue(zero, (short) 1, adcValue);//Retrieve data from channel2
+		                //adcValChannel2 = (20./32768.)*adcValue.getShort(0);
+		                adcValChannel2 = (4/32768.)*adcValue.getShort(0);
+		   //             adcValChannel2 = dacValue;
 
-					value[0] = new PhysicsValue(PhysicsValFactory.fromFloat((float) state[0]), getAcquisitionHeader()
-							.getChannelsConfig(0).getSelectedScale().getDefaultErrorValue(), getAcquisitionHeader()
-							.getChannelsConfig(0).getSelectedScale().getMultiplier());
-					value[1] = new PhysicsValue(PhysicsValFactory.fromFloat((float) state[1]), getAcquisitionHeader()
-							.getChannelsConfig(1).getSelectedScale().getDefaultErrorValue(), getAcquisitionHeader()
-							.getChannelsConfig(1).getSelectedScale().getMultiplier());
-					value[2] = new PhysicsValue(PhysicsValFactory.fromFloat((float) state[2]), getAcquisitionHeader()
-							.getChannelsConfig(2).getSelectedScale().getDefaultErrorValue(), getAcquisitionHeader()
-							.getChannelsConfig(2).getSelectedScale().getMultiplier());
-					value[3] = new PhysicsValue(PhysicsValFactory.fromFloat((float) state[3]), getAcquisitionHeader()
-							.getChannelsConfig(3).getSelectedScale().getDefaultErrorValue(), getAcquisitionHeader()
-							.getChannelsConfig(3).getSelectedScale().getMultiplier());
+		                
+						value = new PhysicsValue[NUM_CHANNELS];
 
-					addDataRow(value);
-					Thread.sleep(100);
-
-				}
+						value[0] = new PhysicsValue(PhysicsValFactory.fromFloat((float) adcValChannel1), getAcquisitionHeader()
+								.getChannelsConfig(0).getSelectedScale().getDefaultErrorValue(), getAcquisitionHeader()
+								.getChannelsConfig(0).getSelectedScale().getMultiplier());
+						value[1] = new PhysicsValue(PhysicsValFactory.fromFloat((float) adcValChannel2), getAcquisitionHeader()
+								.getChannelsConfig(1).getSelectedScale().getDefaultErrorValue(), getAcquisitionHeader()
+								.getChannelsConfig(1).getSelectedScale().getMultiplier());
+						value[2] = new PhysicsValue(PhysicsValFactory.fromFloat((float) serialgauge.getValuefromGauge()), getAcquisitionHeader()
+								.getChannelsConfig(2).getSelectedScale().getDefaultErrorValue(), getAcquisitionHeader()
+								.getChannelsConfig(2).getSelectedScale().getMultiplier());
+						value[3] = new PhysicsValue(PhysicsValFactory.fromFloat((float) timestamp), getAcquisitionHeader()
+								.getChannelsConfig(3).getSelectedScale().getDefaultErrorValue(), getAcquisitionHeader()
+								.getChannelsConfig(3).getSelectedScale().getMultiplier());
+						
+						Thread.sleep(100);
+						
+						addDataRow(value);
+		                
+		                timestamp += stepdac;
+		        
+		        }
 
 				join(100);
 				endProduction();
 
+		        TUeApi.pg_dacPutValue(zero, (short) 1, zero);//Value to DAC Channel 2 controls Fug Voltage
+		        //TUeApi.pg_dacPutValue(zero, zero, zero);//Value to DAC Channel 1 controls Pressure
+		        
+		        Thread.sleep(100); 
+				
+		    	TUeIO.tdClose();
+		    	serialgauge.closeCommPort();
+				
 				driver.stopVirtualHardware();
 			} catch (final InterruptedException ie) {
 			}
@@ -115,12 +198,32 @@ public class PaschenDataProducer extends VirtualBaseDataSource implements Runnab
 	}
 
 	public void endProduction() {
+        TUeApi.pg_dacPutValue(zero, (short) 1, zero);//Value to DAC Channel 2 controls Fug Voltage
+        //TUeApi.pg_dacPutValue(zero, zero, zero);//Value to DAC Channel 1 controls Pressure
+        
+        try {Thread.sleep(100);} 
+        catch (final InterruptedException ex) {
+        } 
+		
+    	TUeIO.tdClose();
+    	serialgauge.closeCommPort();
+		
 		stopped = true;
 		setDataSourceStoped();
 	}
 
 	@Override
 	public void stopNow() {
+        TUeApi.pg_dacPutValue(zero, (short) 1, zero);//Value to DAC Channel 2 controls Fug Voltage
+        //TUeApi.pg_dacPutValue(zero, zero, zero);//Value to DAC Channel 1 controls Pressure
+        
+        try {Thread.sleep(100);} 
+        catch (final InterruptedException ex) {
+        } 
+		
+    	TUeIO.tdClose();
+    	serialgauge.closeCommPort();
+		
 		stopped = true;
 		setDataSourceStoped();
 	}
