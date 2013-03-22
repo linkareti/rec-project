c74f9120318d8bb8e66715291c4290f30dbb0286
handyman <handyman@linkare.com>
2013-03-22 14:32:22 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1103.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@8082 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.web-api/src/main/java/com/linkare/rec/web/mail/LocaleXmlTypeAdapter.java
@@ -0,0 +1,52 @@
+package com.linkare.rec.web.mail;
+
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public class LocaleXmlTypeAdapter extends XmlAdapter<String, Locale> {
+
+	private static final Pattern PATTERN_SERIALIZED_LOCALE = Pattern.compile("([^_]+)(_([^_]+))?(_(.*))?)");
+	
+	@Override
+	public Locale unmarshal(String v) throws Exception {
+		
+		if(v==null)
+		{
+			return Locale.getDefault();
+		}
+		
+		
+		Matcher matcher = PATTERN_SERIALIZED_LOCALE.matcher(v);
+		Locale x=Locale.getDefault();
+		if(matcher.matches()) {
+			String language = matcher.group(1);
+			String country = matcher.group(3);
+			String variant = matcher.group(5);
+			if(variant!=null) {
+				x=new Locale(language,country,variant);
+			} 
+			else if (country!=null) {
+				x=new Locale(language,country);
+			}
+			else
+			{
+				x=new Locale(language);
+			}
+		}
+		return x;
+		
+	}
+
+	@Override
+	public String marshal(Locale v) throws Exception {
+
+		if (v == null) {
+			return null;
+		}
+
+		return v.toString();
+	}
+}
