c60dc83c651e269029b4e1698ad315a0b3c47c38
handyman <handyman@linkare.com>
2013-03-25 01:23:03 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1109.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@8104 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/momentoinercia/src/java/client/pt/utl/ist/elab/client/momentoinercia/displays/VelocityGraph.java
@@ -0,0 +1,33 @@
+/*
+ * Sensor1Graph.java
+ *
+ * Created on February 4, 2004, 4:17 PM
+ */
+
+package pt.utl.ist.elab.client.momentoinercia.displays;
+
+import com.linkare.rec.impl.ui.graph.MultSeriesXYExperimentGraph;
+
+/**
+ * 
+ * @author André Neto - LEFT - IST
+ */
+public class VelocityGraph extends MultSeriesXYExperimentGraph {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3091863273206698440L;
+
+	/** Creates a new instance of Sensor1Graph */
+	public VelocityGraph() {
+		setChannelDisplayX(2);
+		setChannelDisplayY(0);
+		setUpdatePercentage(10);
+	}
+
+	@Override
+	public String getName() {
+		return "Velocidade angular";
+	}
+}
