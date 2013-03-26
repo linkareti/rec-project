3b7c4275845f20c6f40fca286b39a9cf77fb70a5
handyman <handyman@linkare.com>
2013-03-26 20:46:01 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-rec.web-ci-14.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec.web@8130 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successful/rec.web-ci/src/main/java/com/linkare/rec/web/service/RecServiceBean.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.linkare.rec.web.service;
+
+import com.linkare.rec.web.RecServiceRemote;
+import com.linkare.rec.web.model.BadWord;
+import com.linkare.rec.web.repository.BadWordDTO;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Gedsimon Pereira
+ */
+@Stateless(name = "RecService")
+@Remote(value = RecServiceRemote.class)
+@WebService(endpointInterface = "com.linkare.rec.web.RecServiceRemote", name = "RecServiceWS", serviceName = "rec-services", portName = "recservice", targetNamespace = "http://webservices.linkare.com/rec")
+public class RecServiceBean implements RecServiceRemote {
+
+    @PersistenceContext(unitName = "AllocationManagerPU")
+    private EntityManager entityManager;
+
+    @Override
+    public List<BadWordDTO> getBadWordRegexByLocale(String locale) throws RemoteException {
+        if (locale == null) {
+            throw new NullPointerException("locale cannot be null");
+        }
+        final List<BadWordDTO> result = new ArrayList<BadWordDTO>();
+        final List<BadWord> badWords = entityManager.createNamedQuery(BadWord.FIND_FOR_LOCALE_QUERYNAME).setParameter(BadWord.QUERY_PARAM_LOCALE, locale).getResultList();
+
+        for (final BadWord badword : badWords) {
+            result.add(new BadWordDTO(badword.getIdInternal(), badword.getLocale(), badword.getRegex()));
+        }
+        return result;
+    }
+
+    @Override
+    public List<BadWordDTO> getAllBadWordRegex() throws RemoteException {
+        final List<BadWordDTO> result = new ArrayList<BadWordDTO>();
+        final List<BadWord> badWords = entityManager.createNamedQuery(BadWord.FIND_ALL_QUERYNAME).getResultList();
+
+        for (final BadWord badword : badWords) {
+            result.add(new BadWordDTO(badword.getIdInternal(), badword.getLocale(), badword.getRegex()));
+        }
+        return result;
+    }
+}
