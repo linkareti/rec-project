861252845115bce8399f11e616828cf1dd6f1d6a
jpereira <jpereira@linkare.com>
2013-03-26 23:34:29 +0000

    (related to bug 1764) Refactor logger initialization
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/trunk@8147 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- rec/src/java/ui/com/linkare/rec/impl/newface/utils/SendMailToQueue.java
+++ rec/src/java/ui/com/linkare/rec/impl/newface/utils/SendMailToQueue.java
@@ -11,6 +11,7 @@ import com.linkare.rec.web.mail.MailMessageRequest;
 import com.linkare.rec.web.mail.MailServiceRemote;
 import com.linkare.rec.web.mail.NoValidRecipientsFoundForMessage;
 import com.linkare.rec.data.config.HardwareAcquisitionConfig;
+import com.linkare.rec.impl.config.ReCSystemProperty;
 import com.linkare.rec.impl.ui.table.DefaultExperimentDataTable;
 
 /**
@@ -19,54 +20,72 @@ import com.linkare.rec.impl.ui.table.DefaultExperimentDataTable;
  */
 public class SendMailToQueue {
 
-    public SendMailToQueue() {
-    }
+	/**
+	 * 
+	 */
+	private static final Logger LOGGER = Logger.getLogger(SendMailToQueue.class.getName());
 
-    boolean isPrivatePc = Preferences.userRoot().getBoolean("ElabPrivateComputer", true);
-
-    public String sendMail(final String recipients, final HardwareAcquisitionConfig acquisitionConfig, final String dataProducerName) {
-
-	String lookupAddress = System.getProperty("rec.web.endpoint")+"/MailServiceWS";
-	try {
-	    if (checkMaxNumberRecipients(recipients.split(";")))
-		return ResourceBundle.getBundle("com.linkare.rec.impl.newface.component.resources.SendMailBox").getString("error.number.max.addressee");
-
-	    MailMessageRequest mailMessageRequest = new MailMessageRequest();
-	    mailMessageRequest.setSubject(dataProducerName);
-	    mailMessageRequest.setClientLocale(Locale.getDefault());
-
-	    String[] recipientsArray = recipients.split(";");
-	    for (int i = 0; i < recipientsArray.length; i++) {
-		recipientsArray[i] = recipientsArray[i].trim();
-	    }
-
-	    mailMessageRequest.setRecipients(recipientsArray);
-	    mailMessageRequest.setContent(HardwareAcquisitionConfig.translatePropertyBundles(acquisitionConfig).toString());
-
-	    String namespace = ResourceBundle.getBundle("com.linkare.rec.impl.newface.resources.wsconfig").getString("mail.namespace");
-	    String part = java.util.ResourceBundle.getBundle("com.linkare.rec.impl.newface.resources.wsconfig").getString("mail.part");
-	    MailServiceRemote remote = WSServiceLocator.lookup(lookupAddress, namespace, part, MailServiceRemote.class);
- 
-	    remote.queueMessage(mailMessageRequest);
-
-	    if (isPrivatePc)
-		Preferences.userRoot().put("ElabRecipientsMail", recipients);
-
-	} catch (NoValidRecipientsFoundForMessage ex) {
-	    Logger.getLogger(DefaultExperimentDataTable.class.getName()).log(Level.SEVERE, null, ex);
-	    return ResourceBundle.getBundle("com.linkare.rec.impl.newface.component.resources.SendMailBox").getString("email.invalid");
-	} catch (RemoteException ex) {
-	    Logger.getLogger(DefaultExperimentDataTable.class.getName()).log(Level.SEVERE, null, ex);
-	    return ResourceBundle.getBundle("com.linkare.rec.impl.newface.component.resources.SendMailBox").getString("erro.server");
-	} catch (RuntimeException ex) {
-	    Logger.getLogger(DefaultExperimentDataTable.class.getName()).log(Level.SEVERE, null, ex);
-	    return ResourceBundle.getBundle("com.linkare.rec.impl.newface.component.resources.SendMailBox").getString("erro.server");
+	public SendMailToQueue() {
 	}
-	return null;
-    }
 
-    public static boolean checkMaxNumberRecipients(String[] recipients) {
-        int maxNumberRecipientes = Integer.parseInt(ResourceBundle.getBundle("com.linkare.rec.impl.newface.component.resources.SendMailBox").getString("number.max.recipientes"));
-	return recipients.length > maxNumberRecipientes ? true : false;
-    }
+	boolean isPrivatePc = Preferences.userRoot().getBoolean("ElabPrivateComputer", true);
+
+	public String sendMail(final String recipients, final HardwareAcquisitionConfig acquisitionConfig,
+			final String dataProducerName) {
+
+		String lookupAddress = ReCSystemProperty.REC_WEB_ENDPOINT.getValue() + "/MailServiceWS";
+		try {
+			if (checkMaxNumberRecipients(recipients.split(";")))
+				return ResourceBundle.getBundle("com.linkare.rec.impl.newface.component.resources.SendMailBox")
+						.getString("error.number.max.addressee");
+
+			MailMessageRequest mailMessageRequest = new MailMessageRequest();
+			mailMessageRequest.setSubject(dataProducerName);
+			mailMessageRequest.setClientLocale(Locale.getDefault());
+
+			String[] recipientsArray = recipients.split(";");
+			for (int i = 0; i < recipientsArray.length; i++) {
+				recipientsArray[i] = recipientsArray[i].trim();
+			}
+
+			mailMessageRequest.setRecipients(recipientsArray);
+			mailMessageRequest.setContent(HardwareAcquisitionConfig.translatePropertyBundles(acquisitionConfig)
+					.toString());
+
+			String namespace = ResourceBundle.getBundle("com.linkare.rec.impl.newface.resources.wsconfig").getString(
+					"mail.namespace");
+			String part = java.util.ResourceBundle.getBundle("com.linkare.rec.impl.newface.resources.wsconfig")
+					.getString("mail.part");
+			MailServiceRemote remote = WSServiceLocator.lookup(lookupAddress, namespace, part, MailServiceRemote.class);
+
+			remote.queueMessage(mailMessageRequest);
+
+			if (isPrivatePc) {
+				Preferences.userRoot().put("ElabRecipientsMail", recipients);
+			}
+		} catch (NoValidRecipientsFoundForMessage ex) {
+			LOGGER.log(Level.SEVERE, "Error sending email because:" +ex.getMessage(), ex);
+			return ResourceBundle.getBundle("com.linkare.rec.impl.newface.component.resources.SendMailBox").getString(
+					"email.invalid");
+		} catch (RemoteException ex) {
+			LOGGER.log(Level.SEVERE, "Error sending email because:" +ex.getMessage(), ex);
+			return ResourceBundle.getBundle("com.linkare.rec.impl.newface.component.resources.SendMailBox").getString(
+					"erro.server");
+		} catch (RuntimeException ex) {
+			LOGGER.log(Level.SEVERE, "Error sending email because:" +ex.getMessage(), ex);
+			return ResourceBundle.getBundle("com.linkare.rec.impl.newface.component.resources.SendMailBox").getString(
+					"erro.server");
+		} catch(Throwable t) {
+			LOGGER.log(Level.SEVERE, "Error sending email because:" +t.getMessage(), t);
+			return ResourceBundle.getBundle("com.linkare.rec.impl.newface.component.resources.SendMailBox").getString(
+					"erro.server");
+		}
+		return null;
+	}
+
+	public static boolean checkMaxNumberRecipients(String[] recipients) {
+		int maxNumberRecipientes = Integer.parseInt(ResourceBundle.getBundle(
+				"com.linkare.rec.impl.newface.component.resources.SendMailBox").getString("number.max.recipientes"));
+		return recipients.length > maxNumberRecipientes ? true : false;
+	}
 }
