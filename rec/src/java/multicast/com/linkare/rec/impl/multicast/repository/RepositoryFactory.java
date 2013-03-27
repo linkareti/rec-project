5d585fc292bb30dad8d69afba7fa04312493646c
jpereira <jpereira@linkare.com>
2013-03-27 00:45:52 +0000

    (related to bug 2069) System properties code normalization
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/trunk@8179 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- rec/src/java/multicast/com/linkare/rec/impl/multicast/repository/RepositoryFactory.java
+++ rec/src/java/multicast/com/linkare/rec/impl/multicast/repository/RepositoryFactory.java
@@ -3,6 +3,7 @@ package com.linkare.rec.impl.multicast.repository;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.linkare.rec.impl.config.ReCSystemProperty;
 import com.linkare.rec.impl.multicast.repository.impl.FSRepository;
 
 /**
@@ -14,42 +15,31 @@ public final class RepositoryFactory {
 
 	private static final Logger LOGGER = Logger.getLogger(RepositoryFactory.class.getName());
 
-	private static final String SYSPROP_REPOSITORY_CLASS = "rec.multicast.repository";
-
 	private static IRepository REPOSITORY = null;
 
 	static {
 
-		final String repositoryClassName = System.getProperty(SYSPROP_REPOSITORY_CLASS);
+		final String repositoryClassName = ReCSystemProperty.MULTICAST_REPOSITORY_CLASSNAME.getValue();
 
-		if (repositoryClassName == null || repositoryClassName.trim().length() == 0) {
+		try {
+			LOGGER.log(Level.FINE, "Trying to load the Repository class [" + repositoryClassName + "]");
 
-			LOGGER.log(Level.INFO, "Repository System Property not found... Loading DefaultRepository!");
-			REPOSITORY = new FSRepository();
+			REPOSITORY = (IRepository) Class.forName(repositoryClassName).newInstance();
 
-		} else {
-			try {
-				LOGGER.log(Level.FINE, "Trying to load the Repository class [" + repositoryClassName + "]");
-
-				REPOSITORY = (IRepository) Class.forName(repositoryClassName).newInstance();
-
-			} catch (Exception e) {
-				LOGGER.log(
-						Level.INFO,
-						new StringBuilder("Unable to load Repository defined by className : ")
-								.append(repositoryClassName).append(" - loading DefaultSecurityManager!").toString());
-				LOGGER.log(Level.SEVERE, "Error loading specified SecurityManager", e);
-			} catch (LinkageError e) {
-				LOGGER.log(
-						Level.INFO,
-						new StringBuilder("Unable to load Repository defined by className : ")
-								.append(repositoryClassName).append(" - loading DefaultRepository!").toString());
-				LOGGER.log(Level.SEVERE, "Error loading specified SecurityManager", e);
-			} finally {
-				if (REPOSITORY == null) {
-					LOGGER.log(Level.INFO, "Respository not instantiated... Loading FSRepository!");
-					REPOSITORY = new FSRepository();
-				}
+		} catch (Exception e) {
+			LOGGER.log(Level.INFO,
+					new StringBuilder("Unable to load Repository defined by className : ").append(repositoryClassName)
+							.append(" - loading DefaultSecurityManager!").toString());
+			LOGGER.log(Level.SEVERE, "Error loading specified SecurityManager", e);
+		} catch (LinkageError e) {
+			LOGGER.log(Level.INFO,
+					new StringBuilder("Unable to load Repository defined by className : ").append(repositoryClassName)
+							.append(" - loading DefaultRepository!").toString());
+			LOGGER.log(Level.SEVERE, "Error loading specified SecurityManager", e);
+		} finally {
+			if (REPOSITORY == null) {
+				LOGGER.log(Level.INFO, "Respository not instantiated... Loading FSRepository!");
+				REPOSITORY = new FSRepository();
 			}
 		}
 
