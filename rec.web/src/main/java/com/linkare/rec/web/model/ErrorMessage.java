1a08d38e9464abf246e312f3ed1ec058d4647686
jpereira <jpereira@linkare.com>
2013-03-28 15:34:13 +0000

    (related to bug 2097) Refactor the messageId field to idInternal inherited from the Default Domain Entity.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/trunk@8253 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- rec.web/src/main/java/com/linkare/rec/web/model/ErrorMessage.java
+++ rec.web/src/main/java/com/linkare/rec/web/model/ErrorMessage.java
@@ -6,8 +6,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
@@ -24,89 +22,83 @@ import com.linkare.commons.jpa.DefaultDomainObject;
  */
 @Entity
 @Table(name = "ERROR_MESSAGE")
-@NamedQueries({ @NamedQuery(name = ErrorMessage.FIND_ALL_QUERYNAME, query = ErrorMessage.FIND_ALL_QUERY),
-	@NamedQuery(name = ErrorMessage.COUNT_ALL_QUERYNAME, query = ErrorMessage.COUNT_ALL_QUERY) })
+@NamedQueries({
+		@NamedQuery(name = ErrorMessage.FIND_ALL_QUERYNAME, query = ErrorMessage.FIND_ALL_QUERY),
+		@NamedQuery(name = ErrorMessage.COUNT_ALL_QUERYNAME, query = ErrorMessage.COUNT_ALL_QUERY) })
 public class ErrorMessage extends DefaultDomainObject {
 
-    private static final long serialVersionUID = -6390113356819021981L;
+	private static final long serialVersionUID = -6390113356819021981L;
 
-    public static final String FIND_ALL_QUERYNAME = "ErrorMessage.findAll";
-    public static final String FIND_ALL_QUERY = "select e from ErrorMessage e";
+	public static final String FIND_ALL_QUERYNAME = "ErrorMessage.findAll";
+	public static final String FIND_ALL_QUERY = "select e from ErrorMessage e";
 
-    public static final String COUNT_ALL_QUERYNAME = "ErrorMessage.countAll";
-    public static final String COUNT_ALL_QUERY = "select count(e) from ErrorMessage e";
+	public static final String COUNT_ALL_QUERYNAME = "ErrorMessage.countAll";
+	public static final String COUNT_ALL_QUERY = "select count(e) from ErrorMessage e";
 
-    @Id
-    @GeneratedValue
-    @Column(name = "ID")
-    private long messageId;
+	@Column(name = "MESSAGE", length = 500)
+	private String message;
 
-    @Column(name = "MESSAGE", length = 500)
-    private String message;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "CATEGORY")
+	private MessageCategory category;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "CATEGORY")
-    private MessageCategory category;
+	/**
+	 * The date when the error message occurred.
+	 */
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "DATE_OF_ERROR")
+	private Date dateError;
 
-    /**
-     * The date when the error message occurred.
-     */
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "DATE_OF_ERROR")
-    private Date dateError;
+	public String getMessage() {
+		return message;
+	}
 
-    public long getMessageId() {
-	return messageId;
-    }
+	public void setMessage(String message) {
+		this.message = message;
+	}
 
-    public String getMessage() {
-	return message;
-    }
+	public MessageCategory getCategory() {
+		return category;
+	}
 
-    public void setMessage(String message) {
-	this.message = message;
-    }
+	public void setCategory(MessageCategory category) {
+		this.category = category;
+	}
 
-    public MessageCategory getCategory() {
-	return category;
-    }
+	public Date getDateError() {
+		return dateError;
+	}
 
-    public void setCategory(MessageCategory category) {
-	this.category = category;
-    }
+	public void setDateError(Date dateError) {
+		this.dateError = dateError;
+	}
 
-    public Date getDateError() {
-	return dateError;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ (int) (getIdInternal() ^ (getIdInternal() >>> 32));
+		return result;
+	}
 
-    public void setDateError(Date dateError) {
-	this.dateError = dateError;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ErrorMessage other = (ErrorMessage) obj;
+		if (getIdInternal().longValue() != other.getIdInternal().longValue())
+			return false;
+		return true;
+	}
 
-    @Override
-    public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + (int) (messageId ^ (messageId >>> 32));
-	return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-	if (this == obj)
-	    return true;
-	if (obj == null)
-	    return false;
-	if (getClass() != obj.getClass())
-	    return false;
-	ErrorMessage other = (ErrorMessage) obj;
-	if (messageId != other.messageId)
-	    return false;
-	return true;
-    }
-
-    @Override
-    public String toString() {
-	return "ErrorMessage [messageId=" + messageId + ", message=" + message + ", category=" + category + ", dateError=" + dateError + "]";
-    }
+	@Override
+	public String toString() {
+		return "ErrorMessage [id=" + getIdInternal() + ", message=" + message
+				+ ", category=" + category + ", dateError=" + dateError + "]";
+	}
 }
