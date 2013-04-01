f35895a5fc64a9fae4993f350bbd962a7165bfa8
jpereira <jpereira@linkare.com>
2013-04-01 17:52:53 +0000

    (related to bug 1764) Use a different logger for each class, to enable reconfiguration of levels per class and package correctly.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/trunk@8278 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- rec.web/src/main/java/com/linkare/rec/web/aop/LoggerExceptionHandler.java
+++ rec.web/src/main/java/com/linkare/rec/web/aop/LoggerExceptionHandler.java
@@ -1,8 +1,8 @@
 package com.linkare.rec.web.aop;
 
-import static com.linkare.rec.web.util.LoggerSingleton.LOGGER;
-
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * 
@@ -11,10 +11,19 @@ import java.io.Serializable;
  */
 public class LoggerExceptionHandler implements Serializable {
 
-    private static final long serialVersionUID = -4481196900668275898L;
+	private static final Logger LOGGER=Logger.getLogger(LoggerExceptionHandler.class.getPackage().getName());
+	
+	private static final long serialVersionUID = -4481196900668275898L;
 
-    public Object execute(final Object target, final Object methodResult, final Throwable throwable) {
-        LOGGER.log(java.util.logging.Level.WARNING, "Logging the exception with target [" + target + "] methodResult [" + methodResult + "]", throwable);
-        return methodResult;
-    }
+	public Object execute(final Object target, final Object methodResult,
+			final Throwable throwable) {
+		
+		Logger logger = target==null?  LOGGER: Logger.getLogger(target.getClass().getName());
+
+		if (LOGGER.isLoggable(Level.WARNING)) {
+			logger.log(Level.WARNING, "Method result [" + methodResult +"]",throwable);
+		}
+
+		return methodResult;
+	}
 }
