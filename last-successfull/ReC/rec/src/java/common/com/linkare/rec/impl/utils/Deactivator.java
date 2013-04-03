3e54a4d669ad93c1697dc291143609b36929816e
handyman <handyman@linkare.com>
2013-04-03 01:25:43 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-1236.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@8284 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/common/com/linkare/rec/impl/utils/Deactivator.java
@@ -0,0 +1,61 @@
+package com.linkare.rec.impl.utils;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+import org.omg.PortableServer.POAPackage.ObjectNotActive;
+
+import com.linkare.rec.impl.threading.ExecutorScheduler;
+import com.linkare.rec.impl.threading.ScheduledWorkUnit;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public class Deactivator extends ScheduledWorkUnit {
+
+	private Deactivatable deactivatable = null;
+	private final long DEACTIVATION_TIME = 40000;
+	private boolean deactivated = false;
+
+	/**
+	 * Creates a new instance of Deactivator
+	 * 
+	 * @param deactivatable
+	 */
+	public Deactivator(final Deactivatable deactivatable) {
+		this.deactivatable = deactivatable;
+
+		ExecutorScheduler.scheduleAtFixedRate(this, DEACTIVATION_TIME, DEACTIVATION_TIME, TimeUnit.MILLISECONDS);
+	}
+
+	@Override
+	public void run() {
+		tryToDeactivate();
+		if (deactivated) {
+			shutdown();
+		}
+	}
+
+	public void tryToDeactivate() {
+		if (!deactivatable.isDeactivationPossible()) {
+			return;
+		}
+
+		if (deactivatable.getOID() == null) {
+			// System.out.println("deactivatable has an object id = null... returning");
+			return;
+		}
+		final byte[] oid = deactivatable.getOID().getBytes();
+		try {
+			deactivatable.getPOA().deactivate_object(oid);
+			deactivated = true;
+		} catch(final ObjectNotActive e) {
+			deactivated = true;
+			LOGGER.log(Level.SEVERE, "Exception while trying to deactivate. Object was not active anymore, so not trying to deactivate it again!", e);
+		} catch (final Exception e) {
+			deactivated = false;
+			LOGGER.log(Level.SEVERE, "Exception while trying to deactivate.", e);
+		}
+	}
+}
