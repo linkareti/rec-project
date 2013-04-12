440b2056be1791e85957a18b24404d79b4a94534
handyman <handyman@linkare.com>
2013-04-12 15:00:33 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-1251.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@8338 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/voscilador/src/java/client/pt/utl/ist/elab/client/voscilador/displays/VelRadiusTimeChart.java
@@ -0,0 +1,27 @@
+/*
+ * AnguloIntensidadeChart.java
+ *
+ * Created on October 15, 2004, 2:06 PM
+ */
+
+package pt.utl.ist.elab.client.voscilador.displays;
+
+/**
+ * 
+ * @author andre
+ */
+public class VelRadiusTimeChart extends com.linkare.rec.impl.ui.graph.DefaultXYExperimentGraph {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2907610630441529539L;
+
+	/** Creates a new instance of AnguloIntensidadeChart */
+	public VelRadiusTimeChart() {
+		super();
+		setChannelDisplayX(0);
+		setChannelDisplayY(9);
+	}
+
+}
