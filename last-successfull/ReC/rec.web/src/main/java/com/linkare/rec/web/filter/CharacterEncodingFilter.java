f4159ceb8389be59ef3b345a81c1ecfbf19cf182
handyman <handyman@linkare.com>
2013-04-25 00:29:28 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-1268.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@8429 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.web/src/main/java/com/linkare/rec/web/filter/CharacterEncodingFilter.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.linkare.rec.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ *
+ * @author Gedsimon
+ */
+@WebFilter(urlPatterns="/*")
+public class CharacterEncodingFilter implements Filter {
+   @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        chain.doFilter(req, resp);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    } 
+}
