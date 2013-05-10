d3b22026d539177b43dc02ba911ef046b87427b7
handyman <handyman@linkare.com>
2013-05-10 00:24:16 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1157.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@8496 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/worldpendulum/src/pic/physical.h
@@ -0,0 +1,12 @@
+#ifndef _PHYSICAL_H_
+#define _PHYSICAL_H_
+
+void saveSphereDiameter_CM(double);
+double getSphereDiameter_CM();
+
+void savePendulumLength_M(double);
+double getPendulumLength_M();
+
+double getExpectedPeriod_S();
+
+#endif
