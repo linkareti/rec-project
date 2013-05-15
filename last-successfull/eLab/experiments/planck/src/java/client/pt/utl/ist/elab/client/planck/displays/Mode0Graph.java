926012ec7756a4b94c8f8de6edf471761a7fcbd6
handyman <handyman@linkare.com>
2013-05-15 00:31:47 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1162.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@8516 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/planck/src/java/client/pt/utl/ist/elab/client/planck/displays/Mode0Graph.java
@@ -0,0 +1,33 @@
+/*
+ * Sensor1Graph.java
+ *
+ * Created on February 4, 2004, 4:17 PM
+ */
+
+package pt.utl.ist.elab.client.planck.displays;
+
+import com.linkare.rec.impl.ui.graph.MultSeriesXYExperimentGraph;
+
+/**
+ * 
+ * @author André Neto - LEFT - IST
+ */
+public class Mode0Graph extends MultSeriesXYExperimentGraph {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4140016764770630928L;
+
+	/** Creates a new instance of Sensor1Graph */
+	public Mode0Graph() {
+		setChannelDisplayX(1);
+		setChannelDisplayY(2);
+		setUpdateFrequency(10);
+	}
+
+	@Override
+	public String getName() {
+		return "Modo Varrimento";
+	}
+}
