bac968bd7baa3f352e68eea1c114394779f2f0af
handyman <handyman@linkare.com>
2013-08-18 00:22:56 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-1384.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@8906 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/ui/com/linkare/rec/impl/newface/component/media/events/MediaApplicationEventListener.java
@@ -0,0 +1,22 @@
+package com.linkare.rec.impl.newface.component.media.events;
+
+import java.util.EventListener;
+
+/**
+ * Defines the events fired by VLC that the client should handle.
+ * 
+ * @author bcatarino
+ */
+public interface MediaApplicationEventListener extends EventListener {
+
+	/**
+	 * Fired when playing a streaming media and no ES where received.
+	 * 
+	 * @param evt
+	 */
+	void notConnected(MediaNotConnectedEvent evt);
+
+	void stopped(MediaStoppedEvent evt);
+
+	void timeChanged(MediaTimeChangedEvent evt);
+}
