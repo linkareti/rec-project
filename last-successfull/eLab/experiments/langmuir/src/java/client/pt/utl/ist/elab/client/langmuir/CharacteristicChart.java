a68007756e7bdc0dd00ce286e0ba7325af5b9b6e
handyman <handyman@linkare.com>
2013-11-25 01:23:52 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1349.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@9330 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/langmuir/src/java/client/pt/utl/ist/elab/client/langmuir/CharacteristicChart.java
@@ -0,0 +1,26 @@
+/* 
+ * CharacteristicChart.java created on 23 de Fev de 2013
+ * 
+ */
+
+package pt.utl.ist.elab.client.langmuir;
+
+/**
+ * 
+ * @author jloureiro
+ */
+public class CharacteristicChart extends com.linkare.rec.impl.ui.graph.DefaultXYExperimentGraph {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4570732150570211129L;
+
+	/** Creates a new instance of CharacteristicChart */
+	public CharacteristicChart() {
+		super();
+		setChannelDisplayX(0);
+		setChannelDisplayY(1);
+	}
+
+}
