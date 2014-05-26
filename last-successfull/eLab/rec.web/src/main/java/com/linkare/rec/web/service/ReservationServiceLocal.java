3e79ecf5c921b921b4df4595734844bce3322aab
handyman <handyman@linkare.com>
2014-05-26 00:29:23 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1493.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@9951 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.web/src/main/java/com/linkare/rec/web/service/ReservationServiceLocal.java
@@ -0,0 +1,12 @@
+package com.linkare.rec.web.service;
+
+import javax.ejb.Local;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ * 
+ */
+@Local
+public interface ReservationServiceLocal extends ReservationService {
+}
