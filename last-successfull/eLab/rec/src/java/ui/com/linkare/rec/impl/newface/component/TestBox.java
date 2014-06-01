5d74b140d0a59dba7bc1b36e970399d23e1cdb2d
handyman <handyman@linkare.com>
2014-06-01 00:27:18 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1499.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@9975 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/ui/com/linkare/rec/impl/newface/component/TestBox.java
@@ -0,0 +1,30 @@
+/*
+ * TestBox.java created on Apr 23, 2009
+ *
+ * Copyright 2009 HFernandes. All rights reserved.
+ * HFernandes PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.linkare.rec.impl.newface.component;
+
+import javax.swing.JTextField;
+
+import com.linkare.rec.impl.newface.laf.flat.Focus;
+
+/**
+ * 
+ * @author Henrique Fernandes
+ */
+@Focus(display = false)
+public class TestBox extends JTextField {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7211810386748966175L;
+
+	public TestBox() {
+		super();
+	}
+
+}
