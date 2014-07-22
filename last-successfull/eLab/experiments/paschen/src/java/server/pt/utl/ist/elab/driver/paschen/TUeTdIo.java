30697c30226499e6f663eb76d4d453af268c1f47
handyman <handyman@linkare.com>
2014-07-22 00:30:40 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1540.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@10159 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/paschen/src/java/server/pt/utl/ist/elab/driver/paschen/TUeTdIo.java
@@ -0,0 +1,97 @@
+package pt.utl.ist.elab.driver.paschen;
+
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+
+public interface TUeTdIo extends Library {
+
+	TUeTdIo INSTANCE = (TUeTdIo) Native.loadLibrary("tdIo", TUeTdIo.class);
+	
+	
+/*************************/
+/**		  tdIo.h 		**/
+/*************************/	
+		
+	
+	int tdOpen();
+	int tdClose();
+	int tdOpenWithoutDataTesting();
+	int tdTestOpen(Pointer Open);
+
+	int tdInit();
+	int tdReset(short DeviceIndex);
+//	int tdGetSerialNumber(short DeviceIndex, TD_SERIAL_NUMBER *SerialNumber);
+
+	int tdPutWord(int Address, short Data);
+	int tdGetWord(int Address, Pointer Data);
+	int tdPutLong(int Address, int Data);
+	int tdGetLong(int Address, Pointer Data);
+
+	int tdPutWordBlock(int Address, Pointer Data, int BlockLength);
+	int tdGetWordBlock(int Address, Pointer Data, int BlockLength);
+	int tdPutLongBlock(int Address, Pointer Data,  int BlockLength);
+	int tdGetLongBlock(int Address, Pointer Data,  int BlockLength);
+
+	int tdPutWordBlockAutoIncrAddr(int Address, Pointer Data, int BlockLength);
+	int tdGetWordBlockAutoIncrAddr(int Address, Pointer Data, int BlockLength);
+	int tdPutLongBlockAutoIncrAddr(int Address, Pointer Data, int BlockLength);
+	int tdGetLongBlockAutoIncrAddr(int Address, Pointer Data, int BlockLength);
+
+	int tdSetBitsWord(int Address, short BitMask);
+	int tdSetBitsLong(int Address, int BitMask);
+	int tdClearBitsWord(int Address, short BitMask);
+	int tdClearBitsLong(int Address, int BitMask);
+
+	int tdFindNextBaseAddress(short InterfaceID,
+													  Pointer BaseAddress,
+													  Pointer Found);
+	int tdFindFirstBaseAddress(short InterfaceID,
+													   Pointer BaseAddress,
+													   Pointer Found);
+
+	int tdPutWordProbe(int Address,
+												short Data, Pointer BusErrorFlag);
+	int tdPutLongProbe(int Address,
+												int Data, Pointer BusErrorFlag);
+	int tdGetWordProbe(int Address,
+												Pointer Data, Pointer BusErrorFlag);
+	int tdGetLongProbe(int Address,
+												Pointer Data, Pointer BusErrorFlag);
+
+	int tdClearMultiplePutGetList(short DeviceIndex);
+
+	int tdPutMultiplePutList(short DeviceIndex);
+	int tdPutMultipleGetList(short DeviceIndex);
+
+	int tdAppendToMultiplePutList(int Address, Pointer Data, boolean B32Flag);
+	int tdAppendToMultipleGetList(int Address, Pointer Data, boolean B32Flag);
+
+	int tdMultiplePut();
+	int tdMultipleGet();
+	int tdMultiplePutGet();
+
+//	int tdSetupMotionControl(short DeviceIndex, TD_MOTION_CONTROL_SETUP *Setup);
+//	int tdSetMotionControlTimes(short DeviceIndex, TD_MOTION_CONTROL_TIMES *Times);
+//	int tdGetMotionControlTimes(short DeviceIndex, TD_MOTION_CONTROL_TIMES *Times);
+	int tdEnableMotionControl(short DeviceIndex, short SkipFactor);
+	int tdArmMotionControlTrigger(byte TriggerDelay);
+	int tdDisableMotionControl(short DeviceIndex);
+	int tdMotionControlMultiplePutGet(short DeviceIndex);
+//	int tdGetMotionControlStatus(short DeviceIndex, TD_MOTION_CONTROL_STATUS *MotionControlStatus);
+	int tdMotionControlMultiplePutGetStart(short DeviceIndex);
+	int tdMotionControlMultiplePutGetReady(short DeviceIndex);
+
+	int tdSetSwapMode(short SwapMode);
+	void  tdGetNumberOfDevices(Pointer NumberOfDevices);
+	int tdMapAddress(int UserTdAddress, Pointer MappedTdAddress, Pointer DeviceIndex);
+	int tdGetMaxNumberOfAddresses(Pointer NumberOfAddresses);
+
+	String tdErrorMessage(int MessageNumber);
+
+//	void  tdGetLastStatus(TD_TRANSFER_STATUS *Status);
+//	String tdStatusMessage(TD_TRANSFER_STATUS Status);
+	int tdDownloadEpld(short DeviceIndex, short EpldNumber, Pointer EpldData, int DataSize);
+	
+}
