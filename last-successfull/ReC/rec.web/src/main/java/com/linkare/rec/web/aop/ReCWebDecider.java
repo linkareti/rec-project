621e5a357cd43254308e93ee04f345f41d2a8dd2
handyman <handyman@linkare.com>
2014-08-14 00:18:38 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-1746.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@10237 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.web/src/main/java/com/linkare/rec/web/aop/ReCWebDecider.java
@@ -0,0 +1,11 @@
+package com.linkare.rec.web.aop;
+
+import com.linkare.zas.aspectj.decider.GroovyDecider;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ * 
+ */
+public class ReCWebDecider extends GroovyDecider {
+}
