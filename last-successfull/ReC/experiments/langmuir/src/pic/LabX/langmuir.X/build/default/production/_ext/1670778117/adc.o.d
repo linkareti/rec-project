e4ff29fe5e87278f0c92a69f6067b4ad0763cc11
handyman <handyman@linkare.com>
2014-09-06 00:18:11 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-1769.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@10329 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/langmuir/src/pic/LabX/langmuir.X/build/default/production/_ext/1670778117/adc.o.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/1670778117/adc.o: ../../Library/adc.c \
+ ../../Library/elab.h ../../Library/user_defines.h \
+ ../../Library/hardware_uart.h ../../Library/pfeiffer_gauges.h
