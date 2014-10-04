d7aabd89f0bfc2bd561a9a9eeab01bab3189bdc3
handyman <handyman@linkare.com>
2014-10-04 00:18:42 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-1797.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@10421 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/worldpendulumilheus/src/pic/physical.h
@@ -0,0 +1,18 @@
+#ifndef _PHYSICAL_H_
+#define _PHYSICAL_H_
+
+void saveDistanceLaserToStart_CM(double distance);
+double getDistanceLaserToStart_CM();
+
+void saveSphereDiameter_CM(double);
+double getSphereDiameter_CM();
+
+void savePendulumLength_M(double);
+double getPendulumLength_M();
+
+double getExpectedPeriod_S();
+
+void saveStepperMaxFreq_HZ(unsigned int maxFreq);
+int getStepperMaxFreq_HZ();
+
+#endif
