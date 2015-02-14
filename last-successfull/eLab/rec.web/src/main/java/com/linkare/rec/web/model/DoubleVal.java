5632592f4236c734fdbf68fdf70dfa8d69e1fc6c
handyman <handyman@linkare.com>
2015-02-14 01:09:37 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1683.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@10696 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.web/src/main/java/com/linkare/rec/web/model/DoubleVal.java
@@ -0,0 +1,26 @@
+package com.linkare.rec.web.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+/**
+ * 
+ * @author Artur Correia - Linkare TI
+ */
+@Entity
+public class DoubleVal extends PhysicsVal {
+
+    private static final long serialVersionUID = 1L;
+
+    @Column(name = "DOUBLE_VALUE")
+    private Double value;
+
+    public Double getValue() {
+	return value;
+    }
+
+    public void setValue(Double doubleValue) {
+	this.value = doubleValue;
+    }
+
+}
