c967ba58b23e1310a6406b3146c0e11532cfffa1
handyman <handyman@linkare.com>
2015-04-02 08:39:28 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1711.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@10752 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/ui/com/linkare/rec/impl/newface/WindowListenerAdapter.java
@@ -0,0 +1,50 @@
+package com.linkare.rec.impl.newface;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+/**
+ * A WindowListenerAdapter.
+ * 
+ * @see WindowListener
+ * 
+ * @author <A HREF="mailto:hfernandes@linkare.com">Henrique Fernandes<A />
+ */
+public class WindowListenerAdapter implements WindowListener {
+
+	@Override
+	public void windowActivated(final WindowEvent e) {
+		// adapter
+	}
+
+	@Override
+	public void windowClosed(final WindowEvent e) {
+		// adapter
+	}
+
+	@Override
+	public void windowClosing(final WindowEvent e) {
+		// adapter
+	}
+
+	@Override
+	public void windowDeactivated(final WindowEvent e) {
+		// adapter
+	}
+
+	@Override
+	public void windowDeiconified(final WindowEvent e) {
+		// adapter
+	}
+
+	@Override
+	public void windowIconified(final WindowEvent e) {
+		// adapter
+	}
+
+	@Override
+	public void windowOpened(final WindowEvent e) {
+		// adapter
+	}
+
+}
