0fe2c7c35a7469b57e8d0a46b7d3e8f6a5a222db
handyman <handyman@linkare.com>
2015-07-05 00:05:17 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1790.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@11111 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/mola/src/java/server/pt/utl/ist/elab/driver/mola/MolaDriver.java
@@ -0,0 +1,10 @@
+package mola.src.java.server.pt.utl.ist.elab.driver.mola;
+
+import pt.utl.ist.elab.driver.serial.serialportgeneric.genericexperiment.GenericSerialPortDriver;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ */
+public class MolaDriver extends GenericSerialPortDriver {
+}
