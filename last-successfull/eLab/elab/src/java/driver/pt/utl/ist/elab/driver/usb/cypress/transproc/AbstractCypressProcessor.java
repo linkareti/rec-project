c36d469b333df21f432f0d9d98b1f678a0ae1804
handyman <handyman@linkare.com>
2015-07-17 10:19:47 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1798.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@11162 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/elab/src/java/driver/pt/utl/ist/elab/driver/usb/cypress/transproc/AbstractCypressProcessor.java
@@ -0,0 +1,33 @@
+/*
+ * AbstractSerialPortProcessor.java
+ *
+ * Created on 11 de Novembro de 2002, 16:03
+ */
+
+package pt.utl.ist.elab.driver.usb.cypress.transproc;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public abstract class AbstractCypressProcessor implements CypressProcessor {
+	private String commandIdentifier = null;
+
+	/** Creates a new instance of AbstractSerialPortProcessor 
+	 * @param commandIdentifier */
+	public AbstractCypressProcessor(final String commandIdentifier) {
+		this.commandIdentifier = commandIdentifier;
+		CypressTranslatorProcessorManager.registerProcessor(this);
+	}
+
+	@Override
+	public String getCommandIdentifier() {
+		return commandIdentifier;
+	}
+
+	@Override
+	public boolean isData() {
+		return false;
+	}
+
+}
