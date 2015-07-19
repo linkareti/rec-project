87424c5252e5f788f157a8f6cd103d7209fc0dc3
handyman <handyman@linkare.com>
2015-07-19 00:05:32 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1800.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@11179 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/vlooping/src/java/client/pt/utl/ist/elab/client/vlooping/displays/XYTimeChart.java
@@ -0,0 +1,27 @@
+/*
+ * AnguloIntensidadeChart.java
+ *
+ * Created on October 15, 2004, 2:06 PM
+ */
+
+package pt.utl.ist.elab.client.vlooping.displays;
+
+/**
+ * 
+ * @author Emanuel Antunes
+ */
+public class XYTimeChart extends com.linkare.rec.impl.ui.graph.MultSeriesXYExperimentGraph {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5935772358389328281L;
+
+	/** Creates a new instance of AnguloIntensidadeChart */
+	public XYTimeChart() {
+		super();
+		setChannelDisplayX(0);
+		setChannelDisplayYArray(new int[] { 1, 2 });
+	}
+
+}
