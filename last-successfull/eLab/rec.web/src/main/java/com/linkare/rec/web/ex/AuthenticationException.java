ac7b3a076a968d7a34a7ae0e993ad06c1bcda18c
handyman <handyman@linkare.com>
2015-07-29 00:05:19 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1810.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@11238 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.web/src/main/java/com/linkare/rec/web/ex/AuthenticationException.java
@@ -0,0 +1,25 @@
+package com.linkare.rec.web.ex;
+
+public class AuthenticationException extends Exception {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public AuthenticationException() {
+	super();
+    }
+
+    public AuthenticationException(String message, Throwable cause) {
+	super(message, cause);
+    }
+
+    public AuthenticationException(String message) {
+	super(message);
+    }
+
+    public AuthenticationException(Throwable cause) {
+	super(cause);
+    }
+}
