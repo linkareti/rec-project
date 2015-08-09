23a7654c71bac60cc0215d33b798a7809a0570fe
handyman <handyman@linkare.com>
2015-08-09 00:05:33 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1813.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@11250 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/webrobot/src/java/client/pt/utl/ist/elab/client/webrobot/customizer/Comps/Frente.java
@@ -0,0 +1,197 @@
+/*
+ * Frente.java
+ *
+ * Created on 27 de Janeiro de 2003, 16:05
+ */
+
+package pt.utl.ist.elab.client.webrobot.customizer.Comps;
+
+/**
+ * 
+ * @author André Neto - LEFT - IST
+ */
+public class Frente extends pt.utl.ist.elab.client.webrobot.customizer.Comps.Block {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3304161679669966674L;
+	private final int WIDTH = 77;
+	private final int HEIGHT = 45;
+	private final int LINEWIDTH = 6;
+	private final java.awt.Dimension dimension = new java.awt.Dimension(WIDTH, HEIGHT);
+	private final int TIPO = 3;
+	private final javax.swing.ImageIcon image = new javax.swing.ImageIcon(getClass().getResource(
+			"/pt/utl/ist/elab/client/webrobot/customizer/Comps/Icons/frente.gif"));
+	private pt.utl.ist.elab.client.webrobot.customizer.Models.ModelFrente model;
+	private pt.utl.ist.elab.client.webrobot.customizer.Comps.Configs.ConfFrente confFrente;
+	private final java.awt.Color backgroundColor = new java.awt.Color(204, 204, 204);
+	private String text;
+	private final String fullNameDescription = "Segue em frente";
+
+	/** Holds value of property paintBottom. */
+	private boolean paintBottom = false;
+
+	/** Holds value of property paintLeft. */
+	private boolean paintLeft = false;
+
+	/** Holds value of property cancel. */
+	private boolean cancel = false;
+
+	/**
+	 * Creates a new instance of CompInt
+	 * 
+	 * @param parent
+	 */
+	public Frente(final javax.swing.JFrame parent) {
+		super();
+		setCancel(false);
+		model = new pt.utl.ist.elab.client.webrobot.customizer.Models.ModelFrente();
+		confFrente = new pt.utl.ist.elab.client.webrobot.customizer.Comps.Configs.ConfFrente(parent, true, model);
+		new pt.utl.ist.elab.client.webrobot.customizer.Utils.CenterFrame(parent, confFrente);
+		confFrente.setVisible(true);
+		if (confFrente.isCancel()) {
+			setCancel(true);
+			return;
+		}
+		setMinimumSize(dimension);
+		setPreferredSize(dimension);
+		setMaximumSize(dimension);
+		setBorderPainted(false);
+		setBackground(new java.awt.Color(0, 0, 0, 0));// background transparente
+		setOpaque(false);
+		text = "";
+	}
+
+	public Frente(final pt.utl.ist.elab.client.webrobot.customizer.Models.ModelFrente model) {
+		super();
+		setCancel(false);
+		this.model = new pt.utl.ist.elab.client.webrobot.customizer.Models.ModelFrente();
+		this.model.setBaixo(model.getBaixo());
+		this.model.setColuna(model.getColuna());
+		this.model.setD1(model.getD1());
+		this.model.setD2(model.getD2());
+		this.model.setD3(model.getD3());
+		this.model.setEsquerda(model.getEsquerda());
+		this.model.setFlag(model.getFlag());
+		this.model.setNivel(model.getNivel());
+		this.model.setValor(model.getValor());
+		this.model.setValor2(model.getValor2());
+		setMinimumSize(dimension);
+		setPreferredSize(dimension);
+		setMaximumSize(dimension);
+		setBorderPainted(false);
+		setBackground(new java.awt.Color(0, 0, 0, 0));// background transparente
+		setOpaque(false);
+		text = "";
+	}
+
+	@Override
+	public void paintComponent(final java.awt.Graphics g) {
+		final java.awt.Graphics2D g2 = (java.awt.Graphics2D) g;
+		g2.setStroke(new java.awt.BasicStroke(3f));
+		g.setColor(backgroundColor);
+		g.fillRect(0, 0, this.getSize().width, this.getSize().height);
+		g.drawImage(image.getImage(), 0, 0, image.getIconWidth(), image.getIconHeight(), null);
+		g.setColor(java.awt.Color.black);
+		if (paintBottom) {
+			g2.drawLine(image.getIconWidth() / 2, image.getIconHeight(), image.getIconWidth() / 2,
+					image.getIconHeight() + LINEWIDTH);
+		}
+		if (paintLeft) {
+			g2.drawLine(image.getIconWidth(), image.getIconHeight() / 2, image.getIconWidth() + LINEWIDTH,
+					image.getIconHeight() / 2);
+		}
+		g.setColor(java.awt.Color.black);
+		text = "" + model.getValor();
+		g.drawString(text, (image.getIconWidth() / 4), image.getIconWidth() / 3);
+		super.paintComponent(g);
+	}
+
+	@Override
+	public pt.utl.ist.elab.client.webrobot.customizer.Models.ModelBlock getDataModel() {
+		return model;
+	}
+
+	public void setDataModel(final pt.utl.ist.elab.client.webrobot.customizer.Models.ModelFrente model) {
+		this.model = model;
+	}
+
+	@Override
+	public void edit(final javax.swing.JFrame parent) {
+		confFrente = new pt.utl.ist.elab.client.webrobot.customizer.Comps.Configs.ConfFrente(parent, true, model);
+		new pt.utl.ist.elab.client.webrobot.customizer.Utils.CenterFrame(parent, confFrente);
+		confFrente.setVisible(true);
+	}
+
+	@Override
+	public int getTipo() {
+		return TIPO;
+	}
+
+	@Override
+	public String getFullNameDescription() {
+		return fullNameDescription;
+	}
+
+	/**
+	 * Getter for property paintBottom.
+	 * 
+	 * @return Value of property paintBottom.
+	 */
+	@Override
+	public boolean isPaintBottom() {
+		return paintBottom;
+	}
+
+	/**
+	 * Setter for property paintBottom.
+	 * 
+	 * @param paintBottom New value of property paintBottom.
+	 */
+	@Override
+	public void setPaintBottom(final boolean paintBottom) {
+		this.paintBottom = paintBottom;
+		repaint();
+	}
+
+	/**
+	 * Getter for property paintLeft.
+	 * 
+	 * @return Value of property paintLeft.
+	 */
+	@Override
+	public boolean isPaintLeft() {
+		return paintLeft;
+	}
+
+	/**
+	 * Setter for property paintLeft.
+	 * 
+	 * @param paintLeft New value of property paintLeft.
+	 */
+	@Override
+	public void setPaintLeft(final boolean paintLeft) {
+		this.paintLeft = paintLeft;
+		repaint();
+	}
+
+	/**
+	 * Getter for property cancel.
+	 * 
+	 * @return Value of property cancel.
+	 */
+	@Override
+	public boolean isCancel() {
+		return cancel;
+	}
+
+	/**
+	 * Setter for property cancel.
+	 * 
+	 * @param cancel New value of property cancel.
+	 */
+	@Override
+	public void setCancel(final boolean cancel) {
+		this.cancel = cancel;
+	}
+}
