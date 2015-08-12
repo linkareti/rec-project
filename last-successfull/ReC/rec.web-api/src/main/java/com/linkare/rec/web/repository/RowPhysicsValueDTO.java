337f3b483f233472a3aa4823c7471ef15ca9cc62
handyman <handyman@linkare.com>
2015-08-12 00:03:45 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2098.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@11260 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.web-api/src/main/java/com/linkare/rec/web/repository/RowPhysicsValueDTO.java
@@ -0,0 +1,39 @@
+package com.linkare.rec.web.repository;
+
+import java.util.List;
+
+/**
+ * 
+ * 
+ * @author Artur Correia - Linkare TI
+ * 
+ */
+public class RowPhysicsValueDTO extends AbstractBaseDTO {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private List<PhysicsValueDTO> columnValues;
+
+    public RowPhysicsValueDTO() {
+    }
+
+    public List<PhysicsValueDTO> getColumnValues() {
+	return columnValues;
+    }
+
+    public void setColumnValues(List<PhysicsValueDTO> column) {
+	this.columnValues = column;
+    }
+
+    @Override
+    public String toString() {
+	StringBuilder builder = new StringBuilder();
+	builder.append("RowPhysicsValueDTO [columnValues=");
+	builder.append(columnValues);
+	builder.append("]");
+	return builder.toString();
+    }
+
+}
