fe4684f634111b25e652be23179915860fa79b4f
handyman <handyman@linkare.com>
2015-08-29 00:04:54 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1824.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@11294 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/common/com/linkare/rec/data/metadata/ChannelParameterListHolder.java
@@ -0,0 +1,28 @@
+package com.linkare.rec.data.metadata;
+
+public final class ChannelParameterListHolder implements org.omg.CORBA.portable.Streamable {
+	public com.linkare.rec.data.metadata.ChannelParameter value[] = null;
+
+	public ChannelParameterListHolder() {
+	}
+
+	public ChannelParameterListHolder(final com.linkare.rec.data.metadata.ChannelParameter[] initialValue) {
+		value = initialValue;
+	}
+
+	@Override
+	public void _read(final org.omg.CORBA.portable.InputStream i) {
+		value = com.linkare.rec.data.metadata.ChannelParameterListHelper.read(i);
+	}
+
+	@Override
+	public void _write(final org.omg.CORBA.portable.OutputStream o) {
+		com.linkare.rec.data.metadata.ChannelParameterListHelper.write(o, value);
+	}
+
+	@Override
+	public org.omg.CORBA.TypeCode _type() {
+		return com.linkare.rec.data.metadata.ChannelParameterListHelper.type();
+	}
+
+}
