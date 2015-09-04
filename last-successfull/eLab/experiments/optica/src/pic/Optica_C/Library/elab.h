a054b4177922a565ee4c28cd89f0338cd42b2679
handyman <handyman@linkare.com>
2015-09-04 00:04:59 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1828.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@11310 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/optica/src/pic/Optica_C/Library/elab.h
@@ -0,0 +1,156 @@
+#include <p30f4013.h>
+#include <stdio.h>			//biblioteca standart IO do C
+#include <libpic30.h>		//definicoes extra para as bibliotecas do C30
+#include <uart.h>			//biblioteca com as funcoes para usar a UART (porta serie)
+#include <string.h>
+#include "user_defines.h"
+
+#define INPUT  1
+#define OUTPUT 0
+
+//
+// timer.c
+//
+void open_timer1(void);
+void open_timer1_for_communication(void);
+
+//
+//delays.c
+//
+void delay_ms(unsigned int delay);
+void delay_100ys(unsigned int delay);
+void delay_10ys(unsigned int delay);
+
+//
+// uart.c
+//
+typedef struct{
+	char rbuf[80];
+	char *receiveddata;
+} Communication;
+
+extern Communication Serial;
+
+void configure_uart2(void);
+int command_received(void);
+void __attribute__((__interrupt__,auto_psv)) _U2RXInterrupt(void);
+
+//
+//adc.c
+//
+void configure_adc();
+void configure_adc_channel(int);
+int read_adc(int);
+
+
+//
+//step_motor.c
+//
+#define MOTOR_BITS_LOW (~(0xF<<MOTOR_FIRST_BIT))
+extern const unsigned char step [2][8];
+extern unsigned int motor_actual_position;
+extern int last_way;
+extern int real_position;
+//extern int aux_last;
+void open_motor();
+void walk_motor(unsigned int way, unsigned int steps, unsigned int delay);
+void walk_motor_to(unsigned int to, unsigned int delay);
+void calibrate_motor();
+void motor_compensation(unsigned int delay, int way);
+//void motor_clearance(unsigned int inverse_way, unsigned int steps, unsigned int delay);
+
+//
+//step_motor2.c
+//
+#define MOTOR2_BITS_LOW (~(0xF<<MOTOR2_FIRST_BIT))
+extern unsigned int motor2_actual_position;
+extern int last_way2;
+extern int real_position2;
+void open_motor2();
+void walk_motor2(unsigned int way, unsigned int steps, unsigned int delay);
+void walk_motor2_to(unsigned int to, unsigned int delay);
+void calibrate_motor2();
+void motor2_compensation(unsigned int delay, int way);
+//void motor2_clearance(unsigned int inverse_way, unsigned int steps, unsigned int delay);
+
+//
+//servo.c
+//
+void open_servo();
+void move_servo(unsigned int delay);
+
+
+//
+//servo2.c
+//
+//void open_servo2();
+//void move_servo2(unsigned int delay);
+
+
+//
+//captures.c
+//
+//extern unsigned int Capture_counter;
+//void open_inputcapture1();
+
+//
+//(main_file).c
+//
+int main();
+int angle_1_conversion(double);
+double conv_angle_1(int);
+int angle_2_conversion(double);
+double conv_angle_2(int);
+//void __attribute__((__interrupt__,auto_psv)) _ADCInterrupt();
+//void __attribute__((__interrupt__,auto_psv)) _T3Interrupt(void);
+//void __attribute__((__interrupt__,auto_psv)) _IC1Interrupt(void);
+
+//
+// rec_generic_driver.c
+//
+extern char state[20];
+extern int stop;
+void rec_generic_driver(void);
+
+extern int protocolo;
+extern double param_1;
+extern double param_2;
+extern double param_3;
+extern double param_4;
+extern int param_5;
+extern double param_6;
+extern int param_7;
+
+//
+// maq_de_estados.c
+//
+//extern int protocolo;
+//extern double param_1;
+//extern double param_2;
+//extern double param_3;
+//extern double param_4;
+//extern double param_5;
+//extern double param_6;
+//extern int param_7;
+void maq_de_estados(void);
+
+
+//
+// protocolos.c
+//
+extern double param_1_aux;
+void protocolo_1_configuring(void);
+void protocolo_2_configuring(void);
+void protocolo_3_configuring(void);
+void protocolo_4_configuring(void);
+void protocolo_1_starting(void);
+void protocolo_2_starting(void);
+void protocolo_3_starting(void);
+void protocolo_4_starting(void);
+void protocolo_1_started();
+void protocolo_2_started();
+void protocolo_3_started();
+void protocolo_4_started();
+void protocolo_5_started();
+void protocolo_6_started();
+void stopping();
---
+++ last-successfull/eLab/rec/src/java/client/com/linkare/customizer/PropertyObject.java
@@ -0,0 +1,181 @@
+package com.linkare.customizer;
+
+import java.beans.PropertyEditor;
+
+/**
+ * 
+ * @author jp
+ */
+public class PropertyObject {
+
+	/** Holds value of property value. */
+	private Object value;
+
+	/** Holds value of property propertyEditor. */
+	private PropertyEditor propertyEditor;
+
+	/** Holds value of property valueClass. */
+	private Class<?> valueClass;
+
+	/** Holds value of property readOnly. */
+	private boolean editable;
+
+	/** Holds value of property name. */
+	private String name;
+
+	/**
+	 * Creates a new instance of IndexedPropertyObject
+	 * 
+	 * @param name The name of the javabean property being edited
+	 * @param propertyEditor The property editor associated with that property
+	 * @param valueClass the type of the value that should be edited
+	 * @param editable Is the property editable?
+	 */
+	public PropertyObject(final String name, final PropertyEditor propertyEditor, final Class<?> valueClass,
+			final boolean editable) {
+		setName(name);
+		setPropertyEditor(propertyEditor);
+		setValueClass(valueClass);
+		setEditable(editable);
+	}
+
+	/**
+	 * Creates a new instance of IndexedPropertyObject
+	 * 
+	 * @param name The name of the javabean property being edited
+	 * @param propertyEditor The property editor associated with that property
+	 * @param valueClass the type of the value that should be edited
+	 * @param editable Is the property editable?
+	 * @param value The current property's value
+	 */
+	public PropertyObject(final String name, final PropertyEditor propertyEditor, final Class<?> valueClass,
+			final boolean editable, final Object value) {
+		this(name, propertyEditor, valueClass, editable);
+		setValue(value);
+	}
+
+	/**
+	 * Getter for property value.
+	 * 
+	 * @return Value of property value.
+	 * 
+	 */
+	public Object getValue() {
+		return value;
+	}
+
+	/**
+	 * Setter for property value.
+	 * 
+	 * @param value New value of property value.
+	 * 
+	 */
+	public void setValue(final Object value) {
+		this.value = value;
+		if (this.value == null && getValueClass().isPrimitive()) {
+			try {
+				if (getValueClass() == boolean.class) {
+					this.value = Boolean.valueOf(false);
+				} else if (getValueClass() == byte.class) {
+					this.value = Byte.valueOf((byte) 0);
+				} else if (getValueClass() == char.class) {
+					this.value = Character.valueOf(' ');
+				} else if (getValueClass() == short.class) {
+					this.value = Short.valueOf((short) 0);
+				} else if (getValueClass() == int.class) {
+					this.value = Integer.valueOf(0);
+				} else if (getValueClass() == long.class) {
+					this.value = Long.valueOf(0);
+				} else if (getValueClass() == float.class) {
+					this.value = Float.valueOf(0.F);
+				} else if (getValueClass() == double.class) {
+					this.value = Double.valueOf(0.);
+				}
+
+			} catch (final Exception e) {
+				javax.swing.JOptionPane.showMessageDialog(null, "Couldn't init value...\n\r" + e.getMessage());
+			}
+		}
+	}
+
+	/**
+	 * Getter for property propertyEditor.
+	 * 
+	 * @return Value of property propertyEditor.
+	 * 
+	 */
+	public PropertyEditor getPropertyEditor() {
+		return propertyEditor;
+	}
+
+	/**
+	 * Setter for property propertyEditor.
+	 * 
+	 * @param propertyEditor New value of property propertyEditor.
+	 * 
+	 */
+	public void setPropertyEditor(final PropertyEditor propertyEditor) {
+		this.propertyEditor = propertyEditor;
+	}
+
+	/**
+	 * Getter for property valueClass.
+	 * 
+	 * @return Value of property valueClass.
+	 * 
+	 */
+	public Class<?> getValueClass() {
+		return valueClass;
+	}
+
+	/**
+	 * Setter for property valueClass.
+	 * 
+	 * @param valueClass New value of property valueClass.
+	 * 
+	 */
+	public void setValueClass(final Class<?> valueClass) {
+		this.valueClass = valueClass;
+	}
+
+	/**
+	 * Getter for property readOnly.
+	 * 
+	 * @return Value of property readOnly.
+	 * 
+	 */
+	public boolean isEditable() {
+		return editable;
+	}
+
+	/**
+	 * Setter for property readOnly.
+	 * 
+	 * @param editable New value of property editable.
+	 * 
+	 */
+	public void setEditable(final boolean editable) {
+		this.editable = editable;
+	}
+
+	/**
+	 * Getter for property name.
+	 * 
+	 * @return Value of property name.
+	 * 
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Setter for property name.
+	 * 
+	 * @param name New value of property name.
+	 * 
+	 */
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+}
