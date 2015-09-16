95457d12fed39a7df3cdbdef453190ca6903f9a7
handyman <handyman@linkare.com>
2015-09-16 00:04:05 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2133.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@11352 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/multicast/com/linkare/rec/impl/utils/mbean/Hardware.java
@@ -0,0 +1,106 @@
+/* 
+ * Hardware.java created on Jun 11, 2012
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.linkare.rec.impl.utils.mbean;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.management.ListenerNotFoundException;
+import javax.management.MBeanNotificationInfo;
+import javax.management.NotificationEmitter;
+import javax.management.NotificationFilter;
+import javax.management.NotificationListener;
+
+import com.linkare.rec.web.ClientInfoDTO;
+import com.linkare.rec.web.HardwareInfoDTO;
+import com.linkare.rec.web.mbean.IHardwareMXBean;
+import com.linkare.rec.web.mbean.NotificationTypeEnum;
+import com.linkare.rec.impl.multicast.ReCMultiCastHardware;
+import com.linkare.rec.impl.utils.mapping.DTOMapperUtils;
+import com.linkare.rec.impl.wrappers.HardwareWrapper;
+
+/**
+ * 
+ * @author Artur Correia - Linkare TI
+ */
+public class Hardware implements IHardwareMXBean, NotificationEmitter {
+
+	private final ReCMultiCastHardware reCMultiCastHardware;
+
+	private final HardwareWrapper hardware;
+
+
+	public Hardware(final ReCMultiCastHardware reCMultiCastHardware) {
+		this.reCMultiCastHardware = reCMultiCastHardware;
+		hardware = this.reCMultiCastHardware.getHardware();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public HardwareInfoDTO getHardwareInfo() {
+		return DTOMapperUtils.mapToHardwareInfoDTO(hardware.getHardwareInfo());
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getState() {
+		return hardware.getHardwareState().toString();
+	}
+
+	@Override
+	public MBeanNotificationInfo[] getNotificationInfo() {
+		return NotificationTypeEnum.getMBeanNotificationInfo();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void addNotificationListener(NotificationListener listener, NotificationFilter filter, Object handback)
+			throws IllegalArgumentException {
+		reCMultiCastHardware.addNotificationListener(listener, filter, handback);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void removeNotificationListener(NotificationListener listener) throws ListenerNotFoundException {
+		reCMultiCastHardware.removeNotificationListener(listener);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void removeNotificationListener(NotificationListener listener, NotificationFilter filter, Object handback)
+			throws ListenerNotFoundException {
+		reCMultiCastHardware.removeNotificationListener(listener, filter, handback);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<ClientInfoDTO> getClientList() {
+		return DTOMapperUtils.mapToClientInfoDTOList(reCMultiCastHardware.getClientList());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void kickUsers(Set<String> usersToKick) {
+		reCMultiCastHardware.kickUsers(usersToKick);
+	}
+
+}
