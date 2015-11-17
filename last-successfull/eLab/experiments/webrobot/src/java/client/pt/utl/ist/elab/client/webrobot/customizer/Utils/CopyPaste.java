7da8cb5443bbd5223b51edca78a37f0110991a99
handyman <handyman@linkare.com>
2015-11-17 01:04:39 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1871.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@11482 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/webrobot/src/java/client/pt/utl/ist/elab/client/webrobot/customizer/Utils/CopyPaste.java
@@ -0,0 +1,38 @@
+/*
+ * CopyPaste.java
+ *
+ * Created on 19 de Fevereiro de 2003, 11:19
+ */
+
+package pt.utl.ist.elab.client.webrobot.customizer.Utils;
+
+/**
+ * 
+ * @author André Neto - LEFT - IST
+ */
+public class CopyPaste {
+
+	pt.utl.ist.elab.client.webrobot.customizer.Comps.Block[][] matrixToCopy = new pt.utl.ist.elab.client.webrobot.customizer.Comps.Block[3][3];// Security
+	// initialization
+	String[][] matrixWiringToCopy = new String[3][3];// Security initialization
+
+	/** Creates a new instance of CopyPaste */
+	public CopyPaste() {
+	}
+
+	public void addData(final pt.utl.ist.elab.client.webrobot.customizer.Comps.Block[][] matrixToCopy) {
+		this.matrixToCopy = matrixToCopy;
+	}
+
+	public void addData(final String[][] matrixWiringToCopy) {
+		this.matrixWiringToCopy = matrixWiringToCopy;
+	}
+
+	public pt.utl.ist.elab.client.webrobot.customizer.Comps.Block[][] getMatrixData() {
+		return matrixToCopy;
+	}
+
+	public String[][] getMatrixWiringData() {
+		return matrixWiringToCopy;
+	}
+}
