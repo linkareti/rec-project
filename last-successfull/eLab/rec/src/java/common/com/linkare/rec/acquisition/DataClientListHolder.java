382fe7f328e940df17f71d2518f99974e7dea867
handyman <handyman@linkare.com>
2015-11-19 01:04:45 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1873.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@11490 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/common/com/linkare/rec/acquisition/DataClientListHolder.java
@@ -0,0 +1,30 @@
+package com.linkare.rec.acquisition;
+
+/** CORBA Helper class */
+public final class DataClientListHolder implements org.omg.CORBA.portable.Streamable {
+	public com.linkare.rec.acquisition.DataClient value[] = null;
+
+	public DataClientListHolder() {
+	}
+
+	@SuppressWarnings("PMD.ArrayIsStoredDirectly")
+	public DataClientListHolder(final com.linkare.rec.acquisition.DataClient[] initialValue) {
+		value = initialValue;
+	}
+
+	@Override
+	public void _read(final org.omg.CORBA.portable.InputStream i) {
+		value = com.linkare.rec.acquisition.DataClientListHelper.read(i);
+	}
+
+	@Override
+	public void _write(final org.omg.CORBA.portable.OutputStream o) {
+		com.linkare.rec.acquisition.DataClientListHelper.write(o, value);
+	}
+
+	@Override
+	public org.omg.CORBA.TypeCode _type() {
+		return com.linkare.rec.acquisition.DataClientListHelper.type();
+	}
+
+}
