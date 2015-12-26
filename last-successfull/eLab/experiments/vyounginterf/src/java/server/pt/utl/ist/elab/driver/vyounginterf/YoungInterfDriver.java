21eb697b9fe038748d065ba78bd12c8847a1ea68
handyman <handyman@linkare.com>
2015-12-26 01:06:36 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1910.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@11638 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/vyounginterf/src/java/server/pt/utl/ist/elab/driver/vyounginterf/YoungInterfDriver.java
@@ -0,0 +1,89 @@
+package pt.utl.ist.elab.driver.vyounginterf;
+
+import pt.utl.ist.elab.driver.virtual.VirtualBaseDataSource;
+import pt.utl.ist.elab.driver.virtual.VirtualBaseDriver;
+
+import com.linkare.rec.acquisition.IncorrectStateException;
+import com.linkare.rec.acquisition.WrongConfigurationException;
+import com.linkare.rec.data.config.HardwareAcquisitionConfig;
+import com.linkare.rec.data.metadata.HardwareInfo;
+import com.linkare.rec.impl.driver.IDataSource;
+
+/**
+ * 
+ * @author Emanuel Antunes
+ */
+public class YoungInterfDriver extends VirtualBaseDriver {
+
+	// private static final Logger LOGGER =
+	// Logger.getLogger(YoungInterfDriver.class.getName());
+
+	/* Hardware and driver related variables */
+	private static final String DRIVER_UNIQUE_ID = "INTERFERENCIA_YOUNG_V1.0";
+
+	protected VirtualBaseDataSource dataSource = null;
+	protected HardwareAcquisitionConfig config = null;
+	protected HardwareInfo info = null;
+
+	/** Creates a new instance of YoungInterfDriver */
+	public YoungInterfDriver() {
+	}
+
+	@Override
+	public void configure(final HardwareAcquisitionConfig config, final HardwareInfo info)
+			throws WrongConfigurationException {
+		// Recebemos ordem para configurar, no HardwareAcquisitionConfig estao
+		// todas as informacoes escolhidas pelo cliente...agora e' so' pedir
+		this.config = config;
+		this.info = info;
+		final float lambda = Float.parseFloat(config.getSelectedHardwareParameterValue("lambda"));
+		final float dfendas = Float.parseFloat(config.getSelectedHardwareParameterValue("dfendas"));
+		final float dplanos = Float.parseFloat(config.getSelectedHardwareParameterValue("dplanos"));
+		final float lpadrao = Float.parseFloat(config.getSelectedHardwareParameterValue("lpadrao"));
+
+		final int tbs = (int) config.getSelectedFrequency().getFrequency();
+		final int nSamples = config.getTotalSamples();
+
+		// Vamos criar o nosso produtor de dados!
+		dataSource = new YoungInterfDataProducer(this, dfendas, dplanos, lambda, lpadrao, tbs, nSamples);
+
+		// NECESSARIO!! Colocar o numero de amostra para todos os canais!
+		for (int i = 0; i < config.getChannelsConfig().length; i++) {
+			config.getChannelsConfig(i).setTotalSamples(config.getTotalSamples());
+		}
+
+		// NAO ESQUECER ESTA LINHA!
+		dataSource.setAcquisitionHeader(config);
+
+		fireIDriverStateListenerDriverConfigured();
+	}
+
+	@Override
+	public String getDriverUniqueID() {
+		return YoungInterfDriver.DRIVER_UNIQUE_ID;
+	}
+
+	@Override
+	public void shutdown() {
+		if (dataSource != null) {
+			dataSource.stopNow();
+		}
+		super.shutDownNow();
+	}
+
+	@Override
+	public IDataSource start(final HardwareInfo info) throws IncorrectStateException {
+		fireIDriverStateListenerDriverStarting();
+		dataSource.startProduction();
+		fireIDriverStateListenerDriverStarted();
+		return dataSource;
+	}
+
+	@Override
+	public void stop(final HardwareInfo info) throws IncorrectStateException {
+		fireIDriverStateListenerDriverStoping();
+		dataSource.stopNow();
+		fireIDriverStateListenerDriverStoped();
+	}
+
+}
