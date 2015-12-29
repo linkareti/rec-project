0d5a8b25dda4cbc22dbcf34d2784cd46fba8c7f0
handyman <handyman@linkare.com>
2015-12-29 01:04:23 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2254.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@11648 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.web/src/main/java/com/linkare/rec/web/model/ByteArrayVal.java
@@ -0,0 +1,48 @@
+package com.linkare.rec.web.model;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.PrePersist;
+
+/**
+ * 
+ * @author Artur Correia - Linkare TI
+ */
+@Entity
+public class ByteArrayVal extends PhysicsVal {
+
+    private static final long serialVersionUID = 1L;
+
+    @Embedded
+    private ByteArrayValue value;
+
+    public ByteArrayVal() {
+    }
+
+    public ByteArrayValue getValue() {
+	return value;
+    }
+
+    public void setValue(ByteArrayValue value) {
+	this.value = value;
+    }
+
+    /*
+     * @Override public int hashCode() { int hash = 0; hash += (id != null ? id.hashCode() : 0); return hash; }
+     * 
+     * @Override public boolean equals(Object object) { // TODO: Warning - this method won't work in the case the id fields are not set if (!(object instanceof
+     * EByteArrayValue)) { return false; } EByteArrayValue other = (EByteArrayValue) object; if ((this.id == null && other.id != null) || (this.id != null &&
+     * !this.id.equals(other.id))) { return false; } return true; }
+     */
+
+    @PrePersist
+    public void preInsert() {
+	//	try {
+	//	    InitialContext ic = new InitialContext();
+	//	    ConsultarReCLocal beanLocal = (ConsultarReCLocal) ic.lookup("java:comp/env/ejb/com.linkare.rec.ejb.ConsultarReCBeanBean");
+	//	    beanLocal.sendMessage(this.getValue().getVirtualPath(), this.getValue().getData());
+	//	} catch (NamingException ex) {
+	//	}
+
+    }
+}
