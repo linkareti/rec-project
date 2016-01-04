23df4d258a4ab7914f62b7d18382ddcad527eb99
handyman <handyman@linkare.com>
2016-01-04 01:04:27 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2260.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@11672 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/pv/src/java/client/pt/utl/ist/elab/client/pv/PVXYGraphDisplay.java
@@ -0,0 +1,27 @@
+/*
+ * PVXYGraphDisplay.java
+ *
+ * Created on 10 de Julho de 2003, 11:14
+ */
+
+package pt.utl.ist.elab.client.pv;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public class PVXYGraphDisplay extends com.linkare.rec.impl.ui.graph.DefaultXYExperimentGraph {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3177968201292210791L;
+
+	/** Creates a new instance of PVXYGraphDisplay */
+	public PVXYGraphDisplay() {
+		super();
+		setChannelDisplayX(0);
+		setChannelDisplayY(1);
+	}
+
+}
