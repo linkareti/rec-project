3e562a84896607ac380a3ba0ff51cdf88f6599ed
handyman <handyman@linkare.com>
2016-01-23 01:06:25 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1938.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@11750 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/common/com/linkare/rec/jmf/media/protocol/function/PulseFunctorControlComponent.java
@@ -0,0 +1,39 @@
+package com.linkare.rec.jmf.media.protocol.function;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class PulseFunctorControlComponent extends JPanel {
+
+	/**
+     * 
+     */
+	private static final long serialVersionUID = 9020402813301420731L;
+
+	private PulseFunctorControl pulseFunctorControl;
+
+	public PulseFunctorControlComponent(PulseFunctorControl pulseFunctorControl) {
+		this.pulseFunctorControl = pulseFunctorControl;
+		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+
+		this.add(new JLabel("Frequency Pulse"));
+		final JSlider frequencyRangeSlider = new JSlider(20, 20000, 20);
+		this.add(frequencyRangeSlider);
+		frequencyRangeSlider.setValue((int) pulseFunctorControl.getFrequency());
+		
+		frequencyRangeSlider.getModel().addChangeListener(new ChangeListener() {
+
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				int frequency = frequencyRangeSlider.getValue();
+				PulseFunctorControlComponent.this.pulseFunctorControl.setFrequency(frequency);
+			}
+		});
+
+	}
+
+}
