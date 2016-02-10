b85a6dab0a7a51ceb0bc0c8f3beef6752d390d26
handyman <handyman@linkare.com>
2016-02-10 01:05:42 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1956.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@11822 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/ui/com/linkare/rec/impl/newface/laf/flat/elabtheme/RadioButtonMenuItemStyle.java
@@ -0,0 +1,71 @@
+/* 
+ * MenuBarStyle.java created on Mar 27, 2009
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.linkare.rec.impl.newface.laf.flat.elabtheme;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.util.Map;
+
+import javax.swing.plaf.FontUIResource;
+
+import com.linkare.rec.impl.newface.laf.flat.theme.AbstractStyle;
+import com.linkare.rec.impl.newface.laf.flat.theme.Style;
+
+/**
+ * @author Henrique Fernandes
+ */
+@Style
+public class RadioButtonMenuItemStyle extends DefaultStyle {
+
+	// ENABLED COLORS
+	private static final Color COLOR_MENU_ITEM_BG = new Color(0x263537);
+	private static final Color COLOR_MENU_ITEM_FG = new Color(0xE4EEED);
+	private static final Color COLOR_MENU_BORDER = new Color(0xE4EEED);
+	private static final Color COLOR_MENU_DISABLED_FG = new Color(0x22363A);
+	private static final Color COLOR_MENU_ACCELERATOR_FG = new Color(0xE4EEED);
+	// PROPERTIES
+	private static final String PT_MENU_SELECTION_BG = "selectionBackground";
+	private static final String PT_MENU_SELECTION_FG = "selectionForeground";
+	private static final String PT_MENU_DISABLED_FG = "disabledForeground";
+	private static final String PT_MENU_ACCELERATOR_FG = "acceleratorForeground";
+	private static final String PT_MENU_MARGIN = "margin";
+	private static final String PT_FONT_ACCELERATOR = "acceleratorFont";
+
+	// LABEL FONT
+	public static final Font FONT_MENU = new FontUIResource("Arial", Font.BOLD, 10);
+
+	public static final String ID = "RadioButtonMenuItem";
+
+	/**
+	 * Creates the <code>RadioButtonMenuItem</code>.
+	 */
+	public RadioButtonMenuItemStyle() {
+		super();
+	}
+
+	@Override
+	protected String defineStyleId() {
+		return RadioButtonMenuItemStyle.ID;
+	}
+
+	@Override
+	public void updatePropertyMap(final Map<String, Object> map) {
+		super.updatePropertyMap(map);
+		map.put(AbstractStyle.BORDER, DefaultStyle.EMPTY_BORDER_MARGIN_2);
+		map.put(AbstractStyle.FOREGROUND, RadioButtonMenuItemStyle.COLOR_MENU_ITEM_FG);
+		map.put(AbstractStyle.BACKGROUND, RadioButtonMenuItemStyle.COLOR_MENU_ITEM_BG);
+		map.put(RadioButtonMenuItemStyle.PT_MENU_SELECTION_FG, RadioButtonMenuItemStyle.COLOR_MENU_ITEM_BG);
+		map.put(RadioButtonMenuItemStyle.PT_MENU_SELECTION_BG, RadioButtonMenuItemStyle.PT_MENU_SELECTION_FG);
+		map.put(AbstractStyle.FONT, RadioButtonMenuItemStyle.FONT_MENU);
+		map.put(RadioButtonMenuItemStyle.PT_FONT_ACCELERATOR, RadioButtonMenuItemStyle.FONT_MENU);
+		map.put(RadioButtonMenuItemStyle.PT_MENU_ACCELERATOR_FG, RadioButtonMenuItemStyle.COLOR_MENU_ACCELERATOR_FG);
+	}
+
+	// -------------------------------------------------------------------------
+	// TODO Getters (Used to marshal values to xml)
+
+}
