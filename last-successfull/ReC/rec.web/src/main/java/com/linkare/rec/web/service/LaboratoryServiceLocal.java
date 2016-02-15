4dda69dff0f31014700939980acdcafb1571e9d7
handyman <handyman@linkare.com>
2016-02-15 01:04:09 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2302.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@11840 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.web/src/main/java/com/linkare/rec/web/service/LaboratoryServiceLocal.java
@@ -0,0 +1,12 @@
+package com.linkare.rec.web.service;
+
+import javax.ejb.Local;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ * 
+ */
+@Local
+public interface LaboratoryServiceLocal extends LaboratoryService {
+}
---
+++ last-successfull/ReC/rec/src/java/multicast/com/linkare/rec/impl/multicast/IDataClientForQueueListener.java
@@ -0,0 +1,16 @@
+/*
+ * IDataClientForQueueCallback.java
+ *
+ * Created on 12 de Maio de 2003, 18:36
+ */
+
+package com.linkare.rec.impl.multicast;
+
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public interface IDataClientForQueueListener {
+	public void dataClientForQueueIsGone(DataClientForQueue dcfq);
+}
