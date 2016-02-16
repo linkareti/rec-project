b97428d684fcd56c763359cc820f7cac8c3f0f1f
handyman <handyman@linkare.com>
2016-02-16 01:04:02 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2303.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@11844 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/common/com/linkare/rec/impl/threading/TimedOutException.java
@@ -0,0 +1,36 @@
+/*
+ * TimedOutException.java
+ *
+ * Created on 11 May 2003, 14:07
+ */
+
+package com.linkare.rec.impl.threading;
+
+/**
+ * 
+ * @author Jose Pedro Pereira
+ */
+public class TimedOutException extends java.lang.Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7306618511566262991L;
+
+	/**
+	 * Creates a new instance of <code>TimedOutException</code> without detail
+	 * message.
+	 */
+	public TimedOutException() {
+	}
+
+	/**
+	 * Constructs an instance of <code>TimedOutException</code> with the
+	 * specified detail message.
+	 * 
+	 * @param msg the detail message.
+	 */
+	public TimedOutException(final String msg) {
+		super(msg);
+	}
+}
