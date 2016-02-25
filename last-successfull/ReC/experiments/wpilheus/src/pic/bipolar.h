5a4fb6ec78d914f20426be5752720df7fcecd52a
handyman <handyman@linkare.com>
2016-02-25 01:04:25 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2312.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@11880 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/wpilheus/src/pic/bipolar.h
@@ -0,0 +1,8 @@
+#ifndef _BIPOLAR_H_
+#define _BIPOLAR_H_
+
+void stepBipolar(int direction);
+void releaseBipolar();
+void holdBipolar();
+
+#endif
---
+++ last-successfull/ReC/rec/src/java/multicast/com/linkare/rec/impl/multicast/repository/RepositoryFactory.java
@@ -0,0 +1,57 @@
+package com.linkare.rec.impl.multicast.repository;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.linkare.rec.impl.config.ReCSystemProperty;
+import com.linkare.rec.impl.multicast.repository.impl.FSRepository;
+import com.linkare.rec.utils.ClassUtils;
+
+/**
+ * 
+ * 
+ * @author Artur Correia - Linkare TI
+ */
+public final class RepositoryFactory {
+
+	private static final Logger LOGGER = Logger.getLogger(RepositoryFactory.class.getName());
+
+	private static IRepository REPOSITORY = null;
+
+	static {
+
+		final String repositoryClassName = ReCSystemProperty.MULTICAST_REPOSITORY_CLASSNAME.getValue();
+
+		try {
+			LOGGER.log(Level.FINE, "Trying to load the Repository class [" + repositoryClassName + "]");
+                                               
+			REPOSITORY = (IRepository) ClassUtils.findClass(repositoryClassName, RepositoryFactory.class.getClassLoader()).newInstance();
+
+		} catch (Exception e) {
+			LOGGER.log(Level.INFO,
+					new StringBuilder("Unable to load Repository defined by className : ").append(repositoryClassName)
+							.append(" - loading DefaultSecurityManager!").toString());
+			LOGGER.log(Level.SEVERE, "Error loading specified SecurityManager", e);
+		} catch (LinkageError e) {
+			LOGGER.log(Level.INFO,
+					new StringBuilder("Unable to load Repository defined by className : ").append(repositoryClassName)
+							.append(" - loading DefaultRepository!").toString());
+			LOGGER.log(Level.SEVERE, "Error loading specified SecurityManager", e);
+		} finally {
+			if (REPOSITORY == null) {
+				LOGGER.log(Level.INFO, "Respository not instantiated... Loading FSRepository!");
+				REPOSITORY = new FSRepository();
+			}
+		}
+
+	}
+
+	private RepositoryFactory() {
+		throw new UnsupportedOperationException("instance creation not available");
+	}
+
+	public static IRepository getRepository() {
+		return REPOSITORY;
+	}
+
+}
