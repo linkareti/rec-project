ec883c023ac5612d15c6d6dfe255e071ccbab618
handyman <handyman@linkare.com>
2016-02-25 01:05:59 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1971.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@11882 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec.web/src/main/java/com/linkare/rec/web/model/HardwareState.java
@@ -0,0 +1,41 @@
+package com.linkare.rec.web.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Represents the possible states of the Physical Hardware.
+ * 
+ * @author Bruno Catarino - Linkare TI
+ * 
+ */
+public enum HardwareState {
+    
+    UNKNOWN((byte) 0), CONFIGURING((byte) 1), CONFIGURED((byte) 2), STARTING((byte) 3), STARTED((byte) 4), STOPING((byte) 5), STOPED((byte) 6), RESETING(
+	    (byte) 7), RESETED((byte) 8);
+
+    private byte code;
+
+    private HardwareState(byte code) {
+	this.code = code;
+    }
+
+    private static final Map<Byte, HardwareState> codeMap;
+    static {
+	codeMap = new HashMap<Byte, HardwareState>();
+
+	for (final HardwareState state : HardwareState.values()) {
+	    codeMap.put(Byte.valueOf(state.code), state);
+	}
+    }
+
+    /**
+     * Returns the enum value of the state for a given state code.
+     * 
+     * @param code
+     * @return
+     */
+    public static HardwareState valueOfCode(byte code) {
+	return codeMap.get(code);
+    }
+}
