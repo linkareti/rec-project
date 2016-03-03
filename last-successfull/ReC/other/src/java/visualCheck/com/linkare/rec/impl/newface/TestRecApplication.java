1aabfa50b740ab01afe2748e180c694c7966a7fa
handyman <handyman@linkare.com>
2016-03-03 01:04:20 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2319.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@11908 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/other/src/java/visualCheck/com/linkare/rec/impl/newface/TestRecApplication.java
@@ -0,0 +1,20 @@
+/* 
+ * TestRecLauncherWithFlatLAF.java created on Mar 10, 2009
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.linkare.rec.impl.newface;
+
+import org.jdesktop.application.Application;
+
+/**
+ * @author Henrique Fernandes
+ */
+public class TestRecApplication {
+
+	public static void main(String[] args) {
+		Application.launch(ReCApplication.class, args);
+	}
+}
