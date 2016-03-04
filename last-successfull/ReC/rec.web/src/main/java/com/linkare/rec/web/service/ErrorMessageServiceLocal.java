d3d8012faa9e5b5ab5fb8be17ab2af824ef06f8f
handyman <handyman@linkare.com>
2016-03-04 01:04:15 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2320.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@11912 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.web/src/main/java/com/linkare/rec/web/service/ErrorMessageServiceLocal.java
@@ -0,0 +1,14 @@
+package com.linkare.rec.web.service;
+
+import javax.ejb.Local;
+
+
+/**
+ * 
+ * @author Bruno Catarino - Linkare TI
+ * @author Gedsimon Pereira - Linkare TI
+ * 
+ */
+@Local
+public interface ErrorMessageServiceLocal extends ErrorMessageService {
+}
