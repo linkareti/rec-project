56655e51f2d9773a69dce84e7c6491620a8b327e
handyman <handyman@linkare.com>
2016-03-09 01:04:23 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2325.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@11932 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.web-api/src/main/java/com/linkare/rec/web/mbean/MBeanObjectNameFactory.java
@@ -0,0 +1,109 @@
+/* 
+ * MBeanObjectNameFactory.java created on Mar 22, 2011
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.linkare.rec.web.mbean;
+
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+
+/**
+ * 
+ * @author Artur Correia - Linkare TI
+ */
+public final class MBeanObjectNameFactory {
+
+	/**
+	 * This class is an utility and should be used as one!
+	 */
+	private MBeanObjectNameFactory() {
+	}
+
+	private enum DomainsEnum {
+		DEFAULT_DOMAIN("com.linkare.rec");
+
+		private final String domain;
+
+		private DomainsEnum(final String domain) {
+			this.domain = domain;
+		}
+
+		public String getDomain() {
+			return domain + ":";
+		}
+	}
+
+	/**
+	 * Single point to define MBean names
+	 * 
+	 * @author Artur Correia - Linkare TI
+	 */
+	private enum MBeansNameEnum {
+		THREADPOOLEXECUTOR_STATISTICS("ThreadPoolExecutorStatistics"), HARDWARE(
+				"Hardware"), MULTICAST_CONTROLLER("RecMultiCastController");
+
+		private static final String NAME_ATTRIBUTE = "name";
+
+		private final String mbeanName;
+
+		private MBeansNameEnum(final String mbeanName) {
+			this.mbeanName = new StringBuilder(MBeansNameEnum.NAME_ATTRIBUTE)
+					.append(EQUAL).append(mbeanName).toString();
+		}
+
+		public String getMBeanName() {
+			return mbeanName;
+		}
+	}
+
+	private static final String ID = "id";
+	private static final String COMMA_SEPARATOR = ",";
+	private static final String EQUAL = "=";
+
+	private static final ObjectName THREADPOOLEXECUTOR_STATISTICS_OBJECT_NAME;
+	private static final ObjectName MULTICAST_CONTROLLER_OBJECT_NAME;
+
+	private static final String HARDWARE_BASE_NAME;
+
+	static {
+		try {
+			THREADPOOLEXECUTOR_STATISTICS_OBJECT_NAME = new ObjectName(
+					DomainsEnum.DEFAULT_DOMAIN.getDomain()
+							+ MBeansNameEnum.THREADPOOLEXECUTOR_STATISTICS
+									.getMBeanName());
+
+			MULTICAST_CONTROLLER_OBJECT_NAME = new ObjectName(
+					DomainsEnum.DEFAULT_DOMAIN.getDomain()
+							+ MBeansNameEnum.MULTICAST_CONTROLLER
+									.getMBeanName());
+
+		} catch (MalformedObjectNameException e) {
+			throw new MalformedObjectNameRuntimeException(e);
+		}
+
+		HARDWARE_BASE_NAME = new StringBuilder(
+				DomainsEnum.DEFAULT_DOMAIN.getDomain())
+				.append(MBeansNameEnum.HARDWARE.getMBeanName())
+				.append(COMMA_SEPARATOR).append(ID).append(EQUAL).toString();
+	}
+
+	public static ObjectName getThreadPoolExecutorStatisticsObjectName() {
+		return THREADPOOLEXECUTOR_STATISTICS_OBJECT_NAME;
+
+	}
+
+	public static ObjectName getMultiCastControllerObjectName() {
+		return MULTICAST_CONTROLLER_OBJECT_NAME;
+	}
+
+	public static ObjectName getHardwareObjectName(final String hardwareUniqueID) {
+		try {
+			return new ObjectName(HARDWARE_BASE_NAME + hardwareUniqueID);
+		} catch (final MalformedObjectNameException e) {
+			throw new MalformedObjectNameRuntimeException(e);
+		}
+	}
+
+}
