76043e83b91226893dfccdad5f7cd663cc36d510
handyman <handyman@linkare.com>
2016-03-17 01:06:05 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-1992.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@11966 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/ui/com/linkare/rec/impl/newface/laf/flat/elabtheme/TableStyle.java
@@ -0,0 +1,40 @@
+/* 
+ * ListStyle.java created on 2009/04/30
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.linkare.rec.impl.newface.laf.flat.elabtheme;
+
+import java.util.Map;
+
+/**
+ * @deprecated
+ * @author João FLorindo
+ */
+@Deprecated
+public class TableStyle extends DefaultStyle {
+
+	// ENABLED COLORS
+
+	// LABEL FONT
+
+	public static final String ID = "Table";
+
+	/**
+	 * Creates the <code>TableStyle</code>.
+	 */
+	public TableStyle() {
+		super();
+	}
+
+	@Override
+	protected String defineStyleId() {
+		return TableStyle.ID;
+	}
+
+	@Override
+	public void updatePropertyMap(final Map<String, Object> map) {
+		super.updatePropertyMap(map);
+	}
+}
