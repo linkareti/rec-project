48f1c6300521a4cebe46bade90428c83adab3e15
handyman <handyman@linkare.com>
2016-04-16 00:06:14 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-2021.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@12082 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/hidrostat/src/java/server/pt/utl/ist/elab/driver/hidrostat/HidrostatDriver.java
@@ -0,0 +1,17 @@
+/* 
+ * HidrostatDriver.java created on 8 Fev 2014
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package pt.utl.ist.elab.driver.hidrostat;
+
+import pt.utl.ist.elab.driver.serial.serialportgeneric.genericexperiment.GenericSerialPortDriver;
+
+/**
+ * 
+ * @author npadriano
+ */
+public class HidrostatDriver extends GenericSerialPortDriver {
+
+}
