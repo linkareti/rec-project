65a8f64ef30db73d1d1baf2e9330b2d891965c90
handyman <handyman@linkare.com>
2016-04-21 00:06:46 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-2026.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@12102 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/elab/src/java/ui/pt/utl/ist/elab/client/FullClientNewInterface.java
@@ -0,0 +1,22 @@
+package pt.utl.ist.elab.client;
+
+import com.linkare.rec.impl.newface.ReCApplication;
+
+/**
+ *
+ * This class is just a wrapper class for the true main class. The reason for
+ * its existence is only to guarantee that the certificate shown to the user is
+ * the eLab's one and not the ReC's framework Linkare default
+ *
+ * @author André Leitão - Linkare TI
+ */
+public class FullClientNewInterface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(final String[] args) {
+        ReCApplication.main(args);
+    }
+
+}
