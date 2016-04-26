e6756b3b061620efe0d3d637cc6b50fd29dc3e78
handyman <handyman@linkare.com>
2016-04-26 00:04:11 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2372.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@12120 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.web/src/main/java/com/linkare/rec/web/service/ReservationService.java
@@ -0,0 +1,30 @@
+package com.linkare.rec.web.service;
+
+import java.util.Date;
+import java.util.List;
+
+import org.primefaces.model.ScheduleEvent;
+
+import com.linkare.commons.jpa.security.User;
+import com.linkare.rec.web.model.Reservation;
+import com.linkare.rec.web.auth.UserView;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ * 
+ */
+public interface ReservationService extends BusinessService<Reservation, Long> {
+
+    public List<ScheduleEvent> findReservationsFor(final UserView userView);
+
+    public List<ScheduleEvent> findReservationsFor(final Date start, final Date end, final UserView userView);
+
+    public List<ScheduleEvent> findAllReservationsFor(final Date start, final Date end, final UserView userView);
+
+    public Reservation getReservationDetails(final Long id);
+
+    public void addUser(final Reservation reservation, final User user);
+
+    public void removeUser(final Reservation reservation, final User user);
+}
