440f8a3fcdd1713c487734485d4eab9e06ed497b
handyman <handyman@linkare.com>
2016-05-02 00:04:36 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2378.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@12144 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/rollpaper/src/java/client/pt/utl/ist/elab/client/rollpaper/TestCustomizer.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.utl.ist.elab.client.rollpaper;
+
+import javax.swing.ImageIcon;
+
+import com.linkare.rec.impl.client.customizer.AbstractCustomizer;
+
+/**
+ *
+ * @author Gedsimon Pereira
+ */
+public class TestCustomizer  extends AbstractCustomizer {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -6212877202738820359L;
+
+	
+    @Override
+    public ImageIcon getCustomizerIcon() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getCustomizerTitle() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
