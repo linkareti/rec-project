1ec4c275745187dc294940a6d88d1093caea9610
handyman <handyman@linkare.com>
2016-05-12 00:05:40 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-2047.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@12186 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/langmuir/src/pic/LabX/langmuir.X/build/default/production/_ext/1670778117/pfeiffer_gauges.o.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/1670778117/pfeiffer_gauges.o: \
+ ../../Library/pfeiffer_gauges.c ../../Library/pfeiffer_gauges.h \
+ ../../Library/hardware_uart.h ../../Library/pfeiffer_crc.h
