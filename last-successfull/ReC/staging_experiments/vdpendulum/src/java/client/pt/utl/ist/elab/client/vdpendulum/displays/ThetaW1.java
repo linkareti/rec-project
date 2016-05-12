33b98eb85d3f655e7489909dc7b1a20612b0b505
handyman <handyman@linkare.com>
2016-05-12 00:04:05 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2388.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@12184 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/staging_experiments/vdpendulum/src/java/client/pt/utl/ist/elab/client/vdpendulum/displays/ThetaW1.java
@@ -0,0 +1,25 @@
+/*
+ * Theta1.java
+ *
+ * Created on 13 February 2004, 20:23
+ */
+
+package pt.utl.ist.elab.client.vdpendulum.displays;
+
+/**
+ * 
+ * @author André Neto - LEFT - IST
+ */
+public class ThetaW1 extends com.linkare.rec.impl.ui.graph.DefaultXYExperimentGraph {
+
+	/** Creates a new instance of Theta1 */
+	public ThetaW1() {
+		super();
+		setChannelDisplayX(1);
+		setChannelDisplayY(3);
+	}
+
+	public String getName() {
+		return "Grafico 5";
+	}
+}
