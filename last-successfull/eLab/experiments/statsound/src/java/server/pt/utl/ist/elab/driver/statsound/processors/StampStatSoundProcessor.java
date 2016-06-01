df29fdf0816c791509a228054df13a077c699694
handyman <handyman@linkare.com>
2016-06-01 00:06:18 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-2067.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@12266 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/statsound/src/java/server/pt/utl/ist/elab/driver/statsound/processors/StampStatSoundProcessor.java
@@ -0,0 +1,60 @@
+/*
+ * StampHelloProcessor.java
+ *
+ * Created on 12 de Novembro de 2002, 16:19
+ */
+
+package pt.utl.ist.elab.driver.statsound.processors;
+
+import java.util.logging.Logger;
+
+import pt.utl.ist.elab.driver.serial.stamp.transproc.AbstractStampProcessor;
+import pt.utl.ist.elab.driver.serial.stamp.transproc.StampCommand;
+import pt.utl.ist.elab.driver.statsound.StatSoundStampDriver;
+
+/**
+ * 
+ * @author bruno
+ */
+public class StampStatSoundProcessor extends AbstractStampProcessor {
+	public static final String COMMAND_IDENTIFIER = "POS";
+	
+	private static final Logger LOGGER = Logger.getLogger(StatSoundStampDriver.class.getName());
+
+	/** Creates a new instance of StampHelloProcessor */
+	public StampStatSoundProcessor() {
+		super(StampStatSoundProcessor.COMMAND_IDENTIFIER);
+	}
+
+	/**
+	 * This method makes the Processor process the command passed in...
+	 * 
+	 * @param command the command that this processor will have to process
+	 * @return boolean - wether the processing was successfull
+	 * 
+	 */
+	@Override
+	public boolean process(final StampCommand command) {
+		int pos = 0;
+		final String[] splitedStr = command.getCommand().split(" ");
+		if (command.getCommandIdentifier().equalsIgnoreCase(StampStatSoundProcessor.COMMAND_IDENTIFIER)
+				&& splitedStr[0] != null) {
+			try {
+				pos = Integer.parseInt(splitedStr[0]);
+				LOGGER.fine(">>>>> Directly from HARDWARE POS="+pos);
+				command.addCommandData(StampStatSoundProcessor.COMMAND_IDENTIFIER, pos);
+				command.setData(true);
+				return true;
+			} catch (final NumberFormatException e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean isData() {
+		return true;
+	}
+}
