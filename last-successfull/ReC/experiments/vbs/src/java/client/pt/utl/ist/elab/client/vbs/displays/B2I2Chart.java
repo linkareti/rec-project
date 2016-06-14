b415ce5e454f6bdafabc3b58e9ada48c7fa9239a
handyman <handyman@linkare.com>
2016-06-14 00:05:06 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2421.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@12316 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/vbs/src/java/client/pt/utl/ist/elab/client/vbs/displays/B2I2Chart.java
@@ -0,0 +1,25 @@
+/*
+ * B2I2Chart.java
+ *
+ * Created on April 03, 2005, 3:40 PM
+ */
+
+package pt.utl.ist.elab.client.vbs.displays;
+
+/**
+ * 
+ * @author Queiro'
+ */
+public class B2I2Chart extends com.linkare.rec.impl.ui.graph.DefaultXYExperimentGraph {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4555510135304280443L;
+
+	/** Creates a new instance of AnguloIntensidadeChart */
+	public B2I2Chart() {
+		super();
+		setChannelDisplayX(5);
+		setChannelDisplayY(3);
+	}
+}
