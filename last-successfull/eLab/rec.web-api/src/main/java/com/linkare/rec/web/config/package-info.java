d5c571fd52591805022841923be8a9d6c2a597d3
handyman <handyman@linkare.com>
2016-06-19 00:06:40 +0000

    Delete old tag by SvnTag Hudson plugin.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@12337 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- last-successfull/eLab/rec.web-api/src/main/java/com/linkare/rec/web/config/package-info.java
+++
@@ -1,5 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://rec.linkare.com/client/config", elementFormDefault=XmlNsForm.QUALIFIED, attributeFormDefault = XmlNsForm.UNQUALIFIED)
-package com.linkare.rec.web.config;
-
-import javax.xml.bind.annotation.XmlNsForm;
-
