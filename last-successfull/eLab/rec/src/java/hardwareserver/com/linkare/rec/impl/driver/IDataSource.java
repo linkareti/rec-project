c701eb53abe9c638cdfdc4ca7168c9a39c32706f
handyman <handyman@linkare.com>
2016-07-27 00:05:46 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-2123.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@12490 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/rec/src/java/hardwareserver/com/linkare/rec/impl/driver/IDataSource.java
@@ -0,0 +1,41 @@
+/*
+ * IDataSource.java
+ *
+ * Created on 9 de Maio de 2003, 18:09
+ */
+
+package com.linkare.rec.impl.driver;
+
+import com.linkare.rec.data.config.HardwareAcquisitionConfig;
+import com.linkare.rec.impl.data.SamplesSource;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public interface IDataSource extends SamplesSource {
+	public int getPacketSize();
+
+	public int getTotalSamples();
+
+	public String getName();
+
+	public HardwareAcquisitionConfig getAcquisitionHeader();
+
+	public void stopNow();
+
+	/**
+	 * Registers IDataSourceListener to receive events.
+	 * 
+	 * @param listener The listener to register.
+	 */
+	public void addIDataSourceListener(IDataSourceListener listener);
+
+	/**
+	 * Removes IDataSourceListener from the list of listeners.
+	 * 
+	 * @param listener The listener to remove.
+	 */
+	public void removeIDataSourceListener(IDataSourceListener listener);
+
+}
