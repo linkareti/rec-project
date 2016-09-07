6d9be8d9bfe33c283a8f246d0546d5ab1bb1a91e
handyman <handyman@linkare.com>
2016-09-07 00:04:58 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2506.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@12656 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/vdi/src/java/client/pt/utl/ist/elab/client/vdi/displays/VTime1Chart.java
@@ -0,0 +1,25 @@
+/*
+ * VTimeChart.java
+ *
+ * Created on April 03, 2005, 3:40 PM
+ */
+
+package pt.utl.ist.elab.client.vdi.displays;
+
+/**
+ * 
+ * @author Queiro'
+ */
+public class VTime1Chart extends com.linkare.rec.impl.ui.graph.DefaultXYExperimentGraph {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5171114960930617144L;
+
+	/** Creates a new instance of AnguloIntensidadeChart */
+	public VTime1Chart() {
+		super();
+		setChannelDisplayX(0);
+		setChannelDisplayY(5);
+	}
+}
