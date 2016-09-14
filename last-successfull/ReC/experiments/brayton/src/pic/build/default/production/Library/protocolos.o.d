d0ae7d8fe20221213a3fc31d7c2135a7ed4c6696
handyman <handyman@linkare.com>
2016-09-14 00:04:49 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2513.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@12684 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/brayton/src/pic/build/default/production/Library/protocolos.o.d
@@ -0,0 +1,4 @@
+build/default/production/Library/protocolos.o:  \
+Library/protocolos.c  \
+ Library/elab.h  \
+Library/user_defines.h 
---
+++ last-successfull/ReC/experiments/vdi/src/java/client/pt/utl/ist/elab/client/vdi/displays/STime1Chart.java
@@ -0,0 +1,25 @@
+/*
+ * STimeChart.java
+ *
+ * Created on April 03, 2005, 3:40 PM
+ */
+
+package pt.utl.ist.elab.client.vdi.displays;
+
+/**
+ * 
+ * @author Queiro'
+ */
+public class STime1Chart extends com.linkare.rec.impl.ui.graph.DefaultXYExperimentGraph {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5687805864613743351L;
+
+	/** Creates a new instance of AnguloIntensidadeChart */
+	public STime1Chart() {
+		super();
+		setChannelDisplayX(0);
+		setChannelDisplayY(9);
+	}
+}
