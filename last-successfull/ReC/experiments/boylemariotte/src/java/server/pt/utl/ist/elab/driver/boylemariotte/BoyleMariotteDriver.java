27e481e83377778a6c7c937fca22b77b4b3828ff
handyman <handyman@linkare.com>
2016-11-10 01:04:48 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2571.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@12912 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/boylemariotte/src/java/server/pt/utl/ist/elab/driver/boylemariotte/BoyleMariotteDriver.java
@@ -0,0 +1,10 @@
+package pt.utl.ist.elab.driver.boylemariotte;
+
+import pt.utl.ist.elab.driver.serial.serialportgeneric.genericexperiment.GenericSerialPortDriver;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ */
+public class BoyleMariotteDriver extends GenericSerialPortDriver {
+}
