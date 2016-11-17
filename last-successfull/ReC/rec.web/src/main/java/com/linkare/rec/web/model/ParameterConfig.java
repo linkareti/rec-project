fe38f35a12333c1c31eee3c4af9775d69e4e2778
handyman <handyman@linkare.com>
2016-11-17 01:04:06 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2578.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@12940 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.web/src/main/java/com/linkare/rec/web/model/ParameterConfig.java
@@ -0,0 +1,67 @@
+package com.linkare.rec.web.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.linkare.commons.jpa.DefaultDomainObject;
+
+/**
+ * 
+ * @author Artur Correia - Linkare TI
+ */
+@Entity
+@Table(name = "PARAMETER_CONFIG")
+public class ParameterConfig extends DefaultDomainObject {
+
+    private static final long serialVersionUID = 1L;
+
+    @Column(name = "PARAMETER_NAME")
+    private String parameterName;
+
+    @Column(name = "PARAMETER_VALUE")
+    private String parameterValue;
+
+    @ManyToOne
+    @JoinColumn(name = "KEY_CHANNEL_ACQUISITION_CONFIG", nullable = true, updatable = false)
+    private ChannelAcquisitionConfig channelAcquisitionConfig;
+
+    @ManyToOne
+    @JoinColumn(name = "KEY_HARDWARE_ACQUISITION_CONFIG", nullable = true, updatable = false)
+    private HardwareAcquisitionConfig hardwareAcquisitionConfig;
+
+    public String getParameterName() {
+	return parameterName;
+    }
+
+    public void setParameterName(String parameterName) {
+	this.parameterName = parameterName;
+    }
+
+    public String getParameterValue() {
+	return parameterValue;
+    }
+
+    public void setParameterValue(String parameterValue) {
+	this.parameterValue = parameterValue;
+    }
+
+    public ChannelAcquisitionConfig getChannelAcquisitionConfig() {
+	return channelAcquisitionConfig;
+    }
+
+    public void setChannelAcquisitionConfig(ChannelAcquisitionConfig channelAcquisitionConfig) {
+	this.channelAcquisitionConfig = channelAcquisitionConfig;
+    }
+
+    public HardwareAcquisitionConfig getHardwareAcquisitionConfig() {
+	return hardwareAcquisitionConfig;
+    }
+
+    public void setHardwareAcquisitionConfig(HardwareAcquisitionConfig hardwareAcquisitionConfig) {
+	this.hardwareAcquisitionConfig = hardwareAcquisitionConfig;
+    }
+
+}
