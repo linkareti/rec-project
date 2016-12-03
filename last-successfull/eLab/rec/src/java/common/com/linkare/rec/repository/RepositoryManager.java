bfec8b7dbbe5c657c8ca69c4584c4f8dcb3f709d
handyman <handyman@linkare.com>
2016-12-03 01:05:24 +0000

    Delete old tag by SvnTag Hudson plugin.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@13005 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- last-successfull/eLab/rec/src/java/common/com/linkare/rec/repository/RepositoryManager.java
+++
@@ -1,7 +0,0 @@
-package com.linkare.rec.repository;
-
-import org.omg.CORBA.Object;
-import org.omg.CORBA.portable.IDLEntity;
-
-public interface RepositoryManager extends RepositoryManagerOperations, Object, IDLEntity {
-} // interface RepositoryManager
