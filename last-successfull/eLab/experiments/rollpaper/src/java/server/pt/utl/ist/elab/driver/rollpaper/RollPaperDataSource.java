b05ad039359284bc965e798ccfdbdb6e55be98eb
handyman <handyman@linkare.com>
2017-02-04 01:05:53 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-2314.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@13254 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/rollpaper/src/java/server/pt/utl/ist/elab/driver/rollpaper/RollPaperDataSource.java
@@ -0,0 +1,98 @@
+package pt.utl.ist.elab.driver.rollpaper;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.linkare.rec.data.acquisition.PhysicsValue;
+import com.linkare.rec.impl.driver.BaseDataSource;
+
+/**
+ * 
+ * @author Bruno Catarino - Linkare TI
+ */
+public class RollPaperDataSource extends BaseDataSource {
+
+	private static final Logger LOGGER = Logger.getLogger(RollPaperDataSource.class.getName());
+
+	private final int NUM_CHANNELS = 4;
+
+	private int tbs;
+	private final int nSamples;
+
+	private RollPaperDriver driver = null;
+	private RollpaperHardwareSimulator samplesGenerator;
+	private boolean stopped = false;
+
+	/**
+	 * Creates the <code>RollPaperDataProducer</code>.
+	 * 
+	 * @param rollPaperDriver A reference to the driver connected to this
+	 *            DataProducer.
+	 * @param baseHeight1 The initial height of the first paper roll (grabed by
+	 *            tip).
+	 * @param baseHeight2 The initial height of the second paper roll
+	 *            (freefall).
+	 * @param g The gravity acceleration.
+	 * @param totalRadius The radius of the paper roll.
+	 * @param innerRadius The radius of the paper roll's hole.
+	 * @param tbs
+	 * @param nSamples
+	 */
+	public RollPaperDataSource(RollPaperDriver rollPaperDriver, float baseHeight1, float baseHeight2, float g,
+			float totalRadius, float innerRadius, int tbs, int nSamples) {
+		this.driver = rollPaperDriver;
+		this.nSamples = nSamples;
+		this.tbs = tbs;
+
+		samplesGenerator = new RollpaperHardwareSimulator(NUM_CHANNELS, baseHeight1, baseHeight2, g, totalRadius,
+				innerRadius, getAcquisitionHeader());
+	}
+
+	public void startProduction() {
+		stopped = false;
+		new ProducerThread().start();
+	}
+
+	private class ProducerThread extends Thread {
+		private int currentSample = 0;
+
+		@Override
+		public void run() {
+			try {
+				Thread.sleep(1000);
+
+				PhysicsValue[] value;
+				int counter = 0;
+
+				while (currentSample < nSamples && !stopped) {
+
+					if (counter % tbs == 0) {
+						value = samplesGenerator.getNewSample(1 / tbs * currentSample);
+						addDataRow(value);
+						Thread.sleep(tbs);
+						currentSample++;
+					}
+					counter++;
+				}
+
+				join(100);
+				endProduction();
+
+				driver.stopVirtualHardware();
+			} catch (final InterruptedException ie) {
+				LOGGER.log(Level.SEVERE, "Error in Thread: ", ie);
+			}
+		}
+	}
+
+	@Override
+	public void stopNow() {
+		stopped = true;
+		setDataSourceStoped();
+	}
+
+	private void endProduction() {
+		stopped = true;
+		setDataSourceEnded();
+	}
+}
