1db5f2de13ea6b39ec538e9f4e37b31bddd92953
handyman <handyman@linkare.com>
2017-03-15 01:04:35 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2696.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@13408 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/elab/src/java/driver/pt/utl/ist/elab/driver/serial/stamp/transproc/StampCommandListener.java
@@ -0,0 +1,17 @@
+/*
+ * StampCommandListener.java
+ *
+ * Created on 15 de Maio de 2003, 15:38
+ */
+
+package pt.utl.ist.elab.driver.serial.stamp.transproc;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public interface StampCommandListener extends java.util.EventListener {
+
+	public void handleStampCommand(StampCommand command);
+
+}
