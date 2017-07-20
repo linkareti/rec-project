f38d259419e63572663a16639d86e4f7c0132483
handyman <handyman@linkare.com>
2017-07-20 00:04:16 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2822.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@14355 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/vmovproj/src/java/client/pt/utl/ist/elab/client/vmovproj/displays/VelocityChart.java
@@ -0,0 +1,75 @@
+/*
+ * VelocityChart.java
+ *
+ * Created on 20 de Mar�o de 2005, 15:21
+ */
+
+package pt.utl.ist.elab.client.vmovproj.displays;
+
+import pt.utl.ist.elab.virtual.guipack.graphs.GraphSamplesFunction;
+import pt.utl.ist.elab.virtual.guipack.graphs.MultSeriesXYExperimentGraphExtended;
+
+import com.linkare.rec.data.config.HardwareAcquisitionConfig;
+import com.linkare.rec.impl.client.experiment.ExpDataModel;
+
+/**
+ * 
+ * @author unknown 
+ */
+public class VelocityChart extends MultSeriesXYExperimentGraphExtended implements GraphSamplesFunction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7847727721604777694L;
+	private boolean isAbs;
+
+	/** Creates a new instance of PositionChart */
+	public VelocityChart() {
+		super();
+	}
+
+	@Override
+	public double getValueX(final int ch, final double val) {
+		return val;
+	}
+
+	@Override
+	public double getValueY(final int ch, final double val) {
+		if (isAbs) {
+			return Math.abs(val);
+		}
+		return val;
+	}
+
+	@Override
+	public void dataModelStartedNoData() {
+		final HardwareAcquisitionConfig header = model.getAcquisitionConfig();
+
+		setChannelDisplayX(6);
+		if (header.getSelectedHardwareParameterValue("velModulus").trim().equals("1") ? true : false) {
+			setChannelDisplayYArray(new int[] { 3, 4, 5, 11 });
+		} else {
+			setChannelDisplayYArray(new int[] { 3, 4, 5 });
+		}
+
+		isAbs = header.getSelectedHardwareParameterValue("velAbs").trim().equals("1") ? true : false;
+	}
+
+	private ExpDataModel model = null;
+
+	@Override
+	public void setExpDataModel(final ExpDataModel model) {
+		
+		if (this.model != null) {
+			this.model.removeExpDataModelListener(this);
+		}
+		this.model = model;
+		if (this.model != null) {
+			this.model.addExpDataModelListener(this);
+		}
+		
+		super.setExpDataModel(model);
+
+	}
+}
