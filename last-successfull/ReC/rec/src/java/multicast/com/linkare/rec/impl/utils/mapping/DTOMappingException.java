1a5748da8b39f6e10f987222be27748b21e9c527
handyman <handyman@linkare.com>
2017-09-20 00:04:34 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2884.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@14607 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/multicast/com/linkare/rec/impl/utils/mapping/DTOMappingException.java
@@ -0,0 +1,36 @@
+/* 
+ * DTOMappingException.java created on Apr 13, 2011
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.linkare.rec.impl.utils.mapping;
+
+/**
+ * 
+ * @author Artur Correia - Linkare TI
+ */
+public class DTOMappingException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DTOMappingException() {
+		super();
+	}
+
+	public DTOMappingException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DTOMappingException(String message) {
+		super(message);
+	}
+
+	public DTOMappingException(Throwable cause) {
+		super(cause);
+	}
+
+}
