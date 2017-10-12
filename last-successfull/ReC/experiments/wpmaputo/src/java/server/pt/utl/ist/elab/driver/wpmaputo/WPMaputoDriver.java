1256099094609c92c5fadc50cc661d34cda0059b
handyman <handyman@linkare.com>
2017-10-12 00:05:48 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2906.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@14698 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/wpmaputo/src/java/server/pt/utl/ist/elab/driver/wpmaputo/WPMaputoDriver.java
@@ -0,0 +1,17 @@
+/* 
+ * WPMaputoDriver.java created on 1 Fev 2013
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package pt.utl.ist.elab.driver.wpmaputo;
+
+import pt.utl.ist.elab.driver.serial.serialportgeneric.genericexperiment.GenericSerialPortDriver;
+
+/**
+ * 
+ * @author npadriano
+ */
+public class WPMaputoDriver extends GenericSerialPortDriver {
+
+}
