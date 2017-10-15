2558ad9e2de7fec00d6ae1393d2534fbcb8022c7
handyman <handyman@linkare.com>
2017-10-15 00:05:30 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2909.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@14713 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/common/com/linkare/rec/impl/utils/IntersectableEvent.java
@@ -0,0 +1,15 @@
+/*
+ * IntersectableEvent.java
+ *
+ * Created on 7 de Janeiro de 2004, 19:34
+ */
+
+package com.linkare.rec.impl.utils;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public interface IntersectableEvent {
+	public boolean intersectTo(IntersectableEvent other);
+}
