9506c1cea635f3cd763a70b504eea29080281b06
handyman <handyman@linkare.com>
2017-10-23 00:04:43 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2917.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@14760 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/mag3d/src/java/client/pt/utl/ist/elab/client/mag3d/Mag3DXYZGraphDisplay.java
@@ -0,0 +1,28 @@
+/*
+ * Mag3DXYZGraphDisplay.java
+ *
+ * Created on 10 de Julho de 2003, 11:14
+ */
+
+package pt.utl.ist.elab.client.mag3d;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public class Mag3DXYZGraphDisplay extends com.linkare.rec.impl.ui.graph.MultSeriesXYExperimentGraph {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3177968201292210791L;
+
+	/** Creates a new instance of Mag3DXYZGraphDisplay */
+	public Mag3DXYZGraphDisplay() {
+		super();
+		setChannelDisplayX(0);
+                int[] channelList = {4,2,3};
+		setChannelDisplayYArray(channelList);
+	}
+
+}
