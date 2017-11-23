33b6af47e5c09c8b4b805fad49c98729fa142c3e
handyman <handyman@linkare.com>
2017-11-23 01:05:29 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2949.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@15413 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec.web/src/main/java/com/linkare/rec/web/bean/LoginDomainBean.java
@@ -0,0 +1,64 @@
+package com.linkare.rec.web.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.model.SelectItem;
+
+import com.linkare.jsf.utils.JsfUtil;
+import com.linkare.rec.web.model.LoginDomain;
+import com.linkare.rec.web.service.LoginDomainService;
+import com.linkare.rec.web.service.LoginDomainServiceLocal;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ * 
+ */
+@ManagedBean(name = "loginDomainBean")
+@SessionScoped
+public class LoginDomainBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @EJB(beanInterface = LoginDomainServiceLocal.class)
+    private LoginDomainService service;
+
+    private List<LoginDomain> loginDomains;
+
+    /**
+     * @return the service
+     */
+    public LoginDomainService getService() {
+	return service;
+    }
+
+    /**
+     * @param service
+     *            the service to set
+     */
+    public void setFacade(LoginDomainService service) {
+	this.service = service;
+    }
+
+    /**
+     * @return the loginDomains
+     */
+    public SelectItem[] getLoginDomains() {
+	if (loginDomains == null) {
+	    loginDomains = service.findAll();
+	}
+	return JsfUtil.getSelectItems(loginDomains, false);
+    }
+
+    /**
+     * @param loginDomains
+     *            the loginDomains to set
+     */
+    public void setLoginDomains(List<LoginDomain> loginDomains) {
+	this.loginDomains = loginDomains;
+    }
+}
