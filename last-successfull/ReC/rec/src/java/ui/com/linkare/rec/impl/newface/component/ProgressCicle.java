1806a4e85d676ecd40fa536d87d039c1d0061569
handyman <handyman@linkare.com>
2017-12-17 01:04:34 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2973.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@16440 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/rec/src/java/ui/com/linkare/rec/impl/newface/component/ProgressCicle.java
@@ -0,0 +1,101 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.linkare.rec.impl.newface.component;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Icon;
+import javax.swing.JLabel;
+import javax.swing.Timer;
+
+/**
+ * @author Henrique Fernandes
+ */
+public class ProgressCicle extends JLabel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6785925675296083188L;
+
+	public enum State {
+		IDLE, RUNNING
+	}
+
+	private Icon idleIcon;
+
+	private Icon[] busyIcons;
+
+	private Timer busyIconTimer;
+
+	private int busyIconIndex;
+
+	private State currentState;
+
+	public ProgressCicle() {
+		super();
+	}
+
+	public ProgressCicle(final Icon idleIcon, final Icon[] busyIcons, final int busyAnimationRate) {
+		super(idleIcon);
+		setIcon(idleIcon);
+		setBusyIcons(busyIcons);
+		currentState = State.IDLE;
+	}
+
+	private void initBusyIconTimer(final int busyAnimationRate, final Icon[] busyIcons) {
+		busyIconTimer = new Timer(busyAnimationRate, new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				busyIconIndex = (busyIconIndex + 1) % busyIcons.length;
+				ProgressCicle.this.setIcon(busyIcons[busyIconIndex]);
+			}
+		});
+	}
+
+	public void setIdleIcon(final Icon idleIcon) {
+		this.idleIcon = idleIcon;
+		setIcon(idleIcon);
+	}
+
+	@SuppressWarnings("PMD.ArrayIsStoredDirectly")
+	public void setBusyIcons(final Icon[] busyIcons) {
+		this.busyIcons = busyIcons;
+		initBusyIconTimer(30, busyIcons);
+	}
+
+	public boolean isIdle() {
+		return currentState == State.IDLE;
+	}
+
+	public boolean isRunning() {
+		return currentState == State.RUNNING;
+	}
+
+	public void setState(final State newState) {
+		if (currentState != newState) {
+			if (State.RUNNING == newState) {
+				setIcon(busyIcons[0]);
+				busyIconIndex = 0;
+				busyIconTimer.start();
+				currentState = State.RUNNING;
+			} else {
+				busyIconTimer.stop();
+				setIcon(idleIcon);
+				currentState = State.IDLE;
+			}
+		}
+	}
+
+	public void start() {
+		setState(State.RUNNING);
+	}
+
+	public void stop() {
+		setState(State.IDLE);
+	}
+}
