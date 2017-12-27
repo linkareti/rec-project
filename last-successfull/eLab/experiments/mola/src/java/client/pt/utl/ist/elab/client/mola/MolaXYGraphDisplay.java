247b19a52fbfbd08ee589782461505485711455d
handyman <handyman@linkare.com>
2017-12-27 01:07:11 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-2639.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@16683 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/mola/src/java/client/pt/utl/ist/elab/client/mola/MolaXYGraphDisplay.java
@@ -0,0 +1,27 @@
+/*
+ * MolaXYGraphDisplay.java
+ *
+ * Created on 10 de Julho de 2003, 11:14
+ */
+
+package pt.utl.ist.elab.client.mola;
+
+/**
+ * 
+ * @author José Pedro Pereira - Linkare TI
+ */
+public class MolaXYGraphDisplay extends com.linkare.rec.impl.ui.graph.DefaultXYExperimentGraph {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3177968201292210791L;
+
+	/** Creates a new instance of MolaXYGraphDisplay */
+	public MolaXYGraphDisplay() {
+		super();
+		setChannelDisplayX(0);
+		setChannelDisplayY(1);
+	}
+
+}
