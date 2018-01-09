c7b865ca1ba93c8ac8f9cb4861438a820a2414ce
handyman <handyman@linkare.com>
2018-01-09 01:08:43 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-2996.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@16865 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/mag3d/src/java/server/pt/utl/ist/elab/driver/mag3d/Mag3DDriver.java
@@ -0,0 +1,10 @@
+package pt.utl.ist.elab.driver.mag3d;
+
+import pt.utl.ist.elab.driver.serial.serialportgeneric.genericexperiment.GenericSerialPortDriver;
+
+/**
+ * 
+ * @author Paulo Zenida - Linkare TI
+ */
+public class Mag3DDriver extends GenericSerialPortDriver {
+}
