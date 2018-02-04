13404b5994e9f3b10744afaf11d7c29cbce0964a
handyman <handyman@linkare.com>
2018-02-04 01:05:44 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-2677.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@17544 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/conducaocalor/src/java/client/pt/utl/ist/elab/client/conducaocalor/displays/Sensor3Table.java
@@ -0,0 +1,32 @@
+/*
+ * TableFreqRMS.java
+ *
+ * Created on 16 October 2003, 23:33
+ */
+
+package pt.utl.ist.elab.client.conducaocalor.displays;
+
+import com.linkare.rec.impl.ui.table.MultSeriesTable;
+
+/**
+ * 
+ * @author Andr�
+ */
+public class Sensor3Table extends MultSeriesTable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8134406659109401883L;
+
+	/** Creates a new instance of TableFreqRMS */
+	public Sensor3Table() {
+		super();
+		setColArray(new int[] { 9, 2, 5, 8 });
+	}
+
+	@Override
+	public String getName() {
+		return "Sensor 3";
+	}
+}
