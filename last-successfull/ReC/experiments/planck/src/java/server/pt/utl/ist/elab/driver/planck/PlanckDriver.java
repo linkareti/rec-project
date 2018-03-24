dafe014ddd498270dc715b81a50a63766797f1b3
handyman <handyman@linkare.com>
2018-03-24 01:04:37 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-ReC-3070.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/rec@18397 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/ReC/experiments/planck/src/java/server/pt/utl/ist/elab/driver/planck/PlanckDriver.java
@@ -0,0 +1,17 @@
+/* 
+ * DiscosDriver.java created on 8 Sep 2010
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package pt.utl.ist.elab.driver.planck;
+
+import pt.utl.ist.elab.driver.serial.serialportgeneric.genericexperiment.GenericSerialPortDriver;
+
+/**
+ * 
+ * @author npadriano
+ */
+public class PlanckDriver extends GenericSerialPortDriver {
+
+}
