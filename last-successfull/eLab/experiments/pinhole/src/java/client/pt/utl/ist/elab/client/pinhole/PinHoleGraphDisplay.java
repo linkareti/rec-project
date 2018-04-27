c1541218996ed46192600ba3cddbbe3daf4f302b
handyman <handyman@linkare.com>
2018-04-27 00:06:57 +0000

    Tagged by Hudson svn-tag plugin. Build:jenkins-eLab-2755.
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/tags/elab@18826 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ last-successfull/eLab/experiments/pinhole/src/java/client/pt/utl/ist/elab/client/pinhole/PinHoleGraphDisplay.java
@@ -0,0 +1,30 @@
+/* 
+ * PinHoleGraphDisplay.java created on 13 Jan 2011
+ *
+ * Copyright 2009 Linkare TI. All rights reserved.
+ * Linkare TI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package pt.utl.ist.elab.client.pinhole;
+
+import com.linkare.rec.impl.ui.graph.MultSeriesXYInBlockExperimentGraph;
+
+import pt.utl.ist.elab.client.pinhole.PinHoleCustomizerPanel;
+
+/**
+ * 
+ * @author npadriano
+ */
+public class PinHoleGraphDisplay extends MultSeriesXYInBlockExperimentGraph {
+
+	/** Generated UID */
+	private static final long serialVersionUID = 974560783079598747L;
+
+	/**
+	 * Creates the <code>PinHoleGraphDisplay</code>.
+	 */
+	public PinHoleGraphDisplay() {
+		super(PinHoleCustomizerPanel.NUMBER_OF_SAMPLES_FOR_SERIES);
+		setChannelDisplayX(1); // Ângulo sensor óptico
+		setChannelDisplayY(2); // ADC
+	}
+}
