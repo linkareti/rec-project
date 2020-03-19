2779bed4ceebf33d61c6318c89ace16420c6c5ca
pramalho <pramalho@linkare.com>
2020-03-19 10:35:04 +0000

    Task 5579: Adding rec project to migration branch
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/branches/jdk11-migration@19863 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ experiments/wppucrio2/src/pic/pendulum_N_oscs.h
@@ -0,0 +1,11 @@
+#ifndef __PENDULUM_N_OSCS__
+#define __PENDULUM_N_OSCS__
+
+unsigned long getGlobalNumberOfOscs();
+void saveGlobalNumberOfOscs();
+void incGlobalNumberOfOscs();
+void retrieveGlobalNumberOfOscs();
+void resetGlobalNumberOfOscs();
+void setGlobalNumberOfOscs(unsigned long val);
+
+#endif
