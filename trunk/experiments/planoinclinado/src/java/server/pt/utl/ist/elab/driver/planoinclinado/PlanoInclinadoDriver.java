e441ba9fc95e86f762d46a8d5bb93c52b5bafffd
pramalho <pramalho@linkare.com>
2020-03-19 10:10:44 +0000

    Task 5579: adding rec project to migration branch
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/branches/jdk11-migration@19861 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ trunk/experiments/planoinclinado/src/java/server/pt/utl/ist/elab/driver/planoinclinado/PlanoInclinadoDriver.java
@@ -0,0 +1,10 @@
+package pt.utl.ist.elab.driver.planoinclinado;
+
+import pt.utl.ist.elab.driver.serial.serialportgeneric.genericexperiment.GenericSerialPortDriver;
+
+/**
+ * 
+ * @author Andre Sancho Duarte
+ */
+public class PlanoInclinadoDriver extends GenericSerialPortDriver {
+}
