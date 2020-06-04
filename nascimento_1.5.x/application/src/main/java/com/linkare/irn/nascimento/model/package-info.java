31dccb109aab44940fa7177d0eaf6c444b0ffc00
pramalho <pramalho@linkare.com>
2020-06-04 14:20:45 +0000

    Task5703: INserting trunk files into branch
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/branches/rel-8.8.x@20242 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- nascimento_1.5.x/application/src/main/java/com/linkare/irn/nascimento/model/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package contains the domain object (persistent) entities of the application, as well as the corresponding used value type objects such as enums.
- * 
- * @author Paulo Zenida - Linkare TI
- */
-package com.linkare.irn.nascimento.model;
