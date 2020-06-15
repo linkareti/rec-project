2a20c8852ded59ba2d376a707501abd4d9c1408a
pramalho <pramalho@linkare.com>
2020-06-15 09:25:25 +0000

    Task 5728: Inserting trunk files into branch
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/branches/rel-8.9.x@20276 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

---
+++ experiments/fotovoltaico/src/java/server/pt/utl/ist/elab/driver/fotovoltaico/FotovoltaicoDriver.java
@@ -0,0 +1,10 @@
+package pt.utl.ist.elab.driver.fotovoltaico;
+
+import pt.utl.ist.elab.driver.serial.serialportgeneric.genericexperiment.GenericSerialPortDriver;
+
+/**
+ * 
+ * @author Andre Sancho Duarte
+ */
+public class FotovoltaicoDriver extends GenericSerialPortDriver {
+}
