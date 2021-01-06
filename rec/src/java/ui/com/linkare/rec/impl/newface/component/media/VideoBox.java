89ba351fe9b623d930d21fef74cd824f306641f5
jcortes <jcortes@linkare.com>
2021-01-06 14:53:14 +0000

    Task 5823: Adding visual confirmation to check if we are using latest version
    git-svn-id: file:///tmp/svnroot_only_rec/internal/rec-project/rec/branches/jdk11-migration@20618 8c4c7310-a39f-45c4-bd0d-b10b2e5d99a4

--- rec/src/java/ui/com/linkare/rec/impl/newface/component/media/VideoBox.java
+++ rec/src/java/ui/com/linkare/rec/impl/newface/component/media/VideoBox.java
@@ -24,7 +24,7 @@ public class VideoBox extends AbstractContentPane {
 		initComponents();
 		mediaPanel.setVisible(true);
 		mediaPanel.setSize(50,50);
-		mediaPanel.setBackground(Color.GRAY);
+		mediaPanel.setBackground(Color.BLACK);
 		/* 
 		mediaPanel.add(jfxPanel);
 		jfxPanel.setVisible(true);
