4e59da117b6638c60539d8d974c64d8cd938e126
jcorteslinkare <jcortes@linkare.com>
2021-03-29 11:49:20 +0100

    Fix RoundTripTimeout property to not reattempt setting this property more than once

--- rec/src/java/common/com/linkare/rec/impl/utils/ORBBean.java
+++ rec/src/java/common/com/linkare/rec/impl/utils/ORBBean.java
@@ -187,21 +187,21 @@ public class ORBBean {
 			return bidirpol;
 		}
 	}
+
+	private Policy rttpol = null;
 	
 	private Policy getRoundTripTimeOutPolicy() {
-		try {
-
-			final Any any = getORB().create_any();
-			TimeTHelper.insert(any, Integer.valueOf(System.getProperty("corba.rt.timeout", "30")) * 1000 * 10000);// 30 seconds*ms*(10.000 units of 100 ns)
-			return getORB().create_policy(RELATIVE_RT_TIMEOUT_POLICY_TYPE.value, any); //RoundTrip TimeOut
-
-		} catch (final Exception e) {
-			LOGGER.log(Level.SEVERE, e.getMessage(), e);
+		if (rttpol == null) {
+			try {
+				final Any any = getORB().create_any();
+				TimeTHelper.insert(any, Integer.valueOf(System.getProperty("corba.rt.timeout", "30")) * 1000 * 10000);// 30 seconds*ms*(10.000 units of 100 ns)
+				rttpol = getORB().create_policy(RELATIVE_RT_TIMEOUT_POLICY_TYPE.value, any); //RoundTrip TimeOut
+			} catch (final Exception e) {
+				LOGGER.log(Level.SEVERE, e.getMessage(), e);
+			}
+			return rttpol;
 		}
-
 	}
-	
-
 
 	public synchronized POA getDataProducerPOA(final ServantManager deactivator) throws Exception {
 		synchronized (orb_synch) {
