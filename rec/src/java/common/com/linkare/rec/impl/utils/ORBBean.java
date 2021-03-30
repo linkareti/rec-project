02803769e342c2fa17ec50eda4882fc713c1ab7f
jcorteslinkare <jcortes@linkare.com>
2021-03-30 13:03:07 +0100

    Test rttpol inside bidirpol

--- rec/src/java/common/com/linkare/rec/impl/utils/ORBBean.java
+++ rec/src/java/common/com/linkare/rec/impl/utils/ORBBean.java
@@ -176,8 +176,12 @@ public class ORBBean {
 
 					bidirpol = getORB().create_policy(BIDIRECTIONAL_POLICY_TYPE.value, any);
 
+					final Any any2 = getORB().create_any();
+					TimeTHelper.insert(any2, Integer.valueOf(System.getProperty("corba.rt.timeout", "30")) * 1000 * 10000);// 30 seconds*ms*(10.000 units of 100 ns)
+					rttpol = getORB().create_policy(RELATIVE_RT_TIMEOUT_POLICY_TYPE.value, any2); //RoundTrip TimeOut
+
 					final PolicyManager opm = (PolicyManager) getORB().resolve_initial_references("ORBPolicyManager");
-					opm.set_policy_overrides(new Policy[] { bidirpol }, SetOverrideType.ADD_OVERRIDE);
+					opm.set_policy_overrides(new Policy[] { bidirpol,rttpol }, SetOverrideType.ADD_OVERRIDE);
 
 				} catch (final Exception e) {
 					LOGGER.log(Level.SEVERE, e.getMessage(), e);
