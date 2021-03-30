885fcf9b9a7fb01c91e8e6cc8453313edad35c79
jcorteslinkare <jcortes@linkare.com>
2021-03-30 12:07:27 +0100

    Fix Daemon script to start Multicast

--- templates/scripts/MultiCastDaemon.sh
+++ templates/scripts/MultiCastDaemon.sh
@@ -16,92 +16,117 @@
 
 BASE_USER=@daemon.user@
 BASE_USER_HOMEDIR=~@daemon.user@
-BASE_DIR="@install.dir@@deployment.subdir@/@lab.name@/multicast"
-CURDIR=`pwd`
+CURDIR=$(cd `dirname $0` && pwd)
 CURUSER=`id -nu`
 
 RETVAL=0
 
+start_service() {
+  echo "Starting multicast_@lab.name@ $BASE_USER Service"
+  if [ "x$CURUSER" != "x$BASE_USER" ]; then
+    `sudo -u $BASE_USER sh StartMultiCastController.sh &>/dev/null`
+  else
+    `sh $CURDIR/StartMultiCastController.sh &>/dev/null`
+  fi
+  RETVAL=$?
+  if [ $RETVAL -eq 0 ]; then
+    echo OK
+    if [ "x$CURUSER" != "x$BASE_USER" ]; then
+      su -l $BASE_USER -c "touch $CURDIR/multicast_@lab.name@.lock"
+    else
+      touch $CURDIR/multicast_@lab.name@.lock
+    fi
+  else
+    echo NOT OK
+    RETVAL=1
+  fi
+  return $RETVAL
+}
+
 start() {
-		cd $BASE_DIR
-        echo "Starting multicast_@lab.name@ $BASE_USER Service"
-        if [ "x$CURUSER" != "x$BASE_USER" ]; then
-            `sudo -u $BASE_USER sh StartMultiCastController.sh &>/dev/null`
-        else
-            `sh $BASE_DIR/StartMultiCastController.sh &>/dev/null`
-        fi
-        RETVAL=$?
-        echo
-        if [ $RETVAL -eq 0 ]; then
-                if [ "x$CURUSER" != "x$BASE_USER" ]; then
-                        su -l $BASE_USER -c "touch $BASE_DIR/multicast_@lab.name@.lock"
-                else
-                        touch $BASE_DIR/multicast_@lab.name@.lock
-                fi
-        else
-                RETVAL=1
-        fi
-        return $RETVAL
+  cd $CURDIR
+  if [ -f $CURDIR/multicast_@lab.name@.pid ]; then
+    PID=`cat $CURDIR/multicast_@lab.name@.pid`
+    ps -fp ${PID} &>/dev/null
+    RETVAL=$?
+    if [ $RETVAL -eq 0 ]; then
+      echo "The service multicast_@lab.name@ is already running!"
+      return $RETVAL
+    else
+      start_service
+    fi
+  else
+    start_service
+  fi
 }
 stop() {
-		cd $BASE_DIR
-        if [ -f $BASE_DIR/multicast_@lab.name@.pid ]
-        then
-            PID=`cat $BASE_DIR/multicast_@lab.name@.pid`
-            echo "Stopping multicast_@lab.name@ $BASE_USER Service"
-            if [ "x$CURUSER" != "x$BASE_USER" ]; then
-                su -l $BASE_USER -c "kill $PID"
-            else
-                `kill $PID`
-            fi
-            RETVAL=$?
-            echo
-            [ $RETVAL -eq 0 ] && rm -rf $BASE_DIR/multicast_@lab.name@.lock && rm -rf $BASE_DIR/multicast_@lab.name@.pid || \
-               RETVAL=1
-            return $RETVAL
-        else
-            echo "The service multicast_@lab.name@ is not running!"
-            return $RETVAL
-        fi
+  cd $CURDIR
+  if [ -f $CURDIR/multicast_@lab.name@.pid ]; then
+    PID=`cat $CURDIR/multicast_@lab.name@.pid`
+    ps -fp ${PID} &>/dev/null
+    RETVAL=$?
+    if [ $RETVAL -eq 0 ]; then
+      echo "Stopping multicast_@lab.name@ $BASE_USER Service"
+      if [ "x$CURUSER" != "x$BASE_USER" ]; then
+        su -l $BASE_USER -c "kill $PID"
+      else
+        `kill $PID`
+      fi
+      RETVAL=$?
+      if [ $RETVAL -eq 0 ]; then
+        echo OK
+        rm $CURDIR/multicast_@lab.name@.lock $CURDIR/multicast_@lab.name@.pid
+      else
+        echo NOT OK
+        RETVAL=1
+      fi
+      return $RETVAL
+    else
+      echo "The service multicast_@lab.name@ is not running!"
+    fi
+  else
+    echo "The service multicast_@lab.name@ is not running!"
+    return $RETVAL
+  fi
 }
 restart() {
-        stop
-        start
+  stop
+  start
 }
-        
+
 status() {
-		cd $BASE_DIR
-        if [ -f $BASE_DIR/multicast_@lab.name@.pid ]
-        then
-            PID=`cat $BASE_DIR/multicast_@lab.name@.pid`
-            if ps ax | grep -v grep | grep $PID > /dev/null
-            then
-                echo "The service multicast_@lab.name@ is running."
-            else
-                echo "The service multicast_@lab.name@ is not running!"
-            fi
+  cd $CURDIR
+  if [ -f $CURDIR/multicast_@lab.name@.pid ]
+  then
+    PID=`cat $CURDIR/multicast_@lab.name@.pid`
+    if ps ax | grep -v grep | grep $PID > /dev/null
+    then
+      echo "The service multicast_@lab.name@ is running."
+    else
+      echo "The service multicast_@lab.name@ is not running!"
+    fi
 #           status $PID
-        else
-            echo "The service multicast_@lab.name@ is not running!"
-        fi
+  else
+      echo "The service multicast_@lab.name@ is not running!"
+  fi
 }
 
 case "$1" in
   start)
-        start
-        ;;
+    start
+    ;;
   stop)
-        stop
-        ;;
+    stop
+    ;;
   restart)
-        restart
-        ;;
+    restart
+    ;;
   status)
-        status
-        ;;
+    status
+    ;;
   *)
-        echo "Usage: %s {start|stop|restart|status}\n" "$0"
-        exit 1
+    echo "Usage: %s {start|stop|restart|status}\n" "$0"
+    exit 1
 esac
 
-exit $?
+#exit $?
